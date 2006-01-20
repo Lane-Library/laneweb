13147011049a7592f11b89178e58c780c004bf05
Charles Yates <ceyates@stanford.edu>
2006-01-20 21:26:13 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@19954 c440208b-5602-0410-a095-92527a996296

--- src/java/edu/stanford/laneweb/EresourcesQueryGenerator.java
+++ src/java/edu/stanford/laneweb/EresourcesQueryGenerator.java
@@ -30,9 +30,9 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
     private static final String XMLNS = "http://apache.org/cocoon/SQL/2.0";
     private static final String EXECUTE_QUERY = "execute-query";
     private static final String QUERY = "query";
-    private static final String SELECT_FROM = "SELECT " +
-"DISTINCT LINK.LINK_ID, ERESOURCE.ERESOURCE_ID, VERSION.VERSION_ID, ERESOURCE.TITLE, lower(ERESOURCE.TITLE) as LTITLE, VERSION.PUBLISHER, VERSION.HOLDINGS, VERSION.DATES, VERSION.DESCRIPTION, VERSION.PROXY, LINK.URL, LINK.LABEL, LINK.INSTRUCTION " +
-"FROM " +
+    private static final String SELECT = "SELECT " +
+"DISTINCT LINK.LINK_ID, ERESOURCE.ERESOURCE_ID, VERSION.VERSION_ID, ERESOURCE.TITLE, lower(ERESOURCE.TITLE) as LTITLE, VERSION.PUBLISHER, VERSION.HOLDINGS, VERSION.DATES, VERSION.DESCRIPTION, VERSION.PROXY, LINK.URL, LINK.LABEL, LINK.INSTRUCTION";
+    private static final String FROM = " FROM " +
 "ERESOURCE, VERSION, LINK, ERESOURCE_VERSION, VERSION_LINK";
     private static final String WHERE =
 " WHERE " +
@@ -44,12 +44,11 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
 "AND " + 
 "LINK.LINK_ID = VERSION_LINK.LINK_ID";
     private static String UNION = " UNION ";
-    private static final String ALT_SELECT_FROM = "SELECT " +
-    "DISTINCT LINK.LINK_ID, ERESOURCE.ERESOURCE_ID, VERSION.VERSION_ID, ERESOURCE.PREFERRED_TITLE, lower(ERESOURCE.PREFERRED_TITLE) as LTITLE, VERSION.PUBLISHER, VERSION.HOLDINGS, VERSION.DATES, VERSION.DESCRIPTION, VERSION.PROXY, LINK.URL, LINK.LABEL, LINK.INSTRUCTION " +
-    "FROM " +
-    "ERESOURCE, VERSION, LINK, ERESOURCE_VERSION, VERSION_LINK";
+    private static final String ALT_SELECT = "SELECT " +
+    "DISTINCT LINK.LINK_ID, ERESOURCE.ERESOURCE_ID, VERSION.VERSION_ID, ERESOURCE.PREFERRED_TITLE, lower(ERESOURCE.PREFERRED_TITLE) as LTITLE, VERSION.PUBLISHER, VERSION.HOLDINGS, VERSION.DATES, VERSION.DESCRIPTION, VERSION.PROXY, LINK.URL, LINK.LABEL, LINK.INSTRUCTION";
     private static final String ALT_AND = " AND ERESOURCE.PREFERRED_TITLE IS NOT NULL";
-    private static final String ORDER = " ORDER BY LTITLE, VERSION_ID";
+    private static final String ORDER_BY = " ORDER BY ";
+    private static final String ORDER = " LTITLE, LINK_ID";
     private static final Attributes EMPTY_ATTS = new AttributesImpl();
     
     private String type;
@@ -57,18 +56,21 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
     private String subset; 
     private String alpha;
     private String text;
+    private boolean haveParameters;
 
     public void setup(SourceResolver resolver, Map objectModel, String src,
             Parameters par) throws ProcessingException, SAXException,
             IOException {
         super.setup(resolver, objectModel, src, par);
         Request request = ObjectModelHelper.getRequest(objectModel);
+        this.haveParameters = false;
         this.type = request.getParameter(TYPE);
         if (this.type != null) {
             if (this.type.length() == 0) {
                 this.type = null;
             } else {
                 this.type = this.type.toLowerCase();
+                this.haveParameters = true;
             }
         }
         this.mesh = request.getParameter(MESH);
@@ -77,6 +79,7 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
                 this.mesh = null;
             } else {
                 this.mesh = this.mesh.toLowerCase();
+                this.haveParameters = true;
             }
         }
         this.subset = request.getParameter(SUBSET);
@@ -85,6 +88,7 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
                 this.subset = null;
             } else {
                 this.subset = this.subset.toLowerCase();
+                this.haveParameters = true;
             }
         }
         this.alpha = request.getParameter(ALPHA);
@@ -93,20 +97,60 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
                 this.alpha = null;
             } else {
                 this.alpha = this.alpha.toLowerCase();
+                this.haveParameters = true;
             }
         }
         this.text = request.getParameter(TEXT);
         if (this.text != null) {
             if (this.text.length() ==0) {
                 this.text = null;
+            } else {
+                this.haveParameters = true;
             }
         }
     }
 
     public void generate() throws SAXException {
-        StringBuffer query = null;
-        if (this.text == null) {
-        query = new StringBuffer(SELECT_FROM);
+        char[] selectStatmentChars = null;
+        if (this.haveParameters)  {
+            selectStatmentChars = getSelectStatmentChars();
+        } else {
+            selectStatmentChars = new char[0];
+        }
+        this.xmlConsumer.startDocument();
+        this.xmlConsumer.startElement(XMLNS,EXECUTE_QUERY,EXECUTE_QUERY,EMPTY_ATTS);
+        this.xmlConsumer.startElement(XMLNS,QUERY,QUERY,EMPTY_ATTS);
+        this.xmlConsumer.characters(selectStatmentChars,0,selectStatmentChars.length);
+        this.xmlConsumer.endElement(XMLNS,QUERY,QUERY);
+        this.xmlConsumer.endElement(XMLNS,EXECUTE_QUERY,EXECUTE_QUERY);
+        this.xmlConsumer.endDocument();
+    }
+    
+    public void recycle() {
+        this.type = null;
+        this.mesh = null;
+        this.subset = null;
+        this.alpha = null;
+        this.text = null;
+    }
+    
+    private char[] getSelectStatmentChars() {
+        StringBuffer query = new StringBuffer(SELECT);
+        String textAnd = null;
+        if (this.text != null) {
+            query.append(", SCORE(1) AS SCORE");
+            StringTokenizer st = new StringTokenizer(this.text);
+            StringBuffer textAndBuffer = new StringBuffer(" AND CONTAINS(TEXT,'");
+            while (st.hasMoreTokens()) {
+                textAndBuffer.append("fuzzy(").append(st.nextToken()).append(",,,W)");
+                if (st.hasMoreTokens()) {
+                    textAndBuffer.append(" AND ");
+                }
+            }
+            textAndBuffer.append("', 1) > 0");
+            textAnd = textAndBuffer.toString();
+        }
+        query.append(FROM);
         if (this.type != null) {
             query.append(", TYPE");
         }
@@ -129,8 +173,15 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
         if (this.alpha != null) {
             query.append(" AND lower(ERESOURCE.TITLE) LIKE '").append(this.alpha).append("%'");
         }
+        if (this.text != null) {
+            query.append(textAnd);
+        }
         query.append(UNION);
-        query.append(ALT_SELECT_FROM);
+        query.append(ALT_SELECT);
+        if (this.text != null) {
+            query.append(", SCORE(1) AS SCORE");
+        }
+        query.append(FROM);
         if (this.type != null) {
             query.append(", TYPE");
         }
@@ -154,39 +205,15 @@ public class EresourcesQueryGenerator extends AbstractGenerator {
         if (this.alpha != null) {
             query.append(" AND lower(ERESOURCE.PREFERRED_TITLE) LIKE '").append(this.alpha).append("%'");
         }
-        query.append(ORDER);
-        } else {
-            query = new StringBuffer("SELECT " +
-                    "DISTINCT LINK.LINK_ID, SCORE(1), ERESOURCE.ERESOURCE_ID, VERSION.VERSION_ID, ERESOURCE.TITLE, lower(ERESOURCE.TITLE) as LTITLE, VERSION.PUBLISHER, VERSION.HOLDINGS, VERSION.DATES, VERSION.DESCRIPTION, VERSION.PROXY, LINK.URL, LINK.LABEL, LINK.INSTRUCTION " +
-                    "FROM " +
-                    "ERESOURCE, VERSION, LINK, ERESOURCE_VERSION, VERSION_LINK");
-            query.append(WHERE);
-             query.append(" AND CONTAINS(TEXT,'");
-            StringTokenizer st = new StringTokenizer(this.text);
-            while (st.hasMoreTokens()) {
-                query.append(st.nextToken());
-                if (st.hasMoreTokens()) {
-                    query.append(" AND ");
-                }
-            }
-            query.append("', 1) > 0 ORDER BY SCORE(1) DESC, TITLE");
+        if (this.text != null) {
+            query.append(textAnd);
         }
-        char[] queryArray = query.toString().toCharArray();
-        this.xmlConsumer.startDocument();
-        this.xmlConsumer.startElement(XMLNS,EXECUTE_QUERY,EXECUTE_QUERY,EMPTY_ATTS);
-        this.xmlConsumer.startElement(XMLNS,QUERY,QUERY,EMPTY_ATTS);
-        this.xmlConsumer.characters(queryArray,0,queryArray.length);
-        this.xmlConsumer.endElement(XMLNS,QUERY,QUERY);
-        this.xmlConsumer.endElement(XMLNS,EXECUTE_QUERY,EXECUTE_QUERY);
-        this.xmlConsumer.endDocument();
-    }
-    
-    public void recycle() {
-        this.type = null;
-        this.mesh = null;
-        this.subset = null;
-        this.alpha = null;
-        this.text = null;
+        query.append(ORDER_BY);
+        if (this.text != null) {
+            query.append("SCORE DESC,");
+        }
+        query.append(ORDER);
+        return  query.toString().toCharArray();
     }
 
 }
