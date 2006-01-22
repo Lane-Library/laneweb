bddf1eec91822aea107405cdc6628beddb7b2fb2
Charles Yates <ceyates@stanford.edu>
2006-01-22 20:55:12 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@19993 c440208b-5602-0410-a095-92527a996296

--- src/webapp/resources/javascript/laneweb.js
+++ src/webapp/resources/javascript/laneweb.js
@@ -1,30 +1,8 @@
-var clinician = 0;
-var researcher = 0;
-var instructor = 0;
-var student = 0;
-var patient = 0;
+
 var imagePath = '/beta/images/templates/default/';
 var nokeywords = 'Please enter one or more search terms.';
 var searching = false;
 
-function lanewebLoad() {
-    document.searchForm.keywords.focus();
-    setEventHandlers(new Array('tab_clinician','tab_researcher','tab_instructor','tab_student','tab_patient'));
-    MM_preloadImages('pubmed_f2.gif','ejournals_f2.gif','uptodate_f2.gif','mdconsult_f2.gif','skolar_f2.gif','lanecat_f2.gif','askus_f2.gif','tab_clinician_f2.gif','tab_researcher_f2.gif','tab_instructor_f2.gif','tab_student_f2.gif','tab_patient_f2.gif');
-}
-function setEventHandlers(args) {
-  if (typeof document.all != 'undefined') {
-    for (var i = 0; i < args.length; i++) {
-      var tab = document.getElementById(args[i]);
-      tab.onmouseover=function() {
-        this.className='visible';
-      }
-      tab.onmouseout=function() {
-        this.className='hidden';
-      }
-    }
-  }
-}
 function MM_preloadImages() { //v3.0
   var d=document; if(d.images){ if(typeof d.MM_p == 'undefined') d.MM_p=new Array();
     var i,j=d.MM_p.length,a=arguments; for(i=0; i<a.length; i++)
