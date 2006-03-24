33346bf2e0307affb7fc67374735fc19ae8973ed
Charles Yates <ceyates@stanford.edu>
2006-03-24 04:09:18 +0000

    better documentation, remove commented out code
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@21259 c440208b-5602-0410-a095-92527a996296

--- src/java/edu/stanford/laneweb/LanewebHTTPSFilter.java
+++ src/java/edu/stanford/laneweb/LanewebHTTPSFilter.java
@@ -20,19 +20,18 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * @author ceyates
  * 
- * To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Generation - Code and Comments
+ * This filter redirects to https if the scheme is not https or there is not a
+ * gohttps header as set by the BigIP load balancer for urls that it is doing
+ * the ssl stuff.  At the moment we can't do the reverse becuase BigIP goes
+ * into a loop if you try to redirect from https to http.
  */
 public class LanewebHTTPSFilter implements Filter {
 
 	public void init(FilterConfig arg0) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
-	 *           javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	/**
+	 * does the redirect if no gohttps header or scheme is not https
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
@@ -42,21 +41,11 @@ public class LanewebHTTPSFilter implements Filter {
 		String url = queryString == null ? req.getRequestURL().toString() : req
 				.getRequestURL().append('?').append(queryString).toString();
 		int colonIndex = url.indexOf(':');
-//		String userName = req.getRemoteUser();
-//		req.getSession().setAttribute("USER_NAME", userName);
-		//if (req.getRequestURI().indexOf("/secure") > -1) {
-			if (req.getHeader("gohttps") != null
-					|| req.getScheme().equals("https")) {
-				chain.doFilter(request, response);
-			} else {
-				resp.sendRedirect("https" + url.substring(colonIndex));
-			}
-//		} else if (req.getHeader("gohttps") != null
-//				|| req.getScheme().equals("https")) {
-//			resp.sendRedirect("http" + url.substring(colonIndex));
-//		} else {
-//			chain.doFilter(request, response);
-//		}
+		if (req.getHeader("gohttps") != null || req.getScheme().equals("https")) {
+			chain.doFilter(request, response);
+		} else {
+			resp.sendRedirect("https" + url.substring(colonIndex));
+		}
 	}
 
 	public void destroy() {
