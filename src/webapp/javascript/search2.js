56ea13cdea8bb9ff681bd56859c90228420e0aed
Alain Boussard <alainb@stanford.edu>
2007-07-20 20:08:58 +0000

    new version is to get tab count
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@34749 c440208b-5602-0410-a095-92527a996296

--- src/webapp/javascript/search2.js
+++ src/webapp/javascript/search2.js
@@ -1,10 +1,15 @@
 var keywords;
 var results;
+var searchId;
+var startTime = new Date().getTime();
 
-function initSearch(e)
+var spellcheck;
+
+function initSearch()
 {
     window.keywords = escape(document.getElementById('keywordresult').innerHTML.replace(/&amp;/g,'&'));
-    YAHOO.util.Connect.asyncRequest('GET', '/././eresources/count?q='+window.keywords, window.showHitsCallback);
+    YAHOO.util.Connect.asyncRequest('GET', '/././apps/spellcheck?q='+window.keywords, window.spellCheckCallBack);
+    YAHOO.util.Connect.asyncRequest('GET', '/././content/search-tab-results.xml?q='+window.keywords, window.showHitsCallback);
     window.results = new Array();
     var tabs = document.getElementById('eLibraryTabs').getElementsByTagName('div');
     for (i = 0; i  < tabs.length; i++)
@@ -23,10 +28,13 @@ function initSearch(e)
             {
                 return this.result.onclick(event);
             }
-        }
+            
+        }        
     }
-    
+    spellcheck = new Spellcheck();
 }
+
+
 
 function Result(type, tab, zeroHits, container)
 {
@@ -120,8 +128,11 @@ Result.prototype.onclick = function(event)
     {
         alert('search in progress');
     }
+    window.spellcheck.setSource(this._type);
     return false;
 }
+
+
 Result.prototype.show = function()
 {
     if (this.currentList())
@@ -162,9 +173,10 @@ function hideResults()
 
 
 var handleFailure = function(o){
-alert('failure');
+alert('tab callback failure');
 searchType = null;
 }
+
 
 var showHitsCallback =
 {
@@ -172,25 +184,110 @@ var showHitsCallback =
   failure:handleFailure
 };
 
+
 function showHits(o) {
-try {
-var prefix = 'sql';
-var uri = 'http://apache.org/cocoon/SQL/2.0';
-var rows = window.getElementsByTagName(o.responseXML,prefix,uri,'row');
-var tabs = document.getElementById('eLibraryTabs').childNodes;
-for (i = 0; i  < rows.length; i++) {
-var genre = window.getElementsByTagName(rows[i],prefix,uri,'genre')[0].firstChild.nodeValue;
-for (j = 0; j < tabs.length; j++) {
-if (tabs[j].id == genre + 'Tab') {
-var hitSpan = tabs[j].getElementsByTagName('span')[0];
-hitSpan.innerHTML = window.getElementsByTagName(rows[i],prefix,uri,'hits')[0].firstChild.nodeValue;
-hitSpan.style.visibility = 'visible';
-break;
+//try {
+	var uri = 'http://lane.stanford.edu/search-tab-result/ns';
+	var rootNode = window.getElementsByTagName(o.responseXML,"", uri, 'search-tab-results')[0];
+	searchId = rootNode.getAttribute("id");
+	var status = rootNode.getAttribute("status");
+	var rows = window.getElementsByTagName(rootNode, "",uri,'resource');
+	var tabs = document.getElementById('eLibraryTabs').childNodes;
+	
+	for (i = 0; i  < rows.length; i++) 
+	{
+		var genre = rows[i].getAttribute("id");
+		for (j = 0; j < tabs.length; j++)
+		 {
+			if ( tabs[j].id != undefined &&  tabs[j].id == genre + 'Tab') 
+			{
+				var hitSpan = tabs[j].getElementsByTagName('span')[0];
+				if(window.getElementsByTagName(rows[i],"",uri,'hits')[0].firstChild != undefined)
+				{
+					hitSpan.innerHTML = window.getElementsByTagName(rows[i],"",uri,'hits')[0].firstChild.nodeValue;
+					if(window.getElementsByTagName(rows[i],"",uri,'url')[0] != undefined && window.getElementsByTagName(rows[i],"",uri,'url')[0].firstChild != undefined)
+					{
+						var linkValue = window.getElementsByTagName(rows[i],"",uri,'url')[0].firstChild.nodeValue;
+						if(linkValue != null)
+							tabs[j].getElementsByTagName('a')[0].href = linkValue;
+					}
+					hitSpan.style.visibility = 'visible';
+					break;
+				}
+			}
+		}
+	}
+	var sleepingTime = 2000;
+	var remainingTime = (new Date().getTime())-startTime;
+	if(status != 'successful' && ( remainingTime < 60*1000))
+	{	// if time superior at 20 seconds the sleeping time equals 10 seconds 
+		if(remainingTime > 20 *1000)
+			sleepingTime = 10000;
+		setTimeout( "getTabResult()", sleepingTime);
+	}
+//} catch (e) { alert(e.message) }
 }
+
+function getTabResult()
+{
+	  YAHOO.util.Connect.asyncRequest('GET', '/././content/search-tab-results.xml?id='+window.searchId, window.showHitsCallback);
 }
+
+
+
+var spellCheckCallBack =
+{
+  success:showSpellCheck,
+  //do nothing iof google spellcheck is done we dosn't want a alert windows 
+};
+
+function showSpellCheck(o)
+{
+	var uri = 'http://lane.stanford.edu/spellcheck/ns';
+	if( window.getElementsByTagName(o.responseXML,"", uri, 'suggestion')[0] != undefined)
+	{
+		suggestion = window.getElementsByTagName(o.responseXML,"", uri, 'suggestion')[0].firstChild.nodeValue;	
+		var spellCheckContainer = document.getElementById("spellCheck");
+		var link = spellCheckContainer.getElementsByTagName('a')[0];
+		link.innerHTML = suggestion;
+		spellCheckContainer.style.display= 'inline';
+		var initTab = 'er';
+        window.spellcheck.init(initTab,suggestion, link);
+        
+			
+	}
+	
 }
-} catch (e) { alert(e.message) }
+
+function Spellcheck()
+{
 }
+
+Spellcheck.prototype.init = function(currentTab, suggestion, link)
+{
+	if(currentTab != undefined)
+		this.source = currentTab;		
+	 if (suggestion != undefined)
+		this.suggestion = suggestion;
+	link.clicked = function(event)
+	{
+		return window.spellcheck.onclick(event, this);
+	}	   	
+}
+
+Spellcheck.prototype.onclick = function(event, link)
+{
+	link.href = '/search2.html?keywords='+this.suggestion+'&source='+this.source;
+    return false;
+}
+
+
+Spellcheck.prototype.setSource = function(source)
+{
+    if (source != undefined)
+		this.source = source;
+}
+
 
 function getElementsByTagName(node, prefix, uri, name)
 {
---
+++ src/webapp/xsl/search-tab-results.xsl
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:res="http://irt.stanford.edu/search/2.0"
+                xmlns:st="http://lane.stanford.edu/search-templates/ns"
+                xmlns:str="http://lane.stanford.edu/search-tab-results/ns"
+                xmlns="http://lane.stanford.edu/search-tab-result/ns"
+                exclude-result-prefixes="h st str res"
+                version="1.0">
+    
+   
+   <xsl:template match="res:search">
+          <xsl:variable name="successful" select="res:engine/res:resource[@status='successful']/res:hits[.!=0]" /> 
+         <search-tab-results id="{@id}" status="{@status}" >
+            
+            <resource id="clinical">
+	        	<hits> <xsl:value-of  select="count(//st:template[@id='clinical']/st:resource[@idref=$successful/../@id])" /></hits>
+	        	<url><xsl:text>/search.html?source=clinical&amp;id=</xsl:text><xsl:value-of select="@id"/></url>
+		   </resource>
+			<resource id="history">
+	        	<hits> <xsl:value-of  select="count(//st:template[@id='history']/st:resource[@idref=$successful/../@id])" /></hits>
+	        <url><xsl:text>/search.html?source=history&amp;id=</xsl:text><xsl:value-of select="@id"/></url>
+		   </resource>
+			<resource id="peds">
+	        	<hits> <xsl:value-of  select="count(//st:template[@id='peds']/st:resource[@idref=$successful/../@id])" /></hits>
+	        	<url><xsl:text>/search.html?source=peds&amp;id=</xsl:text><xsl:value-of select="@id"/></url>
+		   </resource>	            
+      		<resource id="research">
+	        	<hits> <xsl:value-of  select="count(//st:template[@id='research']/st:resource[@idref=$successful/../@id])" /></hits>
+	        	<url><xsl:text>/search.html?source=research&amp;id=</xsl:text><xsl:value-of select="@id"/></url>
+		   </resource>
+		   <xsl:apply-templates/>
+        </search-tab-results>
+   </xsl:template>   
+   
+   
+      
+     
+    <xsl:template match="res:search/res:engine[@id='google']">
+        <resource id="{@id}">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:resource/res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+
+ 	<xsl:template match="res:search/res:engine[@id='lois']">
+        <resource id="{@id}">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:resource/res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+
+
+    <xsl:template match="res:search/res:engine[@id='crossdb']/res:resource[@id='pubmed']">
+        <resource id="{@id}">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+   
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='lane_all']">
+        <resource id="er">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+   
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='biotools']">
+        <resource id="{@id}">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+    
+   
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='lane_videos']">
+        <resource id="video">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+    
+   
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='calculators']">
+        <resource id="cc">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+    
+   
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='lanedatabase']">
+        <resource id="database">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+    
+   
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='lanefaq']">
+        <resource id="lanefaq">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+       
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='lanebook']">
+        <resource id="book">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+    
+    
+     <xsl:template match="res:search/res:engine[@id='lane']/res:resource[@id='lane_ej']">
+        <resource id="ej">
+        	<hits><xsl:value-of select="res:hits"/></hits>
+        	<url><xsl:value-of select="res:url"/></url>
+            <xsl:apply-templates/>
+        </resource>
+    </xsl:template>
+    
+    
+    
+     
+      <xsl:template match="text()"/>
+   
+   
+</xsl:stylesheet>
