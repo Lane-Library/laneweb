a9d2e3171bb34e72d71ded0a368a1957010142ce
Charles Yates <ceyates@stanford.edu>
2007-09-14 23:27:11 +0000

    comment out test
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@36547 c440208b-5602-0410-a095-92527a996296

--- src/test/edu/stanford/irt/laneweb/tidy/TidyXMLReaderTest.java
+++ src/test/edu/stanford/irt/laneweb/tidy/TidyXMLReaderTest.java
@@ -25,30 +25,30 @@ public class TidyXMLReaderTest extends TestCase {
 	}
 
 	public void testParseInputSource() throws IOException, SAXException, TransformerException {
-		XMLReader reader = new TidyXMLReader();
-//		InputSource input = new InputSource(new FileInputStream(" "));
-//		reader.parse(input);
-		TransformerFactory fact = TransformerFactory.newInstance();
-		Transformer transformer = fact.newTransformer();
-		transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-		transformer.setErrorListener(new ErrorListener(){
-
-			public void error(TransformerException exception)
-					throws TransformerException {
-				System.out.println(exception. getMessage());
-				exception.printStackTrace();
-			}
-
-			public void fatalError(TransformerException exception)
-					throws TransformerException {
-				exception.printStackTrace();
-			}
-
-			public void warning(TransformerException exception)
-					throws TransformerException {
-				exception.printStackTrace();
-			}});
-		transformer.transform(new SAXSource(reader,new InputSource(new FileInputStream("/Users/ceyates/Documents/workspace/laneweb-content/content/search2.html"))), new StreamResult(System.out));
+//		XMLReader reader = new TidyXMLReader();
+////		InputSource input = new InputSource(new FileInputStream(" "));
+////		reader.parse(input);
+//		TransformerFactory fact = TransformerFactory.newInstance();
+//		Transformer transformer = fact.newTransformer();
+//		transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+//		transformer.setErrorListener(new ErrorListener(){
+//
+//			public void error(TransformerException exception)
+//					throws TransformerException {
+//				System.out.println(exception. getMessage());
+//				exception.printStackTrace();
+//			}
+//
+//			public void fatalError(TransformerException exception)
+//					throws TransformerException {
+//				exception.printStackTrace();
+//			}
+//
+//			public void warning(TransformerException exception)
+//					throws TransformerException {
+//				exception.printStackTrace();
+//			}});
+//		transformer.transform(new SAXSource(reader,new InputSource(new FileInputStream("/Users/ceyates/Documents/workspace/laneweb-content/content/search2.html"))), new StreamResult(System.out));
 	}
 
 }
