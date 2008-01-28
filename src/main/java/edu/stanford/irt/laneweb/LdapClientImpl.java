4feacd361dd454c88cfcca8ad14b058944824537
Charles Yates <ceyates@stanford.edu>
2008-01-28 19:03:15 +0000

    reverting changes to ldap stuff, revert to compile dependency on log4j
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@40177 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/LdapClientImpl.java
+++ src/main/java/edu/stanford/irt/laneweb/LdapClientImpl.java
@@ -1,35 +1,35 @@
 package edu.stanford.irt.laneweb;
 
-import org.apache.avalon.framework.activity.Initializable;
-import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.log4j.Logger;
 
-import edu.stanford.irt.SystemException;
 import edu.stanford.irt.directory.LDAPDirectoryFactory;
 import edu.stanford.irt.directory.LDAPDirectoryUtil;
 import edu.stanford.irt.directory.LDAPPerson;
 
-public class LdapClientImpl extends AbstractLogEnabled implements LdapClient, Initializable {
+public class LdapClientImpl implements LdapClient {
 
-    private  LDAPDirectoryFactory directoryFactory = null;
+    Logger log = Logger.getLogger(this.getClass());
 
-    public void initialize() {
-        this.directoryFactory = (LDAPDirectoryFactory) LDAPDirectoryUtil.getLDAPDirectoryFactory("IRT_K5").getDirectoryFactory();
+    private static LDAPDirectoryFactory directoryFactory = null;
+
+    public LdapClientImpl() {
+        try {
+            directoryFactory = (LDAPDirectoryFactory) LDAPDirectoryUtil.getLDAPDirectoryFactory("IRT_K5").getDirectoryFactory();
+
+        } catch (Exception e) {
+            this.log.error(e.getMessage(), e);
+        }
     }
 
     public LDAPPerson getLdapPerson(final String sunetId) {
-        if (getLogger().isDebugEnabled()) {
-            getLogger().debug("LdapClientImpl.getLdapPerson("+sunetId+")");
-        }
-        if (null == sunetId) {
-            throw new IllegalArgumentException("null sunetId");
-        }
-        try {
-            return this.directoryFactory.getSearcher().searchPersonByUID(sunetId);
-        } catch (SystemException e) {
-            if (getLogger().isErrorEnabled()) {
-                getLogger().error(e.getMessage(), e);
+        LDAPPerson ldapPerson = null;
+        if (sunetId != null) {
+            try {
+                ldapPerson = directoryFactory.getSearcher().searchPersonByUID(sunetId);
+            } catch (Exception e) {
+                this.log.error(e.getMessage(), e);
             }
-            return null;
         }
+        return ldapPerson;
     }
 }
--- src/main/java/edu/stanford/irt/laneweb/UserInfoHelperImpl.java
+++ src/main/java/edu/stanford/irt/laneweb/UserInfoHelperImpl.java
@@ -1,16 +1,14 @@
 package edu.stanford.irt.laneweb;
 
+import org.apache.avalon.framework.activity.Initializable;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
-import org.apache.avalon.framework.service.ServiceException;
-import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.framework.service.Serviceable;
 import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.Session;
 
-public class UserInfoHelperImpl extends AbstractLogEnabled implements UserInfoHelper, ThreadSafe, Serviceable {
+public class UserInfoHelperImpl extends AbstractLogEnabled implements UserInfoHelper, ThreadSafe, Initializable {
 
-    private LdapClient ldapClient;
+    LdapClient ldapClient;
 
     public UserInfo getUserInfo(final Request request) {
 
@@ -59,12 +57,12 @@ public class UserInfoHelperImpl extends AbstractLogEnabled implements UserInfoHe
         }
     }
 
+    public void initialize() throws Exception {
+        this.ldapClient = new LdapClientImpl();
+    }
+
     public void setLdapClient(final LdapClient ldapClient) {
         this.ldapClient = ldapClient;
-    }
-    
-    public void service(ServiceManager manager) throws ServiceException {
-        this.ldapClient = (LdapClient) manager.lookup(LdapClient.ROLE);
     }
 
 }
