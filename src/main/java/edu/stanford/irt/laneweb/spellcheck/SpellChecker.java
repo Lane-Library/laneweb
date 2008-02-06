7ee9404ef17cd9d36044f979504ba28a0f0a6fb4
Alain Boussard <alainb@stanford.edu>
2008-02-06 00:59:30 +0000

    overwrite by mistake during the merge
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@40495 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/spellcheck/SpellChecker.java
+++ src/main/java/edu/stanford/irt/laneweb/spellcheck/SpellChecker.java
@@ -3,9 +3,11 @@
  */
 package edu.stanford.irt.laneweb.spellcheck;
 
-import org.apache.avalon.framework.parameters.ParameterException;
-import org.apache.avalon.framework.parameters.Parameterizable;
-import org.apache.avalon.framework.parameters.Parameters;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.avalon.framework.activity.Initializable;
 
 import edu.stanford.irt.spell.GoogleAPISpellChecker;
 
@@ -13,10 +15,12 @@ import edu.stanford.irt.spell.GoogleAPISpellChecker;
  * @author ceyates
  * 
  */
-public class SpellChecker extends GoogleAPISpellChecker implements Parameterizable {
+public class SpellChecker extends GoogleAPISpellChecker implements Initializable {
 
-    public void parameterize(final Parameters params) throws ParameterException {
-        super.setGoogleKey(params.getParameter("google-key"));
+    public void initialize() throws NamingException {
+        Context context = new InitialContext();
+        String googleKey = (String) context.lookup("java:comp/env/google-key");
+        super.setGoogleKey(googleKey);
     }
 
 }
