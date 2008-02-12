b29bbf183e7a8f54c94102e8d831ded968e3721c
Ryan Max Steinberg <ryanmax@stanford.edu>
2008-02-12 17:50:50 +0000

    Google Analytics js often doesn't load; added check for _gat object before trying to report
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@40765 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/javascript/googleanalytics.js
+++ src/main/webapp/javascript/googleanalytics.js
@@ -3,13 +3,15 @@ var pageTracker;
 YAHOO.util.Event.addListener(window,'load',initialize);
 
 function initialize(e) {
-    if("lane.stanford.edu" == getMetaContent('LW.host')){
-        window.pageTracker = _gat._getTracker("UA-3202241-2");
+    if(window._gat != undefined){
+        if("lane.stanford.edu" == getMetaContent('LW.host')){
+            window.pageTracker = _gat._getTracker("UA-3202241-2");
+        }
+        else{
+            window.pageTracker = _gat._getTracker("UA-3203486-2");
+        }
+        window.pageTracker._initData();
+        window.pageTracker._trackPageview();
+        window.pageTracker._setVar(getMetaContent('WT.seg_1'));
     }
-    else{
-        window.pageTracker = _gat._getTracker("UA-3203486-2");
-    }
-    window.pageTracker._initData();
-    window.pageTracker._trackPageview();
-    window.pageTracker._setVar(getMetaContent('WT.seg_1'));
 }
