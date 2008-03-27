54274477f1d14e1d657d789adaaad87553951e6b
Charles Yates <ceyates@stanford.edu>
2008-03-27 22:53:11 +0000

    add X-Laneweb-Version header
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@41902 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/VersionHeaderAction.java
@@ -0,0 +1,32 @@
+package edu.stanford.irt.laneweb;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.ParameterException;
+import org.apache.avalon.framework.parameters.Parameterizable;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Response;
+import org.apache.cocoon.environment.SourceResolver;
+
+public class VersionHeaderAction implements ThreadSafe, Parameterizable {
+    
+    private String version;
+
+    public Map act(final Redirector redirector, final SourceResolver resolver, final Map objectModel, final String source,
+            final Parameters params) {
+        Response response = ObjectModelHelper.getResponse(objectModel);
+        if (null != this.version) {
+            response.addHeader("X-Laneweb-Version", this.version);
+        }
+        return Collections.emptyMap();
+    }
+
+    public void parameterize(Parameters params) throws ParameterException {
+        this.version = params.getParameter("laneweb-version");
+    }
+
+}
