ab4b29c5c8beb891f2b086e9214189b33fd11647
Charles Yates <ceyates@stanford.edu>
2008-05-02 21:18:51 +0000

    implement Disposable so context can be destroyed
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@42864 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/search/LanewebMetaSearchManagerSource.java
+++ src/main/java/edu/stanford/irt/laneweb/search/LanewebMetaSearchManagerSource.java
@@ -1,8 +1,8 @@
 package edu.stanford.irt.laneweb.search;
 
+import org.apache.avalon.framework.activity.Disposable;
 import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.commons.httpclient.HttpClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import edu.stanford.irt.search.MetaSearchManager;
@@ -11,15 +11,17 @@ import edu.stanford.irt.search.MetaSearchManager;
  * @author ceyates
  * 
  */
-public class LanewebMetaSearchManagerSource implements ThreadSafe,
+public class LanewebMetaSearchManagerSource implements ThreadSafe, Disposable,
         MetaSearchManagerSource {
 
     private MetaSearchManager manager;
 
     private HttpClient httpClient;
+    
+    private ClassPathXmlApplicationContext context;
 
     public LanewebMetaSearchManagerSource() {
-        ApplicationContext context = new ClassPathXmlApplicationContext(
+        this.context = new ClassPathXmlApplicationContext(
                 "spring/metasearch.xml");
         this.manager = (MetaSearchManager) context.getBean("manager");
         this.httpClient = (HttpClient) context.getBean("httpClient");
@@ -31,5 +33,9 @@ public class LanewebMetaSearchManagerSource implements ThreadSafe,
 
     public HttpClient getHttpClient() {
         return this.httpClient;
+    }
+
+    public void dispose() {
+        this.context.destroy();
     }
 }
