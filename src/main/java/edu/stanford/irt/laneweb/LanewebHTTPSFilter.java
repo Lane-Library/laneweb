b4225174c1109f3303936e1c1bbfef9c654a7d48
Charles Yates <ceyates@stanford.edu>
2008-06-23 18:45:16 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@44128 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/LanewebHTTPSFilter.java
+++ src/main/java/edu/stanford/irt/laneweb/LanewebHTTPSFilter.java
@@ -13,7 +13,6 @@ import java.util.logging.Logger;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -53,7 +52,7 @@ public class LanewebHTTPSFilter implements Filter {
             this.logger.log(Level.INFO, sb.toString());
             chain.doFilter(request, response);
         } else {
-            sb.append("\nsendRedirect(https" + url.substring(colonIndex));
+            sb.append("\nsendRedirect(https").append(url.substring(colonIndex)).append(")");
             this.logger.log(Level.INFO, sb.toString());
             resp.sendRedirect("https" + url.substring(colonIndex));
         }
