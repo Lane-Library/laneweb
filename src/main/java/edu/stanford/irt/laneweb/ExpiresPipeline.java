2062613b6c1052e38ce3710dcfd09d45b0997f45
Charles Yates <ceyates@stanford.edu>
2008-07-25 20:42:40 +0000

    added ExpiresPipelineTest
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@44996 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/ExpiresPipeline.java
+++ src/main/java/edu/stanford/irt/laneweb/ExpiresPipeline.java
@@ -10,47 +10,49 @@ import org.apache.excalibur.source.impl.validity.ExpiresValidity;
 import org.apache.excalibur.source.impl.validity.NOPValidity;
 
 /**
- * This class extends ExpiresCachingProcessingPipeline to add the caching
- * that is broken for readers in that class.
+ * This class extends ExpiresCachingProcessingPipeline to add the caching that
+ * is broken for readers in that class.
+ * 
  * @author ceyates
- *
  */
 public class ExpiresPipeline extends ExpiresCachingProcessingPipeline {
 
     @Override
-    protected boolean processReader(Environment environment)
+    protected boolean processReader(final Environment environment)
             throws ProcessingException {
-        //get the cache expiration value from the object model or as a parameter
+        // get the cache expiration value from the object model or as a
+        // parameter
         final Map objectModel = environment.getObjectModel();
-        String expiresValue = (String)objectModel.get(CACHE_EXPIRES_KEY);
-        if ( expiresValue == null ) {
-            this.cacheExpires = this.parameters.getParameterAsLong("cache-expires", this.defaultCacheExpires);
+        String expiresValue = (String) objectModel.get(CACHE_EXPIRES_KEY);
+        if (expiresValue == null) {
+            this.cacheExpires = this.parameters.getParameterAsLong(
+                    "cache-expires", this.defaultCacheExpires);
         } else {
             this.cacheExpires = Long.valueOf(expiresValue).longValue();
             objectModel.remove(CACHE_EXPIRES_KEY);
         }
-        
-        //create the cache validity object
-        if ( this.cacheExpires > 0) {
-            this.cacheValidity = new ExpiresValidity(this.cacheExpires*1000);
-        } else if ( this.cacheExpires < 0 ) {
+
+        // create the cache validity object
+        if (this.cacheExpires > 0) {
+            this.cacheValidity = new ExpiresValidity(this.cacheExpires * 1000);
+        } else if (this.cacheExpires < 0) {
             this.cacheValidity = NOPValidity.SHARED_INSTANCE;
         }
-        
-        //create the cache key
-        String key = (String)objectModel.get(CACHE_KEY_KEY);
-        if ( key == null ) {
+
+        // create the cache key
+        String key = (String) objectModel.get(CACHE_KEY_KEY);
+        if (key == null) {
             key = this.parameters.getParameter("cache-key", null);
-            if ( key == null ) {
-                key = environment.getURIPrefix()+environment.getURI();
+            if (key == null) {
+                key = environment.getURIPrefix() + environment.getURI();
             }
         } else {
             objectModel.remove(CACHE_KEY_KEY);
         }
         this.cacheKey = new IdentifierCacheKey(key,
-                                           this.serializer == this.lastConsumer);
-        
-        //hand off
+                this.serializer == this.lastConsumer);
+
+        // hand off
         return super.processReader(environment);
     }
 
