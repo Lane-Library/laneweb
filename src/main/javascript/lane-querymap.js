7b3a1682dd0f8bbe4c62f6c50cedbbc20ba775eb
Charles Yates <ceyates@stanford.edu>
2008-09-11 23:40:30 +0000

    querymap working
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@47272 c440208b-5602-0410-a095-92527a996296

--- src/main/javascript/lane-querymap.js
+++ src/main/javascript/lane-querymap.js
@@ -1,12 +1,60 @@
- //check if there is a query
+//TODO: this needs some cleaning up
+//check if there is a query
 if (LANE.search.getEncodedSearchString()) {
+    //check if there is id=queryMapping
     YAHOO.util.Event.onAvailable('queryMapping',function() {
             YAHOO.util.Connect.asyncRequest('GET', '/././apps/querymap/json?q=' + LANE.search.getEncodedSearchString(), {
             success:function(o) {
-                    var queryMap = YAHOO.lang.JSON.parse(o.responseText),
-                        queryMapContainer = document.getElementById('queryMapping');
-                    alert(queryMap);
-                    queryMapContainer.style.display = 'inline';
+                    var queryMapContainer = document.getElementById('queryMapping');
+                    LANE.search.querymap = YAHOO.lang.JSON.parse(o.responseText);
+                    if (LANE.search.querymap.resourceMap) {
+                        LANE.search.querymap.getResultCounts = function() {
+                            var url = '/././apps/search/proxy/json?q=' + LANE.search.getEncodedSearchString(),
+                                i;
+                            for (i = 0; i < this.resourceMap.resources.length; i++) {
+                                if (!this.resourceMap.resources[i].status) {
+                                    url += '&r=' + this.resourceMap.resources[i].id;
+                                }
+                            }
+                            url += '&rd=' + Math.random();
+                            YAHOO.util.Connect.asyncRequest('GET',url, {
+                                success: function(o){
+                                    var results = YAHOO.lang.JSON.parse(o.responseText),
+                                        rs = LANE.search.querymap.resourceMap.resources,
+                                        i, needMore = false, result;
+                                    for (i = 0; i < rs.length; i++) {
+                                        if (!rs[i].status) {
+                                            result = results.resources[rs[i].id];
+                                            if (!result.status) {
+                                                needMore = true;
+                                            }
+                                            rs[i].status = result.status;
+                                            if (result.status == 'successful') {
+                                                rs[i].anchor.parentNode.appendChild(document.createTextNode(' :' + result.hits + ' '));
+                                            }
+                                            rs[i].anchor.href = result.url;
+                                        }
+                                    }
+                                    if (needMore) {
+                                        LANE.search.querymap.getResultCounts();
+                                    }
+                                    queryMapContainer.style.display = 'inline';
+                                }
+                            });
+                        };
+                        var anchor;
+                        var span;
+                        for (var i = 0; i < LANE.search.querymap.resourceMap.resources.length; i++) {
+                            LANE.search.querymap.resourceMap.resources[i].status = '';
+                            span = document.createElement('span');
+                            anchor = document.createElement('a');
+                            span.appendChild(anchor);
+                            anchor.appendChild(document.createTextNode(LANE.search.querymap.resourceMap.resources[i].label));
+                            queryMapContainer.appendChild(span);
+                            LANE.search.querymap.resourceMap.resources[i].anchor = anchor;
+                        }
+                        LANE.search.querymap.getResultCounts();
+                    }
             }
             });
     });
