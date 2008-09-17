1785e549002c8d04f86c7271f600dc8321e7ac8d
Ryan Max Steinberg <ryanmax@stanford.edu>
2008-09-17 19:25:01 +0000

    popin: works wherever popin elements found instead of only w/in popInContent id
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@47495 c440208b-5602-0410-a095-92527a996296

--- src/main/javascript/lane-popin.js
+++ src/main/javascript/lane-popin.js
@@ -1,27 +1,63 @@
-//check if there is a query
+(function() {
+    LANE.namespace('search.popin');
+
+	// custom onPopin event
+	LANE.search.popin = new YAHOO.util.CustomEvent("onPopin");
+	
+	//list of all possible popin elements, in display precedence order
+    var popinElms = ['spellCheck','queryMapping','findIt'],
+		// TODO: could make markup order dictate precedence 
+		//       (find elm and then fetch parent of elm to see if other popin children
+		//       popInContent id NOT always present when popin required)
+		onPopinHandler = function(type,el){
+		var i, activeEl = 99, elms = YAHOO.util.Dom.get(popinElms);
+		for (i = 0; i < elms.length; i++){
+			if(elms[i].style.display == 'inline'){
+				activeEl = i;
+			}
+		}
+		for (i= 0; i < elms.length; i++){
+			if(el[0].id === elms[i].id && i <= activeEl){
+				activeEl = i;
+				elms[i].style.display = 'inline';
+			}
+			else if (i > activeEl){
+				elms[i].style.display = 'none';
+			}
+		}
+	};
+	LANE.search.popin.subscribe(onPopinHandler);
+})();
+
+/*
+//TODO: revert ... don't want to call onAvailable function once for each element 
+// //check if there is a query
 if (LANE.search.getEncodedSearchString()) {
-    //check if popInContent elm present
-    YAHOO.util.Event.onAvailable('popInContent',function() {
+	//list of all possible popin elements, in display precendence order
+    var popinElms = ['spellCheck','queryMapping','findIt'];
+    //check if any of our popin content elements are present in the DOM
+    YAHOO.util.Event.onAvailable(popinElms,function() {
 		//custom onPopin event and handler
     	LANE.search.popin = new YAHOO.util.CustomEvent("onPopin");
     	function onPopinHandler(type,el){
-    		var i, activeEl = 99, popinElms = YAHOO.util.Dom.getChildren(YAHOO.util.Dom.getChildren('popInContent')[0]);
-    		//display precedence is determined by order of elements w/in popInContent markup
-    		for (i = 0; i < popinElms.length; i++){
-    			if(popinElms[i].style.display == 'inline'){
+    		//TODO: make order of elements dictate display precendence
+    		var i, activeEl = 99, elms = YAHOO.util.Dom.get(popinElms);
+    		for (i = 0; i < elms.length; i++){
+    			if(elms[i].style.display == 'inline'){
     				activeEl = i;
     			}
     		}
-    		for (i= 0; i < popinElms.length; i++){
-    			if(el[0].id === popinElms[i].id && i <= activeEl){
+    		for (i= 0; i < elms.length; i++){
+    			if(el[0].id === elms[i].id && i <= activeEl){
     				activeEl = i;
-    				popinElms[i].style.display = 'inline';
+    				elms[i].style.display = 'inline';
     			}
     			else if (i > activeEl){
-    				popinElms[i].style.display = 'none';
+    				elms[i].style.display = 'none';
     			}
     		}
     	}
 		LANE.search.popin.subscribe(onPopinHandler);
     });
 }
+*/
