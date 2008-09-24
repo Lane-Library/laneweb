0bd338a23a5e94818821d82d1dda296cf3b384cd
Charles Yates <ceyates@stanford.edu>
2008-09-24 17:35:59 +0000

    separate tooltips js
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@47762 c440208b-5602-0410-a095-92527a996296

--- src/main/javascript/lane.js
+++ src/main/javascript/lane.js
@@ -137,38 +137,7 @@ var openNewWindow = LANE.core.openNewWindow;
 
 
 
-//anonymous initializer for tooltips no public properties or functions
-(function() {
-    YAHOO.util.Event.addListener(window,'load', function() {
-        var tc, //tooltip container Elements
-            tt, //tooltips
-            te, //elment to recieve tooltip
-            e, //id of te element
-            w, //width of tooltip
-            i, j;
-        tc = YAHOO.util.Dom.getElementsByClassName('tooltips');
 
-        for(i = 0; i < tc.length; i++) {
-            tt = tc[i].childNodes;
-            for(j = 0; j < tt.length; j++) {
-                if(tt[j].nodeType === 1) {
-                    e = tt[j].id.replace(/Tooltip$/,'');
-                    if(e && YAHOO.util.Dom.inDocument(e)) {
-                        w = tt[j].style.width || '25%';
-                        new YAHOO.widget.Tooltip(
-                                YAHOO.util.Dom.generateId(), 
-                                {
-                                    context:e,
-                                    width:w,
-                                    autodismissdelay:30000,
-                                    text:tt[j].innerHTML
-                                });
-                    }
-                }
-            }
-        }
-    });
-})();
 /*
 
 window.onerror = handleMessage;
