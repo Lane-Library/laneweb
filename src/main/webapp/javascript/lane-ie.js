a7642b677591c52c0cd4696a28e7d7403497b8af
Charles Yates <ceyates@stanford.edu>
2008-10-01 16:48:16 +0000

    http://irt-bugs.stanford.edu/default.php?22089
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@48160 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/javascript/lane-ie.js
+++ src/main/webapp/javascript/lane-ie.js
@@ -1,22 +1,28 @@
 (function(){
     if (YAHOO.env.ua.ie) {
         YAHOO.util.Event.addListener(this, 'load', function(){
-            var ie = YAHOO.env.ua.ie, lists, i, d = document, list;
+            var ie = YAHOO.env.ua.ie, lists, i, d = document, list,
+                //the bottom shaded border z-index has to be reduced . . .
+                sbb = d.getElementById('sb-bot').style,
+                opo = d.getElementById('otherPortalOptions'),
+                ldd = d.getElementById('legend-drop-down');
             if (ie <= 6) {
                 //set hover class for these ids
-                if (d.getElementById('otherPortalOptions')) {
-                    d.getElementById('otherPortalOptions').activate = function(){
+                if (opo) {
+                    opo.activate = function(){
+                        sbb.zIndex = -1;
                         this.className = 'hover';
                     };
-                    d.getElementById('otherPortalOptions').deactivate = function(){
+                    opo.deactivate = function(){
+                        sbb.zIndex = 1;
                         this.className = '';
                     };
                 }
-                if (d.getElementById('legend-drop-down')) {
-                    d.getElementById('legend-drop-down').activate = function(){
+                if (ldd) {
+                    ldd.activate = function(){
                         this.className = 'hover';
                     };
-                    d.getElementById('legend-drop-down').deactivate = function(){
+                    ldd.deactivate = function(){
                         this.className = '';
                     };
                 }
@@ -25,6 +31,16 @@
                 for (i = 0; i < lists.length; i++) {
                     list = d.getElementById(lists[i]);
                     list.getElementsByTagName('li')[0].style.border = 'none';
+                }
+            } else if (ie >= 7 && ie < 8) {
+                //ie 7 stil messes up z-index:
+                if (opo) {
+                    opo.activate = function(){
+                        sbb.zIndex = -1;
+                    };
+                    opo.deactivate = function(){
+                        sbb.zIndex = 1;
+                    };
                 }
             }
         });
