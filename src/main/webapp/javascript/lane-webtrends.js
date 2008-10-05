fcc84c51a347d3badda702055e474b8170a4c1dd
Charles Yates <ceyates@stanford.edu>
2008-10-05 00:47:48 +0000

    working with tracking
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@48329 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/javascript/lane-webtrends.js
+++ src/main/webapp/javascript/lane-webtrends.js
@@ -172,8 +172,8 @@
     	WT={},
     	DCSext={},
     	gQP=[],
-    	gDomain='irt-lane-stage.stanford.edu',
-        //gDomain="irt-sdc.stanford.edu",
+    	//gDomain='irt-lane-stage.stanford.edu',
+        gDomain="irt-sdc.stanford.edu",
     	gDcsId="dcssi6l0t1000004z9mg95sop_9v3k";
 
     function dcsVar(){
