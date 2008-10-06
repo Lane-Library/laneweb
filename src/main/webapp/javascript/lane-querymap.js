f4c0e285fdf1272ff38da242b2f6096ccc57803d
Ryan Max Steinberg <ryanmax@stanford.edu>
2008-10-06 20:18:08 +0000

    moved img logger: resourceMap not always present
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@48413 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/javascript/lane-querymap.js
+++ src/main/webapp/javascript/lane-querymap.js
@@ -53,13 +53,14 @@ if (LANE.search.getEncodedSearchString()) {
                     if (document.getElementById('queryMappingDescriptor')) {
                         document.getElementById('queryMappingDescriptor').appendChild(document.createTextNode(LANE.search.querymap.resourceMap.descriptor));
                     }
+	                // track mapped term and descriptor
+	                img = document.createElement('img');
+	                img.style.display = "none";
+	                img.src = "/././graphics/spacer.gif?log=QM&d=" + LANE.search.querymap.resourceMap.descriptor + "&k=" + LANE.search.getEncodedSearchString();
+	                queryMapContainer.appendChild(img);
+	                
                     LANE.search.querymap.getResultCounts();
                 }
-                // track term and descriptor mapped
-                img = document.createElement('img');
-                img.style.display = "none";
-                img.src = "/././graphics/spacer.gif?log=QM&d=" + LANE.search.querymap.resourceMap.descriptor + "&k=" + LANE.search.getEncodedSearchString();
-                queryMapContainer.appendChild(img);
             }
         });
     });
