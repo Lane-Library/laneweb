a43d1cf9bee805cc5a400e4c52df2579571b0b7d
Charles Yates <ceyates@stanford.edu>
2008-10-17 19:02:31 +0000

    put logging in tracking, not google
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@48984 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/javascript/lane-tracking.js
+++ src/main/webapp/javascript/lane-tracking.js
@@ -96,6 +96,14 @@ LANE.track = function(){
             var td;
             if (this.isTrackable(e)) {
                 td = getTrackingData(e);
+                //TODO: remove this after fixing bugs 22495 and 22496
+                            if (!td.external && td.path.indexOf('secure/login') > -1) {
+                                var msg = 'useragent:' + navigator.userAgent;
+                                msg += ';host:' + document.location.toString();
+                                msg += ';title:' + trackingData.title;
+                                msg += ';path:' + trackingData.path;
+                                LANE.core.log(msg);
+                            }
                 this.track(td);
             }
         },
