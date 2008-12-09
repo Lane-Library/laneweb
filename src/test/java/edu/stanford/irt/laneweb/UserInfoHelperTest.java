95f3068e7328c4f51ffdc4a2d3c82a828ae0a3f9
Alain Boussard <alainb@stanford.edu>
2008-12-09 23:22:13 +0000

    to add the persistent login
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@51117 c440208b-5602-0410-a095-92527a996296

--- src/test/java/edu/stanford/irt/laneweb/UserInfoHelperTest.java
+++ src/test/java/edu/stanford/irt/laneweb/UserInfoHelperTest.java
@@ -5,12 +5,25 @@ import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.verify;
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
+
+import edu.stanford.irt.directory.LDAPPerson;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.servlet.http.Cookie;
+
 import junit.framework.TestCase;
 
 import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.Session;
-
-import edu.stanford.irt.directory.LDAPPerson;
+import org.apache.commons.codec.DecoderException;
 
 public class UserInfoHelperTest extends TestCase {
 
@@ -28,6 +41,7 @@ public class UserInfoHelperTest extends TestCase {
 
     private UserInfoHelper userInfoHelper;
 
+    
     @Override
     protected void setUp() throws Exception {
         super.setUp();
@@ -52,19 +66,21 @@ public class UserInfoHelperTest extends TestCase {
 
         expect(this.request.getSession(true)).andReturn(this.session);
         expect(this.request.getHeader(LanewebConstants.X_FORWARDED_FOR)).andReturn(null);
-        expect(this.request.getRemoteUser()).andReturn(this.sunetid);
         expect(this.request.getParameter(LanewebConstants.PROXY_LINKS)).andReturn("true");
         expect(this.request.getParameter(LanewebConstants.PROXY_LINKS)).andReturn("true");
         replay(this.ldapClient);
         replay(this.session);
 
+        
         this.userInfoHelper = new UserInfoHelper();
         this.userInfoHelper.setLdapClient(this.ldapClient);
         this.userInfoHelper.setEzproxyKey(this.ezproxyKey);
+        
     }
 
     public void testGetUserInfo() {
-        expect(this.request.getRemoteAddr()).andReturn(this.ip);
+        expect(this.request.getRemoteUser()).andReturn(this.sunetid);
+	expect(this.request.getRemoteAddr()).andReturn(this.ip);
         replay(this.request);
         UserInfo userInfo = this.userInfoHelper.getUserInfo(this.request);
         assertEquals(Affiliation.SOM, userInfo.getAffiliation());
@@ -77,9 +93,33 @@ public class UserInfoHelperTest extends TestCase {
         verify(this.request);
     }
 
+    public void testCookieUserInfo() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, IOException, DecoderException {
+        Cryptor cryptor = new Cryptor();
+        cryptor.setKey("testtesttesttesttestt");
+        String cookieValue = cryptor.encrypt(this.sunetid);
+	Cookie cookie = new Cookie(LanewebConstants.USER_COOKIE_NAME, cookieValue);
+	HashMap<String, Cookie> map = new HashMap<String, Cookie>();
+	map.put(LanewebConstants.USER_COOKIE_NAME, cookie);
+	expect(this.request.getRemoteAddr()).andReturn(this.ip);
+	expect(this.request.getRemoteUser()).andReturn(null);
+	expect(this.request.getCookieMap()).andReturn(map);
+	replay(this.request);
+	this.userInfoHelper.setDecryptor(cryptor);
+        UserInfo userInfo = this.userInfoHelper.getUserInfo(this.request);
+        assertEquals(Affiliation.SOM, userInfo.getAffiliation());
+        assertTrue(userInfo.getProxyLinks());
+        assertEquals(this.sunetid, userInfo.getSunetId());
+        assertNotNull(userInfo.getTicket());
+        assertEquals(46, userInfo.getTicket().toString().length());
+        assertEquals(this.sunetid, userInfo.getPerson().getSunetId()[0]);
+        verify(this.session);
+        verify(this.request);
+    }
+    
     public void testAffiliation() {
         String ip = this.ip.concat("FAIL_TEST");
         expect(this.request.getRemoteAddr()).andReturn(ip);
+        expect(this.request.getRemoteUser()).andReturn(this.sunetid);
         replay(this.request);
         UserInfo userInfo = this.userInfoHelper.getUserInfo(this.request);
         assertEquals(Affiliation.ERR, userInfo.getAffiliation());
