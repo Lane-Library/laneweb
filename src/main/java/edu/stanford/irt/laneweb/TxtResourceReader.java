2a8b89c884a0a6d9f9eb6216f1052b1f72aa77e9
Charles Yates <ceyates@stanford.edu>
2009-02-04 04:15:16 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@53279 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/TxtResourceReader.java
+++ src/main/java/edu/stanford/irt/laneweb/TxtResourceReader.java
@@ -1,10 +1,8 @@
 package edu.stanford.irt.laneweb;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.Map;
 
