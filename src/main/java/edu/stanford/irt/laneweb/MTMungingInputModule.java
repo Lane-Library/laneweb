5d715a05075fb6178a46408847c7e79c8aa5be79
Charles Yates <ceyates@stanford.edu>
2009-02-10 00:01:46 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@53543 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/MTMungingInputModule.java
+++ src/main/java/edu/stanford/irt/laneweb/MTMungingInputModule.java
@@ -8,6 +8,14 @@ import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
 import org.apache.cocoon.components.modules.input.InputModule;
 
+/**
+ * this InputModule takes a parameter string and munges it the way MoveableType
+ * does so that the category label resolves to the correct category archive document.
+ * it also is useful in translating the formerly used _id form for faq ids to 
+ * the id without the preceeding underscore.
+ * @author ceyates
+ *
+ */
 public class MTMungingInputModule implements InputModule {
     
     private Pattern pattern = Pattern.compile("[_\\W\\s]+");
