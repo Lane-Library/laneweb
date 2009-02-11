a0d01a995b165d4bbc3215ae49eec93813f7ef3b
Alain Boussard <alainb@stanford.edu>
2009-02-11 17:19:31 +0000

    to add back the redirect to https
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@53621 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/LanewebHTTPSFilter.java
@@ -0,0 +1,50 @@
+/*
+ * Created on Aug 5, 2004 To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package edu.stanford.irt.laneweb;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author ceyates This filter redirects to https if the scheme is not https or
+ *         there is not a gohttps header as set by the BigIP load balancer for
+ *         urls that it is doing the ssl stuff. At the moment we can't do the
+ *         reverse because BigIP goes into a loop if you try to redirect from
+ *         https to http.
+ */
+public class LanewebHTTPSFilter implements Filter {
+
+    /**
+         * does the redirect if no gohttps header or scheme is not https
+         */
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException,
+	    ServletException {
+	HttpServletRequest req = (HttpServletRequest) request;
+	HttpServletResponse resp = (HttpServletResponse) response;
+	String queryString = req.getQueryString();
+	String url = queryString == null ? req.getRequestURL().toString() : req.getRequestURL().append('?').append(queryString).toString();
+	int colonIndex = url.indexOf(':');
+	if ((req.getHeader("gohttps") != null) || "https".equals(req.getScheme())) {
+	    chain.doFilter(request, response);
+	} else {
+	    resp.sendRedirect("https" + url.substring(colonIndex));
+	}
+    }
+
+    public void destroy() {
+    }
+
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
+
+}
