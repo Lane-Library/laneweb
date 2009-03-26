431283490711ddc4b0fff9b20c565f7c7a28c08f
Charles Yates <ceyates@stanford.edu>
2009-03-26 21:35:23 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@56087 c440208b-5602-0410-a095-92527a996296

---
+++ src/test/javascript/lane-popin-test.js
@@ -0,0 +1,14 @@
+/**
+ * @author ceyates
+ */
+(function() {
+var Assert = YAHOO.util.Assert;
+var TestRunner = YAHOO.tool.TestRunner;
+var TestCase = YAHOO.tool.TestCase;
+var UserAction = YAHOO.util.UserAction;
+
+
+new YAHOO.tool.TestLogger();
+
+TestRunner.run();
+})();
