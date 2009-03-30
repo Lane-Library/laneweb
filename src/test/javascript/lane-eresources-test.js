e0e66627b84b8921bcb98217f91b51e6b399b349
Charles Yates <ceyates@stanford.edu>
2009-03-30 20:18:39 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@56231 c440208b-5602-0410-a095-92527a996296

--- src/test/javascript/lane-eresources-test.js
+++ src/test/javascript/lane-eresources-test.js
@@ -2,13 +2,23 @@
  * @author ceyates
  */
 (function() {
-var Assert = YAHOO.util.Assert;
-var TestRunner = YAHOO.tool.TestRunner;
-var TestCase = YAHOO.tool.TestCase;
-var UserAction = YAHOO.util.UserAction;
+	var Assert = YAHOO.util.Assert;
+	var TestRunner = YAHOO.tool.TestRunner;
+	var TestCase = YAHOO.tool.TestCase;
+	var UserAction = YAHOO.util.UserAction;
+
+    var EresourcesTestCase = new TestCase({
+        name: "Eresources TestCase",
+        testFoo: function() {
+    		var result = {};
+    		LANE.search.eresources.setCurrentResult(result);
+    		Assert.areEqual(result, LANE.search.eresources.getCurrentResult());
+    	}
+    });
 
 
-new YAHOO.tool.TestLogger();
 
-TestRunner.run();
+	new YAHOO.tool.TestLogger();
+	TestRunner.add(EresourcesTestCase);
+	TestRunner.run();
 })();
