cb57aeb358b829aa4f9253f984e6d8b5998d8339
Charles Yates <ceyates@stanford.edu>
2009-04-28 22:51:30 +0000

    ie fixes
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@57607 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/javascript/lane-ie.js
+++ src/main/webapp/javascript/lane-ie.js
@@ -1,48 +1,78 @@
 (function(){
     if (YAHOO.env.ua.ie) {
         YAHOO.util.Event.addListener(this, 'load', function(){
-            var ie = YAHOO.env.ua.ie, lists, i, d = document, list,
+            var ie = YAHOO.env.ua.ie,
+                lists, i,
+                d = document,
+                list,
                 //the bottom shaded border z-index has to be reduced . . .
-                sbb = d.getElementById('sb-bot'),
-                opo = d.getElementById('otherPortalOptions'),
-                ldd = d.getElementById('legend-drop-down');
-            if (ie <= 6) {
-                //set hover class for these ids
-                if (opo && sbb) {
-                    opo.activate = function(){
-                        sbb.style.zIndex = -1;
-                        this.className = 'hover';
-                    };
-                    opo.deactivate = function(){
-                        sbb.style.zIndex = 1;
-                        this.className = '';
-                    };
+                shadedBorderBottom = d.getElementById('sb-bot'),
+                otherPortalOptions = d.getElementById('otherPortalOptions'),
+                legendDropDown = d.getElementById('legend-drop-down'),
+                otherSearches = d.getElementById('otherSearches');
+                if (ie <= 6) {
+                    //set hover class for these ids
+                    if (shadedBorderBottom) {
+                        if (otherPortalOptions) {
+                            otherPortalOptions.activate = function(){
+                                shadedBorderBottom.style.zIndex = -1;
+                                this.className = 'hover';
+                            };
+                            otherPortalOptions.deactivate = function(){
+                                shadedBorderBottom.style.zIndex = 1;
+                                this.className = '';
+                            };
+                        }
+                        if (otherSearches) {
+                            otherSearches.activate = function(){
+                                shadedBorderBottom.style.zIndex = -1;
+                                this.className = 'hover';
+                            };
+                            otherSearches.deactivate = function(){
+                                shadedBorderBottom.style.zIndex = 1;
+                                this.className = '';
+                            };
+                        }
+                    }
+                    if (legendDropDown) {
+                        legendDropDown.activate = function(){
+                            this.className = 'hover';
+                        };
+                        legendDropDown.deactivate = function(){
+                            this.className = '';
+                        };
+                    }
+                    //TODO: use onavailable for this?
+                    lists = ['shd', 'ft', 'gft', 'searchTabs'];
+                    for (i = 0; i < lists.length; i++) {
+                        list = d.getElementById(lists[i]);
+                        if (list) {
+                            list.getElementsByTagName('li')[0].style.border = 'none';
+                        }
+                    }
                 }
-                if (ldd) {
-                    ldd.activate = function(){
-                        this.className = 'hover';
-                    };
-                    ldd.deactivate = function(){
-                        this.className = '';
-                    };
-                }
-                //TODO: use onavailable for this?
-                lists = ['shd','ft','gft'];
-                for (i = 0; i < lists.length; i++) {
-                    list = d.getElementById(lists[i]);
-                    list.getElementsByTagName('li')[0].style.border = 'none';
-                }
-            } else if (ie >= 7 && ie < 8) {
-                //ie 7 stil messes up z-index:
-                if (opo && sbb) {
-                    opo.activate = function(){
-                        sbb.style.zIndex = -1;
-                    };
-                    opo.deactivate = function(){
-                        sbb.style.zIndex = 1;
-                    };
-                }
-            }
-        });
-    }
+                else 
+                    if (ie >= 7 && ie < 8) {
+                        //ie 7 stil messes up z-index:
+                        if (shadedBorderBottom) {
+                            if (otherPortalOptions) {
+                                otherPortalOptions.activate = function(){
+                                    shadedBorderBottom.style.zIndex = -1;
+                                };
+                                otherPortalOptions.deactivate = function(){
+                                    shadedBorderBottom.style.zIndex = 1;
+                                };
+                            }
+                            if (otherSearches) {
+                                otherSearches.activate = function(){
+                                    shadedBorderBottom.style.zIndex = -1;
+                                };
+                                otherPortalOptions.deactivate = function(){
+                                    shadedBorderBottom.style.zIndex = 1;
+                                };
+                            }
+                        }
+                    }
+            });
+        }
 })();
