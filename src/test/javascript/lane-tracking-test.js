e8045f39c04086835a5611c0a80c77b8af113951
Charles Yates <ceyates@stanford.edu>
2009-06-17 21:39:20 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@59099 c440208b-5602-0410-a095-92527a996296

--- src/test/javascript/lane-tracking-test.js
+++ src/test/javascript/lane-tracking-test.js
@@ -2,72 +2,69 @@
  * @author ceyates
  */
 (function() {
-var Assert = YAHOO.util.Assert;
-var TestRunner = YAHOO.tool.TestRunner;
-var TestCase = YAHOO.tool.TestCase;
-var UserAction = YAHOO.util.UserAction;
 
-var LANETrackingTestCase = new TestCase({
+var LANETrackingTestCase = new YAHOO.tool.TestCase({
     name: "Lane Tracking TestCase",
-	trackingData:{},
-	track: function(td) {
-		trackingData = td;
-	},
-	setup: function() {
-		trackingData = {};
-	},
+    trackingData:{},
+    track: function(td) {
+        trackingData = td;
+    },
+    setup: function() {
+        trackingData = {};
+    },
     testExists: function(){
-        Assert.isObject(LANE.track);
+        YAHOO.util.Assert.isObject(LANE.track);
     },
     testAddTracker: function() {
         try {
             LANE.tracking.addTracker({});
         } catch (ex) {
-            Assert.areEqual(ex, 'tracker does not implement track()');
+            YAHOO.util.Assert.areEqual(ex, 'tracker does not implement track()');
         }
         LANE.tracking.addTracker(this);
     },
-	testGetTrackingTitle: function() {
-		var i, event = {type:'click'},
-		    a = document.getElementById('testGetTrackedTitle').getElementsByTagName('a'),
-			t = LANE.tracking;
-		for (i = 0; i < a.length; i++) {
-			event.target = a[i];
-			LANE.tracking.trackEvent(event);
-			Assert.areEqual(a[i].rel, trackingData.title);
-		}
-	},
+    testGetTrackingTitle: function() {
+        var i,
+            event = {type:'click'},
+            anchors = document.getElementById('testGetTrackedTitle').getElementsByTagName('a');
+        for (i = 0; i < anchors.length; i++) {
+            event.target = anchors[i];
+            LANE.tracking.trackEvent(event);
+            YAHOO.util.Assert.areEqual(anchors[i].rel, trackingData.title);
+        }
+    },
     testIsTrackable: function() {
-        var i, m, t = LANE.tracking,
-        a = document.getElementById('testIsTrackable').getElementsByTagName('a'),
-        e = {type:'click'};
-        for (i = 0; i < a.length; i++) {
-            e.target = a[i];
-            if (a[i].rel == 'trackable') {
-                Assert.isTrue(t.isTrackable(e));
+        var i,
+            images = document.getElementById('testIsTrackable').getElementsByTagName('img'),
+            anchors = document.getElementById('testIsTrackable').getElementsByTagName('a'),
+            event = {type:'click'};
+        for (i = 0; i < anchors.length; i++) {
+            event.target = anchors[i];
+            if (anchors[i].rel == 'trackable') {
+                YAHOO.util.Assert.isTrue(LANE.tracking.isTrackable(event));
             } else {
-                Assert.isFalse(t.isTrackable(e));
+                YAHOO.util.Assert.isFalse(LANE.tracking.isTrackable(event));
             }
         }
-        m = document.getElementById('testIsTrackable').getElementsByTagName('img');
-        for (i = 0; i < m.length; i++) {
-            e.target = m[i];
-            if (m[i].parentNode.rel == 'trackable') {
-                Assert.isTrue(t.isTrackable(e));
+        for (i = 0; i < images.length; i++) {
+            event.target = images[i];
+            if (images[i].parentNode.rel == 'trackable') {
+                YAHOO.util.Assert.isTrue(LANE.tracking.isTrackable(event));
             } else {
-                Assert.isFalse(t.isTrackable(e));
+                YAHOO.util.Assert.isFalse(LANE.tracking.isTrackable(event));
             }
         }
     },
     testTrack: function() {
         LANE.tracking.track({host:'www.google.com',path:'/path',title:'title'});
-		Assert.areEqual('www.google.com', trackingData.host);
-		Assert.areEqual('/path', trackingData.path);
-		Assert.areEqual('title', trackingData.title);
+        YAHOO.util.Assert.areEqual('www.google.com', trackingData.host);
+        YAHOO.util.Assert.areEqual('/path', trackingData.path);
+        YAHOO.util.Assert.areEqual('title', trackingData.title);
     }
 });
 
-var oLogger = new YAHOO.tool.TestLogger();
-TestRunner.add(LANETrackingTestCase);
-TestRunner.run();
+new YAHOO.tool.TestLogger();
+YAHOO.tool.TestRunner.add(LANETrackingTestCase);
+YAHOO.tool.TestRunner.run();
+
 })();
