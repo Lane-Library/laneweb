f7168f98a812233aa0b05269f078517fe9352675
Ryan Max Steinberg <ryanmax@stanford.edu>
2009-06-19 21:14:40 +0000

    transforming "{emrid}" value of input elements for forms
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@59233 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cme/CMELinkTransformer.java
+++ src/main/java/edu/stanford/irt/laneweb/cme/CMELinkTransformer.java
@@ -11,18 +11,30 @@ import org.xml.sax.helpers.AttributesImpl;
 
 import edu.stanford.irt.laneweb.user.User;
 
+/**
+ * Transformer to rewrite data when a User.EMRID value is present. 
+ * Elements to rewrite include: 
+ * - anchor element href attributes when value is known CME host 
+ * - url element of metasearch results when value is known CME host 
+ * - input element value attributes when found value is REPLACEMENT_STRING
+ * 
+ * @author ryanmax
+ */
 public class CMELinkTransformer extends AbstractTransformer {
 
     private static final String EMPTY_STRING = "";
-
+    
     private static final String HREF = "href";
 
+    private static final String VALUE = "value";
+    
+    private static final String REPLACEMENT_STRING = "{emrid}";
+    
     private static final String UTD_CME_URL = "http://www.uptodate.com/online/content/search.do?";
     
     private static final String UTD_CME_ARGS = "unid=?&srcsys=epicXXX&eiv=2.1.0";
 
-    // TODO: once more vendors, will want to move UTD strings out to
-    // CME_HOSTS<host, cme_args> object
+    // TODO: once more vendors, move UTD strings to collection of host objects
     private static final String[] UTD_HOSTS = { "www.utdol.com", "www.uptodate.com" };
 
     private String emrid;
@@ -52,15 +64,24 @@ public class CMELinkTransformer extends AbstractTransformer {
         this.isSearchUrlElement = false;
         if (!EMPTY_STRING.equals(this.emrid)) {
             if ("a".equals(localName)) {
-                String link = atts.getValue(HREF);
-                if (null != link && link.indexOf("http") == 0 && isCMEHost(link)) {
-                    AttributesImpl newAttributes = new AttributesImpl(atts);
-                    newAttributes.setValue(newAttributes.getIndex(HREF), createCMELink(link));
-                    this.xmlConsumer.startElement(uri, localName, name, newAttributes);
-                    return;
-                }
+              String link = atts.getValue(HREF);
+              if (null != link && link.indexOf("http") == 0 && isCMEHost(link)) {
+                AttributesImpl newAttributes = new AttributesImpl(atts);
+                newAttributes.setValue(newAttributes.getIndex(HREF), createCMELink(link));
+                this.xmlConsumer.startElement(uri, localName, name, newAttributes);
+                return;
+              }
             }
-            if ("url".equals(localName)) {
+            else if ("input".equals(localName)) {
+              String value = atts.getValue(VALUE);
+              if (null != value && REPLACEMENT_STRING.equals(value)) {
+                AttributesImpl newAttributes = new AttributesImpl(atts);
+                newAttributes.setValue(newAttributes.getIndex(VALUE), this.emrid);
+                this.xmlConsumer.startElement(uri, localName, name, newAttributes);
+                return;
+              }
+            }
+            else if ("url".equals(localName)) {
                 this.isSearchUrlElement = true;
             }
         }
