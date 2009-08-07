f5cd23d6d48582613b0a10872d6d7ac33a3d0e36
Charles Yates <ceyates@stanford.edu>
2009-08-07 20:41:00 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@60792 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/LanewebObjectModelProvider.java
+++ src/main/java/edu/stanford/irt/laneweb/LanewebObjectModelProvider.java
@@ -13,15 +13,17 @@ import org.apache.cocoon.processing.ProcessInfoProvider;
 import edu.stanford.irt.laneweb.user.User;
 import edu.stanford.irt.laneweb.user.UserDao;
 
-
 public class LanewebObjectModelProvider implements ObjectModelProvider {
-    
-    private ProcessInfoProvider processInfoProvider;
+
     private Map<String, Object> jndiData;
-    private UserDao userDao;
+
+    private ProcessInfoProvider processInfoProvider;
+
     private ProxyLinks proxyLinks;
-    
-    public LanewebObjectModelProvider(ProcessInfoProvider pip, Map<String, Object> jndiData, UserDao userDao, ProxyLinks proxyLinks) {
+
+    private UserDao userDao;
+
+    public LanewebObjectModelProvider(final ProcessInfoProvider pip, final Map<String, Object> jndiData, final UserDao userDao, final ProxyLinks proxyLinks) {
         this.processInfoProvider = pip;
         this.jndiData = jndiData;
         this.userDao = userDao;
@@ -29,32 +31,29 @@ public class LanewebObjectModelProvider implements ObjectModelProvider {
     }
 
     public Object getObject() {
-        Map objectModel = processInfoProvider.getObjectModel();
-        
+        Map objectModel = this.processInfoProvider.getObjectModel();
         Map<String, Object> cocoonMap = new HashMap<String, Object>();
-        
-        //cocoon.request
+        // cocoon.request
         Request request = ObjectModelHelper.getRequest(objectModel);
         cocoonMap.put("request", request);
-        
-        //cocoon.session
-        HttpSession session = request.getSession(true);
-        cocoonMap.put("session", session);
-        User user = (User) session.getAttribute(LanewebConstants.USER);
-        if (null == user) {
-            user = new User();
-            session.setAttribute(LanewebConstants.USER, user);
+        // cocoon.session
+        HttpSession session = request.getSession(false);
+        if (null != session) {
+            cocoonMap.put("session", session);
+            User user = (User) session.getAttribute(LanewebConstants.USER);
+            if (null == user) {
+                user = new User();
+                session.setAttribute(LanewebConstants.USER, user);
+            }
+            this.userDao.getUserData(user, request);
+            cocoonMap.put("proxyLinks", this.proxyLinks.proxyLinks(user, request));
+        } else {
+            cocoonMap.put("proxyLinks", Boolean.TRUE);
         }
-        this.userDao.getUserData(user, request);
-        
-        cocoonMap.put("proxyLinks", this.proxyLinks.proxyLinks(user, request));
-        
         // cocoon.context
         org.apache.cocoon.environment.Context context = ObjectModelHelper.getContext(objectModel);
         cocoonMap.put("context", context);
-        
         cocoonMap.put("jndi", this.jndiData);
-        
         return cocoonMap;
     }
 }
