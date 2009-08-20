04a9ee04eea32340c44d350b0bbb6a1afa0d4bc8
Charles Yates <ceyates@stanford.edu>
2009-08-20 16:18:17 +0000

    added DeleteCookieAction
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@61303 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/DeleteCookieAction.java
@@ -0,0 +1,31 @@
+package edu.stanford.irt.laneweb;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.Action;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.SourceResolver;
+
+
+public class DeleteCookieAction implements Action {
+
+    @SuppressWarnings("unchecked")
+    public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, String source, Parameters parameters) {
+        String cookieName = parameters.getParameter("cookie-name", null);
+        if (null == cookieName || cookieName.length() == 0) {
+            throw new IllegalArgumentException("no cookie-name");
+        }
+        HttpServletResponse response = ObjectModelHelper.getResponse(objectModel);
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return Collections.emptyMap();
+    }
+}
