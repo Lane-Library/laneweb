2c1a1e7eef02ffabc3a8d634c2cf1d1afcd017f6
Charles Yates <ceyates@stanford.edu>
2009-11-18 21:58:00 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@64635 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/search/AbstractSearchGenerator.java
@@ -0,0 +1,49 @@
+package edu.stanford.irt.laneweb.search;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.generation.Generator;
+import org.apache.cocoon.xml.XMLConsumer;
+import org.xml.sax.SAXException;
+
+import edu.stanford.irt.search.MetaSearchManager;
+import edu.stanford.irt.search.Result;
+import edu.stanford.irt.search.util.SAXResult;
+import edu.stanford.irt.search.util.SAXable;
+
+public abstract class AbstractSearchGenerator implements Generator {
+
+    private XMLConsumer xmlConsumer;
+
+    protected MetaSearchManager metaSearchManager;
+    
+    protected String query;
+
+    public void generate() throws SAXException {
+        Result result = doSearch();
+        SAXable xml = new SAXResult(result);
+        synchronized (result) {
+            xml.toSAX(this.xmlConsumer);
+        }
+    }
+
+    public void setConsumer(final XMLConsumer xmlConsumer) {
+        this.xmlConsumer = xmlConsumer;
+    }
+
+    public void setMetaSearchManagerSource(final MetaSearchManagerSource msms) {
+        this.metaSearchManager = msms.getMetaSearchManager();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setup(final SourceResolver resolver, final Map objectModel, final String src, final Parameters par) {
+        this.query = par.getParameter("query", null);
+        if (this.query == null) {
+            throw new IllegalStateException("null query");
+        }
+    }
+
+    protected abstract Result doSearch();
+}
