76ee8a52530fe07373a7c0dfbd87479c188bf1bd
Charles Yates <ceyates@stanford.edu>
2010-01-05 00:26:50 +0000

    tell saxon that output method is xhtml (handles <div></div>, etc)
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@65702 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/XHTMLSerializer.java
@@ -0,0 +1,38 @@
+package edu.stanford.irt.laneweb;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.cocoon.serialization.AbstractTextSerializer;
+
+public class XHTMLSerializer extends AbstractTextSerializer {
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        this.format.put(OutputKeys.METHOD, "xhtml");
+    }
+
+    /**
+     * Set the {@link OutputStream} where the requested resource should be
+     * serialized.
+     */
+    @Override
+    public void setOutputStream(final OutputStream out) throws IOException {
+        super.setOutputStream(out);
+        try {
+            TransformerHandler handler = this.getTransformerHandler();
+            handler.getTransformer().setOutputProperties(this.format);
+            handler.setResult(new StreamResult(this.output));
+            this.setContentHandler(handler);
+            this.setLexicalHandler(handler);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
