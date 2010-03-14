e535259ab87e0a869f051ab4e60e28f2c2f614ff
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-03-14 03:27:18 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@68067 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-tooltips.js
+++ src/main/webapp/resources/javascript/lane-tooltips.js
@@ -19,19 +19,27 @@
                     for(j = 0; j < tt.length; j++) {
                         if(tt[j].nodeType == 1) {
                             e = tt[j].id.replace(/Tooltip$/,'');
-                            if(e && YUD.inDocument(e) && !YUD.inDocument(e+"-yuitt")) {
+                            if(e && YUD.inDocument(e)) {
                                 document.getElementById(e).trackable = true;
                                 // tooltips where content is in the markup (abstracts, etc.)
                                 if(tt[j].innerHTML && tt[j].innerHTML != e.title){
-                                	w = tt[j].style.width || '25%';
-                                	new YUW.Tooltip(
-                                			e+"-yuitt", 
+                                	if(tt[j].style.width){
+                                		w = tt[j].style.width;
+                                	}
+                                	else if(tt[j].innerHTML.length > 500){
+                                		w = '60%';
+                                	}
+                                	else{
+                                		w = '25%';
+                                	}
+                                	new YUW.Tooltip(e+"-yuitt",
                                 			{
                                 				context:e,
                                 				width:w,
                                 				autodismissdelay:60000,
                                 				text:tt[j].innerHTML
-                                			});
+                                			}
+                                	);
                                 }
                                 // just use title attribute for all other tooltips
                                 else{
