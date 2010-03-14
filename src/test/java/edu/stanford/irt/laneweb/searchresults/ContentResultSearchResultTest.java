2b6bfb6168df0436b4dad86fbe3b24aedac29c16
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-03-14 03:36:04 +0000

    https://irt-bugs.stanford.edu/default.asp?34698
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@68069 c440208b-5602-0410-a095-92527a996296

---
+++ src/test/java/edu/stanford/irt/laneweb/searchresults/ContentResultSearchResultTest.java
@@ -0,0 +1,149 @@
+/**
+ * 
+ */
+package edu.stanford.irt.laneweb.searchresults;
+
+import static org.junit.Assert.*;
+
+import edu.stanford.irt.laneweb.search.QueryTermPattern;
+import edu.stanford.irt.search.impl.DefaultContentResult;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * @author ryanmax
+ *
+ */
+public class ContentResultSearchResultTest {
+
+    Pattern queryTermPattern;
+
+    DefaultContentResult contentResult1;
+
+    DefaultContentResult contentResult2;
+
+    ContentResultSearchResult contentResultSearchResult1;
+
+    ContentResultSearchResult contentResultSearchResult2;
+
+    @Test
+    public void testCompareToNonFiling() {
+        this.queryTermPattern = QueryTermPattern.getPattern("q");
+        this.contentResult1 = new DefaultContentResult("foo");
+        this.contentResult1.setTitle("a title");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("foo");
+        this.contentResult2.setTitle("title");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) == 0);
+        
+        this.queryTermPattern = QueryTermPattern.getPattern("q");
+        this.contentResult1 = new DefaultContentResult("foo");
+        this.contentResult1.setTitle("an title");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("foo");
+        this.contentResult2.setTitle("title");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) == 0);
+        
+        this.queryTermPattern = QueryTermPattern.getPattern("q");
+        this.contentResult1 = new DefaultContentResult("foo");
+        this.contentResult1.setTitle("the title");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("foo");
+        this.contentResult2.setTitle("title");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) == 0);
+    }
+
+    @Test
+    public void testCompareToExactTitle() {
+        this.queryTermPattern = QueryTermPattern.getPattern("exact title match");
+        this.contentResult1 = new DefaultContentResult("pubmed");
+        this.contentResult1.setTitle("exact title match");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("pubmed");
+        this.contentResult2.setTitle("foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) < 0);
+    }
+
+    @Test
+    public void testCompareToTitleBeginsWith() {
+        this.queryTermPattern = QueryTermPattern.getPattern("title begins with");
+        this.contentResult1 = new DefaultContentResult("pubmed");
+        this.contentResult1.setTitle("not title begins with");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("pubmed");
+        this.contentResult2.setTitle("title begins with yes");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) > 0);
+    }
+
+    @Test
+    public void testCompareToDescriptionHits() {
+        this.queryTermPattern = QueryTermPattern.getPattern("foo");
+        this.contentResult1 = new DefaultContentResult("pubmed");
+        this.contentResult1.setTitle("bar");
+        this.contentResult1.setDescription("bar");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("pubmed");
+        this.contentResult2.setTitle("bar");
+        this.contentResult2.setDescription("foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) > 0);
+        
+        // title hits and description hits
+        this.contentResult1 = new DefaultContentResult("pubmed");
+        this.contentResult1.setTitle("title foo bar");
+        this.contentResult1.setDescription("just bar");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        this.contentResult2 = new DefaultContentResult("pubmed");
+        this.contentResult2.setTitle("title foo bar");
+        this.contentResult2.setDescription("i contain foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) > 0);
+    }
+
+    @Test
+    public void testCompareToWeighting() {
+        this.queryTermPattern = QueryTermPattern.getPattern("query terms");
+        this.contentResult1 = new DefaultContentResult("pubmed");
+        this.contentResult1.setTitle("foo");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        // double weight
+        this.contentResult2 = new DefaultContentResult("pubmed_cochrane_reviews");
+        this.contentResult2.setTitle("foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) > 0);
+        
+        this.contentResult1 = new DefaultContentResult("xxxx");
+        this.contentResult1.setTitle("foo");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        // half weight
+        this.contentResult2 = new DefaultContentResult("pubmed_recent_reviews");
+        this.contentResult2.setTitle("foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) < 0);
+        
+        this.contentResult1 = new DefaultContentResult("xxxx");
+        this.contentResult1.setTitle("foo");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        // quarter weight
+        this.contentResult2 = new DefaultContentResult("medlineplus_0");
+        this.contentResult2.setTitle("foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) < 0);
+        
+        this.contentResult1 = new DefaultContentResult("xxxx");
+        this.contentResult1.setTitle("foo");
+        this.contentResultSearchResult1 = new ContentResultSearchResult(this.contentResult1, this.queryTermPattern);
+        // equal weight
+        this.contentResult2 = new DefaultContentResult("yyyy");
+        this.contentResult2.setTitle("foo");
+        this.contentResultSearchResult2 = new ContentResultSearchResult(this.contentResult2, this.queryTermPattern);
+        assertTrue(this.contentResultSearchResult1.compareTo(this.contentResultSearchResult2) == 0);
+    }
+}
---
+++ src/test/java/edu/stanford/irt/laneweb/searchresults/EresourceSearchResultTest.java
@@ -0,0 +1,182 @@
+/**
+ * 
+ */
+package edu.stanford.irt.laneweb.searchresults;
+
+import static org.junit.Assert.*;
+
+import edu.stanford.irt.eresources.Eresource;
+import edu.stanford.irt.eresources.impl.EresourceImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * @author ryanmax
+ *
+ */
+public class EresourceSearchResultTest {
+
+    private Eresource eresource1;
+
+    private Eresource eresource2;
+    
+    private Eresource eresource3;
+    
+    private EresourceSearchResult eresourceSearchResult1;
+    
+    private EresourceSearchResult eresourceSearchResult2;
+    
+    private EresourceSearchResult eresourceSearchResult3;
+    
+    @Before
+    public void setUp() {
+        this.eresource1 = new EresourceImpl();
+        this.eresource2 = new EresourceImpl();
+        this.eresource3 = new EresourceImpl();
+    }
+
+    /**
+     * Test method for {@link edu.stanford.irt.laneweb.searchresults.EresourceSearchResult#compareTo(edu.stanford.irt.laneweb.searchresults.SearchResult)}.
+     */
+    @Test
+    public void testCompareToCase() {
+        this.eresource1.setTitle("foo");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("FOO");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) == 0);
+    }
+    
+    /**
+     * Test method for {@link edu.stanford.irt.laneweb.searchresults.EresourceSearchResult#compareTo(edu.stanford.irt.laneweb.searchresults.SearchResult)}.
+     */
+    @Test
+    public void testCompareToScore() {
+        this.eresource1.setTitle("foo");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) == 0);
+        
+        this.eresource1.setTitle("foo");
+        this.eresource1.setScore(100);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) < 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) > 0);
+    }
+    
+    /**
+     * Test method for {@link edu.stanford.irt.laneweb.searchresults.EresourceSearchResult#compareTo(edu.stanford.irt.laneweb.searchresults.SearchResult)}.
+     */
+    @Test
+    public void testCompareToNonFiling() {
+        this.eresource1.setTitle("a foo");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) == 0);
+        
+        this.eresource1.setTitle("an foo");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) == 0);
+        
+        this.eresource1.setTitle("the foo");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) == 0);
+    }
+    
+    /**
+     * Test method for {@link edu.stanford.irt.laneweb.searchresults.EresourceSearchResult#compareTo(edu.stanford.irt.laneweb.searchresults.SearchResult)}.
+     */
+    @Test
+    public void testCompareToEqual() {
+        this.eresource1.setTitle("foo");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("The Foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) == 0);
+        
+        this.eresource1.setTitle("a foo");
+        this.eresource1.setScore(0);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("Foo");
+        this.eresource2.setScore(0);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        this.eresource3.setTitle("the foo");
+        this.eresource3.setScore(0);
+        this.eresourceSearchResult3 = new EresourceSearchResult(this.eresource3);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) == 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult3) == 0);
+        assertTrue(this.eresourceSearchResult3.compareTo(this.eresourceSearchResult1) == 0);
+    }
+    
+    /**
+     * Test method for {@link edu.stanford.irt.laneweb.searchresults.EresourceSearchResult#compareTo(edu.stanford.irt.laneweb.searchresults.SearchResult)}.
+     */
+    @Test
+    public void testCompareToUnequal() {
+        this.eresource1.setTitle("bar");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("a bar");
+        this.eresource2.setScore(100);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) > 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) < 0);
+        
+        this.eresource1.setTitle("foo bar");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("bar foo");
+        this.eresource2.setScore(99);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) > 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult1) < 0);
+    }
+    
+    /**
+     * Test method for {@link edu.stanford.irt.laneweb.searchresults.EresourceSearchResult#compareTo(edu.stanford.irt.laneweb.searchresults.SearchResult)}.
+     */
+    @Test
+    public void testCompareToIntransitive() {
+        this.eresource1.setTitle("the bar");
+        this.eresource1.setScore(99);
+        this.eresourceSearchResult1 = new EresourceSearchResult(this.eresource1);
+        this.eresource2.setTitle("a bar");
+        this.eresource2.setScore(100);
+        this.eresourceSearchResult2 = new EresourceSearchResult(this.eresource2);
+        this.eresource3.setTitle("a bar");
+        this.eresource3.setScore(101);
+        this.eresourceSearchResult3 = new EresourceSearchResult(this.eresource3);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult2) > 0);
+        assertTrue(this.eresourceSearchResult2.compareTo(this.eresourceSearchResult3) > 0);
+        assertTrue(this.eresourceSearchResult1.compareTo(this.eresourceSearchResult3) > 0);
+    }
+}
