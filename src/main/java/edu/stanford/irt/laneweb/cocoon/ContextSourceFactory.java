6dd3d10b97e14e704287d46bcb37c89ac79326d1
Charles Yates <ceyates@stanford.edu>
2010-04-06 04:47:08 +0000

    locally implement ContextSourceFactory
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@68979 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cocoon/ContextSourceFactory.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/ContextSourceFactory.java
@@ -1,15 +1,47 @@
 package edu.stanford.irt.laneweb.cocoon;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 
-import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.excalibur.source.Source;
+import org.apache.excalibur.source.SourceFactory;
 
+public class ContextSourceFactory implements SourceFactory {
 
-public class ContextSourceFactory extends org.apache.cocoon.components.source.impl.ContextSourceFactory {
+    private ServletContext servletContext;
 
-    public ContextSourceFactory(ServletContext servletContext, ServiceManager serviceManager) {
-        this.manager = serviceManager;
+    private SourceResolver sourceResolver;
+
+    public ContextSourceFactory(final ServletContext servletContext, final SourceResolver sourceResolver) {
         this.servletContext = servletContext;
+        this.sourceResolver = sourceResolver;
     }
 
+    public Source getSource(final String location, final Map arg1) throws IOException, MalformedURLException {
+        // Remove the protocol and the first '/'
+        final int pos = location.indexOf(":/");
+        final String path = location.substring(pos + 2);
+        URL u;
+        // Try to get a file first and fall back to a resource URL
+        String actualPath = this.servletContext.getRealPath(path);
+        if (actualPath != null) {
+            u = new File(actualPath).toURI().toURL();
+        } else {
+            u = this.servletContext.getResource(path);
+        }
+        if (u != null) {
+            Source source = this.sourceResolver.resolveURI(u.toExternalForm());
+            return source;
+        } else {
+            throw new IllegalArgumentException(location + " could not be found.");
+        }
+    }
+
+    public void release(final Source arg0) {
+    }
 }
