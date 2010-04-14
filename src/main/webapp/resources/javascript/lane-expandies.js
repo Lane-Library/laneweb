91d229d76c6d23c1cadbf1d9d59bf2034c10475c
Charles Yates <ceyates@stanford.edu>
2010-04-14 23:08:45 +0000

    expandies.initialize not used
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69273 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-expandies.js
+++ src/main/webapp/resources/javascript/lane-expandies.js
@@ -1,37 +1,32 @@
 (function() {
-    LANE.namespace('expandies');
-    LANE.expandies = function() {
-        return {
-            initialize: function() {
-                var i, j, k, items, expanded, anchors, anchor = document.location.hash, expandies = YAHOO.util.Dom.getElementsByClassName('expandy');
+    YAHOO.util.Event.onDOMReady(function() {
+        var i, j, k, items,
+            expanded, anchors,
+            anchor = document.location.hash,
+            expandies = YAHOO.util.Dom.getElementsByClassName('expandy'),
+            YUD = YAHOO.util.DOM;
+        if (anchor) {
+            anchor = anchor.substring(1);
+        }
+        for (i = 0; i < expandies.length; i++) {
+            expanded = [];
+            items = YUD.getChildren(expandies[i]);
+            for (j = 0; j < items.length; j++) {
                 if (anchor) {
-                    anchor = anchor.substring(1);
-                }
-                for (i = 0; i < expandies.length; i++) {
-                    expanded = [];
-                    items = YAHOO.util.Dom.getChildren(expandies[i]);
-                    for (j = 0; j < items.length; j++) {
-                        if (anchor) {
-                            anchors = items[j].getElementsByTagName('A');
-                            for (k = 0; k < anchors.length; k++) {
-                                if (anchors[i].name == anchor) {
-                                    YAHOO.util.Dom.addClass(items[j], 'expanded');
-                                }
-                            }
-                        }
-                        if (YAHOO.util.Dom.hasClass(items[j], 'expanded')) {
-                            expanded.push(j);
+                    anchors = items[j].getElementsByTagName('A');
+                    for (k = 0; k < anchors.length; k++) {
+                        if (anchors[i].name == anchor) {
+                            YUD.addClass(items[j], 'expanded');
                         }
                     }
-                    new YAHOO.widget.AccordionView(expandies[i], {
-                        expandItems: expanded
-                    });
+                }
+                if (YUD.hasClass(items[j], 'expanded')) {
+                    expanded.push(j);
                 }
             }
-        };
-    }();
-    
-    YAHOO.util.Event.onDOMReady(function() {
-        LANE.expandies.initialize();
+            new YAHOO.widget.AccordionView(expandies[i], {
+                expandItems: expanded
+            });
+        }
     });
 })();
