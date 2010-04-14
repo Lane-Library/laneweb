2e2f0039a98450ecf45c5d781eca1536245bfedf
Charles Yates <ceyates@stanford.edu>
2010-04-14 02:21:40 +0000

    more descriptive variable names
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69223 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-tooltips.js
+++ src/main/webapp/resources/javascript/lane-tooltips.js
@@ -1,65 +1,61 @@
-(function(){
-    LANE.namespace('tooltips');
-    LANE.tooltips = function(){
-        return {
-            initialize: function(){
-                var tc, //tooltip container Elements
-                    tt, //tooltips
-                    e, //id of te element
-                    w, //width of tooltip
-                    i, j,
-                    simpleTtIds = [], // array of contexts that need tt made from their title attribute 
-                    YUD = YAHOO.util.Dom,
-                    YUW = YAHOO.widget;
+(function() {
+	LANE.namespace('tooltips');
+	LANE.tooltips = function() {
+		return {
+			initialize : function() {
+				var tooltipContainerArray, // array of tooltip containers
+				    tooltipContainer, // tooltips
+				    tooltipId, // id of te element
+				    tooltipWidth, // width of tooltip
+				    simpleTooltipIds = [], // array of contexts that need tt made
+										// from their title attribute
+				    YUD = YAHOO.util.Dom, YUW = YAHOO.widget, i, j;
 
-                tc = YUD.getElementsByClassName('tooltips');
-        
-                for(i = 0; i < tc.length; i++) {
-                    tt = tc[i].childNodes;
-                    for(j = 0; j < tt.length; j++) {
-                        if(tt[j].nodeType == 1) {
-                            e = tt[j].id.replace(/Tooltip$/,'');
-                            if(e && YUD.inDocument(e)) {
-                                document.getElementById(e).trackable = true;
-                                // tooltips where content is in the markup (abstracts, etc.)
-                                if(tt[j].innerHTML && tt[j].innerHTML != e.title){
-                                	if(tt[j].style.width){
-                                		w = tt[j].style.width;
-                                	}
-                                	else if(tt[j].innerHTML.length > 500){
-                                		w = '60%';
-                                	}
-                                	else{
-                                		w = '25%';
-                                	}
-                                	new YUW.Tooltip(e+"-yuitt",
-                                			{
-                                				context:e,
-                                				width:w,
-                                				autodismissdelay:60000,
-                                				text:tt[j].innerHTML
-                                			}
-                                	);
-                                }
-                                // just use title attribute for all other tooltips
-                                else{
-                                	simpleTtIds.push(e);
-                                }
-                            }
-                        }
-                    }
-                    // simple, single tooltip for all contexts using title attribute
-                	new YUW.Tooltip("simpleTT-yui", 
-                			{
-                				context:simpleTtIds,
-                				autodismissdelay:60000
-                			}
-                	);
-                }
-            }
-        }
-    }();
+				tooltipContainerArray = YUD.getElementsByClassName('tooltips');
 
-    YAHOO.util.Event.onDOMReady(LANE.tooltips.initialize);
-    
+				for (i = 0; i < tooltipContainerArray.length; i++) {
+					tooltipContainer = tooltipContainerArray[i].childNodes;
+					for (j = 0; j < tooltipContainer.length; j++) {
+						if (tooltipContainer[j].nodeType == 1) {
+							tooltipId = tooltipContainer[j].id.replace(/Tooltip$/, '');
+							if (tooltipId && YUD.inDocument(tooltipId)) {
+								document.getElementById(tooltipId).trackable = true;
+								// tooltips where content is in the markup
+								// (abstracts, etc.)
+								if (tooltipContainer[j].innerHTML
+										&& tooltipContainer[j].innerHTML != tooltipId.title) {
+									if (tooltipContainer[j].style.width) {
+										tooltipWidth = tooltipContainer[j].style.width;
+									} else if (tooltipContainer[j].innerHTML.length > 500) {
+										tooltipWidth = '60%';
+									} else {
+										tooltipWidth = '25%';
+									}
+									new YUW.Tooltip(tooltipId + "-yuitt", {
+										context : tooltipId,
+										width : tooltipWidth,
+										autodismissdelay : 60000,
+										text : tooltipContainer[j].innerHTML
+									});
+								} else {
+								    // just use title attribute for all other
+								    // tooltips
+									simpleTooltipIds.push(tooltipId);
+								}
+							}
+						}
+					}
+					// simple, single tooltip for all contexts using title
+					// attribute
+					new YUW.Tooltip("simpleTT-yui", {
+						context : simpleTooltipIds,
+						autodismissdelay : 60000
+					});
+				}
+			}
+		}
+	}();
+
+	YAHOO.util.Event.onDOMReady(LANE.tooltips.initialize);
+
 })();
