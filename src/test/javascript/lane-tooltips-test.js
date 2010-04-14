bfccff10bf5f5f0776bd2a854efae2c6e9c12abd
Charles Yates <ceyates@stanford.edu>
2010-04-14 05:37:43 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69226 c440208b-5602-0410-a095-92527a996296

--- src/test/javascript/lane-tooltips-test.js
+++ src/test/javascript/lane-tooltips-test.js
@@ -2,8 +2,22 @@
  * @author ceyates
  */
 (function() {
-
+	
+	var tooltipTestCase = new YAHOO.tool.TestCase({
+		name: 'Lane Tooltip Test Case',
+		testFoo: function() {
+			var yuitt = document.getElementById('a-yuitt');
+			var a = document.getElementById('a');
+			YAHOO.util.Assert.areEqual('hidden', yuitt.style.visibility);
+			YAHOO.util.UserAction.mouseover(a);
+//			YAHOO.util.Assert.areEqual('visible', yuitt.style.visibility);
+			YAHOO.util.UserAction.mouseout(a);
+//			YAHOO.util.Assert.areEqual('hidden', yuitt.style.visibility);
+		}
+	})
     new YAHOO.tool.TestLogger();
-    
-    YAHOO.tool.TestRunner.run();
+	YAHOO.tool.TestRunner.add(tooltipTestCase);
+	YAHOO.util.Event.addListener(window, 'load', function() {
+    	YAHOO.tool.TestRunner.run();
+	});
 })();
