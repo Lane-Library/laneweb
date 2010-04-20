cdedd40c63c20f178a35eec365a9acc0cd430969
Charles Yates <ceyates@stanford.edu>
2010-04-20 22:22:11 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69595 c440208b-5602-0410-a095-92527a996296

--- src/test/javascript/lane-metasearch-test.js
+++ src/test/javascript/lane-metasearch-test.js
@@ -3,7 +3,43 @@
  */
 (function() {
 
-    new YAHOO.tool.TestLogger();
+    var url;
+    var success;
+    var jsonResult;
+    YAHOO.util.Connect = {
+        asyncRequest: function(method, theUrl, object) {
+            url = theUrl;
+            success = object.success;
+        }
+    };
+    YAHOO.lang.JSON = {
+        parse: function() {
+            return jsonResult;
+        }
+    };
+    var metasearchTestCase = new YAHOO.tool.TestCase({
     
-    YAHOO.tool.TestRunner.run();
+        name: "Lane Metasearch Test Case",
+        
+        testURL: function() {
+            LANE.search.metasearch.initialize();
+            LANE.search.metasearch.getResultCounts();
+            YAHOO.util.Assert.isTrue(url.indexOf('/././apps/search/json?q=foo&r=foo&rd=') === 0);
+        },
+        
+        testSuccess: function() {
+            LANE.search.metasearch.initialize();
+            LANE.search.metasearch.getResultCounts();
+            jsonResult = {
+                resources: []
+            };
+            success({});
+        }
+    });
+
+    new YAHOO.tool.TestLogger();
+    YAHOO.tool.TestRunner.add(metasearchTestCase);
+    YAHOO.util.Event.addListener(this, 'load', function() {
+        YAHOO.tool.TestRunner.run();
+    });
 })();
