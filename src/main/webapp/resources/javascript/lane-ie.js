883ec1c1cf2a9b0f95bdc98d6f5bf1d8a4548b0a
Charles Yates <ceyates@stanford.edu>
2010-04-21 23:11:59 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69656 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-ie.js
+++ src/main/webapp/resources/javascript/lane-ie.js
@@ -1,119 +1,27 @@
-YUI().use('yui2-event', 'yui2-dom', function(Y){
-    if (Y.YUI2.env.ua.ie && Y.YUI2.env.ua.ie == 6) {
+YUI().use('yui2-event', 'node', function(Y){
+    if (Y.UA.ie < 7) {
        //instead of :first-child selector remove background image:
        Y.YUI2.util.Event.onAvailable('stanfordMedicineBrand', function() {
-            this.style.backgroundImage = 'none';
+           new Y.Node(this).setStyle('background-image','none');
         });
         //set up hover class on somNav menu:
         Y.YUI2.util.Event.onContentReady('somNav', function() {
-            var i, uls = this.getElementsByTagName('UL');
-            for (i = 0; i < uls.length; i++) {
-                uls[i].parentNode.activate = function() {
-                    Y.YUI2.util.Dom.addClass(this, 'hover');
+            var i, uls = new Y.Node(this).getElementsByTagName('UL');
+            for (i = 0; i < uls.size(); i++) {
+                Y.Node.getDOMNode(uls.item(i).get('parentNode')).activate = function() {
+                    new Y.Node(this).addClass('hover');
                 };
-                uls[i].parentNode.deactivate = function() {
-                     Y.YUI2.util.Dom.removeClass(this, 'hover');
+                Y.Node.getDOMNode(uls.item(i).get('parentNode')).deactivate = function() {
+                    new Y.Node(this).removeClass('hover');
                 };
             }
         });
         //instead of :first-child selector remove left border on .nav2 menus:
         Y.YUI2.util.Event.onDOMReady(function() {
-            var i, uls = Y.YUI2.util.Dom.getElementsByClassName('nav2', 'UL');
-            for (i = 0; i < uls.length; i++) {
-                uls[i].getElementsByTagName('LI')[0].style.border = 'none';
+            var i, uls = Y.all('.nav2');
+            for (i = 0; i < uls.size(); i++) {
+                uls.item(i).getElementsByTagName('LI').item(0).setStyle('border','none');
             }
         });
     }
 });
-        /*
-YAHOO.util.Event.addListener(this, 'load', function(){
-            var ie = YAHOO.env.ua.ie,
-                lists, i,
-                d = document,
-                list,
-                //the bottom shaded border z-index has to be reduced . . .
-                shadedBorderBottom = d.getElementById('sb-bot'),
-                otherPortalOptions = d.getElementById('otherPortalOptions'),
-                legendDropDown = d.getElementById('legend-drop-down'),
-                superHeader = d.getElementById('shd'),
-                searchSelect = d.getElementById('searchSelect'),
-                searchSubmit = d.getElementById('searchSubmit');
-                if (ie <= 6) {
-                    //set hover class for these ids
-                    if (otherPortalOptions && shadedBorderBottom) {
-                        otherPortalOptions.activate = function(){
-                            shadedBorderBottom.style.zIndex = -1;
-                            this.className = 'hover';
-                        };
-                        otherPortalOptions.deactivate = function(){
-                            shadedBorderBottom.style.zIndex = 1;
-                            this.className = '';
-                        };
-                    }
-                    if (legendDropDown) {
-                        legendDropDown.activate = function(){
-                            this.className = 'hover';
-                        };
-                        legendDropDown.deactivate = function(){
-                            this.className = '';
-                        };
-                    }
-                    if (superHeader) {
-                        var uls = superHeader.getElementsByTagName('UL');
-                        for (i = 0; i < uls.length; i++) {
-                            //hide search select for 1st superheader drop down
-                            if (i == 0 ) {
-                                uls[i].parentNode.activate = function() {
-                                    searchSelect.style.visibility = 'hidden';
-                                    this.className = 'hover';
-                                };
-                                uls[i].parentNode.deactivate = function() {
-                                    this.className = '';
-                                    searchSelect.style.visibility = 'visible';
-                                };
-                            } else {
-                                uls[i].parentNode.activate = function() {
-                                    this.className = 'hover';
-                                };
-                                uls[i].parentNode.deactivate = function() {
-                                    this.className = '';
-                                };
-                            }
-                        }
-                    }
-                    if (searchSubmit) {
-                        searchSubmit.activate = function() {
-                            this.className = 'hover';
-                        };
-                        searchSubmit.deactivate = function() {
-                            this.className = '';
-                        };
-                    }
-                    lists = ['ft', 'gft'];
-                    for (i = 0; i < lists.length; i++) {
-                        list = d.getElementById(lists[i]);
-                        if (list) {
-                            list.getElementsByTagName('li')[0].style.border = 'none';
-                        }
-                    }
-                    list = d.getElementById('shd');
-                    if (list) {
-                        list.getElementsByTagName('li')[0].style.backgroundImage = 'none';
-                    }
-                }
-                else 
-                    if (ie >= 7 && ie < 8) {
-                        //ie 7 stil messes up z-index:
-                        if (otherPortalOptions && shadedBorderBottom) {
-                            if (otherPortalOptions) {
-                                otherPortalOptions.activate = function(){
-                                    shadedBorderBottom.style.zIndex = -1;
-                                };
-                                otherPortalOptions.deactivate = function(){
-                                    shadedBorderBottom.style.zIndex = 1;
-                                };
-                            }
-                        }
-                    }
-            });
-*/
