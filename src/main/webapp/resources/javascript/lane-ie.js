81f57561c2505ceeca690893b048108101a1025c
Charles Yates <ceyates@stanford.edu>
2010-04-21 23:24:11 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69659 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-ie.js
+++ src/main/webapp/resources/javascript/lane-ie.js
@@ -2,7 +2,7 @@ YUI().use('yui2-event', 'node', function(Y){
     if (Y.UA.ie < 7) {
        //instead of :first-child selector remove background image:
        Y.YUI2.util.Event.onAvailable('stanfordMedicineBrand', function() {
-           new Y.Node(this).setStyle('background-image','none');
+           new Y.Node(this).setStyle('backgroundImage','none');
         });
         //set up hover class on somNav menu:
         Y.YUI2.util.Event.onContentReady('somNav', function() {
@@ -18,7 +18,7 @@ YUI().use('yui2-event', 'node', function(Y){
         });
         //instead of :first-child selector remove left border on .nav2 menus:
         Y.YUI2.util.Event.onDOMReady(function() {
-            var i, uls = Y.all('.nav2');
+            var i, uls = Y.all('.nav2, #libraryContact');
             for (i = 0; i < uls.size(); i++) {
                 uls.item(i).getElementsByTagName('LI').item(0).setStyle('border','none');
             }
