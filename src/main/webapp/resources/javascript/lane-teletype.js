821f7ec29152fd48d226cb2a529e341ab95c88b4
Charles Yates <ceyates@stanford.edu>
2010-04-22 23:52:28 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69752 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-teletype.js
+++ src/main/webapp/resources/javascript/lane-teletype.js
@@ -1,10 +1,10 @@
 // teletype functionality for PubMed/UTD through Lane pages
 // typing in #searchTeletyper .teletypeInput sends keystrokes to #search #searchTerms
-YUI().use('yui2-event','yui2-dom', function(Y) {
+YUI().use('yui2-event','node','event', function(Y) {
 Y.YUI2.util.Event.onContentReady('searchTeletyper', function(){
-    var targetElm = document.getElementById('searchTerms'),
-    inputElms = Y.YUI2.util.Dom.getElementsByClassName('teletypeInput',null,this),
-    searchForm = document.getElementById('search'),
+    var targetElm = Y.one('#searchTerms'),
+    inputElms = Y.all('.teletypeInput'),
+    searchForm = Y.one('#search'),
     YE = Y.YUI2.util.Event,
     i,
     teletype = function(inputElm){
@@ -12,14 +12,14 @@ Y.YUI2.util.Event.onContentReady('searchTeletyper', function(){
         if (!targetElm){
             return false;
         }
-        if (inputElm.value != inputElm.title) {
-            qString = inputElm.value;
+        if (inputElm.get('value') != inputElm.get('title')) {
+            qString = inputElm.get('value');
         }
         if (!qString){
-            qString = targetElm.title;
+            qString = targetElm.get('title');
         }
-        targetElm.value = qString;
-    }
+        targetElm.set('value', qString);
+    };
 
     // send #searchTeletyper submit events to #search
     YE.addListener(this, 'submit', function(e){
@@ -27,20 +27,20 @@ Y.YUI2.util.Event.onContentReady('searchTeletyper', function(){
         YE.preventDefault(e);
     });
     
-    for (i = 0; i < inputElms.length; i++){
+    for (i = 0; i < inputElms.size(); i++){
         // clear #teletypeInput input if it matches title (help text) value
-        YE.addListener(inputElms[i], 'focus', function(){
-            if (this.value == this.title){
-                this.value = '';
+        inputElms.item(i).on('focus', function(){
+            if (this.get('value') == this.get('title')){
+                this.set('value', '');
             }
         });
-        YE.addListener(inputElms[i], 'keyup', function(){
+        inputElms.item(i).on( 'keyup', function(){
             teletype(this);
         });
         // if #teletypeInput input value is blank, set to title (help text)
-        YE.addListener(inputElms[i], 'blur', function(){
-            if (this.value === ''){
-                this.value = this.title;
+        inputElms.item(i).on( 'blur', function(){
+            if (this.get('value') === ''){
+                this.set('value', this.get('title'));
             }
             teletype(this);
         });
