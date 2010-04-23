1ccc43e0e01ab4d384f161f573d0f4aa9466c409
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-04-23 16:37:07 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69783 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-textinputs.js
+++ src/main/webapp/resources/javascript/lane-textinputs.js
@@ -4,31 +4,33 @@
  * 	applies hintStyle only to default help text
  *  clears default help text on focus and adds back on blur
  */
-YUI().use('yui2-event','node',function(Y) {
-    var Event = Y.YUI2.util.Event; //shorthand for Event
-    Event.onDOMReady(function() {
+YUI().use('event-base','node-base',function(Y) {
+    Y.on("domready", function() {
     	var textInputs, i,
     	hintStyle = 'inputHint';
+    	
         textInputs = new Y.all('input[type="text"]');
-	    for (i = 0; i < textInputs.size(); i++){
-	    	// default style
-    	    if (textInputs.item(i).get('value') == textInputs.item(i).get('title')){
-    	    	textInputs.item(i).addClass(hintStyle);
+        
+        // default to hintStyle when value and title are same
+        for (i = 0; i < textInputs.size(); i++){
+        	if (textInputs.item(i).get('value') == textInputs.item(i).get('title')){
+        		textInputs.item(i).addClass(hintStyle);
+        	}
+        }
+        // if input value is blank, set to title (help text)
+        textInputs.on('blur', function(e){
+    	    if (e.currentTarget.get('value') === ''){
+    	    	e.currentTarget.set('value',e.currentTarget.get('title'));
+    	    	e.currentTarget.addClass(hintStyle);
     	    }
-	    	// clear input if it matches title (help text) value
-			Event.addListener(Y.Node.getDOMNode(textInputs.item(i)), 'focus', function(){
-	    	    if (this.value == this.title){
-	    	        this.value = '';
-	    	        Y.one(this).removeClass(hintStyle);
-	    	    }
-	    	});
-	    	// if input value is blank, set to title (help text)
-	    	Event.addListener(Y.Node.getDOMNode(textInputs.item(i)), 'blur', function(){
-	    	    if (this.value === ''){
-	    	        this.value = this.title;
-	    	        Y.one(this).addClass(hintStyle);
-	    	    }
-	    	});
-	    }
+    	});
+    	// clear input if it matches title (help text) value
+        textInputs.on('focus', function(e){
+	    	if (e.currentTarget.get('value') == e.currentTarget.get('title')){
+	    		e.currentTarget.set('value','');
+	    		e.currentTarget.removeClass(hintStyle);
+	    	}
+    	});
+    	
     });
 });
