88ad98afc3d9c8742d6652c971a7507530a85219
Charles Yates <ceyates@stanford.edu>
2010-04-23 23:46:51 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69897 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-search-history.js
+++ src/main/webapp/resources/javascript/lane-search-history.js
@@ -1,33 +1,34 @@
-YUI().use('node','yui2-event','yui2-history', function(Y) {
-    var searchString = LANE.search.getEncodedSearchString(),
-        YE = Y.YUI2.util.Event,
-        YH = Y.YUI2.util.History,
-        facetChangeHandler = function(facetId){
-            var result = Y.one('#'+facetId+'Facet').getData('result');// result facet to make active
-            //TODO: this reads private attributes from Result ... change?
-            if (result !== undefined){
-                if (result._state == 'initialized') {
-                    result.show();
-                } else if (result._state == 'searched') {
-                    LANE.search.facets.getCurrentResult().hide();
-                    LANE.search.facets.setCurrentResult(r);
-                    result.show();
-                } else {
-                    // TODO: necessary?
-                    //result._state = 'initialized';
+YUI().use('node', 'event-custom', 'yui2-history', function(Y) {
+    Y.Global.on('lane:searchready', function() {
+        var searchString = LANE.search.getEncodedSearchString(),
+            YH = Y.YUI2.util.History,
+            facetChangeHandler = function(facetId) {
+                var result = Y.one('#' + facetId + 'Facet').getData('result');// result facet to make active
+                //TODO: this reads private attributes from Result ... change?
+                if (result !== undefined) {
+                    if (result._state == 'initialized') {
+                        result.show();
+                    } else if (result._state == 'searched') {
+                        LANE.search.facets.getCurrentResult().hide();
+                        LANE.search.facets.setCurrentResult(r);
+                        result.show();
+                    } else {
+                        // TODO: necessary?
+                        //result._state = 'initialized';
+                    }
                 }
-            }
-        },
-        initializeHistory = function(){
-            var initialFacet = YH.getBookmarkedState("facet") || YH.getQueryStringParameter("source");
-            YH.onReady(function() {
-                facetChangeHandler(YH.getCurrentState("facet"));
-            });
-            YH.register("facet", initialFacet, facetChangeHandler);
-            YH.initialize("yui-history-field", "yui-history-iframe");
-        };
-
-    if ( searchString && Y.one('#yui-history-field') && Y.one('#yui-history-iframe') ) {
-        YE.onDOMReady(initializeHistory);
-    }
+            },
+            initializeHistory = function() {
+                var initialFacet = YH.getBookmarkedState("facet") || YH.getQueryStringParameter("source");
+                YH.onReady(function() {
+                    facetChangeHandler(YH.getCurrentState("facet"));
+                });
+                YH.register("facet", initialFacet, facetChangeHandler);
+                YH.initialize("yui-history-field", "yui-history-iframe");
+            };
+        //TODO: create these rather than rely on presence in markup
+        if (searchString && Y.one('#yui-history-field') && Y.one('#yui-history-iframe')) {
+            initializeHistory();
+        }
+    });
 });
