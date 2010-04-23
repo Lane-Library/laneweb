aecdaf96c156f154ce96cb2c7b52823e4b70bee5
Charles Yates <ceyates@stanford.edu>
2010-04-23 21:00:30 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69822 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-teletype.js
+++ src/main/webapp/resources/javascript/lane-teletype.js
@@ -1,31 +1,29 @@
 // teletype functionality for PubMed/UTD through Lane pages
 // typing in #searchTeletyper .teletypeInput sends keystrokes to #search #searchTerms
-YUI().use('yui2-event','node','event', function(Y) {
-Y.YUI2.util.Event.onContentReady('searchTeletyper', function(){
+YUI().use('node', function(Y) {
     var targetElm = Y.one('#searchTerms'),
-    inputElms = Y.all('.teletypeInput'),
-    searchForm = Y.one('#search'),
-    YE = Y.YUI2.util.Event,
-    i,
-    teletype = function(inputElm){
-        var qString;
-        if (!targetElm){
-            return false;
-        }
-        if (inputElm.get('value') != inputElm.get('title')) {
-            qString = inputElm.get('value');
-        }
-        if (!qString){
-            qString = targetElm.get('title');
-        }
-        targetElm.set('value', qString);
-    };
+        inputElms = Y.all('.teletypeInput'),
+        searchForm = Y.one('#search'),
+        searchTeletyper = Y.one('#searchTeletyper'),
+        i,
+        teletype = function(inputElm){
+            var qString;
+            if (!targetElm){
+                return false;
+            }
+            if (inputElm.get('value') != inputElm.get('title')) {
+                qString = inputElm.get('value');
+            }
+            if (!qString){
+                qString = targetElm.get('title');
+            }
+            targetElm.set('value', qString);
+        };
 
     // send #searchTeletyper submit events to #search
-    YE.addListener(this, 'submit', function(e){
-        searchForm.submit();
-        YE.preventDefault(e);
-    });
+    if (searchTeletyper) {
+        Y.on('submit', searchForm.submit, searchTeletyper);
+    }
     
     for (i = 0; i < inputElms.size(); i++){
         // clear #teletypeInput input if it matches title (help text) value
@@ -45,5 +43,4 @@ Y.YUI2.util.Event.onContentReady('searchTeletyper', function(){
             teletype(this);
         });
     }
-});
 });
