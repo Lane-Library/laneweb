b6a5c21d719e498650bd7c26c31288dbfaa61a14
Charles Yates <ceyates@stanford.edu>
2010-04-23 21:11:38 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69823 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-spellcheck.js
+++ src/main/webapp/resources/javascript/lane-spellcheck.js
@@ -1,11 +1,8 @@
-//check if there is a query
-YUI().use('yui2-event','yui2-connection','yui2-json', function(Y) {
-if (LANE.search.getEncodedSearchString()) {
-    //wait until id=spellcheck available
-    Y.YUI2.util.Event.onAvailable('spellCheck',function() {
+YUI().use('node', 'yui2-connection', 'yui2-json', function(Y) {
+    if (Y.one('#spexllCheck') && LANE.search.getEncodedSearchString()) {
         //get the suggestion
         Y.YUI2.util.Connect.asyncRequest('GET', '/././apps/spellcheck/json?q=' + LANE.search.getEncodedSearchString(), {
-            success: function(o){
+            success: function(o) {
                 var sc = Y.YUI2.lang.JSON.parse(o.responseText), s, a;
                 if (sc.suggestion) {
                     //if there is a suggestion show the spellcheck markup 
@@ -14,14 +11,11 @@ if (LANE.search.getEncodedSearchString()) {
                     //s.style.display = 'inline';
                     LANE.search.popin.fire(s);
                     a = s.getElementsByTagName('a')[0];
-                    a.href = document.location.href.replace('q='+LANE.search.getEncodedSearchString(),'q='+encodeURIComponent(sc.suggestion));
+                    a.href = document.location.href.replace('q=' + LANE.search.getEncodedSearchString(), 'q=' + encodeURIComponent(sc.suggestion));
                     a.innerHTML = sc.suggestion;
                 }
-            //failure:don't do anything
             }
         });
-    });
-}
+    }
 });
-
 
