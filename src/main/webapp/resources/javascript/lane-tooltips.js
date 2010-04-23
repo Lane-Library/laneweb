0d141b91a186fb956e49310b8310670151af215d
Charles Yates <ceyates@stanford.edu>
2010-04-23 00:34:15 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69759 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-tooltips.js
+++ src/main/webapp/resources/javascript/lane-tooltips.js
@@ -1,4 +1,4 @@
-YUI().use('yui2-dom','yui2-container',function(Y) {
+YUI().use('node','yui2-container',function(Y) {
     var tooltips = [],
         destroyTooltips = function() {
             while (tooltips.length > 0) {
@@ -6,42 +6,41 @@ YUI().use('yui2-dom','yui2-container',function(Y) {
             }
         },
         createTooltips = function() {
-            var tooltipContainerArray, // array of tooltip containers
+            var tooltipContainerNodeList, // array of tooltip containers
                 tooltipContainer, // tooltips
                 tooltipId, // id of te element
                 tooltipWidth, // width of tooltip
                 simpleTooltipIds = [], // array of contexts that need tt made
                                        // from their title attribute
                 tooltipElement,
-                YUD = Y.YUI2.util.Dom,
                 YUW = Y.YUI2.widget,
                 i, j;
             
-            tooltipContainerArray = YUD.getElementsByClassName('tooltips');
+            tooltipContainerNodeList = Y.all('.tooltips');
             
-            for (i = 0; i < tooltipContainerArray.length; i++) {
-                tooltipContainer = tooltipContainerArray[i].childNodes;
-                for (j = 0; j < tooltipContainer.length; j++) {
-                    if (tooltipContainer[j].nodeType == 1) {
-                        tooltipId = tooltipContainer[j].id.replace(/Tooltip$/, '');
-                        if (tooltipId && YUD.inDocument(tooltipId)) {
-                            tooltipElement = document.getElementById(tooltipId);
-                            tooltipElement.trackable = true;
+            for (i = 0; i < tooltipContainerNodeList.size(); i++) {
+                tooltipContainer = tooltipContainerNodeList.item(i).get('childNodes');
+                for (j = 0; j < tooltipContainer.size(); j++) {
+                    if (tooltipContainer.item(j).get('nodeType') == 1) {
+                        tooltipId = tooltipContainer.item(j).get('id').replace(/Tooltip$/, '');
+                        tooltipElement = Y.one('#' + tooltipId);
+                        if (tooltipElement) {
+                            tooltipElement.setAttribute('trackable',true);
                             // tooltips where content is in the markup
                             // (abstracts, etc.)
-                            if (tooltipContainer[j].innerHTML) {
-                                if (tooltipContainer[j].style.width) {
-                                    tooltipWidth = tooltipContainer[j].style.width;
-                                } else if (tooltipContainer[j].innerHTML.length > 500) {
+                            if (tooltipContainer.item(j).get('innerHTML')) {
+                                if (tooltipContainer.item(j).getStyle('width') != 'auto') {
+                                    tooltipWidth = tooltipContainer.item(j).getStyle('width');
+                                } else if (tooltipContainer.item(j).get('innerHTML').length > 500) {
                                     tooltipWidth = '60%';
                                 } else {
                                     tooltipWidth = '25%';
                                 }
                                 tooltips.push(new YUW.Tooltip(tooltipId + "-yuitt", {
-                                    context: tooltipElement,
+                                    context: Y.Node.getDOMNode(tooltipElement),
                                     width: tooltipWidth,
                                     autodismissdelay: 60000,
-                                    text: tooltipContainer[j].innerHTML
+                                    text: tooltipContainer.item(j).get('innerHTML')
                                 }));
                             } else {
                                 // just use title attribute for all other
