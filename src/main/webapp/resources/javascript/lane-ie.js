47ff58f00c105d32bc8a9051461549ddf24294f2
Charles Yates <ceyates@stanford.edu>
2010-04-23 19:48:26 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69811 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-ie.js
+++ src/main/webapp/resources/javascript/lane-ie.js
@@ -1,27 +1,27 @@
-YUI().use('yui2-event', 'node', function(Y){
-    if (Y.UA.ie < 7) {
-       //instead of :first-child selector remove background image:
-       Y.YUI2.util.Event.onAvailable('stanfordMedicineBrand', function() {
-           new Y.Node(this).setStyle('backgroundImage','none');
-        });
+YUI().use('node', function(Y) {
+    if (Y.UA.ie && Y.UA.ie < 7) {
+        //instead of :first-child selector remove background image:
+        var node = Y.one('#stanfordMedicineBrand'), i, nodes;
+        if (node) {
+            node.setStyle('backgroundImage', 'none');
+        }
         //set up hover class on somNav menu:
-        Y.YUI2.util.Event.onContentReady('somNav', function() {
-            var i, uls = new Y.Node(this).getElementsByTagName('UL');
-            for (i = 0; i < uls.size(); i++) {
-                Y.Node.getDOMNode(uls.item(i).get('parentNode')).activate = function() {
-                    new Y.Node(this).addClass('hover');
-                };
-                Y.Node.getDOMNode(uls.item(i).get('parentNode')).deactivate = function() {
-                    new Y.Node(this).removeClass('hover');
-                };
+        node = Y.one('#somNav');
+        if (node) {
+            nodes = node.all('ul');
+            for (i = 0; i < nodes.size(); i++) {
+                nodes.item(i).get('parentNode').on('mouseover', function() {
+                    this.addClass('hover');
+                });
+                nodes.item(i).get('parentNode').on('mouseout', function() {
+                    this.removeClass('hover');
+                });
             }
-        });
+        }
         //instead of :first-child selector remove left border on .nav2 menus:
-        Y.YUI2.util.Event.onDOMReady(function() {
-            var i, uls = Y.all('.nav2, #libraryContact');
-            for (i = 0; i < uls.size(); i++) {
-                uls.item(i).getElementsByTagName('LI').item(0).setStyle('border','none');
-            }
-        });
+        nodes = Y.all('.nav2, #libraryContact');
+        for (i = 0; i < nodes.size(); i++) {
+            nodes.item(i).one('li').setStyle('backgroundColor', 'blue');
+        }
     }
 });
