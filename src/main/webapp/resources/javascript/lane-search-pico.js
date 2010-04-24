6a89940633cac58d90c7a89c36bc39528c0d64a5
Charles Yates <ceyates@stanford.edu>
2010-04-24 00:00:33 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69899 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-search-pico.js
+++ src/main/webapp/resources/javascript/lane-search-pico.js
@@ -1,98 +1,97 @@
-YUI().use('node','yui2-event','yui2-container','yui2-history', function(Y) {
+YUI().use('node','event','yui2-container','yui2-history', function(Y) {
     // pico form functionality
     //  - remove default text values onfocus
     //  - adds auto complete mesh listener on p i c inputs
     //  - ties editing of pico to searchTermsInput for query builder effect
     var i,y,
-    d = document, 
-    picoForm,             //container of all pico input fields
-    searchTermsInput,     //input for built query terms
-    inputs,               //input elements
-    acInputs,             //input elements requiring auto complete
-     E = Y.YUI2.util.Event, 
-     W = Y.YUI2.widget,
-     H = Y.YUI2.util.History,
-     queryBuilder = function(targetId){ //build query terms from pico inputs
-        var qString = '', i;
-        if(targetId == searchTermsInput.id) {
-            return 0;
-        }
-        for (i = 0; i < inputs.length; i++) {
-            if (inputs[i].id != searchTermsInput.id && inputs[i].value !== '' && inputs[i].value != inputs[i].title) {
-                qString += '(' + inputs[i].value + ')';
+        picoForm,             //container of all pico input fields
+        searchTermsInput,     //input for built query terms
+        inputs,               //input elements
+        acInputs,             //input elements requiring auto complete
+         W = Y.YUI2.widget,
+         H = Y.YUI2.util.History,
+         queryBuilder = function(targetId){ //build query terms from pico inputs
+            var qString = '', i;
+            if(targetId == searchTermsInput.id) {
+                return 0;
             }
-        }
-        if ( qString.length ){
-            qString = qString.replace(/\)\(/g, ") AND (");
-            if (qString.indexOf('(') === 0 && qString.indexOf(')') == qString.length - 1) {
-                qString = qString.replace(/(\(|\))/g, '');
+            for (i = 0; i < inputs.length; i++) {
+                if (inputs[i].id != searchTermsInput.id && inputs[i].value !== '' && inputs[i].value != inputs[i].title) {
+                    qString += '(' + inputs[i].value + ')';
+                }
             }
-            searchTermsInput.value = qString;
-        }
-    },
-    // when a suggest list item is selected ...
-    //  - track the suggest-selection-event
-    onItemSelect = function(sType, aArgs) {
-        var item, 
-            trackingObject = {};
-        item = aArgs[2];
-        trackingObject.title = aArgs[0].getInputEl().id + '--suggest-selection-event';
-        trackingObject.path = item[0];
-        LANE.tracking.track(trackingObject);
-    };
-    
-    E.onContentReady('clinicalP', function() {
+            if ( qString.length ){
+                qString = qString.replace(/\)\(/g, ") AND (");
+                if (qString.indexOf('(') === 0 && qString.indexOf(')') == qString.length - 1) {
+                    qString = qString.replace(/(\(|\))/g, '');
+                }
+                searchTermsInput.value = qString;
+            }
+        },
+        // when a suggest list item is selected ...
+        //  - track the suggest-selection-event
+        onItemSelect = function(sType, aArgs) {
+            var item, 
+                trackingObject = {};
+            item = aArgs[2];
+            trackingObject.title = aArgs[0].getInputEl().id + '--suggest-selection-event';
+            trackingObject.path = item[0];
+            LANE.tracking.track(trackingObject);
+        },
+        picofield = Y.one('#clinicalP');
+    if (picofield) {
         // change text of default input values
         // add event listeners to p,i,c,o inputs for building search terms
-        picoForm = new Y.Node(this).ancestor('form');//D.getAncestorByTagName(this,'form');
+        picoForm = picofield.ancestor('form');//D.getAncestorByTagName(this,'form');
         inputs = picoForm.all('.picoInput');//D.getElementsByClassName('picoInput',null,picoForm);
         searchTermsInput = Y.one('#searchTerms');//document.getElementById('searchTerms');
-        for (i = 0; i < inputs.size(); i++){
+        for (i = 0; i < inputs.size(); i++) {
             //TODO: extracting pico values from request ... move to laneweb.xsl?
-            if (H.getQueryStringParameter(inputs.item(i).get('name'))){
-                inputs.item(i).set('value', H.getQueryStringParameter(inputs.item(i).get('name')).replace(/\+/g,' '));
+            if (H.getQueryStringParameter(inputs.item(i).get('name'))) {
+                inputs.item(i).set('value', H.getQueryStringParameter(inputs.item(i).get('name')).replace(/\+/g, ' '));
             }
-            if(!inputs.item(i).get('value')||inputs.item(i).get('value') == 'null'){
+            if (!inputs.item(i).get('value') || inputs.item(i).get('value') == 'null') {
                 inputs.item(i).set('value', inputs.item(i).get('title'));
             }
-            E.addListener(Y.Node.getDOMNode(inputs.item(i)), 'focus', function(){
-                if (this.value == this.title){
-                    this.value = '';
+            Y.on('focus', function() {
+                if (this.get('value') == this.title) {
+                    this.set('value', '');
                 }
-            });
-            E.addListener(Y.Node.getDOMNode(inputs.item(i)), 'blur', function(){
-                if (this.value === ''){
-                    this.value = this.title;
+            }, intputs.item(i));
+            Y.on('blur', function() {
+                if (this.get('value') === '') {
+                    this.set('value', this.get('title'));
                 }
-                queryBuilder(this.id);
-            });
-            E.addListener(Y.Node.getDOMNode(inputs.item(i)), 'keyup', function(){
-                queryBuilder(this.id);
-            });
+                queryBuilder(this.get('id'));
+            }, inputs.item(i));
+            Y.on('keyup', function() {
+                queryBuilder(this.get('id'));
+            }, inputs.item(i));
         }
         
         // auto complete mesh on p, i, c inputs
         acInputs = picoForm.all('.acMesh');
-        if(acInputs.size()){
-            for (y = 0; y < acInputs.size(); y++){
-                var acCont,   // container element to hang the auto complete widget on
-                    acMesh,   // auto complete widget
-                    acDs;     // data source
-                acCont = new Y.Node(d.getElementsByName(acInputs.item(y).get('id').substring(0,1))[0]);
-                acDs = new W.DS_XHR("/././apps/suggest/json", ["suggest"]);
-                acDs.responseType = W.DS_XHR.TYPE_JSON;
-                acDs.scriptQueryParam = "q";
+        if (acInputs.size()) {
+            for (y = 0; y < acInputs.size(); y++) {
+                var acCont, // container element to hang the auto complete widget on
+                    acMesh, // auto complete widget
+                    acDs; // data source
+                    acCont = Y.one('#' + acInputs.item(y).get('id').substring(0, 1));
+                    acDs = new Y.YUI2.util.XHRDataSource("/././apps/suggest/json");
+                    acDs.responseType =Y.YUI2.util.XHRDataSource.TYPE_JSON;
+                    acDs.responseSchema = {
+                        resultsList: 'suggest'
+                    };
+                    acDs.scriptQueryParam = "q";
                 // limit added to patient, intervention, comparison
-                if(acCont.get('name') == 'p'){
+                if (acCont.get('name') == 'p') {
                     acDs.scriptQueryAppend = 'l=mesh-d';
-                }
-                else if(acCont.get('name') == 'i'){
+                } else if (acCont.get('name') == 'i') {
                     acDs.scriptQueryAppend = 'l=mesh-i';
-                }
-                else if(acCont.get('name') == 'c'){
+                } else if (acCont.get('name') == 'c') {
                     acDs.scriptQueryAppend = 'l=mesh-di';
                 }
-                acDs.connTimeout = 3000; 
+                acDs.connTimeout = 3000;
                 acDs.maxCacheEntries = 100;
                 acMesh = new W.AutoComplete(acCont, Y.Node.getDOMNode(acInputs.item(y)), acDs);
                 acMesh.minQueryLength = 3;
@@ -104,11 +103,13 @@ YUI().use('node','yui2-event','yui2-container','yui2-history', function(Y) {
                 acMesh.animVert = false;
                 acMesh.queryDelay = 0.1;
                 acMesh.setHeader(acInputs.item(y).get('title'));
-                acMesh.itemSelectEvent.subscribe(function(sType,aArgs){queryBuilder();});
+                acMesh.itemSelectEvent.subscribe(function(sType, aArgs) {
+                    queryBuilder();
+                });
                 acMesh.itemSelectEvent.subscribe(onItemSelect);
             }
         }
-    });
+    }
 });
 /*
 
