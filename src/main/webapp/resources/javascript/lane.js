6202d63e3685a1c458d27ad0b50fa05c82172a4e
Charles Yates <ceyates@stanford.edu>
2010-04-24 00:12:05 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69900 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane.js
+++ src/main/webapp/resources/javascript/lane.js
@@ -1,7 +1,6 @@
 /**
  * @author ceyates
  */
-//TODO: including all dependencies here so they are loaded only once, will explicity specify in template when finalized
 YUI().use('yui2-event','event-custom',function(Y) {
 if (typeof LANE == "undefined" || !LANE) {
     /**
@@ -28,86 +27,44 @@ LANE.namespace = function(){
     return o;
 };
 LANE.core = LANE.core || function() {
-    var E = Y.YUI2.util.Event, //shorthand
-        changeEvent =  new Y.YUI2.util.CustomEvent('change'),
-        m = {}; //the meta element name/values
-    // initialize on load
-    E.onDOMReady(function() {
-        var d = document,
+    var changeEvent =  new Y.YUI2.util.CustomEvent('change'),
+        m = {}, //the meta element name/values
             meta, //the meta elements
 //            p, //anchors for finding popup links
 //            a, //popup anchor
             i;
-        meta = d.getElementsByTagName('meta');
-        for (i = 0; i < meta.length; i++) {
-            m[meta[i].getAttribute('name')] = meta[i].getAttribute('content');
+        meta = Y.all('meta');
+        for (i = 0; i < meta.size(); i++) {
+            m[meta.item(i).get('name')] = meta.item(i).get('content');
         }
-        //calls 'activate' function on target
-        E.addListener(d, 'mouseover', function(e) {
-            var t = e.srcElement || e.target;
-            while (t) {
-                if (t.activate) {
-                    t.activate(e);
-                }
-                t = t.parentNode;
-            }
-        });
-        //calls 'deactivate' function on target
-        E.addListener(d, 'mouseout', function(e) {
-            var t = e.srcElement || e.target;
-            while (t) {
-            if (t.deactivate) {
-                t.deactivate(e);
-            }
-                t = t.parentNode;
-            }
-        });
-//        E.addListener(d, 'submit',function(e) {
-//            var t = e.srcElement || e.target;
-//            if (t.submit !== undefined) {
-//                t.submit(e);
-//            }
-//            if (LANE.tracking) {
-//                LANE.tracking.trackEvent(e);
-//            }
-//        });
-        //calls 'clicked' function on target and any parent elements
-        E.addListener(d, 'click', function(e){
-            var t = e.srcElement || e.target, parent,
-                f;
-            while (t) {
-                if (t.clicked) {
-                    t.clicked(e);
-                }
-                t = t.parentNode;
-            }
+        Y.on('click', function(e){
             if (LANE.tracking) {
                 LANE.tracking.trackEvent(e);
             //put in a delay for safari to make the tracking request:
-            if (Y.YUI2.env.ua.webkit && LANE.tracking.isTrackable(e)) {
-                    t = e.target;
-                    parent = t;
-                    while (parent) {
-                        if (parent.clicked !== undefined) {
-                            return;
-                        }
-                        parent = parent.parentNode;
-                    }
-                    while (t) {
-                        if (t.href && (!t.rel && !t.target)) {
-                            f = function() {
-                                window.location = t.href;
-                            };
-                            Y.YUI2.util.Event.preventDefault(e);
-                            setTimeout(f, 200);
-                            break;
-                        }
-                        t = t.parentNode;
-                    }
+            //TODO: revisit this and make sure it actually is useful
+//            if (Y.UA.webkit && LANE.tracking.isTrackable(e)) {
+//                    t = e.target;
+//                    parent = t;
+//                    while (parent) {
+//                        if (parent.clicked !== undefined) {
+//                            return;
+//                        }
+//                        parent = parent.parentNode;
+//                    }
+//                    while (t) {
+//                        if (t.href && (!t.rel && !t.target)) {
+//                            f = function() {
+//                                window.location = t.href;
+//                            };
+//                            Y.YUI2.util.Event.preventDefault(e);
+//                            setTimeout(f, 200);
+//                            break;
+//                        }
+//                        t = t.parentNode;
+//                    }
+//            }
             }
-            }
-        });
-    });
+        }, document);
     return {
         getChangeEvent: function() {
             return changeEvent;
@@ -124,31 +81,6 @@ LANE.core = LANE.core || function() {
                 msg = escape(msg);
             }
             Y.YUI2.util.Connect.asyncRequest('GET','/././javascriptLogger?' + msg);
-        },
-        // document.importNode not supported in IE
-        importNode: function(importedNode, deep) {
-            var newNode, i, attr;
-            if (importedNode.nodeType == 1) { // Node.ELEMENT_NODE
-                newNode = document.createElement(importedNode.nodeName);
-                for (i = 0; i < importedNode.attributes.length; i++) {
-                    attr = importedNode.attributes[i];
-                    if (attr.nodeValue !== undefined && attr.nodeValue !== '') {
-                        newNode.setAttribute(attr.name, attr.nodeValue);
-                        if (attr.name == 'class') {
-                            newNode.className = attr.nodeValue;
-                        }
-                    }
-                }
-            } else 
-                if (importedNode.nodeType == 3) { // Node.TEXT_NODE
-                    newNode = document.createTextNode(importedNode.nodeValue);
-                }
-            if (deep && importedNode.hasChildNodes()) {
-                for (i = 0; i < importedNode.childNodes.length; i++) {
-                    newNode.appendChild(LANE.core.importNode(importedNode.childNodes[i], true));
-                }
-            }
-            return newNode;
         }
     };
 }();
