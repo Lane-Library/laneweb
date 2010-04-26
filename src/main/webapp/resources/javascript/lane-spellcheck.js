7ba9ee1d6a6e6b7c3107ba00a0b97683b0af502a
Charles Yates <ceyates@stanford.edu>
2010-04-26 23:32:12 +0000

    on('lane:ready')
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@69988 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-spellcheck.js
+++ src/main/webapp/resources/javascript/lane-spellcheck.js
@@ -1,21 +1,23 @@
-YUI().use('node', 'yui2-connection', 'json-parse', function(Y) {
-    if (Y.one('#spellCheck') && LANE.search.getEncodedSearchString()) {
-        //get the suggestion
-        Y.YUI2.util.Connect.asyncRequest('GET', '/././apps/spellcheck/json?q=' + LANE.search.getEncodedSearchString(), {
-            success: function(o) {
-                var sc = Y.JSON.parse(o.responseText), s, a;
-                if (sc.suggestion) {
-                    //if there is a suggestion show the spellcheck markup 
-                    //and add the suggestion to the href
-                    s = document.getElementById('spellCheck');
-                    //s.style.display = 'inline';
-                    LANE.search.popin.fire(s);
-                    a = s.getElementsByTagName('a')[0];
-                    a.href = document.location.href.replace('q=' + LANE.search.getEncodedSearchString(), 'q=' + encodeURIComponent(sc.suggestion));
-                    a.innerHTML = sc.suggestion;
+YUI().use('node', 'event-custom', 'yui2-connection', 'json-parse', function(Y) {
+    Y.Global.on('lane:ready', function() {
+        if (Y.one('#spellCheck') && LANE.search.getEncodedSearchString()) {
+            //get the suggestion
+            Y.YUI2.util.Connect.asyncRequest('GET', '/././apps/spellcheck/json?q=' + LANE.search.getEncodedSearchString(), {
+                success: function(o) {
+                    var sc = Y.JSON.parse(o.responseText), s, a;
+                    if (sc.suggestion) {
+                        //if there is a suggestion show the spellcheck markup 
+                        //and add the suggestion to the href
+                        s = document.getElementById('spellCheck');
+                        //s.style.display = 'inline';
+                        LANE.search.popin.fire(s);
+                        a = s.getElementsByTagName('a')[0];
+                        a.href = document.location.href.replace('q=' + LANE.search.getEncodedSearchString(), 'q=' + encodeURIComponent(sc.suggestion));
+                        a.innerHTML = sc.suggestion;
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+    });
 });
 
