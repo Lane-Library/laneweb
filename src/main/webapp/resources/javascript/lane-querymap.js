1ca19da8e141216c8564df5392e60383a7896f2a
Charles Yates <ceyates@stanford.edu>
2010-04-27 17:31:32 +0000

    yui2-connect -> io
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70033 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-querymap.js
+++ src/main/webapp/resources/javascript/lane-querymap.js
@@ -1,14 +1,15 @@
 //TODO: this needs some cleaning up
 //check if there is a query
-YUI().use('node', 'event-custom', 'yui2-connection', 'json-parse', function(Y) {
+YUI().use('node', 'event-custom', 'io-base', 'json-parse', function(Y) {
     Y.Global.on('lane:searchready', function() {
         if (LANE.search.getEncodedSearchString()) {
             //check if there is id=queryMapping
             var queryMapping = Y.one('#queryMapping');
             if (queryMapping) {
-                Y.YUI2.util.Connect.asyncRequest('GET', '/././apps/querymap/json?q=' + LANE.search.getEncodedSearchString(), {
-                    success: function(o) {
-                        var anchor, span, img, i, queryMapContainer = document.getElementById('queryMapping');
+                Y.io('/././apps/querymap/json?q=' + LANE.search.getEncodedSearchString(), {
+                    on:{
+                    success: function(id, o) {
+                        var anchor, span, img, i;
                         LANE.search.querymap = Y.JSON.parse(o.responseText);
                         if (LANE.search.querymap.resourceMap) {
                             LANE.search.querymap.getResultCounts = function() {
@@ -19,50 +20,55 @@ YUI().use('node', 'event-custom', 'yui2-connection', 'json-parse', function(Y) {
                                     }
                                 }
                                 url += '&rd=' + Math.random();
-                                Y.YUI2.util.Connect.asyncRequest('GET', url, {
-                                    success: function(o) {
-                                        var results = Y.JSON.parse(o.responseText), rs = LANE.search.querymap.resourceMap.resources, i, needMore = false, result;
-                                        for (i = 0; i < rs.length; i++) {
-                                            if (!rs[i].status) {
-                                                result = results.resources[rs[i].id];
-                                                if (!result.status) {
-                                                    needMore = true;
+                                Y.io(url, {
+                                    on:{
+                                        success: function(id, o) {
+                                                var results = Y.JSON.parse(o.responseText),
+                                                    rs = LANE.search.querymap.resourceMap.resources,
+                                                    i, needMore = false, result;
+                                                for (i = 0; i < rs.length; i++) {
+                                                    if (!rs[i].status) {
+                                                        result = results.resources[rs[i].id];
+                                                        if (!result.status) {
+                                                            needMore = true;
+                                                        }
+                                                        rs[i].status = result.status;
+                                                        if (result.status == 'successful') {
+                                                            rs[i].anchor.parentNode.appendChild(document.createTextNode(': ' + result.hits + ' '));
+                                                        }
+                                                        rs[i].anchor.href = result.url;
+                                                    }
                                                 }
-                                                rs[i].status = result.status;
-                                                if (result.status == 'successful') {
-                                                    rs[i].anchor.parentNode.appendChild(document.createTextNode(': ' + result.hits + ' '));
+                                                if (needMore) {
+                                                    setTimeout(LANE.search.querymap.getResultCounts, 2000);
                                                 }
-                                                rs[i].anchor.href = result.url;
+                                                //queryMapping.style.display = 'inline';
+                                                Y.fire('lane:popin', queryMapping);
                                             }
                                         }
-                                        if (needMore) {
-                                            setTimeout(LANE.search.querymap.getResultCounts, 2000);
-                                        }
-                                        //queryMapContainer.style.display = 'inline';
-                                        Y.fire('lane:popin', queryMapContainer);
-                                    }
-                                });
-                            };
-                            for (i = 0; i < LANE.search.querymap.resourceMap.resources.length; i++) {
-                                LANE.search.querymap.resourceMap.resources[i].status = '';
-                                span = document.createElement('span');
-                                anchor = document.createElement('a');
-                                anchor.title = 'QueryMapping: ' + LANE.search.querymap.resourceMap.resources[i].label;
-                                span.appendChild(anchor);
-                                anchor.appendChild(document.createTextNode(LANE.search.querymap.resourceMap.resources[i].label));
-                                queryMapContainer.appendChild(span);
-                                LANE.search.querymap.resourceMap.resources[i].anchor = anchor;
+                                    });
+                                };
+                                for (i = 0; i < LANE.search.querymap.resourceMap.resources.length; i++) {
+                                    LANE.search.querymap.resourceMap.resources[i].status = '';
+                                    span = document.createElement('span');
+                                    anchor = document.createElement('a');
+                                    anchor.title = 'QueryMapping: ' + LANE.search.querymap.resourceMap.resources[i].label;
+                                    span.appendChild(anchor);
+                                    anchor.appendChild(document.createTextNode(LANE.search.querymap.resourceMap.resources[i].label));
+                                    queryMapping.appendChild(span);
+                                    LANE.search.querymap.resourceMap.resources[i].anchor = anchor;
+                                }
+                                if (document.getElementById('queryMappingDescriptor')) {
+                                    document.getElementById('queryMappingDescriptor').appendChild(document.createTextNode(LANE.search.querymap.resourceMap.descriptor));
+                                }
+                                // track mapped term and descriptor
+                                img = document.createElement('img');
+                                img.style.display = "none";
+                                img.src = "/././graphics/spacer.gif?log=QM&d=" + LANE.search.querymap.resourceMap.descriptor + "&k=" + LANE.search.getEncodedSearchString();
+                                queryMapping.appendChild(img);
+                                
+                                LANE.search.querymap.getResultCounts();
                             }
-                            if (document.getElementById('queryMappingDescriptor')) {
-                                document.getElementById('queryMappingDescriptor').appendChild(document.createTextNode(LANE.search.querymap.resourceMap.descriptor));
-                            }
-                            // track mapped term and descriptor
-                            img = document.createElement('img');
-                            img.style.display = "none";
-                            img.src = "/././graphics/spacer.gif?log=QM&d=" + LANE.search.querymap.resourceMap.descriptor + "&k=" + LANE.search.getEncodedSearchString();
-                            queryMapContainer.appendChild(img);
-                            
-                            LANE.search.querymap.getResultCounts();
                         }
                     }
                 });
