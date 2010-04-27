5126988e9dde3d7b668a69f17a6d7a28515b2f35
Charles Yates <ceyates@stanford.edu>
2010-04-27 17:46:23 +0000

    yui2-connect -> io
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70037 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-spellcheck.js
+++ src/main/webapp/resources/javascript/lane-spellcheck.js
@@ -1,19 +1,21 @@
-YUI().use('node', 'event-custom', 'yui2-connection', 'json-parse', function(Y) {
+YUI().use('node', 'event-custom', 'io-base', 'json-parse', function(Y) {
     Y.Global.on('lane:searchready', function() {
-        if (Y.one('#spellCheck') && LANE.search.getEncodedSearchString()) {
+        var spellCheck = Y.one('#spellCheck'),
+            searchTerms = LANE.search.getEncodedSearchString();
+        if (spellCheck && searchTerms) {
             //get the suggestion
-            Y.YUI2.util.Connect.asyncRequest('GET', '/././apps/spellcheck/json?q=' + LANE.search.getEncodedSearchString(), {
-                success: function(o) {
-                    var sc = Y.JSON.parse(o.responseText), s, a;
-                    if (sc.suggestion) {
-                        //if there is a suggestion show the spellcheck markup 
-                        //and add the suggestion to the href
-                        s = Y.one('#spellCheck');
-                        //s.style.display = 'inline';
-                        Y.fire('lane:popin', s);
-                        a = s.one('a');
-                        a.set('href', document.location.href.replace('q=' + LANE.search.getEncodedSearchString(), 'q=' + encodeURIComponent(sc.suggestion)));
-                        a.set('innerHTML', sc.suggestion);
+            Y.io('/././apps/spellcheck/json?q=' + searchTerms, {
+                on: {
+                    success:function(id, o) {
+                        var sc = Y.JSON.parse(o.responseText), a;
+                        if (sc.suggestion) {
+                            //if there is a suggestion show the spellcheck markup 
+                            //and add the suggestion to the href
+                            a = spellCheck.one('a');
+                            a.set('href', document.location.href.replace('q=' + searchTerms, 'q=' + encodeURIComponent(sc.suggestion)));
+                            a.set('innerHTML', sc.suggestion);
+                            Y.fire('lane:popin', spellCheck);
+                        }
                     }
                 }
             });
