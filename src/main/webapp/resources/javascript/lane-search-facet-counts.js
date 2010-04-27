3acda2c8d9ac2c71070fc81e4198242553b1f1de
Charles Yates <ceyates@stanford.edu>
2010-04-27 17:36:40 +0000

    yui2-connect -> io
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70034 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-search-facet-counts.js
+++ src/main/webapp/resources/javascript/lane-search-facet-counts.js
@@ -1,58 +1,58 @@
 //TODO: should this stop polling when all facets are complete? currently polls until search app done or timeout
-YUI().use('node','yui2-connection','json-parse',function(Y) {
+YUI().use('node','io-base','json-parse',function(Y) {
     
     Y.Global.on('lane:searchready', function() {
-    var searchString = LANE.search.getEncodedSearchString(),
-        facets = Y.all('.searchFacet'),
-        startTime = new Date().getTime(),
-        requestString, j,
-        updateHits = function(o) {
-            var response = Y.JSON.parse(o.responseText),
-                hitLink, hits, sleepingTime,
-                remainingTime, searchStatus, engineStatus, facetId;
-            
-            for (j = 0; j < facets.size(); j++) {
-            	hits = null;
-                hitLink = facets.item(j).one('a');
-                facetId = facets.item(j).getAttribute('facetId');
-                if (undefined != response.results.facets[facetId]) {
-                    hits = parseInt(response.results.facets[facetId].hits);
-                    engineStatus = response.results.facets[facetId].status;
+        var searchString = LANE.search.getEncodedSearchString(),
+            facets = Y.all('.searchFacet'),
+            startTime = new Date().getTime(),
+            requestString, j,
+            makeRequest = function() {
+                requestString = '';
+                for (j = 0; j < facets.size(); j++) {
+                    var id = facets.item(j).get('id');
+                    facets.item(j).setAttribute('facetId', id.substring(0, id.indexOf('Facet')));
+                    if(facets.item(j).hasClass('searchableFacet')){
+                    	requestString+=facets.item(j).getAttribute('facetId') + ',';
+                    }
                 }
-                if (!facetId.match("-all") && engineStatus == 'successful' && hitLink !== null && hits === 0) {
-                	hitLink.get('parentNode').addClass('inactiveFacet');
-                	hitLink.get('parentNode').removeClass('searchableFacet');
-                	hitLink.set('title','no search results for '+hitLink.get('innerHTML'));
+                if(requestString !== ''){
+                    Y.io('/././apps/search/facets/json?q=' + searchString + '&f=' + requestString + '&rd=' + Math.random(), {
+                        on: {
+                            success:function(id, o) {
+                                var response = Y.JSON.parse(o.responseText),
+                                    hitLink, hits, sleepingTime,
+                                    remainingTime, searchStatus, engineStatus, facetId;
+                                
+                                for (j = 0; j < facets.size(); j++) {
+                                    hits = null;
+                                    hitLink = facets.item(j).one('a');
+                                    facetId = facets.item(j).getAttribute('facetId');
+                                    if (undefined != response.results.facets[facetId]) {
+                                        hits = parseInt(response.results.facets[facetId].hits);
+                                        engineStatus = response.results.facets[facetId].status;
+                                    }
+                                    if (!facetId.match("-all") && engineStatus == 'successful' && hitLink !== null && hits === 0) {
+                                        hitLink.get('parentNode').addClass('inactiveFacet');
+                                        hitLink.get('parentNode').removeClass('searchableFacet');
+                                        hitLink.set('title','no search results for '+hitLink.get('innerHTML'));
+                                    }
+                                }
+                                sleepingTime = 2000;
+                                remainingTime = (new Date().getTime()) - startTime;
+                                searchStatus = response.results.status;
+                                if (searchStatus != 'successful' && (remainingTime <= 60 * 1000)) { // at more than 20 seconds the sleeping time becomes 10 seconds
+                                    if (remainingTime > 20 * 1000) {
+                                        sleepingTime = 10000;
+                                    }
+                                    setTimeout(makeRequest,sleepingTime);
+                                }
+                            }
+                        }
+                    });
                 }
-            }
-            sleepingTime = 2000;
-            remainingTime = (new Date().getTime()) - startTime;
-            searchStatus = response.results.status;
-            if (searchStatus != 'successful' && (remainingTime <= 60 * 1000)) { // at more than 20 seconds the sleeping time becomes 10 seconds
-                if (remainingTime > 20 * 1000) {
-                    sleepingTime = 10000;
-                }
-                setTimeout(makeRequest,sleepingTime);
-            }
-        },
-        responseHandler = {
-            success:updateHits
-        },
-        makeRequest = function() {
-            requestString = '';
-            for (j = 0; j < facets.size(); j++) {
-                var id = facets.item(j).get('id');
-                facets.item(j).setAttribute('facetId', id.substring(0, id.indexOf('Facet')));
-                if(facets.item(j).hasClass('searchableFacet')){
-                	requestString+=facets.item(j).getAttribute('facetId') + ',';
-                }
-            }
-            if(requestString !== ''){
-            	Y.YUI2.util.Connect.asyncRequest('GET', '/././apps/search/facets/json?q=' + searchString + '&f=' + requestString + '&rd=' + Math.random(), responseHandler);
-            }
-    };
-    if (searchString && facets.size() > 0) {
-        makeRequest();
-    }
+        };
+        if (searchString && facets.size() > 0) {
+            makeRequest();
+        }
     });
 });
