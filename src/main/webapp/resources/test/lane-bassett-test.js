6392a2324b96f24855d3032d64d56cbe058647f6
Alain Boussard <alainb@stanford.edu>
2010-04-28 18:43:55 +0000

    Test bassett javascript
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70100 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/test/lane-bassett-test.js
+++ src/main/webapp/resources/test/lane-bassett-test.js
@@ -8,13 +8,37 @@ YUI({
 }).use('node-event-simulate', 'console', 'test', function(Y){
 
     var bassettTestCase = new Y.Test.Case({
-        name: 'Lane Basset Test Case'
+        name: 'Lane Basset Test Case',
+        
+        testAccordeonLink: function() {
+    		Y.all('a').item(0).simulate('click');
+    },
+
+    testImageLink: function() {
+    	Y.one('#bassettContent').all('a').item(2).simulate('click');
+	},
+
+    
+    testDiagramLink: function() {
+    	Y.one('#bassettContent').all('a').item(1).simulate('click');
+	},
+
+
+    testPhotoLink: function() {
+    	Y.one('#bassettContent').all('a').item(0).simulate('click');
+	}
+	
+        	
     });
+
     
     Y.one('body').addClass('yui3-skin-sam');
     new Y.Console({
         newestOnTop: false
     }).render('#log');
+    
+    
+
     
     
     Y.Test.Runner.add(bassettTestCase);
