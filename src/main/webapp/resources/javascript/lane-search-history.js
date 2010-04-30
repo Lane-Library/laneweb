eb1efb5cdbd510473d7526d63a3059d19821f0e2
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-04-30 00:49:14 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70265 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-search-history.js
+++ src/main/webapp/resources/javascript/lane-search-history.js
@@ -1,5 +1,6 @@
 YUI().use('lane-search', 'node', 'event-custom', 'history', function(Y) {
 	var searchString = LANE.search.Result.getEncodedSearchTerms(),
+	facetNodes = Y.one('#searchFacets'),
 	initializeHistory = function() {
 		var initialFacet = Y.History.getBookmarkedState("facet") || Y.History.getQueryStringParameter("source");
 		Y.History.register("facet", initialFacet).on("history:moduleStateChange", LANE.search.facets.setActiveFacet);
@@ -9,7 +10,7 @@ YUI().use('lane-search', 'node', 'event-custom', 'history', function(Y) {
 		Y.History.initialize("#yui-history-field", "#yui-history-iframe");
 	};
 	
-	if (searchString) {
+	if (searchString && facetNodes) {
 		Y.one('body').insert(Y.Node.create('<iframe id="yui-history-iframe" src="/graphics/spacer.gif"></iframe>'),0);
 		Y.one('body').insert(Y.Node.create('<input id="yui-history-field" type="hidden"/>'),1);
 		Y.Global.on('lane:searchFacetsReady', initializeHistory);
