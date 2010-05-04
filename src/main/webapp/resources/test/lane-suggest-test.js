d7748cd953ea51181442f7b2d5396612c9d8ec0d
Charles Yates <ceyates@stanford.edu>
2010-05-04 22:39:38 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70552 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/test/lane-suggest-test.js
+++ src/main/webapp/resources/test/lane-suggest-test.js
@@ -8,11 +8,7 @@ YUI({
 }).use('node-event-simulate', 'console', 'test', function(Y){
 
     var suggestTestCase = new Y.Test.Case({
-        name: "Lane Suggest Testcase",
-        testAddACElements: function() {
-            var searchTermsElm = document.getElementById('searchTerms');
-            YAHOO.util.Assert.areEqual('acContainer yui-ac', searchTermsElm.parentNode.className);
-        }
+        name: "Lane Suggest Testcase"
     });
     
     Y.one('body').addClass('yui3-skin-sam');
