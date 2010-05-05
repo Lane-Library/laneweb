25628ac56b38aa9762a5961ab63ea8cdb60ed7f3
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-05-05 00:47:57 +0000

    suggest yui3
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70575 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-suggest.js
+++ src/main/webapp/resources/javascript/lane-suggest.js
@@ -1,4 +1,167 @@
-YUI().use('yui2-yahoo','yui2-dom','yui2-event','yui2-autocomplete','yui2-datasource','yui2-connection','node', function(Y){
+YUI().use('gallery-ac-plugin', 'plugin', 'node-base', 'datasource', function (Y) {
+
+    var suggestElms = Y.all('.laneSuggest'), i,
+        baseUrl = '/././apps/suggest/json?',
+        init = function (input) {
+    		var acDS, 
+    			acWidget;
+    		
+            input.plug(Y.Plugin.ACPlugin);
+            acDS = new Y.DataSource.IO({
+                source : baseUrl
+            });
+            acDS.plug({fn : Y.Plugin.DataSourceJSONSchema, cfg : {
+                schema : { resultListLocator : "suggest" }
+            }});
+            input.plug(Y.Plugin.ACPlugin, {
+                queryTemplate : function (q) { return "q=" + encodeURIComponent(
+                    q.replace(/(["\\])/g, '\\$1')
+                )},
+                dataSource : acDS
+            });
+            
+            acWidget = (function () {
+                var data = [],
+                    contNode = Y.Node.create('<ul class="aclist"> </ul>'),
+                    selected = -1;
+                input.insert(contNode,"after");
+                
+                contNode.delegate("click", function (e) {
+                    var target = e.currentTarget, index = target.get("className").split('-')[1];
+                    acWidget.focus(index).select(e);
+                }, "li");
+                contNode.delegate("mouseover", function (e) {
+                    var target = e.currentTarget, index = target.get("className").split('-')[1];
+                    acWidget.focus(index);
+                }, "li");
+                
+                return {
+                    setData : function (d) {
+                        if (d.length) {
+                            data = d;
+                            selected = -1;
+                        }
+                        return this;
+                    },
+                    render : function () {
+                        if (!data.length){
+                            return this;
+                        }
+                        this.visible = true;
+                        contNode.set("innerHTML", "");
+                        for (var i = 0, l = data.length; i < l; i ++) {
+                            contNode.appendChild(Y.Node.create('<li class="ac-'+i+'">'+data[i]+"</li>"));
+                        }
+                        contNode.setStyle("display", "block");
+                        return this;
+                    },
+                    hide : function () {
+                        contNode.setStyle("display", "none");
+                        this.visible = false;
+                        selected = -1;
+                        return this;
+                    },
+                    focus : function (i) {
+                        i = i || selected;
+                        if (!data.length) return this;
+                        if (i >= data.length) i = 0;
+                        if (i < 0) i = data.length - 1;
+                        
+                        var list = contNode.all("li"),
+                            current = contNode.one(".selected"),
+                            intent = list.item(i);
+                        if (!intent || intent === current) return this;
+                        selected = i;
+                        if (current) current.removeClass("selected");
+                        intent.addClass("selected");
+                        input.ac.set("queryValue", acWidget.getValue());
+                        return this;
+                    },
+                    next : function () {
+                        selected = selected || 0;
+                        selected ++;
+                        if (!this.visible) this.render();
+                        return this.focus(selected);
+                    },
+                    previous : function () {
+                        selected = selected || data.length;
+                        selected --;
+                        return this.focus(selected);
+                    },
+                    getValue : function () {
+                        if (!data.length || !data[selected]) return "";
+                        return data[selected];
+                    },
+                    select : function (e) {
+                        if(acWidget.getValue()){
+                        	input.ac.set("queryValue", acWidget.getValue());
+                        }
+                        else{
+                        	input.ac.set("queryValue", input.get("value"));
+                        }
+                        acWidget.hide();
+                        input.focus();
+                        e && e.preventDefault();
+                        LANE.search.Search.startSearch();
+                        //Y.Node.getDOMNode(input.ancestor("form")).submit();
+                    },
+                    setWidth : function (w) {
+                        if (w) {
+                             contNode.setStyle("width",w);
+                        }
+                    }
+                };
+            })();
+        	
+            input.on("focus", function (e) {
+            	var sourceElm = input.ancestor("form").one('#searchSource'), searchSource, minQueryLength = 3;
+                searchSource = (sourceElm) ? sourceElm.get('value') : null;
+                if (searchSource && searchSource.match(/^(all|articles|catalog)/)) {
+                    input.ac.get("dataSource").set("source",baseUrl+"l=er-mesh&");
+                } else if (searchSource && searchSource.match(/^bioresearch/)) {
+                    input.ac.get("dataSource").set("source",baseUrl+"l=mesh&");
+                } else if (searchSource && searchSource.match(/^history/)) {
+                	input.ac.get("dataSource").set("source",baseUrl+"l=history&");
+                } else if (null == searchSource) { // assume source-less is metasearch form on peds portal, etc.
+                	input.ac.get("dataSource").set("source",baseUrl+"l=mesh-di&");
+                } else {
+                	minQueryLength = 999;
+                }
+                input.ac.set("minQueryLength", minQueryLength);
+            });
+            input.on("blur", function (e) {
+            	acWidget.hide();
+            });
+            input.ac.on("ac:load", function (e) {
+                acWidget.setWidth(input.getStyle("width"));
+                acWidget.setData(e.results).render();
+            });
+            input.ac.on("ac:query", function (e) {
+                if (acWidget.visible && e.value === acWidget.value) {
+                    e.halt();
+                }
+                acWidget.value = e.value;
+            });
+            input.ac.on("ac:show", function () {
+                if (!acWidget.visible) acWidget.render();
+            }, acWidget);
+            input.ac.on("ac:hide", acWidget.hide, acWidget);
+            input.ac.on("ac:next", acWidget.next, acWidget);
+            input.ac.on("ac:previous", acWidget.previous, acWidget);
+            Y.on("key", acWidget.select, input, "down:13,10");
+        };
+
+        for (i = 0; i < suggestElms.size(); i++) {
+            init(suggestElms.item(i));
+        }
+
+    
+    
+});
+
+
+
+/*YUI().use('yui2-yahoo','yui2-dom','yui2-event','yui2-autocomplete','yui2-datasource','yui2-connection','node', function(Y){
         var suggestElms = Y.all('.laneSuggest'), i,
             acWidgets = [],
             trackItemSelect = function(sType, aArgs) {
@@ -87,6 +250,9 @@ YUI().use('yui2-yahoo','yui2-dom','yui2-event','yui2-autocomplete','yui2-datasou
             initialize(suggestElms.item(i));
         }
 });
+*/
+
+
 //YUI.add('gallery-ac-plugin', function(Y) {
 //
 ///**
