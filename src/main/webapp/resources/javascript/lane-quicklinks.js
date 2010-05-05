a5973eabb719a4e4cfa7e3b7d8107916d63606d0
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-05-05 16:17:06 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70622 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-quicklinks.js
+++ src/main/webapp/resources/javascript/lane-quicklinks.js
@@ -1,10 +1,10 @@
 YUI().use('node', function(Y) {
 	var qlNode = Y.one('#qlinks'), qlOptions = qlNode.all("option");
 	qlNode.on('change', function() {
-		var i = qlNode.get('selectedIndex');
-		if (i) {
+		var i = qlNode.get('selectedIndex'), v = qlOptions.item(i).get('value');
+		if (i && v) {
 			// TODO: add tracking
-			window.location = qlOptions.item(i).get('value');
+			window.location = v;
 		}
 	});
 });
