e97c3f524e5ff283dd7183964641a36a73497e53
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-05-10 23:31:16 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70947 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/webapp/resources/javascript/lane-search-results.js
@@ -0,0 +1,65 @@
+/*
+ * adds and animates show/hide toggle for search results
+ * 
+ */
+YUI().use('node-base','anim', function(Y) {
+	var closeResultsAnim = new Y.Anim( {
+		node : '.searchResults',
+		easing : Y.Easing.easeOut,
+		duration : 0.3,
+		to : {
+			marginLeft : '-185px'
+		}
+	}),
+	closeFacetsAnim = new Y.Anim( {
+		node : '#searchFacets',
+		easing : Y.Easing.easeOut,
+		duration : 0.3,
+		to : {
+			opacity : 0
+		}
+	}),
+	openResultsAnim = new Y.Anim( {
+		node : '.searchResults',
+		easing : Y.Easing.easeOut,
+		duration : 0.3,
+		to : {
+			marginLeft : 0
+		}
+	}),
+	openFacetsAnim = new Y.Anim( {
+		node : '#searchFacets',
+		easing : Y.Easing.easeOut,
+		duration : 0.3,
+		to : {
+			opacity : 1
+		}
+	}),
+	onControlClick = function(e){
+		var parent = this.get("parentNode");
+		if (parent.hasClass("closed")) {
+			closeFacetsAnim.run();
+			parent.removeClass("closed");
+			this.set('innerHTML', 'Show Options');
+			parent.addClass("open");
+		} else {
+			openResultsAnim.run();
+			parent.removeClass("open");
+			this.set('innerHTML', 'Hide Options');
+			parent.addClass("closed");
+		}
+		e.preventDefault();
+	};
+
+	closeFacetsAnim.on('end', function() {
+		closeResultsAnim.run();
+	});
+
+	openResultsAnim.on('end', function() {
+		openFacetsAnim.run();
+	});
+	
+	var options = Y.Node.create('<span id="optionsToggle" class="closed"><a href="">Hide Options</a></span>');
+	Y.one("#searchOptions").insert(options);
+	options.one("a").on("click", onControlClick);
+});
