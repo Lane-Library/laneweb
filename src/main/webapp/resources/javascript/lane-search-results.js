c2f897f46b3a6833112d80e6dc6a1a799d622ef2
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-05-11 18:36:08 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@70980 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-search-results.js
+++ src/main/webapp/resources/javascript/lane-search-results.js
@@ -3,53 +3,55 @@
  * 
  */
 YUI().use('node-base','anim', function(Y) {
-	var closeResultsAnim = new Y.Anim( {
-		node : '.searchResults',
-		easing : Y.Easing.easeOut,
-		duration : 0.3,
-		to : {
-			marginLeft : '-185px'
-		}
-	}),
-	closeFacetsAnim = new Y.Anim( {
-		node : '#searchFacets',
-		easing : Y.Easing.easeOut,
-		duration : 0.3,
-		to : {
-			opacity : 0
-		}
-	}),
-	openResultsAnim = new Y.Anim( {
-		node : '.searchResults',
-		easing : Y.Easing.easeOut,
-		duration : 0.3,
-		to : {
-			marginLeft : 0
-		}
-	}),
-	openFacetsAnim = new Y.Anim( {
-		node : '#searchFacets',
-		easing : Y.Easing.easeOut,
-		duration : 0.3,
-		to : {
-			opacity : 1
-		}
-	}),
-	onControlClick = function(e){
-		var parent = this.get("parentNode");
-		if (parent.hasClass("closed")) {
-			closeFacetsAnim.run();
-			parent.removeClass("closed");
-			this.set('innerHTML', 'Show Options');
-			parent.addClass("open");
-		} else {
-			openResultsAnim.run();
-			parent.removeClass("open");
-			this.set('innerHTML', 'Hide Options');
-			parent.addClass("closed");
-		}
-		e.preventDefault();
-	};
+	var options = Y.Node.create('<span id="optionsToggle" class="closed"><a href="">Hide Options</a></span>'),
+		optionsNode = Y.one("#searchOptions"),
+		closeResultsAnim = new Y.Anim( {
+			node : '#searchResults',
+			easing : Y.Easing.easeOut,
+			duration : 0.3,
+			to : {
+				marginLeft : '-185px'
+			}
+		}),
+		closeFacetsAnim = new Y.Anim( {
+			node : '#searchFacets',
+			easing : Y.Easing.easeOut,
+			duration : 0.3,
+			to : {
+				opacity : 0
+			}
+		}),
+		openResultsAnim = new Y.Anim( {
+			node : '#searchResults',
+			easing : Y.Easing.easeOut,
+			duration : 0.3,
+			to : {
+				marginLeft : 0
+			}
+		}),
+		openFacetsAnim = new Y.Anim( {
+			node : '#searchFacets',
+			easing : Y.Easing.easeOut,
+			duration : 0.3,
+			to : {
+				opacity : 1
+			}
+		}),
+		onControlClick = function(e){
+			var parent = this.get("parentNode");
+			if (parent.hasClass("closed")) {
+				closeFacetsAnim.run();
+				parent.removeClass("closed");
+				this.set('innerHTML', 'Show Options');
+				parent.addClass("open");
+			} else {
+				openResultsAnim.run();
+				parent.removeClass("open");
+				this.set('innerHTML', 'Hide Options');
+				parent.addClass("closed");
+			}
+			e.preventDefault();
+		};
 
 	closeFacetsAnim.on('end', function() {
 		closeResultsAnim.run();
@@ -59,7 +61,8 @@ YUI().use('node-base','anim', function(Y) {
 		openFacetsAnim.run();
 	});
 	
-	var options = Y.Node.create('<span id="optionsToggle" class="closed"><a href="">Hide Options</a></span>');
-	Y.one("#searchOptions").insert(options);
-	options.one("a").on("click", onControlClick);
+	if(optionsNode){
+		optionsNode.insert(options);
+		options.one("a").on("click", onControlClick);
+	}
 });
