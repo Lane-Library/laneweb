45f118fe9d9b4b8cdb6492d6647a3d1f033bd9f3
Charles Yates <ceyates@stanford.edu>
2010-05-18 00:38:45 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@71493 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/webapp/resources/javascript/lane-faq-expandies.js
@@ -0,0 +1,64 @@
+YUI({
+    gallery: 'gallery-2010.04.02-17-26'
+}).use('gallery-node-accordion', 'plugin', 'node', 'anim', "io", function(Y) {
+
+    var expandies = Y.all('.faq-expandy'), expandy, i, j, anchor, href, faqId, children, panel;
+    
+    for (i = 0; i < expandies.size(); i++) {
+        expandy = expandies.item(i).one("ul");
+        expandy.addClass('yui3-accordion');
+        children = expandy.get('children');
+        for (j = 0; j < children.size(); j++) {
+            panel = children.item(j);
+            anchor = panel.one("a");
+            href = anchor.get("href");
+            faqId = parseInt(href.substring(href.indexOf("=") + 1), 10);
+            if (Y.Lang.isNumber(faqId)) {
+                panel.addClass('yui3-accordion-item');
+                anchor.addClass('yui3-accordion-item-trigger yui3-accordion-item-hd');
+                panel.append("<div class='yui3-accordion-item-bd'/>")
+                Y.io("/././plain/howto/index.html?id=" + faqId, {
+                    on: {
+                        success: function(id, o, arguments) {
+                            var content = new Y.Node(o.responseXML), panel = arguments.panel;
+                            content = content.one("#mainColumn");
+                                if (content.one("h1")) {
+                                    content.one("h1").remove();
+                                }
+                            panel.append(content);
+                        },
+                        failure: function() {
+                            arguments.panel.set("innerHTML", "faq not found");
+                        }
+                    },
+                    arguments: {
+                        panel: panel.one(".yui3-accordion-item-bd")
+                    }
+                });
+            } else {
+                anchor.on("click", function(e) {
+                    e.preventDefault();
+                    Y.io("/././plain/howto/index.html?" + href.substring(href.indexOf("?") + 1), {
+                        on: {
+                            success: function(id, o, arguments) {
+                                var item = arguments.anchor.get("parentNode");
+                                arguments.anchor.remove();
+                                var content = new Y.Node(o.responseXML);
+                                item.append(content.one("dl"));
+                            },
+                            failure: function() {
+                                document.location = argument.anchor.get("href");
+                            }
+                        },
+                        arguments: {
+                            anchor: this
+                        }
+                    });
+                });
+            }
+        }
+        expandy.plug(Y.Plugin.NodeAccordion, {
+            anim: Y.Easing.backIn
+        });
+    }
+});
---
+++ src/main/webapp/resources/test/faq-expandy/faq-expandies-test.js
@@ -0,0 +1,19 @@
+/**
+ * @author ceyates
+ */YUI({
+     gallery: 'gallery-2010.04.02-17-26'
+}).use('gallery-node-accordion', 'node', 'node-event-simulate', 'console', 'test', function(Y){
+
+    var faqExpandiesTestCase = new Y.Test.Case({
+        name: 'FAQ Expandies TestCase'
+    });
+    
+    Y.one('body').addClass('yui3-skin-sam');
+    new Y.Console({
+        newestOnTop: false
+    }).render('#log');
+    
+    
+    Y.Test.Runner.add(faqExpandiesTestCase);
+    Y.Test.Runner.run();
+});
