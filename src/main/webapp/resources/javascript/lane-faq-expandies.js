ec71f896062966e44afdfffc03e1dae6d1b9f733
Charles Yates <ceyates@stanford.edu>
2010-05-21 02:24:27 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@71791 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-faq-expandies.js
+++ src/main/webapp/resources/javascript/lane-faq-expandies.js
@@ -1,48 +1,6 @@
-if (!document.ELEMENT_NODE) {
-    document.ELEMENT_NODE = 1;
-    document.ATTRIBUTE_NODE = 2;
-    document.TEXT_NODE = 3;
-    document.CDATA_SECTION_NODE = 4;
-    document.ENTITY_REFERENCE_NODE = 5;
-    document.ENTITY_NODE = 6;
-    document.PROCESSING_INSTRUCTION_NODE = 7;
-    document.COMMENT_NODE = 8;
-    document.DOCUMENT_NODE = 9;
-    document.DOCUMENT_TYPE_NODE = 10;
-    document.DOCUMENT_FRAGMENT_NODE = 11;
-    document.NOTATION_NODE = 12;
-}
-
-document._importNode = function(node, allChildren) {
-    /* find the node type to import */
-    switch (node.nodeType) {
-        case document.ELEMENT_NODE:
-            /* create a new element */
-            var newNode = document.createElement(node.nodeName);
-            /* does the node have any attributes to add? */
-            if (node.attributes && node.attributes.length > 0)
-                /* add all of the attributes */
-                for (var i = 0, il = node.attributes.length; i < il;) {
-//                    alert(node.nodeName + "\n" + node.attributes[i].nodeName + "=" + node.getAttribute(node.attributes[i].nodeName));
-                    newNode.setAttribute(node.attributes[i].nodeName, node.getAttribute(node.attributes[i++].nodeName));
-                }
-            /* are we going after children too, and does the node have any? */
-            if (allChildren && node.childNodes && node.childNodes.length > 0)
-                /* recursively get all of the child nodes */
-                for (var i = 0, il = node.childNodes.length; i < il;)
-                    newNode.appendChild(document._importNode(node.childNodes[i++], allChildren));
-            return newNode;
-            break;
-        case document.TEXT_NODE:
-        case document.CDATA_SECTION_NODE:
-        case document.COMMENT_NODE:
-            return document.createTextNode(node.nodeValue);
-            break;
-    }
-};
 YUI({
     gallery: 'gallery-2010.04.02-17-26'
-}).use('gallery-node-accordion', 'plugin', 'node', 'anim', "io", function(Y) {
+}).use("lane", 'gallery-node-accordion', 'plugin', 'node', 'anim', "io", function(Y) {
 
 
     var expandies = Y.all('.faq-expandy'), expandy, i, j, anchor, href, faqId, children, panel, eventHandle;
@@ -89,7 +47,13 @@ YUI({
                             success: function(id, o, arguments) {
                                 var i, items, j, present, item = arguments.anchor.get("parentNode"), list = item.get("parentNode"), initialItems = list.all("li");
                                 arguments.anchor.detach(eventHandle);
-                                items = new Y.Node(o.responseXML).all(".middleColumn .bd li");
+                                items = o.responseXML.documentElement;
+                                if (document.importNode) {
+                                    items = document.importNode(result, true);
+                                } else  {
+                                    items = document._importNode(result, true);
+                                }
+								items = items.all("li");
                                 for (i = 0; i < items.size(); i++) {
                                     present = false;
                                     item = items.item(i);
@@ -100,7 +64,7 @@ YUI({
                                         }
                                     }
                                     if (!present) {
-                                        list.append(items.item(i).cloneNode(true));
+                                        list.append(items.item(i));
                                     }
                                 }
                             },
