569678c66149716ea82635eb17667241814b8398
Charles Yates <ceyates@stanford.edu>
2010-06-25 18:46:25 +0000

    Firefox 3.0 fix
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@74060 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-faq-expandies.js
+++ src/main/webapp/resources/javascript/lane-faq-expandies.js
@@ -1,9 +1,11 @@
 YUI({
-    gallery: 'gallery-2010.04.02-17-26'
+    gallery: 'gallery-2010.04.02-17-26',
+    filter:"debug"
 }).use("lane", 'gallery-node-accordion', 'plugin', 'node', 'anim', "io", function(Y) {
 
-
-    var expandies = Y.all('.faq-expandy'), expandy, i, j, anchor, href, faqId, children, panel, eventHandle;
+    var expandies = Y.all('.faq-expandy'),
+        expandy, i, j, anchor, href, faqId,
+        children, panel, eventHandle;
     for (i = 0; i < expandies.size(); i++) {
         expandy = expandies.item(i).one("ul");
         expandy.addClass('yui3-accordion');
@@ -23,7 +25,7 @@ YUI({
                             var result = o.responseXML.documentElement;
                             result = document.importNode(result, true);
                             result = new Y.Node(result);
-                            result = result.all("div").item(5);
+                            result = result.one(".middleColumn .bd");
                             arguments.panel.append(result);
                         },
                         failure: function(id, o, arguments) {
@@ -35,42 +37,6 @@ YUI({
                     }
                 });
             }
-//            else {
-//                eventHandle = anchor.on("click", function(e) {
-//                    e.preventDefault();
-//                    Y.io("/././plain/howto/index.html?" + href.substring(href.indexOf("?") + 1), {
-//                        on: {
-//                            success: function(id, o, arguments) {
-//                                var i, items, j, present, item = arguments.anchor.get("parentNode"), list = item.get("parentNode"), initialItems = list.all("li");
-//                                arguments.anchor.detach(eventHandle);
-//                                items = o.responseXML.documentElement;
-//                                items = document.importNode(items, true);
-//                                items = new Y.Node(items);
-//                                items = items.all("li");
-//                                for (i = 0; i < items.size(); i++) {
-//                                    present = false;
-//                                    item = items.item(i);
-//                                    for (j = 0; j < initialItems.size() - 1; j++) {
-//                                        if (initialItems.item(j).one("a").get("href") == item.one("a").get("href")) {
-//                                            present = true;
-//                                            break;
-//                                        }
-//                                    }
-//                                    if (!present) {
-//                                        list.append(items.item(i));
-//                                    }
-//                                }
-//                            },
-//                            failure: function() {
-//                                document.location = argument.anchor.get("href");
-//                            }
-//                        },
-//                        arguments: {
-//                            anchor: this
-//                        }
-//                    });
-//                });
-//            }
         }
         expandy.plug(Y.Plugin.NodeAccordion, {
             anim: Y.Easing.backIn
