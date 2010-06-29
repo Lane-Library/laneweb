c8b723637a6676938290b0738f6f863e93bbbfa7
Charles Yates <ceyates@stanford.edu>
2010-06-29 20:35:59 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@74242 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cocoon/YUIProxyReader.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/YUIProxyReader.java
@@ -3,28 +3,29 @@ package edu.stanford.irt.laneweb.cocoon;
 import org.apache.excalibur.source.SourceValidity;
 import org.apache.excalibur.source.impl.validity.NOPValidity;
 
-
 public class YUIProxyReader extends NoCacheBigReader {
-    
-    private String mimeType;
 
+    /*
+     * determines mimetype from end of parameters 
+     */
     @Override
     public String getMimeType() {
-        if (this.mimeType == null) {
-            String uri = this.source.getURI();
-            int index = uri.lastIndexOf('.');
-            if (index > 0) {
-                String suffix = uri.substring(index);
-                if (".css".equals(suffix)) {
-                    this.mimeType = "text/css";
-                } else if (".js".equals(suffix)) {
-                    this.mimeType = "application/javascript";
-                }
+        String uri = this.source.getURI();
+        int index = uri.lastIndexOf('.');
+        if (index > 0) {
+            String suffix = uri.substring(index);
+            if (".css".equals(suffix)) {
+                return "text/css";
+            } else if (".js".equals(suffix)) {
+                return "application/javascript";
             }
         }
-        return this.mimeType;
+        return null;
     }
 
+    /*
+     * always valid
+     */
     @Override
     public SourceValidity getValidity() {
         return NOPValidity.SHARED_INSTANCE;
