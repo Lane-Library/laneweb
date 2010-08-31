42c9c0348bdbcfac8204c3a15af111f39d8cae14
Alain Boussard <alainb@stanford.edu>
2010-08-31 19:05:15 +0000

    now the pubmed normalizer is done in the search application
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@76793 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/search/AbstractSearchGenerator.java
+++ src/main/java/edu/stanford/irt/laneweb/search/AbstractSearchGenerator.java
@@ -10,7 +10,6 @@ public abstract class AbstractSearchGenerator extends AbstractGenerator {
     @Override
     protected void initialize() {
         this.query = this.model.getString(Model.QUERY);
-        this.query = PMIDNormalizer.normalize(this.query);
         if (null == this.query) {
             throw new IllegalArgumentException("null query");
         }
