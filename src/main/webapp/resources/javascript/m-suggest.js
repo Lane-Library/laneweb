9a5d7d2251f6ec108282e0dead7bc8e39a193f2b
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-09-01 21:50:20 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@76887 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/m-suggest.js
+++ src/main/webapp/resources/javascript/m-suggest.js
@@ -28,9 +28,9 @@
                     : this.suggestUrl;
         },
         onKeyup : function(e) {
-            console.log("keyupPid->"+this.suggest._keyupPid);
-            console.log("onKeyUp event->");
-            console.log(e);
+            //console.log("keyupPid->"+this.suggest._keyupPid);
+            //console.log("onKeyUp event->");
+            //console.log(e);
             if (this.suggest._keyupPid) {
                 clearTimeout(this.suggest._keyupPid);
             }
@@ -43,8 +43,8 @@
             }
         },
         syncUI : function(data) {
-            console.log("sync UI: this -> ");
-            console.log(this);
+            //console.log("sync UI: this -> ");
+            //console.log(this);
             var inputElement = this.element,
                 ul = iui.$(this.element.id+"Sgst"),
                 li, i;
@@ -56,13 +56,12 @@
                 }
                 for(i = 0; i < data.suggest.length && i < 5; i++){
                     li = document.createElement("li");
-    //                li.addEventListener("click",this.select,false);
                     li.addEventListener("click",function(e){
                         e.inputElement = inputElement;
                         LANE.suggestSelect(e);
-                        console.log("inner click");
-                        console.log(inputElement.id);
-                        console.log(e.target.innerHTML);
+                        //console.log("inner click");
+                        //console.log(inputElement.id);
+                        //console.log(e.target.innerHTML);
                     },false);
     //                if(data.suggest[i].length > 37){
     //                    li.innerHTML = data.suggest[i].substring(0,34)+'...';
@@ -84,8 +83,8 @@
             }
         },
         update : function(e) {
-            console.log("update event:->");
-            console.log(e);
+            //console.log("update event:->");
+            //console.log(e);
             var srElm = document.getElementById("sgstRes"),
                 scriptUrl = e.target.suggest.suggestUrl + "?l="+e.target.suggest.requestLimit+"&callback=iui.$('"+e.target.id+"').suggest.syncUI&q=" + encodeURIComponent(e.target.value);
             if (srElm) {
@@ -94,7 +93,7 @@
             LANE.suggestScriptTag(scriptUrl);
         },
         cancel : function(e) {
-            console.log('cancel and hide');
+            //console.log('cancel and hide');
             var ul = iui.$(e.target.suggest.element.id+"Sgst"),
                 f = function(){
                     ul.parentNode.style.display = 'none';
@@ -102,12 +101,12 @@
             setTimeout(f,100);
         },
         position : function(e) {
-            console.log('positioning page Y->'+window.pageYOffset);
-            console.log('positioning input->'+e.target.offsetTop);
+            //console.log('positioning page Y->'+window.pageYOffset);
+            //console.log('positioning input->'+e.target.offsetTop);
             setTimeout(function(){
                 scrollTo(0,e.target.offsetTop+30);
-                console.log('post positioning page Y->'+window.pageYOffset);
-                console.log('post positioning input->'+e.target.offsetTop);
+                //console.log('post positioning page Y->'+window.pageYOffset);
+                //console.log('post positioning input->'+e.target.offsetTop);
             }, 50);
         }
     };;
@@ -122,9 +121,9 @@
     }
     
     LANE.suggestSelect = function(e) {
-        console.log("LANE suggest select");
-        console.log(e.inputElement.id);
-        console.log(e.target.innerHTML);
+        //console.log("LANE suggest select");
+        //console.log(e.inputElement.id);
+        //console.log(e.target.innerHTML);
         //if this is the laneSearch form then submit, otherwise assume pico and just update input
         e.inputElement.value = e.target.textContent;
         if(e.inputElement.id == 'searchInput'){
