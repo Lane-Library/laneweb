cdf16d6aa92185c4fc40bf843abd5e291a8d59cb
Charles Yates <ceyates@stanford.edu>
2010-09-19 23:51:23 +0000

    use XMLizable.toSAX() if possible
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@77708 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cocoon/ContentAggregator.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/ContentAggregator.java
@@ -1,11 +1,76 @@
 package edu.stanford.irt.laneweb.cocoon;
 
+import java.io.IOException;
+
 import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.components.source.util.SourceUtil;
 import org.apache.cocoon.sitemap.DefaultContentAggregator;
+import org.apache.cocoon.xml.XMLUtils;
+import org.apache.excalibur.xml.sax.XMLizable;
+import org.xml.sax.SAXException;
 
 public class ContentAggregator extends DefaultContentAggregator {
 
     public ContentAggregator(final ServiceManager serviceManager) {
         this.manager = serviceManager;
+    }
+    
+    public void generate()
+    throws IOException, SAXException, ProcessingException {
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("Generating aggregated content");
+        }
+        this.contentHandler.startDocument();
+        startElem(this.rootElement);
+
+        for (int i = 0; i < this.parts.size(); i++) {
+            final Part part = (Part) this.parts.get(i);
+//            this.rootElementIndex = part.stripRootElement ? -1 : 0;
+            if (part.element != null) {
+                this.currentElement = part.element;
+                startElem(part.element);
+            } else {
+                this.currentElement = this.rootElement;
+            }
+            if (part.source instanceof XMLizable) {
+                ((XMLizable)part.source).toSAX(this);
+            } else {
+                SourceUtil.parse(this.manager, part.source, this);
+            }
+            if (part.element != null) {
+                endElem(part.element);
+            }
+        }
+
+        endElem(this.rootElement);
+        this.contentHandler.endDocument();
+            
+        getLogger().debug("Finished aggregating content");
+    }
+
+    /**
+     * Private method generating startElement event for the aggregated parts
+     * and the root element
+     */
+    private void startElem(Element element)
+    throws SAXException {
+        final String qname = (element.prefix.equals("")) ? element.name : element.prefix + ':' + element.name;
+        if (!element.namespace.equals("")) {
+            this.contentHandler.startPrefixMapping(element.prefix, element.namespace);
+        }
+        this.contentHandler.startElement(element.namespace, element.name, qname, XMLUtils.EMPTY_ATTRIBUTES);
+    }
+
+    /**
+     * Private method generating endElement event for the aggregated parts
+     * and the root element
+     */
+    private void endElem(Element element) throws SAXException {
+        final String qname = (element.prefix.equals("")) ? element.name : element.prefix + ':' + element.name;
+        this.contentHandler.endElement(element.namespace, element.name, qname);
+        if (!element.namespace.equals("")) {
+            this.contentHandler.endPrefixMapping(element.prefix);
+        }
     }
 }
