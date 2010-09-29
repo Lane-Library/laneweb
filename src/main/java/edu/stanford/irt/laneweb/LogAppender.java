ed0a52da6111c95e090e13048947eab6dfe16937
Charles Yates <ceyates@stanford.edu>
2010-09-29 16:55:24 +0000

    added log4j appender that filters stack out stack traces when FileNotFoundException
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@78112 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/LogAppender.java
@@ -0,0 +1,31 @@
+package edu.stanford.irt.laneweb;
+
+import java.io.FileNotFoundException;
+
+import org.apache.log4j.RollingFileAppender;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
+
+/**
+ * A log appender that doesn't spew out error stack traces in the log just
+ * because someone requested a file that doesn't exist.
+ */
+public class LogAppender extends RollingFileAppender {
+
+    @Override
+    public synchronized void doAppend(final LoggingEvent event) {
+        ThrowableInformation ti = event.getThrowableInformation();
+        if (null != ti) {
+            Throwable t = ti.getThrowable();
+            while (t.getCause() != null) {
+                t = t.getCause();
+            }
+            if (t instanceof FileNotFoundException) {
+                LoggingEvent newEvent = new LoggingEvent(event.fqnOfCategoryClass, event.getLogger(), event.getLevel(), t.toString(), null);
+                super.doAppend(newEvent);
+                return;
+            }
+        }
+        super.doAppend(event);
+    }
+}
