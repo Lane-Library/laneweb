59c858ee907fea39c81be4556bd983e07df6c605
Charles Yates <ceyates@stanford.edu>
2010-10-07 19:32:56 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@78487 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/RobotsDotTextServlet.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/RobotsDotTextServlet.java
@@ -24,9 +24,12 @@ public class RobotsDotTextServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Proxy servers add this header, may be comma separated list
         String hostHeader = req.getHeader("X-Forwarded-Host");
         if (hostHeader == null) {
             hostHeader = req.getHeader("Host");
+        } else if (hostHeader.indexOf(',') > -1) {
+            hostHeader = hostHeader.substring(0, hostHeader.indexOf(','));
         }
         ServletOutputStream outputStream = resp.getOutputStream();
         if ("irt-lane-stage.stanford.edu".equals(hostHeader)) {
