464dff3d56235e73c6d99db0bb94fab3e82ff93b
Charles Yates <ceyates@stanford.edu>
2010-10-12 14:01:49 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@78640 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/RequestMethodDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/RequestMethodDataBinder.java
@@ -6,13 +6,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import edu.stanford.irt.laneweb.model.Model;
 
-
 public class RequestMethodDataBinder implements DataBinder {
 
     public void bind(Map<String, Object> model, HttpServletRequest request) {
-        model.put(Model.QUERY_STRING, request.getQueryString());
-        model.put(Model.REQUEST_URI, request.getRequestURI());
-        model.put(Model.REFERRER, request.getHeader("referer"));
+        String value = request.getQueryString();
+        if (value != null) {
+            model.put(Model.QUERY_STRING, request.getQueryString());
+        }
+        value = request.getRequestURI();
+        if (value != null) {
+            model.put(Model.REQUEST_URI, request.getRequestURI());
+        }
+        value = request.getHeader("referer");
+        {
+            model.put(Model.REFERRER, request.getHeader("referer"));
+        }
     }
-
 }
