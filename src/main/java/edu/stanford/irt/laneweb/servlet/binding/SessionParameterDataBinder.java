174fd92fac78858d0c88df432098fc602f7e1472
Charles Yates <ceyates@stanford.edu>
2010-10-12 13:57:55 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@78639 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/SessionParameterDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/SessionParameterDataBinder.java
@@ -5,31 +5,34 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-
 public abstract class SessionParameterDataBinder<T> implements DataBinder {
 
     private String parameterName;
+
     private String modelKey;
 
     public void bind(Map<String, Object> model, HttpServletRequest request) {
         HttpSession session = request.getSession();
         String parameterValue = request.getParameter(this.parameterName);
+        T value = null;
         if (parameterValue == null) {
-            model.put(this.modelKey, session.getAttribute(this.modelKey));
+            value = (T) session.getAttribute(this.modelKey);
         } else {
-            T value = getParameterAsObject(parameterValue);
+            value = getParameterAsObject(parameterValue);
+        }
+        if (value != null) {
             session.setAttribute(this.modelKey, value);
             model.put(this.modelKey, value);
         }
     }
-    
+
     public void setParameterName(String parameterName) {
         this.parameterName = parameterName;
     }
-    
+
     public void setModelKey(String modelKey) {
         this.modelKey = modelKey;
     }
-    
+
     protected abstract T getParameterAsObject(String parameterValue);
 }
