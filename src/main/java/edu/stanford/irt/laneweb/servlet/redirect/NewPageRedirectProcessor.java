f81a0a81b39dcd085d1a789f1086ba82136c18e3
Charles Yates <ceyates@stanford.edu>
2010-10-20 16:40:21 +0000

    handle http redirect in /shc
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@79030 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/servlet/redirect/NewPageRedirectProcessor.java
@@ -0,0 +1,20 @@
+package edu.stanford.irt.laneweb.servlet.redirect;
+
+
+public class NewPageRedirectProcessor extends DefaultRedirectProcessor {
+
+    private static final String NEW_PAGE_BASE_URL = "/newpage.html?page=";
+
+    @Override
+    public String getRedirectURL(String uri, String basePath, String queryString) {
+        String redirectURL = super.getRedirectURL(uri, basePath, queryString);
+        if (redirectURL != null) {
+            // add basePath to the page parameter of the newpage.html url
+            int newPage = redirectURL.indexOf(NEW_PAGE_BASE_URL);
+            if (newPage == 0) {
+                redirectURL = NEW_PAGE_BASE_URL + basePath + redirectURL.substring(NEW_PAGE_BASE_URL.length());
+            }
+        }
+        return redirectURL;
+    }
+}
