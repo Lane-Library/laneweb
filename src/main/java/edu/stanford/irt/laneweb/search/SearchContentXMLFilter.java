84abbc658450dfc666cb359ce187dcd3da9953f1
Alain Boussard <alainb@stanford.edu>
2010-10-26 22:42:22 +0000

    to remove super.unparsedEntityDecl("DOCTYPE", null, null, null);
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@79284 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/search/SearchContentXMLFilter.java
+++ src/main/java/edu/stanford/irt/laneweb/search/SearchContentXMLFilter.java
@@ -1,34 +1,22 @@
 package edu.stanford.irt.laneweb.search;
 
-import org.xml.sax.Attributes;
-
-import org.xml.sax.DTDHandler;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLFilterImpl;
 
 public class SearchContentXMLFilter extends XMLFilterImpl {
 
-    public SearchContentXMLFilter() {}
     
     public SearchContentXMLFilter(XMLReader parent) {
         super(parent);
-        
     }
 
-    public void unparsedEntityDecl(String name, String publicId,String systemId, String notationName)
-    throws SAXException{
-        super.unparsedEntityDecl("DOCTYPE", null, null, null);
-        
-    }
-
+   
     
     /**
      * Filter the Namespace URI for start-element events.
      */
     public void startDocument(  ) throws SAXException {
-        super.unparsedEntityDecl("DOCTYPE", null, null, null);
         //do nothing
     }
 
