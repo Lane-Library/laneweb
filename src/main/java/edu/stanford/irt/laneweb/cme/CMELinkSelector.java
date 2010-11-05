54c621809a7e6a77f93ede0a3e7b217ada26a753
Charles Yates <ceyates@stanford.edu>
2010-11-05 18:57:14 +0000

    use selectors to only add cme and proxy-link transformers to pipeline when necessary
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@79716 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/cme/CMELinkSelector.java
@@ -0,0 +1,21 @@
+package edu.stanford.irt.laneweb.cme;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.selection.Selector;
+
+import edu.stanford.irt.laneweb.model.DefaultModelAware;
+import edu.stanford.irt.laneweb.model.Model;
+
+
+public class CMELinkSelector extends DefaultModelAware implements Selector {
+    
+    private static final String EMPTY_STRING = "";
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public boolean select(String expression, Map objectModel, Parameters parameters) {
+        return !EMPTY_STRING.equals(getString(objectModel, Model.EMRID, EMPTY_STRING));
+    }
+}
---
+++ src/main/java/edu/stanford/irt/laneweb/proxy/ProxyLinkSelector.java
@@ -0,0 +1,19 @@
+package edu.stanford.irt.laneweb.proxy;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.selection.Selector;
+
+import edu.stanford.irt.laneweb.model.DefaultModelAware;
+import edu.stanford.irt.laneweb.model.Model;
+
+
+public class ProxyLinkSelector extends DefaultModelAware implements Selector {
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public boolean select(String expression, Map objectModel, Parameters parameters) {
+        return getObject(objectModel, Model.PROXY_LINKS, Boolean.class, Boolean.FALSE);
+    }
+}
