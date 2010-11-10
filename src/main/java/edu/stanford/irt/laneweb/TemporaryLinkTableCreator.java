f7ab1f8eb3c2e8f27e62748a3578430d80773bf3
Charles Yates <ceyates@stanford.edu>
2010-11-10 17:05:02 +0000

    add temporary component to create user_links data table
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@79900 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/TemporaryLinkTableCreator.java
@@ -0,0 +1,36 @@
+package edu.stanford.irt.laneweb;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemporaryLinkTableCreator {
+    
+    private Logger log = LoggerFactory.getLogger(TemporaryLinkTableCreator.class);
+    
+    private DataSource dataSource;
+    
+    public void initialize() {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = this.dataSource.getConnection();
+            stmt = conn.createStatement();
+            stmt.executeQuery("CREATE TABLE USER_LINKS (USER VARCHAR2(32) PRIMARY KEY, URL VARCHAR2(356), LABEL VARCHAR2(64), ORDER NUMBER(2))");
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            JdbcUtils.closeConnection(conn);
+            JdbcUtils.closeStatement(stmt);
+        }
+    }
+    
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+}
