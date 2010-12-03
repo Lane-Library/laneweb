750b3ee3bb9f3773294e62aac3fda286d88cc473
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-12-03 23:41:21 +0000

    teletype to PICO fields as well
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@80432 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-teletype.js
+++ src/main/webapp/resources/javascript/lane-teletype.js
@@ -1,40 +1,54 @@
 // teletype functionality for PubMed/UTD through Lane pages
-// typing in #searchTeletyper .teletypeInput sends keystrokes to #search #searchTerms
+// typing in .teletypeInput sends keystrokes to #search #searchTerms
+// if .teletypeInput has name p, i, c, o, keystrokes go to respective pico input
 (function() {
     var Y = LANE.Y,
-        targetElm = Y.one('#searchTerms'),
-        inputElms = Y.all('.teletypeInput'),
-        searchForm = Y.one('#search'),
-        searchTeletyper = Y.one('#searchTeletyper'),
-        i,
-        teletype = function(inputElm){
-            var qString;
-            if (!targetElm){
-                return false;
-            }
-            if (inputElm.get('value') != inputElm.get('title')) {
-                qString = inputElm.get('value');
-            }
-            if (!qString){
-                qString = targetElm.get('title');
-            }
-            targetElm.set('value', qString);
-        };
-
-        // send #searchTeletyper submit events to #search
-        if (searchTeletyper) {
-            searchTeletyper.on("submit",function(e){
+    i, targetInput, 
+    teletypeInputs = Y.all('.teletypeInput'),
+    teletype = function(inputElm,targetElm){
+        var qString;
+        if (!targetElm){
+            return false;
+        }
+        if (inputElm.get('value') != inputElm.get('title')) {
+            qString = inputElm.get('value');
+        }
+        if (!qString){
+            qString = targetElm.get('title');
+        }
+        targetElm.set('value', qString);
+    },
+    Teletype = function(sourceInput, targetInput) {
+        var targetElm = targetInput || Y.one('#searchTerms'),
+        teletyperForm = sourceInput.ancestor('form');
+        // send #teletyperForm submit events to #search; no need to send more than one
+        if (teletyperForm && teletyperForm.hasHandler == undefined) {
+            teletyperForm.hasHandler = true;
+            teletyperForm.on("submit",function(e){
                 e.halt();
-                searchForm.submit();
+                try {
+                    LANE.Search.submitSearch();
+                } catch (e) {
+                    alert(e);
+                }
             });
         }
-    
-    for (i = 0; i < inputElms.size(); i++){
-        inputElms.item(i).on( 'keyup', function(){
-            teletype(this);
+        sourceInput.on( 'keyup', function(){
+            teletype(this,targetElm);
         });
-        inputElms.item(i).on( 'blur', function(){
-            teletype(this);
+        sourceInput.on( 'blur', function(){
+            teletype(sourceInput,targetElm);
         });
+    };
+
+    for (i = 0; i < teletypeInputs.size(); i++) {
+        // target elements can either be PICO or main search box
+        if(teletypeInputs.item(i).get('name').match(/(p|i|c|o)/)){
+            targetInput = Y.one("#clinical"+teletypeInputs.item(i).get('name').toUpperCase());
+        }
+        else{
+            targetInput = Y.one("#searchTerms");
+        }
+        new Teletype(teletypeInputs.item(i), targetInput);
     }
 })();
