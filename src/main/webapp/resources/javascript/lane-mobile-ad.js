63eb4586a558ddbea62e67c79ed5934a4229f5dd
Ryan Max Steinberg <ryanmax@stanford.edu>
2010-12-14 19:06:46 +0000

    mobile ad Overlay conflicts with searchTerms textInput focus events: https://irt-bugs.stanford.edu/default.asp?45542
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@80738 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-mobile-ad.js
+++ src/main/webapp/resources/javascript/lane-mobile-ad.js
@@ -1,17 +1,11 @@
 // advertise existence of mobile interface to iPad, iPhone, iPod and Android clients
 (function() {
-    var Y = LANE.Y, advert, device;
+    var Y = LANE.Y, advert, id, device;
     if(navigator.userAgent.match(/(iPhone|iP.d|Android)/)){
         device = RegExp.$1;
-        advert = new Y.Overlay({
-            id : (device == 'iPad') ? 'ipadAd' : 'mphoneAd',
-            width : "988px",
-            height : (device == 'iPad') ? '45px' : '112px',
-            bodyContent : "Try <a href='/././m/'>Express Lane</a> for your "+device+" &raquo;",
-            xy:[6,0],
-            zIndex : 2
-        });
-        advert.render("#custom-doc");
+        id = (device == 'iPad') ? 'ipadAd' : 'mphoneAd';
+        advert = Y.Node.create('<div id="'+id+'"><div><div style="height: 64px;">Try <a href="/././m/">Express Lane</a> for your '+device+' &raquo;</div></div></div>');
+        Y.one("#custom-doc").insert(advert);
         advert.on("click",function(){
             window.location.href = "/././m/";
         });
