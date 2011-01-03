3a54268aa4eae13af732d81b846b72bca91ed88a
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-01-03 18:21:36 +0000

    large decrease in PAVA IP Group: https://irt-bugs.stanford.edu/?44419
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@80843 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/RemoteProxyIPDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/RemoteProxyIPDataBinder.java
@@ -5,9 +5,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import edu.stanford.irt.laneweb.ipgroup.IPGroup;
 import edu.stanford.irt.laneweb.model.Model;
 
@@ -19,32 +16,26 @@ import edu.stanford.irt.laneweb.model.Model;
  */
 public class RemoteProxyIPDataBinder implements DataBinder {
 
+    private static final String PAVA_IP = "152.131.10.128";
+    
     private static final String X_FORWARDED_FOR = "X-FORWARDED-FOR";
     
     private ProxyLinks proxyLinks;
-    
-    private static final Logger LOGGER = LoggerFactory.getLogger(RemoteProxyIPDataBinder.class);
-
 
     public void bind(Map<String, Object> model, HttpServletRequest request) {
         String currentIP = getRemoteAddress(request);
         HttpSession session = request.getSession();
         boolean isSameIP = currentIP.equals(session.getAttribute(Model.REMOTE_ADDR));
-        LOGGER.info("currentIP:"+currentIP);
-        LOGGER.info("isSameIP:"+isSameIP);
         if (!isSameIP) {
             session.setAttribute(Model.REMOTE_ADDR, currentIP);
         }
         model.put(Model.REMOTE_ADDR, currentIP);
         IPGroup ipGroup = (IPGroup) session.getAttribute(Model.IPGROUP);
-        LOGGER.info("session ipGroup:"+ipGroup);
         if (ipGroup == null || !isSameIP) {
             ipGroup = IPGroup.getGroupForIP(currentIP);
             session.setAttribute(Model.IPGROUP, ipGroup);
-            LOGGER.info("new session ipGroup:"+ipGroup);
         }
         model.put(Model.IPGROUP, ipGroup);
-        LOGGER.info("model ipGroup:"+model.get(Model.IPGROUP));
         Boolean proxyLinks = null;
         String requestParameter = request.getParameter(Model.PROXY_LINKS);
         if (requestParameter != null) {
@@ -65,16 +56,16 @@ public class RemoteProxyIPDataBinder implements DataBinder {
         // Load balancer also does this
         String header = request.getHeader(X_FORWARDED_FOR);
         if (header == null) {
-            header = request.getRemoteAddr();
-            LOGGER.info("x-forwarded-for header null:"+header);
-            return header;
+            return request.getRemoteAddr();
+        }
+        // PAVA proxy address is not first IP in x-forwarded-for list 
+        else if (header.indexOf(PAVA_IP) > -1) {
+            return PAVA_IP;
+        }
+        else if (header.indexOf(',') > 0) {
+            return header.substring(0, header.indexOf(','));
         } else {
-            LOGGER.info("x-forwarded-for header:"+header);
-            if (header.indexOf(',') > 0) {
-                return header.substring(0, header.indexOf(','));
-            } else {
-                return header;
-            }
+            return header;
         }
     }
 
