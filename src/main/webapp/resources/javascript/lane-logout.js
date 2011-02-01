3e937ab2eb2b45fe173e89334578d137f886fdd1
Alain Boussard <alainb@stanford.edu>
2011-02-01 20:51:40 +0000

    to delete cookie client side
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@81670 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-logout.js
+++ src/main/webapp/resources/javascript/lane-logout.js
@@ -8,5 +8,12 @@
 			Y.Cookie.remove("user");
 		});
 	}
+	pl_button = Y.one('#persistence-login');
+	pl = Y.one('#pl');
+	if (null != pl_button)
+		pl_button.on("click", function() {
+			if (false == pl.get('checked')) 
+				Y.Cookie.remove("user");
+		});
 
 })();
