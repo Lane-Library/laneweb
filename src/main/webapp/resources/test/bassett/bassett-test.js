fcdf02ef3e4229378eee8f14ea1dcd652e39ad0b
Charles Yates <ceyates@stanford.edu>
2011-02-02 20:01:02 +0000

    reverting some changes that I have moved to refactorjs branch
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@81727 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/test/bassett/bassett-test.js
+++ src/main/webapp/resources/test/bassett/bassett-test.js
@@ -6,37 +6,40 @@ YUI({
         TestRunner: true
     }
 }).use('node-event-simulate', 'console', 'test', function(Y){
-	
-	LANE.bassett.set("io", function(a, b, c) { 
-		b.on.success(0, {responseText:"<a href='?foo=bar'>foo</a>"}, b.arguments);
-		});
 
-	var bassettTestCase = new Y.Test.Case({
-		name : 'Lane Basset Test Case',
+    var bassettTestCase = new Y.Test.Case({
+        name: 'Lane Basset Test Case',
+        
+        testAccordeonLink: function() {
+            Y.all('a').item(0).simulate('click');
+    },
 
-		testAccordeonLink : function() {
-			Y.all('a').item(0).simulate('click');
-		},
+    testImageLink: function() {
+        Y.one('#bassettContent').all('a').item(2).simulate('click');
+    },
 
-		testImageLink : function() {
-			Y.one('#bassettContent').all('a').item(2).simulate('click');
-		},
+    
+    testDiagramLink: function() {
+        Y.one('#bassettContent').all('a').item(1).simulate('click');
+    },
 
-		testDiagramLink : function() {
-			Y.one('#bassettContent').all('a').item(1).simulate('click');
-		},
 
-		testPhotoLink : function() {
-			Y.one('#bassettContent').all('a').item(0).simulate('click');
-		}
-
-	});
+    testPhotoLink: function() {
+        Y.one('#bassettContent').all('a').item(0).simulate('click');
+    }
+    
+            
+    });
 
     
     Y.one('body').addClass('yui3-skin-sam');
     new Y.Console({
         newestOnTop: false
     }).render('#log');
+    
+    
+
+    
     
     Y.Test.Runner.add(bassettTestCase);
     Y.Test.Runner.run();
