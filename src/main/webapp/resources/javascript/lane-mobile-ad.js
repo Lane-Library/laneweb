d92ee3a10d7e6b827c1406865d89f1eca4678d7a
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-02-09 04:55:37 +0000

    adjust height and add toggle to turn off mobile ad
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@81949 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-mobile-ad.js
+++ src/main/webapp/resources/javascript/lane-mobile-ad.js
@@ -1,10 +1,10 @@
 // advertise existence of mobile interface to iPad, iPhone, iPod and Android clients
 (function() {
     var Y = LANE.Y, advert, id, device;
-    if(navigator.userAgent.match(/(iPhone|iP.d|Android)/)){
+    if(navigator.userAgent.match(/(iPhone|iP.d|Android)/) && !location.search.match(/mad=0/)){
         device = RegExp.$1;
         id = (device == 'iPad') ? 'ipadAd' : 'mphoneAd';
-        advert = Y.Node.create('<div id="'+id+'"><div><div style="height: 64px;">Try <a href="/././m/">Express Lane</a> for your '+device+' &raquo;</div></div></div>');
+        advert = Y.Node.create('<div id="'+id+'"><div><div>Try <a href="/././m/">Express Lane</a> for your '+device+' &raquo;</div></div></div>');
         Y.one("#custom-doc").insert(advert);
         advert.on("click",function(){
             window.location.href = "/././m/";
