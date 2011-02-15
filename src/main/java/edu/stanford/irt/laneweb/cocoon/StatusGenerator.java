00144501004668b8caee1fc55eafa3b4ba4c41ef
Charles Yates <ceyates@stanford.edu>
2011-02-15 01:45:36 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@82184 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cocoon/StatusGenerator.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/StatusGenerator.java
@@ -1,13 +1,11 @@
 package edu.stanford.irt.laneweb.cocoon;
 
 import org.apache.excalibur.store.Store;
-import org.apache.excalibur.store.StoreJanitor;
 
 public class StatusGenerator extends org.apache.cocoon.generation.StatusGenerator {
 
-    public StatusGenerator(final Settings settings, final StoreJanitor storeJanitor, final Store store) {
+    public StatusGenerator(final Settings settings, final Store store) {
         this.settings = settings;
-        this.storeJanitor = storeJanitor;
         this.storePersistent = store;
     }
 }
