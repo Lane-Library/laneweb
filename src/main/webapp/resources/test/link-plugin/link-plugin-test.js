8a5925fddc58835f45bf8e6414b6f853c7729221
Charles Yates <ceyates@stanford.edu>
2011-02-16 17:16:43 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@82250 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/test/link-plugin/link-plugin-test.js
+++ src/main/webapp/resources/test/link-plugin/link-plugin-test.js
@@ -5,7 +5,7 @@ YUI({
     logInclude: {
         TestRunner: true
     }
-}).use("test", "console", "node-event-simulate", function(T){
+}).use("test", "console", function(T){
     
 
     var linkTestCase = new T.Test.Case({
@@ -13,36 +13,26 @@ YUI({
         
         testIsLocalLink : function() {
         	var anchor = T.all("a").item(0);
-        	anchor.simulate("click");
         	T.Assert.isTrue(anchor.link.isLocal());
         },
         
         testIsProxyLoginLink : function () {
         	var anchor = T.all("a").item(1);
-        	anchor.simulate("click");
         	T.Assert.isTrue(anchor.link.isProxyLogin());
         },
         
         testIsProxyLink : function() {
         	var anchor = T.all("a").item(2);
-        	anchor.simulate("click");
         	T.Assert.isTrue(anchor.link.isProxy());
         },
         
         testProxyLoginIsNotLocal : function() {
         	var anchor = T.all("a").item(1);
-        	anchor.simulate("click");
         	T.Assert.isFalse(anchor.link.isLocal());
         }
     });
     
-	T.all("a").on("click", function(e) {
-		e.preventDefault();
-		e.target.plug(Y.lane.LinkPlugin);
-		var foo = e.target;
-		var bar = foo;
-//		T.mix(e.target, Y.lane.Link);
-	});
+	T.all("a").plug(Y.lane.LinkPlugin);
     
     T.one("body").addClass("yui3-skin-sam");
     new T.Console({
