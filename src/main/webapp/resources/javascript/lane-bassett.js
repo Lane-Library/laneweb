7698c41dd77dfe5c55e71288e6779ad52bd48b7c
Charles Yates <ceyates@stanford.edu>
2011-02-17 22:05:09 +0000

    use refactored bassett.js
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@82311 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-bassett.js
+++ src/main/webapp/resources/javascript/lane-bassett.js
@@ -1,88 +1,124 @@
 (function() {
-    
-    
+  if (Y.one("#accordion") && Y.one("#bassettContent")) {
+	function Bassett(config) {
+		Bassett.superclass.constructor.apply(this, arguments);
+	};
+	Bassett.NAME = "bassett";
+	Bassett.ATTRS = {
+		content : {
+			valueFn : function() {
+				return Y.one('#bassettContent');
+			}
+		},
+		accordion : {
+			valueFn : function() {
+				return Y.one('#accordion');
+			}
+		},
+		diagramDisplay : {
+			value : false
+		},
+		history : {
+			valueFn : function() {
+				return new Y.History();
+			}
+		},
+		io : {
+			value : Y.io
+		}
+	};
+	Y.extend(Bassett, Y.Base, {
+		initializer : function(cfg) {
+			this._initializeHistory();
+			this._registerLinksContainer(this.get("content"));
+			this._registerLinksContainer(this.get("accordion"));
+		},
+		//TODO: redirect to the non ajax page.
+//		_failureHandler : function(id, o) {
+//			alert(o);
+//		},
+		_formatAjaxUrl : function(href) {
+			var url;
+			href = href.replace("search.html",
+					"/biomed-resources/bassett/bassettsView.html");
+			href = href.substr(href.indexOf("/bassett/") + 8);
+			href = href.split("?");
+			if (href.length == 1) {
+				url = href[0];
+			}
+			if (href.length > 1) {
+				url = href[0] + "?" + href[1];
+			}
+			if (url !== "/index.html" && this.get("diagramDisplay")) {
+				url = url + "&t=diagram";
+			}
+			return url;
+		},
+		_handleClickEvent : function(event) {
+			var url;
+			event.preventDefault();
+			if (event.currentTarget.get('id') == "diagram-choice") {
+				this.set("diagramDisplay", true);
+			}
+			if (event.currentTarget.get('id') == "photo-choice") {
+				this.set("diagramDisplay", false);
+			}
+			url = this._formatAjaxUrl(event.currentTarget.get('href'));
+			try {
+				this.get("history").addValue("bassett", url);
+			} catch (e) {
+				this._loadContent(url);
+			}
+		
+		},
+		_initializeHistory : function() {
+			var history = this.get("history"),
+			    bassettHistory = history.get("bassett");
+			if (bassettHistory) {
+				this._loadContent(bassettHistory);
+			}
+			history.on("bassettChange", function(e) {
+				this._loadContent(e.newVal);
+			}, this);
+			history.on("bassettRemove", function(e) {
+				this._loadContent(this._formatAjaxUrl(window.location.toString()));
+			}, this);
+		},
+		_loadContent : function(url) {
+			url = "/././plain/biomed-resources/bassett/raw".concat(url);
+			this.get("io").call(this, url, {
+				on : {
+					success : this._successHandler
+//					failure : this._failureHandler
+					},
+				arguments : {
+					bassett : this
+				}
+			});
 
-
-        var Y = LANE.Y,
-            bassettContent = Y.one('#bassettContent'),
-            diagramDisplay = false,
-            accordion,
-            history,
-            registerLinksContainer = function(container) {
-            var anchor, i, url;
-            if (container) {
-                anchor = container.all('a');
-                for (i = 0; i < anchor.size(); i++) {
-                    if (anchor.item(i).get('rel') === null || anchor.item(i).get('rel') === "" ||  anchor.item(i).get('rel') === "propagation") {
-                        anchor.item(i).on('click',function(ev) {
-                            if (this.get('id') == "diagram-choice") {
-                                diagramDisplay = true;
-                            }
-                            if (this.get('id') == "photo-choice") {
-                                diagramDisplay = false;
-                            }
-                            url = formatAjaxUrl(this.get('href'));
-                            try {
-                                history.addValue("bassett",url);
-                            } catch (e) {
-                                loadContent(url);
-                            }
-                            ev.preventDefault();
-                        });
-                    }
-                }
-            }
-        },
-
-        loadContent = function(url) {
-            url = "/././plain/biomed-resources/bassett/raw".concat(url);
-            function successHandler(id, o, args) {
-                var content = new Y.Node(o.responseText),
-                    container = Y.one('#bassettContent');
-                container.setContent(content);
-                registerLinksContainer(container);
-                Y.fire('lane:change');
-            }
-            Y.io(url, {on : {success : successHandler}});
-        },
-
-        formatAjaxUrl = function(href) {
-            var url;
-            href = href.replace("search.html", "/biomed-resources/bassett/bassettsView.html");
-            href = href.substr(href.indexOf("/bassett/") + 8);
-            href = href.split("?");
-            if (href.length == 1) {
-                url = href[0];
-            }
-            if (href.length > 1) {
-                url = href[0] + "?" + href[1];
-            }
-            if (diagramDisplay) {
-                url = url + "&t=diagram";
-            }
-            return url;
-        },
-
-        initializeHistory = function() {
-            history = new Y.HistoryHash();
-            if(history.get('bassett')){
-                loadContent(history.get('bassett'));
-            }
-            history.on("bassettChange",function(e) {
-                loadContent(e.newVal);
-            });
-            history.on("bassettRemove",function(e) {
-                loadContent(formatAjaxUrl(window.location.toString()));
-            });
-        };
-
-        if (bassettContent) {
-            accordion = Y.one('#accordion');
-            if (accordion) // not if largerView.html
-            {
-                registerLinksContainer(accordion);
-                registerLinksContainer(Y.one('#bassettContent'));
-                initializeHistory();
-            }
-        }
-    })();
+		},
+		_registerLinksContainer : function(container) {
+			var anchors, anchor, i, size, rel;
+			if (container) {
+				anchors = container.all('a');
+				size = anchors.size();
+				for (i = 0; i < size; i++) {
+					anchor = anchors.item(i);
+					rel = anchor.get("rel");
+					if (!rel || rel === "propagation") {
+						anchor.on('click', this._handleClickEvent, this);
+					}
+				}
+			}
+		},
+		_successHandler : function(id, o, args) {
+			var content = new Y.Node(o.responseText),
+			    container = args.bassett.get("content");
+			container.setContent(content);
+			args.bassett._registerLinksContainer(container);
+			Y.fire('lane:change');
+		}
+	});
+	Y.lane.Bassett = new Bassett();
+  }
+})();
--- src/main/webapp/resources/test/bassett/bassett-test.js
+++ src/main/webapp/resources/test/bassett/bassett-test.js
@@ -5,42 +5,60 @@ YUI({
     logInclude: {
         TestRunner: true
     }
-}).use('node-event-simulate', 'console', 'test', function(Y){
+}).use('node-event-simulate', 'console', 'test', function(T) {
+	
+	var response = '<?xml version="1.0" encoding="UTF-8"?>' +
+		'<html xmlns="http://www.w3.org/1999/xhtml"><body>' +
+		'<a href="/biomed-resources/bassett/bassettsView.html?r=Abdomen--Central Nervous System&amp;pageNumber=0" id="photo-choice">' +
+		'<a href="/biomed-resources/bassett/bassettsView.html?r=Abdomen--Central Nervous System&amp;t=diagram&amp;pageNumber=0" id="diagram-choice">' +
+		'<span>Diagrams</span></a> </span><div id="thumbnail" class="bassett-images">' +
+		'<div class="image-container"><div class="hr"><a href="/biomed-resources/bassett/bassettView.html?bn=151-3"><img title="Kidneys, suprarenal glands and posterior abdominal vessels, nerves and muscles. Aortic plexus, superior hypogastric plexus, testicular plexuses; lymphatic structures along lower part of abdominal aorta" src="http://elane.stanford.edu/public/L254573/small/AD.B.0151.003.L.jpg" alt="bassett Number 151-3"/></a></div><div class="image-text">#151-3<br/><a title="Kidneys, suprarenal glands and posterior abdominal vessels, nerves and muscles. Aortic plexus, superior hypogastric plexus, testicular plexuses; lymphatic structures along lower part of abdominal aorta" href="/biomed-resources/bassett/bassettView.html?bn=151-3"> View Larger</a></div></div><div class="image-container"><div class="hr"><a href="/biomed-resources/bassett/bassettView.html?bn=152-1"><img title="Kidneys, suprarenal glands and posterior abdominal vessels, nerves and muscles. Nerves, blood vessels, and muscle attachments in relation to lumbar vertebrae, close-up lateral view" src="http://elane.stanford.edu/public/L254573/small/AD.B.0152.001.L.jpg" alt="bassett Number 152-1"/></a></div><div class="image-text">#152-1<br/><a title="Kidneys, suprarenal glands and posterior abdominal vessels, nerves and muscles. Nerves, blood vessels, and muscle attachments in relation to lumbar vertebrae, close-up lateral view" href="/biomed-resources/bassett/bassettView.html?bn=152-1"> View Larger</a></div></div></div></body></html>';
+	
+	Y.lane.Bassett.set("io", function(id, o) {
+	    o.on.success(0, {responseText:response}, o.arguments);
+	});
+	
+	var eventOccurred = false;
+	
+	Y.on("lane:change", function() {
+		eventOccurred = true;
+	});
 
-    var bassettTestCase = new Y.Test.Case({
-        name: 'Lane Basset Test Case',
-        
-        testAccordeonLink: function() {
-            Y.all('a').item(0).simulate('click');
-    },
+	var bassettTestCase = new T.Test.Case({
+		name : 'Lane Basset Test Case',
+		
+		setUp : function() {
+			eventOccurred = false;
+		},
 
-    testImageLink: function() {
-        Y.one('#bassettContent').all('a').item(2).simulate('click');
-    },
+		testAccordeonLink : function() {
+			T.all('a').item(0).simulate('click');
+			T.Assert.isTrue(eventOccurred);
+		},
+
+		testImageLink : function() {
+			T.one('#bassettContent').all('a').item(2).simulate('click');
+			T.Assert.isTrue(eventOccurred);
+		},
+
+		testDiagramLink : function() {
+			T.one('#bassettContent').all('a').item(1).simulate('click');
+			T.Assert.isTrue(eventOccurred);
+		},
+
+		testPhotoLink : function() {
+			T.one('#bassettContent').all('a').item(0).simulate('click');
+			T.Assert.isTrue(eventOccurred);
+		}
+
+	});
 
     
-    testDiagramLink: function() {
-        Y.one('#bassettContent').all('a').item(1).simulate('click');
-    },
-
-
-    testPhotoLink: function() {
-        Y.one('#bassettContent').all('a').item(0).simulate('click');
-    }
-    
-            
-    });
-
-    
-    Y.one('body').addClass('yui3-skin-sam');
-    new Y.Console({
+    T.one('body').addClass('yui3-skin-sam');
+    new T.Console({
         newestOnTop: false
     }).render('#log');
     
-    
-
-    
-    
-    Y.Test.Runner.add(bassettTestCase);
-    Y.Test.Runner.run();
+    T.Test.Runner.add(bassettTestCase);
+    T.Test.Runner.run();
 });
