7543b8860d7d7b2b47903556d59c13d60c30ca83
Charles Yates <ceyates@stanford.edu>
2011-02-18 21:51:25 +0000

    query can't be null
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@82364 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/QueryMapController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/QueryMapController.java
@@ -19,6 +19,6 @@ public class QueryMapController {
     @ResponseBody
     public QueryMap getQueryMap(@RequestParam String q) {
         QueryMap queryMap = this.queryMapper.getQueryMap(q);
-        return new QueryMap(null, null, queryMap.getResourceMap(), null, null);
+        return new QueryMap(q, null, queryMap.getResourceMap(), null, null);
     }
 }
