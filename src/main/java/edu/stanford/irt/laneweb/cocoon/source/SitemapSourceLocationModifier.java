f20a2bc5354176a026eb8b7d1b95436dc2547d3f
Charles Yates <ceyates@stanford.edu>
2011-03-03 00:14:33 +0000

    add deprecation warning logging to old forms of cocoon:/ urls
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@82767 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cocoon/source/SitemapSourceLocationModifier.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/source/SitemapSourceLocationModifier.java
@@ -1,8 +1,24 @@
 package edu.stanford.irt.laneweb.cocoon.source;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SitemapSourceLocationModifier {
+    
+    private final Logger log = LoggerFactory.getLogger(SitemapSourceLocationModifier.class);
 
     public String modify(final String location) {
+        String modifiedLocation = getModifiedLocation(location);
+        if (modifiedLocation != null) {
+            if (log.isWarnEnabled()) {
+                log.warn("the url form: " + location + "is deprecated, use the form: " + modifiedLocation + " instead.");
+            }
+            return modifiedLocation;
+        }
+        return location;
+    }
+    
+    private String getModifiedLocation(String location) {
         if (location.indexOf("cocoon://eresources") == 0) {
             return "eresources:" + location.substring("cocoon://eresources".length());
         }
@@ -21,6 +37,6 @@ public class SitemapSourceLocationModifier {
         if (location.indexOf("cocoon:") == 0) {
             return "content:" + location.substring("cocoon:".length());
         }
-        return location;
+        return null;
     }
 }
