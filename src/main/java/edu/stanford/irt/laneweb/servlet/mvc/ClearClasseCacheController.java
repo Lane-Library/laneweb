bc0714e0270f773601f7cf0b6cecf15f1a9ec8be
Alain Boussard <alainb@stanford.edu>
2011-03-11 00:35:50 +0000

    to redirect to the other server
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@83102 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ClearClasseCacheController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ClearClasseCacheController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,7 +19,7 @@ import edu.stanford.irt.laneweb.model.Model;
 
 @Controller
 public class ClearClasseCacheController {
-    
+
     @Autowired
     private ServletContext servletContext;
 
@@ -26,38 +27,52 @@ public class ClearClasseCacheController {
     private Cache cache;
 
     @RequestMapping(value = "/secure/classes/reload.html")
-    public void clearClassCache(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        String contentBase = request.getAttribute(Model.CONTENT_BASE).toString();
+    public void clearClassCache(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        resetCache(request.getAttribute(Model.CONTENT_BASE).toString());
+        Cookie[] cookies = request.getCookies();
+        for (int i = 0; i < cookies.length; i++) {
+            Cookie cookie = cookies[i];
+            String cookieValue = cookie.getValue();
+            if (cookieValue.endsWith("JSESSIONID")) {
+                if (cookieValue.endsWith("tc-laneweb-08"))
+                    cookieValue = cookieValue.replace("tc-laneweb-08", "tc-laneweb-07");
+                else
+                    cookieValue = cookieValue.replace("tc-laneweb-07", "tc-laneweb-08");
+                cookie.setValue(cookieValue);
+                response.addCookie(cookie);
+            }
+        }
+        response.sendRedirect("https://".concat(request.getServerName()).concat("/secure/classes/reloadAll.html"));
+    }
+
+    @RequestMapping(value = "/secure/classes/reloadAll.html")
+    public void clearAllClassCache(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        resetCache(request.getAttribute(Model.CONTENT_BASE).toString());
+        this.servletContext.getRequestDispatcher("/classes-consult/laneclasses.html").forward(request, response);
+    }
+
+    private void resetCache(String contentBase) {
         PipelineCacheKey key = new PipelineCacheKey();
         key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes-aggregator",
                 "apps:/classes/onlineregistrationcenter.xml"));
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "saxon",
+                "jndi:/localhost/resources/xsl/laneclasses-lastmodified.xsl"));
         this.cache.remove(key);
         key = new PipelineCacheKey();
         key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes",
                 "http://onlineregistrationcenter.com/registerlistxml.asp?p=2&dsd=0&s=4&m=257"));
         this.cache.remove(key);
         key = new PipelineCacheKey();
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "html", contentBase
-                .concat("/includes/classes-thisweek.html")));
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "xinclude", "XInclude"));
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes-aggregator",
+                "apps:/classes/all-onlineregistrationcenter.xml"));
         key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "saxon",
-                "jndi:/localhost/resources/xsl/rss2html.xsl"));
+                "jndi:/localhost/resources/xsl/laneclasses-lastmodified.xsl"));
         this.cache.remove(key);
         key = new PipelineCacheKey();
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "html", contentBase
-                .concat("/classes-consult/laneclasses.html")));
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "xinclude", "XInclude"));
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "saxon",
-                "jndi:/localhost/resources/xsl/rss2html.xsl"));
-        this.cache.remove(key);
-        key = new PipelineCacheKey();
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes", "apps:/classes/data.xml"));
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "saxon",
-                "jndi:/localhost/resources/xsl/classes-thisweek.xsl"));
-        key.addKey(new ComponentCacheKey(
-                ComponentCacheKey.ComponentType_Serializer,
-                "xhtml",
-                ";doctype-public=-//W3C//DTD XHTML 1.0 Strict//EN;doctype-system=http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd;encoding=UTF-8;indent=yes;omit-xml-declaration=yes"));
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes",
+                "http://onlineregistrationcenter.com/registerlistxml.asp?p=2&dsd=0&s=4&m=257&amp;dall=1"));
         this.cache.remove(key);
         key = new PipelineCacheKey();
         key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes", "apps:/classes/data.xml"));
@@ -65,16 +80,19 @@ public class ClearClasseCacheController {
                 "jndi:/localhost/resources/xsl/laneclasses.xsl"));
         this.cache.remove(key);
         key = new PipelineCacheKey();
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "html", contentBase
+                .concat("/classes-consult/laneclasses.html")));
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "xinclude", "XInclude"));
+        this.cache.remove(key);
+        key = new PipelineCacheKey();
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "html", contentBase
+                .concat("/includes/classes-thisweek.html")));
+        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "xinclude", "XInclude"));
+        this.cache.remove(key);
+        key = new PipelineCacheKey();
         key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "classes", "apps:/classes/data.xml"));
         key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "saxon",
                 "jndi:/localhost/resources/xsl/classes-thisweek.xsl"));
         this.cache.remove(key);
-        key = new PipelineCacheKey();
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Generator, "html", contentBase.concat("index.html")));
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "xinclude", "XInclude"));
-        key.addKey(new ComponentCacheKey(ComponentCacheKey.ComponentType_Transformer, "saxon",
-                "jndi:/localhost/resources/xsl/rss2html.xsl"));
-        this.cache.remove(key);
-        this.servletContext.getRequestDispatcher("/classes-consult/laneclasses.html").forward(request, response);
     }
 }
