dd60c06e78a3d7c4c06db2de48bfc8ed91f7119a
Charles Yates <ceyates@stanford.edu>
2011-03-17 16:18:02 +0000

    LANE.Search may be undefined
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@83291 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-search-history.js
+++ src/main/webapp/resources/javascript/lane-search-history.js
@@ -1,22 +1,26 @@
 (function() {
-    var Y = LANE.Y,
-        history,
-        searchFacets = Y.one('#searchFacets');
-    LANE.Search.History = function(){
-        if(searchFacets){
-            history = new Y.HistoryHash();        
-            if(history.get('facet')){
-                LANE.search.facets.setActiveFacet(history.get('facet'));
-            }
-            history.on("facetChange",function(e) {
-                LANE.search.facets.setActiveFacet(e.newVal);
-            });
-            history.on("facetRemove",function(e) {
-                LANE.search.facets.setActiveFacet(LANE.Search.getSearchSource());
-            });
-        }
-        return history;
-        
-    }();
-        
+	//LANE.Search may be undefined
+	if (LANE.Search) {
+		var Y = LANE.Y,
+		    history,
+		    searchFacets = Y.one('#searchFacets');
+		
+		LANE.Search.History = function() {
+			if (searchFacets) {
+				history = new Y.HistoryHash();
+				if (history.get('facet')) {
+					LANE.search.facets.setActiveFacet(history.get('facet'));
+				}
+				history.on("facetChange", function(e) {
+					LANE.search.facets.setActiveFacet(e.newVal);
+				});
+				history.on("facetRemove", function(e) {
+					LANE.search.facets.setActiveFacet(LANE.Search.getSearchSource());
+				});
+			}
+			return history;
+		}();
+
+	}
+
 })();
