833e9d380cb578986bd15b6c7dbd9372ce3dbc47
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-03-19 17:42:36 +0000

    missing @ResponseBody; fixed output
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@83396 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ExtensionsSuggestController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ExtensionsSuggestController.java
@@ -1,35 +1,42 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
+import edu.stanford.irt.suggest.Suggestion;
+import edu.stanford.irt.suggest.SuggestionManager;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import edu.stanford.irt.suggest.Suggestion;
-import edu.stanford.irt.suggest.SuggestionManager;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ExtensionsSuggestController {
+
+    private static final int RETURN_LIMIT = 10;
 
     @Resource(name = "edu.stanford.irt.suggest.SuggestionManager/extensions-suggest")
     private SuggestionManager suggestionManager;
 
     @RequestMapping(value = "/eresources/extensions-suggest")
-    public Collection<Suggestion> getSuggestions(@RequestParam String q) {
+    @ResponseBody
+    public List<Object> getSuggestions(@RequestParam final String q) {
         String query = q;
-        if ((query.indexOf('\'') > -1) || (query.indexOf('"') > -1)) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < query.length(); i++) {
-                char c = query.charAt(i);
-                if (('\'' != c) && ('"' != c)) {
-                    sb.append(c);
-                }
+        Collection<String> suggestions = new LinkedList<String>();
+        List<Object> objectList = new ArrayList<Object>();
+        for (Suggestion suggestion : this.suggestionManager.getSuggestionsForTerm(query)) {
+            suggestions.add(suggestion.getSuggestionTitle());
+            if (suggestions.size() >= RETURN_LIMIT) {
+                break;
             }
-            query = sb.toString();
         }
-        return this.suggestionManager.getSuggestionsForTerm(query);
+        objectList.add(query);
+        objectList.add(suggestions);
+        return objectList;
     }
 }
