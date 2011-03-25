336525626f58640dda37833e6a21f62ab94c7a03
Charles Yates <ceyates@stanford.edu>
2011-03-25 17:16:40 +0000

    copy XIncludePipeTest from 1.11 branch
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@83563 c440208b-5602-0410-a095-92527a996296

---
+++ src/test/java/edu/stanford/irt/laneweb/cocoon/XIncludePipeTest.java
@@ -0,0 +1,52 @@
+package edu.stanford.irt.laneweb.cocoon;
+
+import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.components.xpointer.parser.ParseException;
+import org.apache.cocoon.environment.SourceResolver;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+public class XIncludePipeTest {
+	
+	private XIncludePipe pipe;
+	
+	private SourceResolver sourceResolver;
+	
+	private Attributes attributes;
+	
+	@Before
+	public void setUp() {
+		this.sourceResolver = createMock(SourceResolver.class);
+		this.pipe = new XIncludePipe(this.sourceResolver, null, null, null);
+		this.attributes = createMock(Attributes.class);
+	}
+
+	//This test demonstrated a bug where an exception occurred during include processing but
+	//before the try/catch block incrementing the fallback state.
+	@Test
+	public void testStartXIncludeElement() throws MalformedURLException, IOException, SAXException {
+		expect(this.attributes.getValue("http://www.w3.org/XML/1998/namespace", "base")).andReturn(null);
+		expect(this.attributes.getValue("", "href")).andReturn("foo");
+		expect(this.attributes.getValue("", "parse")).andReturn(null);
+		expect(this.attributes.getValue("", "xpointer")).andReturn(null);
+		expect(this.sourceResolver.resolveURI("foo")).andThrow(new RuntimeException("oopsie"));
+		replay(this.sourceResolver, this.attributes);
+		//must call init() to create xmlBaseSupport object
+		this.pipe.init(null, null);
+		try {
+			this.pipe.startElement("http://www.w3.org/2001/XInclude", "include", "xi:include", this.attributes);
+		} catch (Exception e) {
+			fail("should catch all exceptions");
+		}
+		verify(this.sourceResolver, this.attributes);
+	}
+
+}
