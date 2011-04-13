0d96d1f6ca816b28542d608b849e91b386327b2e
Charles Yates <ceyates@stanford.edu>
2011-04-13 15:21:09 +0000

    simplifying google tracking events
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@84267 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-google.js
+++ src/main/webapp/resources/javascript/lane-google.js
@@ -37,15 +37,11 @@
         
         // for search result event tracking
     Y.publish("lane:searchResultClick",{
-        broadcast:1,
-        emitFacade: true,
         searchTerms:null,
         resultTitle:null,
         resultPosition:null
     });
     Y.publish("lane:browseResultClick",{
-//        broadcast:1,
-        emitFacade: true,
         resultTitle:null,
         resultPosition:null
     });
