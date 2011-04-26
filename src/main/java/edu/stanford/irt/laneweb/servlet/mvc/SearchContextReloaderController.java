8808cae8a1fac5ae11db199b6ba4201f990f605e
Alain Boussard <alainb@stanford.edu>
2011-04-26 23:00:18 +0000

    clean-up
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@84756 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/SearchContextReloaderController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/SearchContextReloaderController.java
@@ -1,12 +1,10 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.stanford.irt.laneweb.search.MetaSearchManagerSource;
@@ -25,20 +23,15 @@ public class SearchContextReloaderController {
 
     private String svnUrlProject = "https://irt-svn.stanford.edu/repos/irt/lane/search/tags/search-lane-";
 
-    
-    @RequestMapping(method=RequestMethod.POST)
-    public ModelAndView reloadContext(@RequestParam final String release, @RequestParam final String sunetid, @RequestParam final String password) {
-      if(!"".equals(release) && !"".equals(sunetid) && !"".equals(password))
-          this.msms.reload(this.svnUrlProject.concat(release).concat(this.svnUrlPath), sunetid, password);
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView reloadContext(@RequestParam final String release, @RequestParam final String sunetid,
+            @RequestParam final String password) {
+        if (!"".equals(release) && !"".equals(sunetid) && !"".equals(password)) {
+            this.msms.reload(this.svnUrlProject.concat(release).concat(this.svnUrlPath), sunetid, password);
+        }
         return new ModelAndView("/reloadresources.html");
     }
-    
-    @RequestMapping(method=RequestMethod.GET)
-    public ModelAndView view() {
-        return new ModelAndView("/reloadresources.html");
-    }
-    
-    
+
     public void setMetaSearchManagerSource(final MetaSearchManagerSource msms) {
         this.msms = msms;
     }
@@ -49,5 +42,10 @@ public class SearchContextReloaderController {
 
     public void setSvnUrlProject(final String svnUrlProject) {
         this.svnUrlProject = svnUrlProject;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView view() {
+        return new ModelAndView("/reloadresources.html");
     }
 }
