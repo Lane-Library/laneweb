5bf05ebd2f9b298ebb794e7eb5e350579f1fe925
Charles Yates <ceyates@stanford.edu>
2011-05-03 16:32:10 +0000

    refactor lightbox
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@84990 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-lightbox.js
+++ src/main/webapp/resources/javascript/lane-lightbox.js
@@ -2,40 +2,23 @@
 
     var LightboxBg = Y.Base.create("lightboxbg", Y.Widget, []);
 
-    Y.lane.LightboxBg = new LightboxBg({
-        visible : false,
-        render : true
-    });
-
     var Lightbox = Y.Base.create("lightbox", Y.Widget, [ Y.WidgetPosition, Y.WidgetPositionAlign, Y.WidgetPositionConstrain ], {
-            setContent : function(content) {
-                this.get("contentBox").set("innerHTML", content);
-                this.fire("contentChanged");
-                this.centered();
+        bindUI : function () {
+            this.on("visibleChange", this._onVisibleChange);
+            this.after("visibleChange", this._afterVisibleChange);
+        },
+        setContent : function(content) {
+            this.get("contentBox").set("innerHTML", content);
+            this.fire("contentChanged");
+            this.centered();
+        },
+        _afterVisibleChange : function(event) {
+            if (!event.newVal) {
+            	//TODO: make the background a property of Lightbox
+                Y.lane.LightboxBg.hide();
             }
-    });
-
-    Y.lane.Lightbox = new Lightbox({
-        visible : false,
-        render : true,
-        constrain : true
-    });  
-    
-    Y.lane.Lightbox.get("boundingBox").append("<a id='lightboxClose'></a>");
-    Y.lane.Lightbox.get("boundingBox").one("#lightboxClose").on("click", function(event) {
-        event.preventDefault();
-      Y.lane.Lightbox.hide();
-    });
-    
-    Y.lane.Lightbox.after("visibleChange", function(event) {
-        if (!event.newVal) {
-            Y.lane.LightboxBg.hide();
-        }
-    });
-    
-    Y.lane.Lightbox.on("visibleChange", function(event) {
-        if (event.newVal) {
-            Y.lane.LightboxBg.show();
+        },
+        _animate : function() {
             var boundingBox = this.get("boundingBox"),
                 width = boundingBox.get("clientWidth"),
                 height = boundingBox.get("clientHeight"),
@@ -55,23 +38,29 @@
                     to : {left:0,top:0}
                 });
             anim1.on("end", function() {
-            	boundingBox.setStyle("overflow", "visible");
-            	boundingBox.setStyle("width","auto");
-            	boundingBox.setStyle("height","auto");
+                boundingBox.setStyle("overflow", "visible");
+                boundingBox.setStyle("width","auto");
+                boundingBox.setStyle("height","auto");
             });
             boundingBox.setStyle("overflow", "hidden");
-        	boundingBox.setStyle("width", "0");
-        	boundingBox.setStyle("height", "0");
+            boundingBox.setStyle("width", "0");
+            boundingBox.setStyle("height", "0");
             anim1.run();
             anim2.run();
+        },
+        _onVisibleChange : function(event) {
+            if (event.newVal) {
+                Y.lane.LightboxBg.show();
+                this._animate();
+            }
         }
-    });
-    
+    }); 
+
 
     Y.on("click", function(event) {
-        var args, href, popupElement, title, body, regex, anchor = event.target
-                .ancestor("a")
-                || event.target, rel = anchor.get("rel");
+        var args, href, popupElement, title, body, regex,
+            anchor = event.target.ancestor("a") || event.target,
+            rel = anchor.get("rel");
         if (rel && rel.indexOf("lightbox") === 0) {
             event.preventDefault();
             // need to dynamically create regex for getting /plain url because
@@ -79,12 +68,33 @@
             regex = new RegExp("(.+)//([^/]+)(/././)(.+)".replace(/\//g, "\\\/"));
             href = anchor.get("href").replace(regex, "$1//$2$3plain/$4");
             Y.io(href, {
-            	//TODO: failure handler use window.location
                 on : {
                     success : function(id, o, args) {
+                        //TODO: put more of this initialization into the Lightbox object
+                        if (!Y.lane.Lightbox) {
+                            Y.lane.Lightbox = new Lightbox({
+                                visible : false,
+                                constrain : true,
+                                render : true
+                            });
+
+                            Y.lane.LightboxBg = new LightboxBg({
+                                visible : false,
+                                render : true
+                            });
+
+                            Y.lane.Lightbox.get("boundingBox").append("<a id='lightboxClose'></a>");
+                            Y.lane.Lightbox.get("boundingBox").one("#lightboxClose").on("click", function(event) {
+                                event.preventDefault();
+                                Y.lane.Lightbox.hide();
+                            });
+                        }
                         var lightbox = Y.lane.Lightbox;
                         lightbox.setContent(o.responseText);
                         lightbox.show();
+                    },
+                    failure : function() {
+                        window.location = anchor.get("href");
                     }
                 }
             });
