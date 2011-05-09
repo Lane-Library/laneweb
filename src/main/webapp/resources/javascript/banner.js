297ed91e456b472108026b1f0b1f42202db67baf
Charles Yates <ceyates@stanford.edu>
2011-05-09 21:00:59 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@85258 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/banner.js
+++ src/main/webapp/resources/javascript/banner.js
@@ -2,21 +2,37 @@
  * 
  */
 Y.lane.Banner = Y.Base.create("banner", Y.Widget, [], {
+    autoNext : function() {
+    	var self = this;
+    	var recursion = function() {
+    		if (self.get("automate")) {
+        		self.next();
+        		self.autoNext();
+    		}
+    	};
+    	window.setTimeout(recursion, 5000);
+    },
 	bindUI : function() {
-		this.get("contentBox").all(".banner-nav a").on("click", this._handleNavClick, this);
+		this.get("navNodes").on("click", this._handleNavClick, this);
+		this.on("indexChange", this._handleIndexChange);
+		this.autoNext();
+	},
+	next : function() {
+		var index = this.get("index"),
+		    navNodes = this.get("navNodes"),
+		    next = (index + 1) >= navNodes.size() ? 0 : index + 1;
+		this.set("index", next);
 	},
 	setNewContent : function(imgSrc, content) {
 		var contentBox = this.get("contentBox");
 		contentBox.one("img").set("src", imgSrc);
 		contentBox.one(".banner-content").set("innerHTML", content);
 	},
-	setNewImage : function(src) {
-	},
-    _handleNavClick : function(event) {
-    	event.preventDefault();
-    	this.get("contentBox").all(".banner-nav a").removeClass("banner-nav-active");
-    	event.target.addClass("banner-nav-active");
-        Y.io("/plain/samples/banner/banners.html" + event.target.getAttribute("href"), {
+	_handleIndexChange : function(event) {
+		var navNodes = this.get("navNodes");
+		navNodes.item(event.prevVal).removeClass("banner-nav-active");
+		navNodes.item(event.newVal).addClass("banner-nav-active");
+        Y.io("/plain/samples/banner/banners.html?banner=" + (event.newVal + 1), {
             on : {
                 success : function(id, o, args) {
                 	var fragment = Y.Node.create(o.responseText),
@@ -31,12 +47,30 @@ Y.lane.Banner = Y.Base.create("banner", Y.Widget, [], {
             },
             context : this
         });
+	},
+    _handleNavClick : function(event) {
+    	event.preventDefault();
+    	this.set("automate", false);
+    	this.set("index", this.get("navNodes").indexOf(event.target));
     }
 });
+
+Y.lane.Banner.ATTRS = {
+	automate : {},
+	index : {
+		value : 0
+	},
+	navNodes : {
+		valueFn : function() {
+			return this.get("contentBox").all(".banner-nav a");
+		}
+	}
+};
 
 if (Y.one("#banner")) {
 	var banner = new Y.lane.Banner({
 		srcNode : "#banner",
-		render : true
+		render : true,
+		automate : true
 	});
 }
