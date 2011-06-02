7c30178e9aa48544235d17e52979598b7ebb5816
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-06-02 20:55:42 +0000

    name change for mobile interface
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@86200 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-mobile-ad.js
+++ src/main/webapp/resources/javascript/lane-mobile-ad.js
@@ -4,7 +4,7 @@
     if(navigator.userAgent.match(/(iPhone|iP.d|Android)/) && !location.search.match(/mad=0/)){
         device = RegExp.$1;
         id = (device == 'iPad') ? 'ipadAd' : 'mphoneAd';
-        advert = Y.Node.create('<div id="'+id+'"><div><div>Try <a href="/././m/">Express Lane</a> for your '+device+' &raquo;</div></div></div>');
+        advert = Y.Node.create('<div id="'+id+'"><div><div>Try <a href="/././m/">Lane Mobile</a> for your '+device+' &raquo;</div></div></div>');
         Y.one("#custom-doc").insert(advert);
         advert.on("click",function(){
             window.location.href = "/././m/";
