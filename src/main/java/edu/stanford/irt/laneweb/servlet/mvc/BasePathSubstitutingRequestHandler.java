f5ebef318929882f4c29f11738da257d839d5b0f
Charles Yates <ceyates@stanford.edu>
2011-06-23 22:16:48 +0000

    Cache substituted resources
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@87311 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/BasePathSubstitutingRequestHandler.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/BasePathSubstitutingRequestHandler.java
@@ -1,5 +1,7 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +11,10 @@ import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.caching.Cache;
+import org.apache.cocoon.caching.CachedResponse;
+import org.apache.excalibur.source.SourceValidity;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
@@ -18,19 +24,19 @@ import edu.stanford.irt.laneweb.util.BasePathSubstitutingInputStream;
 
 public class BasePathSubstitutingRequestHandler extends ResourceHttpRequestHandler {
 
-    private static class BasePathSubstitutingResource implements Resource {
+    private static class CachedResponseResource implements Resource {
 
-        private String basePath;
+        private CachedResponse cachedResponse;
 
-        private Resource resource;
+        protected Resource resource;
 
-        public BasePathSubstitutingResource(final String basePath, final Resource resource) {
-            this.basePath = basePath;
+        public CachedResponseResource(final CachedResponse cachedResponse, final Resource resource) {
+            this.cachedResponse = cachedResponse;
             this.resource = resource;
         }
 
         public long contentLength() throws IOException {
-            throw new IOException("can't determine length before substituting");
+            return this.cachedResponse.getResponse().length;
         }
 
         public Resource createRelative(final String relativePath) throws IOException {
@@ -53,9 +59,8 @@ public class BasePathSubstitutingRequestHandler extends ResourceHttpRequestHandl
             return this.resource.getFilename();
         }
 
-        @Override
         public InputStream getInputStream() throws IOException {
-            return new BasePathSubstitutingInputStream(this.resource.getInputStream(), this.basePath);
+            return new ByteArrayInputStream(this.cachedResponse.getResponse());
         }
 
         public URI getURI() throws IOException {
@@ -79,12 +84,39 @@ public class BasePathSubstitutingRequestHandler extends ResourceHttpRequestHandl
         }
     }
 
+    private Cache cache;
+    
+    public void setCache(final Cache cache) {
+        this.cache = cache;
+    }
+
     @Override
     protected Resource getResource(final HttpServletRequest request) {
         Resource resource = super.getResource(request);
         if (resource != null) {
-            String basePath = (String) request.getAttribute(Model.BASE_PATH);
-            return new BasePathSubstitutingResource(basePath, super.getResource(request));
+            try {
+                String basePath = (String) request.getAttribute(Model.BASE_PATH);
+                String cacheKey = basePath + ":" + resource.getURI();
+                long lastModified = resource.lastModified();
+                CachedResponse cachedResponse = this.cache.get(cacheKey);
+                if (cachedResponse == null || lastModified > cachedResponse.getLastModified()) {
+                    InputStream input = new BasePathSubstitutingInputStream(resource.getInputStream(), basePath);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int i;
+                    while ((i = input.read()) != -1) {
+                        baos.write(i);
+                    }
+                    input.close();
+                    baos.close();
+                    cachedResponse = new CachedResponse((SourceValidity[]) null, baos.toByteArray());
+                    this.cache.store(cacheKey, cachedResponse);
+                }
+                return new CachedResponseResource(cachedResponse, resource);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ProcessingException e) {
+                throw new RuntimeException(e);
+            }
         } else {
             return null;
         }
