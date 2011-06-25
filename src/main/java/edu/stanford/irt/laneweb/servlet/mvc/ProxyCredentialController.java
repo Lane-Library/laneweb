2612b18f80018d872a52837ea172a18e2d019a42
Charles Yates <ceyates@stanford.edu>
2011-06-25 18:05:28 +0000

    handle /apps/proxy/credential, fix problem with @SessionAttributes being null
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@87396 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ProxyCredentialController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ProxyCredentialController.java
@@ -5,18 +5,17 @@ import java.io.IOException;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import edu.stanford.irt.laneweb.model.Model;
 import edu.stanford.irt.laneweb.proxy.Ticket;
 import edu.stanford.irt.laneweb.servlet.SunetIdSource;
 
 @Controller
-@SessionAttributes({ Model.TICKET, Model.SUNETID })
 public class ProxyCredentialController {
 
     private static final String PROXY_URL_BASE = "http://laneproxy.stanford.edu/login?user=";
@@ -34,12 +33,32 @@ public class ProxyCredentialController {
     }
 
     @ModelAttribute(Model.TICKET)
-    public Ticket getTicket(@ModelAttribute(Model.SUNETID) final String sunetid) {
-        return new Ticket(sunetid, this.ezproxyKey);
+    public Ticket getTicket(@ModelAttribute(Model.SUNETID) final String sunetid, final HttpSession session) {
+        Ticket ticket = null;
+        if (sunetid != null) {
+            ticket = (Ticket) session.getAttribute(Model.TICKET);
+            if (ticket == null || !ticket.isValid()) {
+                ticket = new Ticket(sunetid, this.ezproxyKey);
+                session.setAttribute(Model.TICKET, ticket);
+            }
+        }
+        return ticket;
+    }
+
+    @RequestMapping(value = "**/apps/proxy/credential")
+    public void proxyRedirect(final HttpServletResponse response, final HttpServletRequest request,
+            @ModelAttribute(Model.SUNETID) final String sunetid, @ModelAttribute(Model.TICKET) final Ticket ticket)
+            throws IOException {
+        String queryString = request.getQueryString();
+        if (sunetid == null || ticket == null) {
+            response.sendRedirect("/secure/apps/proxy/credential?" + queryString);
+        } else {
+            secureProxyRedirect(response, request, sunetid, ticket);
+        }
     }
 
     @RequestMapping(value = "**/secure/apps/proxy/credential")
-    public void proxyRedirect(final HttpServletResponse response, final HttpServletRequest request,
+    public void secureProxyRedirect(final HttpServletResponse response, final HttpServletRequest request,
             @ModelAttribute(Model.SUNETID) final String sunetid, @ModelAttribute(Model.TICKET) final Ticket ticket)
             throws IOException {
         String queryString = request.getQueryString();
