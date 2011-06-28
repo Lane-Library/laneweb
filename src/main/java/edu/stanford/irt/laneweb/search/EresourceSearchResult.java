09acf1b5dd360d5f136302a1ccc2722e4a85258b
Charles Yates <ceyates@stanford.edu>
2011-06-28 15:52:21 +0000

    refactor compareTo method to handle edge case 52343
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@87557 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/search/EresourceSearchResult.java
+++ src/main/java/edu/stanford/irt/laneweb/search/EresourceSearchResult.java
@@ -16,18 +16,24 @@ public class EresourceSearchResult extends EresourceResource implements SearchRe
         this.sortTitle = this.sortTitle.toLowerCase().replaceAll("\\W", "");
     }
 
-    public int compareTo(final SearchResult o) {
-        int scoreCmp = o.getScore() - this.eresource.getScore();
-        return (scoreCmp != 0 ? scoreCmp : this.sortTitle.compareTo(o.getSortTitle()));
+    public int compareTo(final SearchResult other) {
+        int scoreCmp = other.getScore() - this.eresource.getScore();
+        if (scoreCmp == 0) {
+            scoreCmp = this.sortTitle.compareTo(other.getSortTitle());
+            if (scoreCmp == 0 && !other.equals(this)) {
+                //This happens when more than one eresource has the same title as the query
+                scoreCmp = -1;
+            }
+        }
+        return (scoreCmp != 0 ? scoreCmp : this.sortTitle.compareTo(other.getSortTitle()));
     }
 
     @Override
     public boolean equals(final Object other) {
-        if (!(other instanceof EresourceSearchResult)) {
-            return false;
+        if (other instanceof EresourceSearchResult) {
+            return ((EresourceSearchResult) other).eresource.equals(this.eresource);
         }
-        EresourceSearchResult eres = (EresourceSearchResult) other;
-        return eres.sortTitle.equals(this.sortTitle);
+        return false;
     }
 
     public int getScore() {
