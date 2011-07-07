b14fdf527c13e806d6f6bbac9a910ea737845766
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-07-07 21:06:13 +0000

    mobile redirects: https://irt-bugs.stanford.edu/default.asp?52750
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@87960 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-mobile-ad.js
+++ src/main/webapp/resources/javascript/lane-mobile-ad.js
@@ -1,13 +1,14 @@
 // advertise existence of mobile interface to iPad, iPhone, iPod and Android clients
 (function() {
-    var advert, id, device;
+    var advert, id, device, link;
     if(navigator.userAgent.match(/(iPhone|iP.d|Android)/) && !location.search.match(/mad=0/)){
         device = RegExp.$1;
         id = (device == 'iPad') ? 'ipadAd' : 'mphoneAd';
-        advert = Y.Node.create('<div id="'+id+'"><div><div>Try <a href="/././m/">Lane Mobile</a> for your '+device+' &raquo;</div></div></div>');
+        link = '/././index.html?site_preference=mobile';
+        advert = Y.Node.create('<div id="'+id+'"><div><div>Try <a href="'+link+'">Lane Mobile</a> for your '+device+' &raquo;</div></div></div>');
         Y.one("#custom-doc").insert(advert);
         advert.on("click",function(){
-            window.location.href = "/././m/";
+            window.location.href = link;
         });
     }
 })();
