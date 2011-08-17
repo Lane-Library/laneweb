195e1a7aa40282f651d33ac72147e4493af76f04
Charles Yates <ceyates@stanford.edu>
2011-08-17 03:53:18 +0000

    implement equals() and hashCode()
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@89892 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/bookmarks/Bookmark.java
+++ src/main/java/edu/stanford/irt/laneweb/bookmarks/Bookmark.java
@@ -4,44 +4,66 @@ import java.io.Serializable;
 
 public class Bookmark implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String label;
+	private String label;
 
-    private String url;
+	private String url;
 
-    protected Bookmark() {
-    }
+	protected Bookmark() {
+	}
 
-    public Bookmark(final String label, final String url) {
-        this.label = label;
-        this.url = url;
-    }
+	public Bookmark(final String label, final String url) {
+		this.label = label;
+		this.url = url;
+	}
 
-    public String getLabel() {
-        return this.label;
-    }
+	public String getLabel() {
+		return this.label;
+	}
 
-    public String getUrl() {
-        return this.url;
-    }
+	public String getUrl() {
+		return this.url;
+	}
 
-    public void setLabel(final String label) {
-        this.label = label;
-    }
+	public void setLabel(final String label) {
+		this.label = label;
+	}
 
-    public void setUrl(final String url) {
-        this.url = url;
-    }
-    
-    public boolean equals(Bookmark other) {
-        if (this.label == null && this.url == null) {
-            return other.label == null && other.url == null;
-        } else if (this.label == null) {
-            return other.label == null && this.url.equals(other.url);
-        } else if (this.url == null) {
-            return other.url == null && this.label.equals(other.label);
-        }
-        return this.label.equals(other.label) && this.url.equals(other.label);
-    }
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+	
+	@Override
+	public int hashCode() {
+		int hashcode;
+		if (this.label != null && this.url != null) {
+			hashcode = this.label.hashCode() + this.url.hashCode();
+		} else if (this.label != null) {
+			hashcode = this.label.hashCode();
+		} else if (this.url != null) {
+			hashcode = this.url.hashCode();
+		} else {
+			hashcode = super.hashCode();
+		}
+		return hashcode;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		boolean equals = false;
+		if (other instanceof Bookmark) {
+			Bookmark bookmark = (Bookmark) other;
+			if (this.label == null && this.url == null) {
+				equals = bookmark.label == null && bookmark.url == null;
+			} else if (this.label == null) {
+				equals = bookmark.label == null && this.url.equals(bookmark.url);
+			} else if (this.url == null) {
+				equals = bookmark.url == null && this.label.equals(bookmark.label);
+			} else {
+				equals = this.label.equals(bookmark.label) && this.url.equals(bookmark.url);
+			}
+		}
+		return equals;
+	}
 }
