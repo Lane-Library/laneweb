c7ca36fa183873cae13f43e8aab047631c5a4444
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-09-26 22:31:45 +0000

    extend grace period to 3 days: user will be notified that cookie is about to expire if visits w/in 3 days of Lane user cookie expiration
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@91748 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginCookieValidatorController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginCookieValidatorController.java
@@ -14,15 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Checks the validity of the Lane user cookie AND if cookie will still be valid one day from now. Used to provide an
- * alert to mobile users when their cookie is about to expire.
+ * Checks the validity of the Lane user cookie AND if cookie will still be valid GRACE_PERIOD from now. Used to provide
+ * an alert to mobile users when their cookie is about to expire.
  * 
  * @author ryanmax
  */
 @Controller
 public class PersistentLoginCookieValidatorController {
 
-    private static long ONE_DAY = 1000 * 60 * 60 * 24 * 1;
+    private static long GRACE_PERIOD = 1000 * 60 * 60 * 24 * 3; // 3 days
 
     private static final String USER_AGENT_HEADER = "User-Agent";
 
@@ -32,20 +32,20 @@ public class PersistentLoginCookieValidatorController {
 
     private boolean valid;
 
-    private boolean validTomorrow;
+    private boolean validDuringGracePeriod;
 
     @RequestMapping(value = "**/apps/loginCookieValidator")
     public void validateCookie(final HttpServletRequest request, final HttpServletResponse response,
             @RequestParam(required = false) final String callback) throws IOException {
         this.valid = false;
-        this.validTomorrow = false;
+        this.validDuringGracePeriod = false;
         this.userAgent = request.getHeader(USER_AGENT_HEADER);
         for (Cookie cookie : request.getCookies()) {
             if (SunetIdCookieCodec.LANE_COOKIE_NAME.equals(cookie.getName())) {
                 PersistentLoginToken token = this.codec.restoreLoginToken(cookie.getValue());
                 if (token.isValidFor(System.currentTimeMillis(), this.userAgent.hashCode())) {
                     this.valid = true;
-                    this.validTomorrow = token.isValidFor(System.currentTimeMillis() + ONE_DAY,
+                    this.validDuringGracePeriod = token.isValidFor(System.currentTimeMillis() + GRACE_PERIOD,
                             this.userAgent.hashCode());
                 }
                 break;
@@ -53,9 +53,9 @@ public class PersistentLoginCookieValidatorController {
         }
         response.setHeader("Content-Type", "application/x-javascript");
         if (callback != null) {
-            response.getWriter().write(callback + "(" + this.valid + "," + this.validTomorrow + ");");
+            response.getWriter().write(callback + "(" + this.valid + "," + this.validDuringGracePeriod + ");");
         } else {
-            response.getWriter().write(this.valid + "," + this.validTomorrow);
+            response.getWriter().write(this.valid + "," + this.validDuringGracePeriod);
         }
     }
 }
