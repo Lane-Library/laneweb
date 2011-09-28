eedb9fa594fb062815e1d4f0722d391db76e3e51
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-09-28 00:09:40 +0000

    sonar fixes
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@91818 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginCookieValidatorController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginCookieValidatorController.java
@@ -22,40 +22,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PersistentLoginCookieValidatorController {
 
-    private static long GRACE_PERIOD = 1000 * 60 * 60 * 24 * 3; // 3 days
+    private static final long GRACE_PERIOD = 1000 * 60 * 60 * 24 * 3; // 3 days
 
     private static final String USER_AGENT_HEADER = "User-Agent";
 
     private SunetIdCookieCodec codec = new SunetIdCookieCodec();
 
-    private String userAgent;
-
-    private boolean valid;
-
-    private boolean validDuringGracePeriod;
-
     @RequestMapping(value = "**/apps/loginCookieValidator")
     public void validateCookie(final HttpServletRequest request, final HttpServletResponse response,
             @RequestParam(required = false) final String callback) throws IOException {
-        this.valid = false;
-        this.validDuringGracePeriod = false;
-        this.userAgent = request.getHeader(USER_AGENT_HEADER);
+        boolean valid = false;
+        boolean validDuringGracePeriod = false;
+        String userAgent = request.getHeader(USER_AGENT_HEADER);
         for (Cookie cookie : request.getCookies()) {
             if (SunetIdCookieCodec.LANE_COOKIE_NAME.equals(cookie.getName())) {
                 PersistentLoginToken token = this.codec.restoreLoginToken(cookie.getValue());
-                if (token.isValidFor(System.currentTimeMillis(), this.userAgent.hashCode())) {
-                    this.valid = true;
-                    this.validDuringGracePeriod = token.isValidFor(System.currentTimeMillis() + GRACE_PERIOD,
-                            this.userAgent.hashCode());
+                if (token.isValidFor(System.currentTimeMillis(), userAgent.hashCode())) {
+                    valid = true;
+                    validDuringGracePeriod = token.isValidFor(System.currentTimeMillis() + GRACE_PERIOD,
+                            userAgent.hashCode());
                 }
                 break;
             }
         }
         response.setHeader("Content-Type", "application/x-javascript");
         if (callback != null) {
-            response.getWriter().write(callback + "(" + this.valid + "," + this.validDuringGracePeriod + ");");
+            response.getWriter().write(callback + "(" + valid + "," + validDuringGracePeriod + ");");
         } else {
-            response.getWriter().write(this.valid + "," + this.validDuringGracePeriod);
+            response.getWriter().write(valid + "," + validDuringGracePeriod);
         }
     }
 }
