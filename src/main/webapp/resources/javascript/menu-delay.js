beb410cefe470dc3e2a5fa9482b4198f5c003a94
Charles Yates <ceyates@stanford.edu>
2011-10-07 21:51:48 +0000

    case #55704 Time delay for the dropdown menus in the main nav
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@92201 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/webapp/resources/javascript/menu-delay.js
@@ -0,0 +1,13 @@
+/*
+ * A simple script that delays the visibility of the drop downs for a short time
+ */
+(function() {
+    
+    Y.all("#laneNav>li").on("mouseenter", function(event) {
+        event.currentTarget.one("ul").setStyle("visibility","hidden");
+        setTimeout(function() {
+            event.currentTarget.one("ul").setStyle("visibility", "visible");
+        }, 300);
+    });
+
+})();
