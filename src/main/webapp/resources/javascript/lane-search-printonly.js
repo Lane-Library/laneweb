b0bb8e7c897fba1c57acbf6c17bc80864bc68146
Ryan Max Steinberg <ryanmax@stanford.edu>
2011-10-12 19:24:20 +0000

    https://irt-bugs.stanford.edu/default.asp?56635 show print results when no other results present
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@92452 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/webapp/resources/javascript/lane-search-printonly.js
@@ -0,0 +1,14 @@
+/*
+ * Display PRINT MATERIALS (catalog-lois) filter if print results are only results present
+ */
+(function() {
+    var Y = LANE.Y, printId = 'catalog-lois', 
+    noHits = Y.one('#noHitsText'), 
+    printFacet = Y.one('#'+printId+'Facet');
+    if(noHits && printFacet && !printFacet.hasClass('inactiveFacet')){
+        LANE.search.facets.setActiveFacet(printId);
+        LANE.Search.History.addValue("facet", printId);
+        Y.one('#all-allFacet').addClass('inactiveFacet');
+        Y.fire('lane:change');
+    }
+})();
