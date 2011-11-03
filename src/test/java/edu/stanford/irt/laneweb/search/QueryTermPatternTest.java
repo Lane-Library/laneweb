cc01ef08cc6c2c2d33838b499259793e927be240
Charles Yates <ceyates@stanford.edu>
2011-11-03 00:02:34 +0000

    fix for trailing backslash problem case# 59291
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@93699 c440208b-5602-0410-a095-92527a996296

--- src/test/java/edu/stanford/irt/laneweb/search/QueryTermPatternTest.java
+++ src/test/java/edu/stanford/irt/laneweb/search/QueryTermPatternTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.junit.Test;
