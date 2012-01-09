df633ad245fc123a42f59580ad8f4b0c3575afd8
Charles Yates <ceyates@stanford.edu>
2012-01-09 17:17:05 +0000

    code cleanup
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@95505 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/cocoon/AbstractSitemapModelComponent.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/AbstractSitemapModelComponent.java
@@ -11,26 +11,14 @@ import org.apache.excalibur.source.Source;
 
 public abstract class AbstractSitemapModelComponent implements SitemapModelComponent {
 
-    protected Map<String, Object> getModel() {
-		return model;
-	}
-
-	protected Map<String, String> getParameterMap() {
-		return parameterMap;
-	}
-
-	protected Source getSource() {
-		return source;
-	}
-
-	private Map<String, Object> model;
+    private Map<String, Object> model;
 
     private Map<String, String> parameterMap;
 
     private Source source;
 
     @Override
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public final void setup(final SourceResolver resolver, final Map objectModel, final String src, final Parameters par) {
         this.model = objectModel;
         this.parameterMap = new ParametersMap(par);
@@ -44,18 +32,30 @@ public abstract class AbstractSitemapModelComponent implements SitemapModelCompo
         initialize();
     }
 
+    protected Map<String, Object> getModel() {
+        return this.model;
+    }
+
+    protected Map<String, String> getParameterMap() {
+        return this.parameterMap;
+    }
+
+    protected Source getSource() {
+        return this.source;
+    }
+
     protected void initialize() {
     }
-    //TODO: implement these more useful protected methods instead of ModelUtil
-//    protected String getValue(String name) {
-//    	return getValue(name, null);
-//    }
-//    
-//    protected String getValue(String name, String defaultValue) {
-//    	String value = this.parameterMap.get(name);
-//    	if (value == null) {
-//    		value = ModelUtil.getString(this.model, name, defaultValue);
-//    	}
-//    	return value;
-//    }
+    // TODO: implement these more useful protected methods instead of ModelUtil
+    // protected String getValue(String name) {
+    // return getValue(name, null);
+    // }
+    //
+    // protected String getValue(String name, String defaultValue) {
+    // String value = this.parameterMap.get(name);
+    // if (value == null) {
+    // value = ModelUtil.getString(this.model, name, defaultValue);
+    // }
+    // return value;
+    // }
 }
