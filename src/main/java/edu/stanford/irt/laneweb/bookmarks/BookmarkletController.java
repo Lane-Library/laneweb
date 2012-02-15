3603a9e55baf74dae425758e5d13d4c6d704a249
Charles Yates <ceyates@stanford.edu>
2012-02-15 01:48:54 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@97167 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkletController.java
+++ src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkletController.java
@@ -2,26 +2,28 @@ package edu.stanford.irt.laneweb.bookmarks;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.stanford.irt.laneweb.model.Model;
 
 @Controller
-@RequestMapping(value = "/bookmarklet", method = RequestMethod.GET, consumes = "application/x-www-form-urlencoded")
 public class BookmarkletController extends BookmarkController {
-    
-    @ResponseStatus(value = HttpStatus.OK)
-    public void addBookmark(
+
+    @RequestMapping(value = "/bookmarklet", method = RequestMethod.GET)
+    public String addBookmark(
+            final RedirectAttributes redirectAttrs,
             @ModelAttribute(Model.BOOKMARKS) final List<Bookmark> bookmarks,
             @ModelAttribute(Model.SUNETID) final String sunetid,
-            @RequestBody final Bookmark bookmark) {
+            @RequestParam final String url,
+            @RequestParam final String label) {
+        Bookmark bookmark = new Bookmark(label, url);
         bookmarks.add(0, bookmark);
         saveLinks(sunetid, bookmarks);
+        return "redirect:" + url;
     }
 }
