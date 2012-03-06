0f97ea55e527fd3c7e99f82a2a5937877397b3b4
Alain Boussard <alainb@stanford.edu>
2012-03-06 21:34:48 +0000

    add comment
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@97733 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginController.java
@@ -1,5 +1,14 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
-
+/**
+ * This class will add two cookies the persistent-preference and user.  
+ * The user coolie will have the sunetid, the userAgent and the expired date appended and  encrypted.   
+ * The persistent-preference have the expired date minus 3 days
+ * only pl=true have to have the secure in the path but not the other 
+ * But if pl=renew the status of the user is looked up see it is active or not.
+ * Before to delete the cookie, we check if the persistent-preference value is not equals to
+ * denied because if it is equals denied the persistent window will never appear.  
+ * 
+ */
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -26,75 +35,81 @@ public class PersistentLoginController {
 	public static final String PERSISTENT_LOGIN_PREFERENCE = "persistent-preference";
 
 	@RequestMapping(value = "/persistentLogin.html", params = { "url", "pl=renew" })
-	public String renewCookieAndRedirect(final String url, HttpServletRequest request, HttpServletResponse response){
+	public String renewCookieAndRedirect(final String url, HttpServletRequest request, HttpServletResponse response) {
 		Boolean isActiveSunetID = (Boolean) request.getSession().getAttribute(Model.IS_ACTIVE_SUNETID);
 		if (null != isActiveSunetID && isActiveSunetID) {
-			setCookies(request, response);
+			checkSunetIdAndSetCookies(request, response);
 		}
 		return "redirect:".concat(url);
 	}
 
-	@RequestMapping(value = "/secure/persistentLogin.html", params = { "url", "pl=true" })
-	public String loginAndRedirect(final String url, HttpServletRequest request, HttpServletResponse response){
-		setCookies(request, response);
-		return "redirect:".concat(url);
+	@RequestMapping(value = "/secure/persistentLogin.html", params = {  "pl=true" })
+	public String createCookie(final String url, HttpServletRequest request, HttpServletResponse response) {
+		checkSunetIdAndSetCookies(request, response);
+		return getView(url, response);
 	}
 
-	@RequestMapping(value = "/**/persistentLogin.html", params = { "url", "pl=false" })
-	public String removeCookieAndRedirect(final String url, HttpServletRequest request, HttpServletResponse response){
-		removeLoginCookie(request, response);
-		return "redirect:".concat(url);
+
+	@RequestMapping(value = "/persistentLogin.html", params = { "pl=false" })
+	public String removeCookieAndView(final String url, HttpServletRequest request, HttpServletResponse response) {
+		removeCookies(request, response);
+		return getView(url, response);
+	}
+	
+	// /**/persistentLogin do not work for /secure/ not sure why but is working for anything else  
+	@RequestMapping(value = "/secure/persistentLogin.html", params = { "pl=false" })
+	public String secureRemoveCookie(final String url, HttpServletRequest request, HttpServletResponse response) {
+		 removeCookies(request, response);
+		 return getView(url, response);
 	}
 
-	@RequestMapping(value = "/secure/persistentLogin.html", params = { "!url", "pl=true"})
-	public String setCookieAndView(HttpServletRequest request, HttpServletResponse response){
-		setCookies(request, response);
-		response.setCharacterEncoding("UTF-8");
-		return "/persistentlogin.html";
-	}
-
-	@RequestMapping(value = "/**/persistentLogin.html", params = { "!url", "pl=false"})
-	public String removeCookieAndView(HttpServletRequest request, HttpServletResponse response){
-		removeLoginCookie(request, response);
-		response.setCharacterEncoding("UTF-8");
-		return "/persistentlogin.html";
-	}
-
-	private void setCookies(HttpServletRequest request, HttpServletResponse response){
-		String sunetid = this.sunetIdSource.getSunetid(request);
-		if (null != sunetid) {
-			setLoginCookie(request, response, sunetid);
-		} else{
-			removeLoginCookie(request, response);
-		}
-	}
 
 	/**
 	 * set the lane-user cookie max age to zero.
 	 * 
 	 * @param response
 	 */
-	private void removeLoginCookie(HttpServletRequest request,final HttpServletResponse response) {
-		 Cookie[] cookies = request.getCookies();
-	        if (cookies != null) {
-	            for (Cookie webCookie : cookies) {
-	                if (PersistentLoginFilter.PERSISTENT_LOGIN_PREFERENCE.equals(webCookie.getName())) {
-	                    String cookieValue = webCookie.getValue();
-	                	if(!"denied".equals(cookieValue)){//don't want to overwrite denied cookie
-	                        webCookie.setPath("/");
-	                        webCookie.setMaxAge(0);
-	                        response.addCookie(webCookie);
-	                	}
-	                	break;
-	                }
-	            }
-	        }
+	
+	private String getView(final String url, final HttpServletResponse response) {
+		if (null == url) {
+			response.setCharacterEncoding("UTF-8");
+			return "/persistentlogin.html";
+		} else {
+			return "redirect:".concat(url);
+		}
+	}
+	
+	private void removeCookies(HttpServletRequest request, final HttpServletResponse response) {
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie webCookie : cookies) {
+				if (PersistentLoginFilter.PERSISTENT_LOGIN_PREFERENCE.equals(webCookie.getName())) {
+					String cookieValue = webCookie.getValue();
+					if (!"denied".equals(cookieValue)) {
+						webCookie.setPath("/");
+						webCookie.setMaxAge(0);
+						response.addCookie(webCookie);
+					}
+					break;
+				}
+			}
+		}
 		Cookie cookie = new Cookie(SunetIdCookieCodec.LANE_COOKIE_NAME, null);
 		cookie.setPath("/");
 		cookie.setMaxAge(0);
 		response.addCookie(cookie);
 	}
 
+	private void checkSunetIdAndSetCookies(HttpServletRequest request, HttpServletResponse response) {
+		String sunetid = this.sunetIdSource.getSunetid(request);
+		if (null != sunetid) {
+			setCookies(request, response, sunetid);
+		} else {
+			removeCookies(request, response);
+		}
+	}
+
+	
 	/**
 	 * create and set the lane-user cookie
 	 * 
@@ -102,7 +117,7 @@ public class PersistentLoginController {
 	 * @param request
 	 * @param response
 	 */
-	private void setLoginCookie(final HttpServletRequest request, final HttpServletResponse response, String sunetid) {
+	private void setCookies(final HttpServletRequest request, final HttpServletResponse response, String sunetid) {
 		String userAgent = request.getHeader("User-Agent");
 		if (null != userAgent && null != sunetid) {
 			int twoWeeks = 3600 * 24 * 7 * 2;
