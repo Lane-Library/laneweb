668021472043cf5ee7a051f8ca3009dab335adfe
Charles Yates <ceyates@stanford.edu>
2012-04-02 19:58:29 +0000

    sync with some of laneweb-1.13, mostly involving exception handling
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@98698 c440208b-5602-0410-a095-92527a996296

--- src/test/java/edu/stanford/irt/laneweb/proxy/AbstractProxyLinkTransformerTest.java
+++ src/test/java/edu/stanford/irt/laneweb/proxy/AbstractProxyLinkTransformerTest.java
@@ -1,9 +1,6 @@
 package edu.stanford.irt.laneweb.proxy;
 
 import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.isA;
-import static org.easymock.EasyMock.isNull;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
