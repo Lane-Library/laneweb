679decd754254798c2f735626d861ead9efe2d2d
Charles Yates <ceyates@stanford.edu>
2012-04-09 19:07:30 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@98970 c440208b-5602-0410-a095-92527a996296

---
+++ src/test/java/edu/stanford/irt/laneweb/cocoon/TransformerSerializerTest.java
@@ -0,0 +1,86 @@
+package edu.stanford.irt.laneweb.cocoon;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Properties;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.excalibur.source.SourceValidity;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.stanford.irt.laneweb.LanewebException;
+
+
+public class TransformerSerializerTest {
+    
+    private TransformerSerializer serializer;
+    
+    private SAXTransformerFactory factory;
+    
+    private Properties properties;
+
+    private TransformerHandler handler;
+
+    private Transformer transformer;
+
+    @Before
+    public void setUp() {
+        this.factory = createMock(SAXTransformerFactory.class);
+        this.properties = createMock(Properties.class);
+        this.handler = createMock(TransformerHandler.class);
+        this.transformer = createMock(Transformer.class);
+        expect(this.properties.keySet()).andReturn(Collections.<Object>singleton("foo"));
+        expect(this.properties.get("foo")).andReturn("bar");
+        replay(this.properties);
+        this.serializer = new TransformerSerializer(this.factory, this.properties);
+        verify(this.properties);
+        reset(this.properties);
+    }
+
+    @Test
+    public void testGetKey() {
+        assertEquals(";foo=bar", this.serializer.getKey());
+    }
+
+    @Test
+    public void testGetValidity() {
+        assertEquals(SourceValidity.VALID, this.serializer.getValidity().isValid());
+    }
+
+    @Test
+    public void testSetOutputStream() throws IOException, TransformerConfigurationException {
+        expect(this.factory.newTransformerHandler()).andReturn(this.handler);
+        expect(this.handler.getTransformer()).andReturn(this.transformer);
+        this.transformer.setOutputProperties(this.properties);
+        this.handler.setResult(isA(StreamResult.class));
+        replay(this.factory, this.handler, this.transformer);
+        this.serializer.setOutputStream(null);
+        verify(this.factory, this.handler, this.transformer);
+    }
+
+    @Test
+    public void testSetOutputStreamThrows() throws IOException, TransformerConfigurationException {
+        expect(this.factory.newTransformerHandler()).andThrow(new TransformerConfigurationException());
+        replay(this.factory);
+        try {
+            this.serializer.setOutputStream(null);
+            fail();
+        } catch (LanewebException e) {}
+        verify(this.factory);
+    }
+}
