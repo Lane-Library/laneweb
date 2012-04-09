c3491938c4dbdd3be211233761a8928e9da5ae2f
Charles Yates <ceyates@stanford.edu>
2012-04-09 21:42:48 +0000

    add HTML5Serializer
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@98987 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/HTML5Serializer.java
@@ -0,0 +1,39 @@
+package edu.stanford.irt.laneweb.cocoon;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import javax.xml.transform.sax.SAXTransformerFactory;
+
+import org.xml.sax.SAXException;
+
+import edu.stanford.irt.laneweb.LanewebException;
+
+
+public class HTML5Serializer extends TransformerSerializer {
+
+    private static final byte[] HTML5_DOCTYPE_DECLARATION = "<!DOCTYPE html>\n".getBytes();
+    
+    private OutputStream outputStream;
+
+    public HTML5Serializer(SAXTransformerFactory factory, Properties properties) {
+        super(factory, properties);
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        try {
+            this.outputStream.write(HTML5_DOCTYPE_DECLARATION);
+        } catch (IOException e) {
+            throw new LanewebException(e);
+        }
+        super.startDocument();
+    }
+
+    @Override
+    public void setOutputStream(OutputStream out) {
+        super.setOutputStream(out);
+        this.outputStream = out;
+    }
+}
