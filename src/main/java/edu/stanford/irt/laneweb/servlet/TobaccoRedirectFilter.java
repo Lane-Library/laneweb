d5469d99b183fad73446e7f70f3df908e8d9f4b7
Charles Yates <ceyates@stanford.edu>
2012-04-11 00:08:49 +0000

    removed some unnecessary imports
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@99053 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/TobaccoRedirectFilter.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/TobaccoRedirectFilter.java
@@ -1,21 +1,15 @@
 package edu.stanford.irt.laneweb.servlet;
 
-import java.io.IOException;
-
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.cocoon.environment.http.HttpResponse;
 
 
 public class TobaccoRedirectFilter extends AbstractLanewebFilter {
 
     @Override
-    protected void internalDoFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        response.setStatus(HttpResponse.SC_MOVED_PERMANENTLY);
+    protected void internalDoFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
         response.addHeader("Location", "http://tobacco.stanford.edu/");
     }
 }
