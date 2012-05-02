b1f0b76cda26d3664811a926cab809179a2eed43
Charles Yates <ceyates@stanford.edu>
2012-05-02 16:57:37 +0000

    make DescribeGenerator implement CacheableProcessingComponent case 69074
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@99826 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/search/DescribeGenerator.java
+++ src/main/java/edu/stanford/irt/laneweb/search/DescribeGenerator.java
@@ -1,19 +1,36 @@
 package edu.stanford.irt.laneweb.search;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Map;
+
+import org.apache.cocoon.caching.CacheableProcessingComponent;
+import org.apache.excalibur.source.SourceValidity;
+import org.apache.excalibur.source.impl.validity.NOPValidity;
 
 import edu.stanford.irt.laneweb.model.Model;
 import edu.stanford.irt.laneweb.model.ModelUtil;
 import edu.stanford.irt.search.Result;
 import edu.stanford.irt.search.impl.SimpleQuery;
 
-public class DescribeGenerator extends AbstractMetasearchGenerator {
+public class DescribeGenerator extends AbstractMetasearchGenerator implements CacheableProcessingComponent {
 
     private static final String[] NO_ENGINES = new String[0];
 
     private String[] engines;
+
+    private String key = null;
+
+    private SourceValidity validity = null;
+
+    public Serializable getKey() {
+        return this.key;
+    }
+
+    public SourceValidity getValidity() {
+        return this.validity;
+    }
 
     @Override
     public void setModel(final Map<String, Object> model) {
@@ -21,6 +38,11 @@ public class DescribeGenerator extends AbstractMetasearchGenerator {
         // instead this can be null:
         this.query = ModelUtil.getString(model, Model.QUERY);
         this.engines = ModelUtil.getObject(model, Model.ENGINES, String[].class, NO_ENGINES);
+        //this is cacheable if there are no engines or query
+        if (this.query == null && this.engines.length == 0) {
+            this.key = "describe";
+            this.validity = NOPValidity.SHARED_INSTANCE;
+        }
     }
 
     @Override
