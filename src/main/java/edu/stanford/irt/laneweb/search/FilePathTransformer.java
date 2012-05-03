03b8c4fb67be275b924c6c9d90dbd0a405bd3092
Charles Yates <ceyates@stanford.edu>
2012-05-03 15:26:29 +0000

    scoping out simplifying and reducing memory/cpu footprint of search-template pipeline
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@99872 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/search/FilePathTransformer.java
@@ -0,0 +1,100 @@
+package edu.stanford.irt.laneweb.search;
+
+import java.io.IOException;
+
+import org.apache.cocoon.caching.CacheableProcessingComponent;
+import org.apache.cocoon.components.source.impl.MultiSourceValidity;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.xml.EmbeddedXMLPipe;
+import org.apache.cocoon.xml.XMLConsumer;
+import org.apache.excalibur.source.Source;
+import org.apache.excalibur.source.SourceValidity;
+import org.apache.xerces.parsers.AbstractSAXParser;
+import org.cyberneko.html.HTMLConfiguration;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import edu.stanford.irt.laneweb.cocoon.AbstractTransformer;
+
+public class FilePathTransformer extends AbstractTransformer implements CacheableProcessingComponent {
+
+    private static class HtmlSAXParser extends AbstractSAXParser {
+
+        protected HtmlSAXParser(final HTMLConfiguration conf) {
+            super(conf);
+        }
+    }
+
+    private static final String TYPE = "file-path";
+
+    private HtmlSAXParser htmlSaxParser;
+
+    private XMLConsumer pipe;
+
+    private SourceResolver sourceResolver;
+
+    private MultiSourceValidity validity;
+
+    public FilePathTransformer(final SourceResolver sourceResolver) {
+        this.sourceResolver = sourceResolver;
+        HTMLConfiguration conf = new HTMLConfiguration();
+        // TODO: review properties
+        conf.setProperty("http://cyberneko.org/html/properties/default-encoding", "UTF-8");
+        conf.setProperty("http://cyberneko.org/html/properties/names/elems", "lower");
+        conf.setFeature("http://cyberneko.org/html/features/insert-namespaces", true);
+        conf.setProperty("http://cyberneko.org/html/properties/namespaces-uri", "http://www.w3.org/1999/xhtml");
+        this.htmlSaxParser = new HtmlSAXParser(conf);
+        this.validity = new MultiSourceValidity(sourceResolver, MultiSourceValidity.CHECK_ALWAYS);
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        this.validity.close();
+        super.endDocument();
+    }
+
+    @Override
+    public void endElement(final String uri, final String localName, final String qName) throws SAXException {
+        if (!"file".equals(localName)) {
+            super.endElement(uri, localName, qName);
+        }
+    }
+
+    public String getKey() {
+        return TYPE;
+    }
+
+    public String getType() {
+        return TYPE;
+    }
+
+    public SourceValidity getValidity() {
+        return this.validity;
+    }
+
+    @Override
+    public void setConsumer(final XMLConsumer xmlConsumer) {
+        this.pipe = new EmbeddedXMLPipe(xmlConsumer);
+        this.htmlSaxParser.setContentHandler(this.pipe);
+        super.setConsumer(xmlConsumer);
+    }
+
+    @Override
+    public void startElement(final String uri, final String localName, final String qName, final Attributes atts)
+            throws SAXException {
+        if ("file".equals(localName)) {
+            try {
+                Source source = this.sourceResolver.resolveURI("file:" + atts.getValue("path"));
+                this.validity.add(source.getValidity());
+                InputSource inputSource = new InputSource(source.getInputStream());
+                inputSource.setSystemId(source.getURI());
+                this.htmlSaxParser.parse(inputSource);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            super.startElement(uri, localName, qName, atts);
+        }
+    }
+}
---
+++ src/main/java/edu/stanford/irt/laneweb/search/SearchDirectoryTransformer.java
@@ -0,0 +1,64 @@
+package edu.stanford.irt.laneweb.search;
+
+import java.io.File;
+import java.util.Map;
+
+import org.apache.cocoon.caching.CacheableProcessingComponent;
+import org.apache.excalibur.source.SourceValidity;
+import org.apache.excalibur.source.impl.validity.NOPValidity;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import edu.stanford.irt.laneweb.cocoon.AbstractTransformer;
+import edu.stanford.irt.laneweb.cocoon.ParametersAware;
+
+public class SearchDirectoryTransformer extends AbstractTransformer implements ParametersAware, CacheableProcessingComponent {
+
+    private static final String TYPE = "search-directory";
+
+    private String[] directories;
+
+    @Override
+    public void endDocument() throws SAXException {
+        for (String directory : this.directories) {
+            parseDirectory(new File(directory.substring(directory.indexOf(':') + 1)));
+        }
+        endElement("http://lane.stanford.edu/search-templates/ns", "search-templates", "search-templates");
+        super.endDocument();
+    }
+
+    public String getKey() {
+        return TYPE;
+    }
+
+    public String getType() {
+        return TYPE;
+    }
+
+    public SourceValidity getValidity() {
+        return NOPValidity.SHARED_INSTANCE;
+    }
+
+    public void setParameters(final Map<String, String> parameters) {
+        this.directories = parameters.get("directories").split(",");
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        startElement("http://lane.stanford.edu/search-templates/ns", "search-templates", "search-templates", new AttributesImpl());
+    }
+
+    private void parseDirectory(final File directory) throws SAXException {
+        for (File file : directory.listFiles()) {
+            if (file.isDirectory() && !".svn".equals(file.getName())) {
+                parseDirectory(file);
+            } else if (file.isFile() && file.canRead() && file.getName().endsWith(".html")) {
+                AttributesImpl attributes = new AttributesImpl();
+                attributes.addAttribute("", "path", "path", "CDATA", file.getAbsolutePath());
+                startElement("", "file", "file", attributes);
+                endElement("", "file", "file");
+            }
+        }
+    }
+}
