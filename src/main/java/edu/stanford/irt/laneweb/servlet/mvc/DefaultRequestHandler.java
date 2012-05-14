e23df10ce542a5795dcbc10a03fcb52a786cd685
Charles Yates <ceyates@stanford.edu>
2012-05-14 22:25:37 +0000

    remove base path, already part of getRequestURI()
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@100215 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/DefaultRequestHandler.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/DefaultRequestHandler.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.HttpRequestHandler;
 
-import edu.stanford.irt.laneweb.model.Model;
-
 /**
  * A request handler that is designed to catch any requests that aren't handled
  * by others, It redirects to request.getRequestURI()/index.html, mimicking the
@@ -20,10 +18,8 @@ public class DefaultRequestHandler implements HttpRequestHandler {
 
     public void handleRequest(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         String uri = request.getRequestURI();
-//        String basePath = (String) request.getAttribute(Model.BASE_PATH);
         String queryString = request.getQueryString();
         String location = uri + "/index.html";
-//        location = basePath == null ? location : basePath + location;
         location = queryString == null ? location : location + '?' + queryString;
         response.sendRedirect(location);
     }
