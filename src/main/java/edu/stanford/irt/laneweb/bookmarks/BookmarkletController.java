2bce4602a4adb6b810df705a60cb36744a204952
Charles Yates <ceyates@stanford.edu>
2012-05-21 17:13:56 +0000

    case 70022 go to webauth if not logged in for bookmarklet
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@100451 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkletController.java
+++ src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkletController.java
@@ -1,11 +1,14 @@
 package edu.stanford.irt.laneweb.bookmarks;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -14,16 +17,31 @@ import edu.stanford.irt.laneweb.model.Model;
 @Controller
 public class BookmarkletController extends BookmarkController {
 
-    @RequestMapping(value = "/bookmarklet", method = RequestMethod.GET)
-    public String addBookmark(
-            final RedirectAttributes redirectAttrs,
+    @RequestMapping(value = { "/bookmarklet", "/secure/bookmarklet" })
+    public String addBookmark(final RedirectAttributes redirectAttrs,
             @ModelAttribute(Model.BOOKMARKS) final List<Bookmark> bookmarks,
             @ModelAttribute(Model.SUNETID) final String sunetid,
             @RequestParam final String url,
-            @RequestParam final String label) {
+            @RequestParam final String label) throws UnsupportedEncodingException {
+        if (sunetid == null) {
+            // not logged in, redirect through webauth:
+            return "redirect:/secure/bookmarklet?url=" + URLEncoder.encode(url, "UTF-8") + "&label="
+                    + URLEncoder.encode(label, "UTF-8");
+        }
         Bookmark bookmark = new Bookmark(label, url);
         bookmarks.add(0, bookmark);
         saveLinks(sunetid, bookmarks);
         return "redirect:" + url;
+    }
+
+    @Override
+    // provide null values for sunetid and bookmarks in model to prevent
+    // BeanInstantiationException before addBookmark
+    protected void bind(final HttpServletRequest request, final org.springframework.ui.Model model) {
+        super.bind(request, model);
+        if (!model.containsAttribute(Model.SUNETID)) {
+            model.addAttribute(Model.SUNETID, null);
+            model.addAttribute(Model.BOOKMARKS, null);
+        }
     }
 }
