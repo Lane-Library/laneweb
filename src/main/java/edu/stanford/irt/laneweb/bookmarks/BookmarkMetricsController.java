0c5e65db422fe5422018909494a1aeceec0638c7
Charles Yates <ceyates@stanford.edu>
2012-05-30 21:04:02 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@100787 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkMetricsController.java
+++ src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkMetricsController.java
@@ -3,6 +3,7 @@ package edu.stanford.irt.laneweb.bookmarks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/secure/metrics/bookmarks")
@@ -12,6 +13,7 @@ public class BookmarkMetricsController {
     private BookmarkDAO bookmarkDAO;
 
     @RequestMapping(value = "/rowcount")
+    @ResponseBody
     public int getRowCount() {
         return this.bookmarkDAO.getRowCount();
     }
