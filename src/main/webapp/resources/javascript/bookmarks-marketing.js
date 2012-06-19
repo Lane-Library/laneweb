1b818f8f776d8639105593a86713f63fe2639abd
Charles Yates <ceyates@stanford.edu>
2012-06-19 18:38:12 +0000

    case 71544 shaded border appears on top of marketing div in IE7
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101394 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmarks-marketing.js
+++ src/main/webapp/resources/javascript/bookmarks-marketing.js
@@ -4,14 +4,6 @@
     var marketingNode = Y.one("#look-here");
         if (marketingNode) {
             setTimeout(function() {
-                var sb;
-                if (Y.UA.ie && Y.UA.ie < 8) {
-                    sb = Y.one(".sb-tb");
-                    //need to hide shaded background in IE < 8, case 71544
-                    if (sb) {
-                        sb.setStyle("visibility", "hidden");
-                    }
-                }
                 marketingNode.setStyle("display", "block");
             }, 1500);
         }
