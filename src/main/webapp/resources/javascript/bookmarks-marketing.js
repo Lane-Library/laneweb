7f1ff76ce14244ee66adb3576d92c66b598e0d51
Charles Yates <ceyates@stanford.edu>
2012-06-19 17:37:33 +0000

    case 71544 shaded border appears on top of marketing div in IE7
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101389 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmarks-marketing.js
+++ src/main/webapp/resources/javascript/bookmarks-marketing.js
@@ -1,9 +1,16 @@
 (function(){
 //Show the look here bookmarks marketing div after 1.5 seconds
     //TODO: remove this file when done.
-        if (Y.one("#look-here")) {
+    var marketingNode = Y.one("#look-here");
+        if (marketingNode) {
             setTimeout(function() {
-                Y.one("#look-here").setStyle("display", "block");
+                if (Y.UA.ie && Y.UA.ie < 8) {
+                    //need to increase z-index in IE, case 71544
+                    if (Y.one("#sb")) {
+                        Y.one("#sb").setStyle("z-index", "1");
+                    }
+                }
+                marketingNode.setStyle("display", "block");
             }, 1500);
         }
 })();
