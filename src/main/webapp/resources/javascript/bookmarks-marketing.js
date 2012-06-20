a68649f68a562a5acad9ab254d2938fc89013028
Charles Yates <ceyates@stanford.edu>
2012-06-20 20:14:12 +0000

    change look-here to class from id
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101479 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmarks-marketing.js
+++ src/main/webapp/resources/javascript/bookmarks-marketing.js
@@ -2,7 +2,7 @@
 //Animate the look here bookmarks marketing div to move into position after 1.5 seconds and fade out after 10
     
     //TODO: remove this file when done.
-    var marketingNode = Y.one("#look-here"), anim1, anim2;
+    var marketingNode = Y.one(".look-here"), anim1, anim2;
         if (marketingNode) {
             anim1 = new Y.Anim({node : marketingNode, to: {top:"-35px"}});
             anim2 = new Y.Anim({node : marketingNode, to: {opacity : 0}});
