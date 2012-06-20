437b6050846ee440b4c3502f4f711002be9c7c78
Charles Yates <ceyates@stanford.edu>
2012-06-20 17:05:34 +0000

    case 71526 animate bookmarks marketing div
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101461 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmarks-marketing.js
+++ src/main/webapp/resources/javascript/bookmarks-marketing.js
@@ -1,10 +1,16 @@
 (function(){
-//Show the look here bookmarks marketing div after 1.5 seconds
+//Animate the look here bookmarks marketing div to move into position after 1.5 seconds and fade out after 10
+    
     //TODO: remove this file when done.
-    var marketingNode = Y.one("#look-here");
+    var marketingNode = Y.one("#look-here"), anim1, anim2;
         if (marketingNode) {
+            anim1 = new Y.Anim({node : marketingNode, to: {top:"-35px"}});
+            anim2 = new Y.Anim({node : marketingNode, to: {opacity : 0}});
             setTimeout(function() {
-                marketingNode.setStyle("display", "block");
+                anim1.run();
             }, 1500);
+            setTimeout(function() {
+                anim2.run();
+            }, 10000);
         }
 })();
