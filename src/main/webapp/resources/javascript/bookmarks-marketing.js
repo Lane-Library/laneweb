ed147a75ba2cbe5bbdf2a4d9d0df40e89a77fe17
Charles Yates <ceyates@stanford.edu>
2012-06-21 00:24:01 +0000

    don't show look-here if there are bookmarks
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101508 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmarks-marketing.js
+++ src/main/webapp/resources/javascript/bookmarks-marketing.js
@@ -2,8 +2,9 @@
 //Animate the look here bookmarks marketing div to move into position after 1.5 seconds and fade out after 10
     
     //TODO: remove this file when done.
-    var marketingNode = Y.one(".look-here"), anim1, anim2;
-        if (marketingNode) {
+    var marketingNode = Y.one(".look-here"), 
+        bookmarks = Y.all("#bookmarks li"), anim1, anim2;
+        if (marketingNode && bookmarks.size() === 0) {
             anim1 = new Y.Anim({node : marketingNode, to: {top:"-35px"}});
             anim2 = new Y.Anim({node : marketingNode, to: {opacity : 0}});
             anim2.on("end", function() {
