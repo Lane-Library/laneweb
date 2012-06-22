d500588c19af5d573c08abf551552ce481b5bb28
Charles Yates <ceyates@stanford.edu>
2012-06-22 14:23:22 +0000

    case 70132 show/hide instructions based on absence/presence of bookmarks
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101584 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmark-instructions.js
+++ src/main/webapp/resources/javascript/bookmark-instructions.js
@@ -1,7 +1,7 @@
 (function(){
     //case 70132, bookmark instructions expanded when no bookmarks.
     var bookmarks = Y.all("#bookmarks li"),
-        instructions = Y.one("#bookmarks-instructions");
+        instructions = Y.one("#bookmark-instructions");
     if (instructions && bookmarks.size() === 0) {
         instructions.one("div").addClass("expanded");
     }
