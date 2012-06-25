f49a32912a55c68ce8a8b80247c8f4cb2d79c74a
Charles Yates <ceyates@stanford.edu>
2012-06-25 23:25:57 +0000

    revert previous changes, didn't work right
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@101712 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/bookmarks-marketing.js
+++ src/main/webapp/resources/javascript/bookmarks-marketing.js
@@ -3,22 +3,12 @@
     
     //TODO: remove this file when done.
     var marketingNode = Y.one(".look-here"), 
-        bookmarks = Y.all("#bookmarks li"),
-        sb = Y.one("#sb"), anim1, anim2;
+        bookmarks = Y.all("#bookmarks li"), anim1, anim2;
         if (marketingNode && bookmarks.size() === 0) {
             anim1 = new Y.Anim({node : marketingNode, to: {top:"-35px"}});
             anim2 = new Y.Anim({node : marketingNode, to: {opacity : 0}});
-            //set style for IE < 8 so background behind marketingNode.
-            if (Y.UA.ie > 0 && Y.UA.ie < 8 && sb) {
-                sb.setStyle("zoom", "1");
-                sb.setStyle("z-index", "1");
-            }
             anim2.on("end", function() {
                 marketingNode.remove(true);
-                //restore:
-                if (Y.UA.ie > 0 && Y.UA.ie < 8 && sb) {
-                    sb.setStyle("z-index", "0");
-                }
             });
             setTimeout(function() {
                 anim1.run();
