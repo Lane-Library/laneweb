f225d503ab30b67ec92beb3f8a3d61cfdfbc8d43
Alain Boussard <alainb@stanford.edu>
2012-07-23 21:33:25 +0000

    To see this will work with yui3.2.0
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@102419 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-persistent-login.js
+++ src/main/webapp/resources/javascript/lane-persistent-login.js
@@ -1,129 +1,146 @@
 (function() {
 
-	var redirectUrl,
-	metaGroup = Y.one('html head meta[name="ipGroup"]'),
-	PERSISTENT_PREFERENCE_COOKIE_NAME = 'persistent-preference';
+	var redirectUrl,  
+	PERSISTENT_PREFERENCE_COOKIE_NAME = 'persistent-preference', 
+	persistentStatusCookie = Y.Cookie.get(PERSISTENT_PREFERENCE_COOKIE_NAME), 
+	now = new Date(),
+	needPopup= true,
+	auth = Y.one('html head meta[name="auth"]'),
+	metaGroup = Y.one('html head meta[name="ipGroup"]'),	
+	fromHospital = false;
 	
+	if (!metaGroup && ("SHC" == metaGroup.get("content") || "LPCH" == metaGroup.get("content"))){
+		fromHospital = true;
+	}
+	if(fromHospital ||  'denied' == persistentStatusCookie || (persistentStatusCookie  && now.getTime() < persistentStatusCookie )){
+		needPopup = false;
+	}
+	
+	
+
+	//if not from hospital and user click on a link that going to be proxy
+	if (needPopup && Y.one("a[href *='secure/apps/proxy/credential'] , a[href *='laneproxy']")) {
+		Y.all("a[href *='secure/apps/proxy/credential'] , a[href *='laneproxy']").on("click",function(event) {
+			var link = event.target, isActive;
+			redirectUrl = escape(link.get('href'));
+			event.preventDefault();
+			// don't want a redirect with the tracking see tracking.js code if !rel documment.location is not set
+			link.set('rel', 'persistentLogin');
+			
+			//if preference cookie  is present but date get into grace period
+			if (persistentStatusCookie && now.getTime() >persistentStatusCookie ){
+				isActive = Y.io('/././user/active', {sync : true});
+				if (isActive.responseText === 'true') {
+					LANE.persistentlogin.newWindow(event,'/././plain/persistent-extension-popup.html');
+					}
+				} //no preference cookie at all
+				else if (!persistentStatusCookie){
+					LANE.persistentlogin.newWindow(event,'/././plain/persistent-popup.html');						
+				}
+		}, document);
+	}
+
+	if ( Y.one("a[href ='/././secure/login.html']")) {
+		Y.one("a[href ='/secure/login.html']").on("click",function(event) {
+			if (persistentStatusCookie	&& 'denied' === persistentStatusCookie) {
+				//will be redirected to same page after the webauth
+				document.location = '/././secure/persistentLogin.html?pl=false&url='+ escape(document.location);
+			} else {
+				var link = event.target;
+				link.set('rel', 'persistentLogin');
+				redirectUrl = escape(document.location);
+				LANE.persistentlogin.newWindow(event,'/././plain/persistent-login-popup.html');
+			}
+			event.preventDefault();
+		}, document);
+	}
+	
+
+//		The popup window 
+	var popupWindow = function(id, o, args) {
+		var lightbox = Y.lane.Lightbox, date = new Date();
+		lightbox.setContent(o.responseText);
+		lightbox.show();
+		// To hide the close button on the window the window
+		Y.one('#lightboxClose').setStyle('visibility', 'hidden');
+
+		// Click on YES --
+		Y.one('#yes-persistent-login').on('click',function(event) {
+			//if the checkbox "don't ask me again" is enable so no action happen    
+			if (Y.one('#dont-ask-again') && Y.one('#dont-ask-again').get('checked')) {
+				event.preventDefault();
+			} else {
+				setLink(event); // cookie set in the PerssitentLoginFilter class
+			}
+		});
+
+		// Click on NO
+		if (Y.one('#no-persistent-login')) {
+			Y.one('#no-persistent-login').on('click',function(event) {
+				setLink(event);
+				//if the checkbox "don't ask me again" is enable the cookie is set to denied for 10 years
+				//otherwise it is set for the session only
+				if (Y.one('#dont-ask-again') && Y.one('#dont-ask-again').get('checked')) {
+						date.setFullYear(date.getFullYear() + 10);
+						Y.Cookie.set(PERSISTENT_PREFERENCE_COOKIE_NAME,	'denied', {
+								path : "/",
+								expires : date
+							});
+				} else
+					Y.Cookie.set(PERSISTENT_PREFERENCE_COOKIE_NAME,'denied', {	path : "/"	});
+			});
+		}
+		if (Y.one('#dont-ask-again')) {
+			//if someone click on the don't ask me again" the yes button clas should look disable
+			Y.one('#dont-ask-again').on('click',function(event) {
+				if (Y.one('#dont-ask-again')&& Y.one('#dont-ask-again').get('checked')) {
+					Y.one('#yes-persistent-login').replaceClass('red-btn', 'disabled-btn');
+				} else {
+					Y.one('#yes-persistent-login').replaceClass('disabled-btn', 'red-btn');
+				}
+			});
+		}
+	};
+
+	
+	// for the static page persistentlogin.hrml Click on YES this way the user 
+	// will not have to go through webauth.
+	if (Y.one('#yes-persistent-login')) {
+		Y.one('#yes-persistent-login').on('click',function(event) {
+		event.preventDefault();
+		if (auth && "" != auth.get("content")) {
+			document.location = '/././persistentLogin.html?pl=renew&url=/myaccounts.html';
+		} else {
+			document.location = '/././secure/persistentLogin.html?pl=true';
+		}
+		});
+	}
+	
+	
+
+	var setLink = function(event) {
+		var node = event.target, url = '/././';
+		if (node.get('tagName') === 'SPAN') {
+			node = node.get('parentNode');
+		}
+		if ( auth && "" === auth.get("content") ||  node.get('search').indexOf("pl=true") >0 ) {	
+			url = url + 'secure/';
+		}
+		url = url + 'persistentLogin.html' + node.get('search') + '&url='+ redirectUrl;
+		node.set('href', url);
+	}
+
 	
 	LANE.persistentlogin = function() {
 		return {
 			newWindow : function(event, urlPage) {
 				Y.io(urlPage, {
 					on : {
-						success : show
-					}
+						success : popupWindow
+						}
 				});
 			}
 		};
 	}();
-
-	//no persistent login for people from the hospital
-	if( !metaGroup || ("SHC" != metaGroup.get("content") && "LPCH" != metaGroup.get("content"))){
-		Y.on('click', function(event) {
-			var link = event.target,
-			now = new Date(),
-			isActive,
-			persistentStatusCookie = Y.Cookie.get(PERSISTENT_PREFERENCE_COOKIE_NAME);
-			if (link && link.get('nodeName') == 'A' && (link.get('pathname').indexOf('secure/apps/proxy/credential') > -1 || link.get('host').indexOf('laneproxy') === 0)) {
-				var logoutLink = Y.one("#logout");
-				if ('denied' !== persistentStatusCookie && (!logoutLink || persistentStatusCookie < now.getTime())){
-					// don't want a redirect with the tracking see tracking.js code if !rel documment.location is not set
-					link.set('rel', 'persistentLogin');
-					redirectUrl = escape(link.get('href'));
-					if(logoutLink){
-						isActive = Y.io('/././user/active', { sync: true});
-						if(isActive.responseText === 'true' ){
-							LANE.persistentlogin.newWindow(event, '/././plain/persistent-extension-popup.html');
-							event.preventDefault();
-						}
-					}
-					else{
-						LANE.persistentlogin.newWindow(event, '/././plain/persistent-popup.html');
-						event.preventDefault();
-					}
-				}
-			} else if (link && link.get('nodeName') == 'A'	&& (link.get('pathname').indexOf('secure/login.html') > -1)) {
-				if (persistentStatusCookie && 'denied' === persistentStatusCookie) {
-					document.location =  '/././secure/persistentLogin.html?pl=false&url='+escape(document.location);
-				} else{
-					link.set('rel', 'persistentLogin');
-					redirectUrl = escape(document.location);
-					LANE.persistentlogin.newWindow(event,'/././plain/persistent-login-popup.html');
-				}
-				event.preventDefault();
-			}
-		}, document);
-	}	
-	
-	var show = function(id, o, args) {
-		var lightbox = Y.lane.Lightbox,
-		date = new Date();
-		lightbox.setContent(o.responseText);
-		lightbox.show();
-		// To hide tclose the window
-		Y.one('#lightboxClose').setStyle('visibility', 'hidden');
-		
-		// Click on YES --
-		Y.one('#yes-persistent-login').on('click', function(event) {
-			if(Y.one('#dont-ask-again') && Y.one('#dont-ask-again').get('checked')){
-				event.preventDefault();
-			}
-			else{
-				setLink(event); //cookie set in the PerssitentLoginFilter class
-			}
-		});
-		
-		// Click on NO
-		if(Y.one('#no-persistent-login')){
-			Y.one('#no-persistent-login').on('click', function(event) {
-				setLink(event);
-				if(Y.one('#dont-ask-again') && Y.one('#dont-ask-again').get('checked')){
-					date.setFullYear(date.getFullYear()+10);
-					Y.Cookie.set(PERSISTENT_PREFERENCE_COOKIE_NAME, 'denied', {path: "/",expires: date});
-				} else
-					Y.Cookie.set(PERSISTENT_PREFERENCE_COOKIE_NAME, 'denied', {path: "/"});
-			});
-		}
-		if(Y.one('#dont-ask-again')){
-			Y.one('#dont-ask-again').on('click', function(event) {
-				if(Y.one('#dont-ask-again') && Y.one('#dont-ask-again').get('checked')){
-					 Y.one('#yes-persistent-login').replaceClass('red-btn','disabled-btn');
-				} else{
-					 Y.one('#yes-persistent-login').replaceClass('disabled-btn','red-btn');
-				}
-			});
-		}
-	};
-		
-
-	//for the static page persistentlogin.hrml
-	// Click on YES --
-	if (Y.one('#yes-persistent-login')) {
-		Y.one('#yes-persistent-login').on('click',
-			function(event) {
-				event.preventDefault();
-				if (Y.Cookie.get('user')) {
-					document.location = '/././persistentLogin.html?pl=renew&url=/myaccounts.html';
-				} else {
-					document.location = '/././secure/persistentLogin.html?pl=true';
-				}
-			});
-	}
-	
-	
-	var setLink = function(event) {
-		var node = event.target, 
-		url = '/././', 
-		logoutLink = Y.one("#logout"),
-		persistentExpirationDate = Y.Cookie.get("persistent-expiration-date");
-		if( node.get('tagName') === 'SPAN'){
-			node = node.get('parentNode');
-		}
-		if (!logoutLink || !persistentExpirationDate ||  persistentExpirationDate < (new Date().getTime())){
-			url = url + 'secure/';
-		}
-		url = url + 'persistentLogin.html' + node.get('search') + '&url='+ redirectUrl;
-		node.set('href', url);
-	};
 	
 })();
