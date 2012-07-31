215a5711f96fd0bfcab41fb1230a0ed9769fb8fc
Charles Yates <ceyates@stanford.edu>
2012-07-31 00:24:56 +0000

    use SunetIdAndTicketDataBinder to get ticket and sunetid, add RedirectAttributes
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@102811 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ProxyCredentialController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ProxyCredentialController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.stanford.irt.laneweb.model.Model;
 import edu.stanford.irt.laneweb.proxy.Ticket;
@@ -28,6 +29,7 @@ public class ProxyCredentialController {
     @RequestMapping(value = "**/apps/proxy/credential")
     public String proxyRedirect(
             final HttpServletRequest request,
+            final RedirectAttributes attrs,
             @ModelAttribute(Model.SUNETID) final String sunetid,
             @ModelAttribute(Model.TICKET) final Ticket ticket) {
         StringBuilder sb = new StringBuilder("redirect:");
@@ -46,6 +48,7 @@ public class ProxyCredentialController {
     @RequestMapping(value = "**/secure/apps/proxy/credential")
     public String secureProxyRedirect(
             final HttpServletRequest request,
+            final RedirectAttributes attrs,
             @ModelAttribute(Model.SUNETID) final String sunetid,
             @ModelAttribute(Model.TICKET) final Ticket ticket) {
         String queryString = request.getQueryString();
