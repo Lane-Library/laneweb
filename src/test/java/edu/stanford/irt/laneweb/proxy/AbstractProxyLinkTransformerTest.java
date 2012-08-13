cdfcec03e21a0a10b87b8c6102d5260c7dcfb5f3
Charles Yates <ceyates@stanford.edu>
2012-08-13 16:01:27 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@103204 c440208b-5602-0410-a095-92527a996296

--- src/test/java/edu/stanford/irt/laneweb/proxy/AbstractProxyLinkTransformerTest.java
+++ src/test/java/edu/stanford/irt/laneweb/proxy/AbstractProxyLinkTransformerTest.java
@@ -1,7 +1,11 @@
 package edu.stanford.irt.laneweb.proxy;
 
-import static org.easymock.EasyMock.*;
-import static org.junit.Assert.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
