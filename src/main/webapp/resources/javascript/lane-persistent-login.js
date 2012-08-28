97e8b32bd7db0fec84a4a8ecbcf2b381519f85ec
Alain Boussard <alainb@stanford.edu>
2012-08-28 21:22:39 +0000

    It's largely a stylistic preference, but there are some technical differences when passing a selector string as a the third argument to Y.on() or Y.delegate() (ala Y.on('click', callback, '#some select.or-string').
        Y.on() uses the Selector engine directly rather than calling through Y.all(...). 
    
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@103826 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/lane-persistent-login.js
+++ src/main/webapp/resources/javascript/lane-persistent-login.js
@@ -16,33 +16,20 @@
 	if(fromHospital ||  'denied' == persistentStatusCookie || (persistentStatusCookie  && now.getTime() < persistentStatusCookie )){
 		needPopup = false;
 	}
+
 	
 	
 	//if not from hospital and user click on a link that going to be proxy
-	if (needPopup && Y.all('a[href*=secure/apps/proxy/credential]' , 'a[href*=laneproxy]')) {
-		Y.all('a[href*=secure/apps/proxy/credential]' , 'a[href*=laneproxy]').on("click",function(event) {
-			var link = event.target, isActive;
-			redirectUrl = encodeURI(link.get('href'));
-			event.preventDefault();
-			// don't want a redirect with the tracking see tracking.js code if !rel documment.location is not set
-			link.set('rel', 'persistentLogin');
-			
-			//if preference cookie  is present but date get into grace period
-			if (persistentStatusCookie && now.getTime() >persistentStatusCookie ){
-				isActive = Y.io('/././user/active', {sync : true});
-				if (isActive.responseText === 'true') {
-					LANE.persistentlogin.newWindow(event,'/././plain/persistent-extension-popup.html');
-					}
-				} //no preference cookie at all
-				else if (!persistentStatusCookie){
-					LANE.persistentlogin.newWindow(event,'/././plain/persistent-popup.html');						
-				}
-		}, document);
-	}
+    if (needPopup ){
+        Y.on("click", _proxiedResourceClickedOn, 'a[href*=secure/apps/proxy/credential]');        
+        Y.on("click", _proxiedResourceClickedOn, 'a[href*=laneproxy]');
+    }
+
+    
 	
-	
+	//if someone click on MyLane Login 
 	if(Y.one('a[href=/././secure/login.html]')){
-		 Y.all('a[href=/././secure/login.html]').on("click",function(event) {
+		 Y.on("click",function(event) {
 			 if (persistentStatusCookie	&& 'denied' === persistentStatusCookie) {
 				//will be redirected to same page after the webauth
 				document.location = '/././secure/persistentLogin.html?pl=false&url='+ encodeURI(document.location);
@@ -53,11 +40,35 @@
 				LANE.persistentlogin.newWindow(event,'/././plain/persistent-login-popup.html');
 			}
 			event.preventDefault();
-		}, document);
+		}, 'a[href=/././secure/login.html]');
 	}
-	
 
-//		The popup window 
+	
+	
+    function _proxiedResourceClickedOn(event) {
+		var link = event.target, isActive;
+		redirectUrl = encodeURI(link.get('href'));
+		event.preventDefault();
+		// don\'t want a redirect with the tracking see tracking.js code if !rel
+		// documment.location is not set
+		link.set('rel', 'persistentLogin');
+
+		// if preference cookie is present but date get into grace period
+		if (persistentStatusCookie && now.getTime() > persistentStatusCookie) {
+			isActive = Y.io('/././user/active', {
+				sync : true
+			});
+			if (isActive.responseText === 'true') {
+				LANE.persistentlogin.newWindow(event,'/././plain/persistent-extension-popup.html');
+			}
+		} // no preference cookie at all
+		else if (!persistentStatusCookie) {
+			LANE.persistentlogin.newWindow(event,'/././plain/persistent-popup.html');
+		}
+	}
+
+	
+// The popup window
 	var popupWindow = function(id, o, args) {
 		var lightbox = Y.lane.Lightbox, date = new Date();
 		if (Y.UA.ie && Y.UA.ie < 8) {
