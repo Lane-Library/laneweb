f2d012e70fd53f903c1a1950809c374dd04bf4a7
Charles Yates <ceyates@stanford.edu>
2012-08-28 02:16:39 +0000

    fix some broken tests
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@103801 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/test/search-pico/search-pico-test.js
+++ src/main/webapp/resources/test/search-pico/search-pico-test.js
@@ -1,11 +1,11 @@
 /**
  * @author ceyates
  */
-YUI().use('node','console', 'test', function(Y){
+YUI().use('console', "node-event-simulate", 'test', function(T){
     
-    Y.publish('lane:searchSourceChange',{broadcast:2});
+//    Y.publish('lane:searchSourceChange',{broadcast:2});
 
-    var searchpicoTestCase = new Y.Test.Case({
+    var searchpicoTestCase = new T.Test.Case({
         name: "Lane Search PICO Testcase",
         testSourceChangeClinical : function() {
             var nav = Y.one('#laneNav');
@@ -15,9 +15,9 @@ YUI().use('node','console', 'test', function(Y){
                     oldVal:'all-all'
                 });
 //            this.wait(function() {
-                Y.Assert.isTrue(nav.hasClass('clinical'), 'nav not class clinical');
-                Y.Assert.isTrue(search.hasClass('clinical'), 'search form not class clinical');
-                Y.Assert.isTrue(Y.Lang.isObject(Y.one('#picoFields')), 'no pico fields');
+                T.Assert.isTrue(nav.hasClass('clinical'), 'nav not class clinical');
+                T.Assert.isTrue(search.hasClass('clinical'), 'search form not class clinical');
+                T.Assert.isTrue(T.Lang.isObject(Y.one('#picoFields')), 'no pico fields');
 //            }, 1000);
         },
         testSourceChangeNotClinical : function() {
@@ -29,14 +29,14 @@ YUI().use('node','console', 'test', function(Y){
                     oldVal:'clinical-all'
                 });
 //            this.wait(function() {
-                Y.Assert.isFalse(nav.hasClass('clinical'), 'nav class clinical');
-                Y.Assert.isFalse(search.hasClass('clinical'), 'search form class clinical');
-                Y.Assert.areEqual("none", picoFields.getStyle("display"));
+                T.Assert.isFalse(nav.hasClass('clinical'), 'nav class clinical');
+                T.Assert.isFalse(search.hasClass('clinical'), 'search form class clinical');
+                T.Assert.areEqual("none", picoFields.getStyle("display"));
 //            }, 1000);
         },
         testSetPatientCondition: function() {
-            var clinicalP = Y.one("#clinicalP");
-            var clinicalI = Y.one("#clinicalI");
+            var clinicalP = T.one("#clinicalP");
+            var clinicalI = T.one("#clinicalI");
             var searchTerms = Y.one("#searchTerms");
             Y.publish("lane:suggestSelect",{
                 broadcast:2
@@ -46,20 +46,20 @@ YUI().use('node','console', 'test', function(Y){
                 oldVal:'all-all'
             });
             clinicalP.set("value", "foo");
-            Y.fire("lane:suggestSelect");
-            Y.Assert.areEqual("foo", searchTerms.get("value"));
-            clinicalI.set("value", "bar");
-            Y.fire("lane:suggestSelect");
-            Y.Assert.areEqual("(foo) AND (bar)", searchTerms.get("value"));
+            clinicalP.simulate("blur");
+            T.Assert.areEqual("foo", searchTerms.get("value"));
+            clinicalI.set("value","bar");
+            clinicalI.simulate("blur");
+            T.Assert.areEqual("(foo) AND (bar)", searchTerms.get("value"));
         }
     });
     
-    Y.one('body').addClass('yui3-skin-sam');
-    new Y.Console({
+    T.one('body').addClass('yui3-skin-sam');
+    new T.Console({
         newestOnTop: false
     }).render('#log');
     
     
-    Y.Test.Runner.add(searchpicoTestCase);
-    Y.Test.Runner.run();
+    T.Test.Runner.add(searchpicoTestCase);
+    T.Test.Runner.run();
 });
