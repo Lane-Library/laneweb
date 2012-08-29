61e15c52ad3ae8c613f66e6e64dd67a38d0eff4b
Charles Yates <ceyates@stanford.edu>
2012-08-29 16:04:57 +0000

    case 74996 added netscape bookmark format
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@103881 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkController.java
+++ src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkController.java
@@ -6,11 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import edu.stanford.irt.laneweb.model.Model;
 import edu.stanford.irt.laneweb.servlet.binding.SunetIdAndTicketDataBinder;
 
 public abstract class BookmarkController {
@@ -23,28 +19,6 @@ public abstract class BookmarkController {
 
     @Autowired
     private SunetIdAndTicketDataBinder sunetidTicketDataBinder;
-
-    /**
-     * Converts bookmarks to a list of tab separated values.
-     * 
-     * @param bookmarks
-     *            the Bookmarks
-     * @return the bookmarks as a list of tab separated values
-     */
-    @RequestMapping(value = "/bookmarks.tsv", method = RequestMethod.GET, produces = "text/tab-separated-values")
-    @ResponseBody
-    public String getBookmarksTSV(@ModelAttribute(Model.BOOKMARKS) final List<Bookmark> bookmarks) {
-        StringBuilder sb = new StringBuilder("label\turl\n");
-        for (Bookmark bookmark : bookmarks) {
-            sb.append(bookmark.getLabel()).append('\t');
-            String url = bookmark.getUrl();
-            if (url.charAt(0) == '/') {
-                sb.append("http://lane.stanford.edu");
-            }
-            sb.append(url).append('\n');
-        }
-        return sb.toString();
-    }
 
     public void setBookmarkDAO(final BookmarkDAO bookmarkDAO) {
         this.bookmarkDAO = bookmarkDAO;
---
+++ src/main/java/edu/stanford/irt/laneweb/bookmarks/BookmarkExportController.java
@@ -0,0 +1,69 @@
+package edu.stanford.irt.laneweb.bookmarks;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.stanford.irt.laneweb.model.Model;
+
+@Controller
+@RequestMapping(value = "/bookmarks/export")
+public class BookmarkExportController extends BookmarkController {
+
+    private static final String HTML_HEADER = "<!DOCTYPE NETSCAPE-Bookmark-file-1>\n"
+            + "<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\">\n"
+            + "<TITLE>Lane Bookmarks</TITLE>\n"
+            + "<H1>Lane Bookmarks</H1>\n"
+            + "<DL><p\n>";
+
+    private static final String HTML_ITEM = "<DT><A HREF=\"";
+
+    /**
+     * Produces importable html bookmarks.
+     * 
+     * @param bookmarks
+     *            the Bookmarks
+     * @return the bookmarks in netscape bookmark format
+     */
+    @RequestMapping(value = "/bookmarks.html", method = RequestMethod.GET, produces = "application/x-netscape-bookmarks")
+    @ResponseBody
+    public String getBookmarksHTML(@ModelAttribute(Model.BOOKMARKS) final List<Bookmark> bookmarks) {
+        StringBuilder sb = new StringBuilder(HTML_HEADER);
+        for (Bookmark bookmark : bookmarks) {
+            sb.append(HTML_ITEM);
+            maybePrependLane(sb, bookmark.getUrl());
+            sb.append("\">").append(bookmark.getLabel()).append("</A>\n");
+        }
+        sb.append("</DL><p>");
+        return sb.toString();
+    }
+
+    /**
+     * Converts bookmarks to a list of tab separated values.
+     * 
+     * @param bookmarks
+     *            the Bookmarks
+     * @return the bookmarks as a list of tab separated values
+     */
+    @RequestMapping(value = "/bookmarks.tsv", method = RequestMethod.GET, produces = "text/tab-separated-values")
+    @ResponseBody
+    public String getBookmarksTSV(@ModelAttribute(Model.BOOKMARKS) final List<Bookmark> bookmarks) {
+        StringBuilder sb = new StringBuilder("label\turl\n");
+        for (Bookmark bookmark : bookmarks) {
+            sb.append(bookmark.getLabel()).append('\t');
+            maybePrependLane(sb, bookmark.getUrl());
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    private void maybePrependLane(final StringBuilder sb, final String url) {
+        if (url.charAt(0) == '/') {
+            sb.append("http://lane.stanford.edu");
+        }
+    }
+}
