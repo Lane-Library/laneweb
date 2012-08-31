4f197d2d9193c2507a7964b9516a352669841704
Charles Yates <ceyates@stanford.edu>
2012-08-31 21:30:27 +0000

    also test a non-local links
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@104081 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/test/bookmarks/bookmark-link-test.js
+++ src/main/webapp/resources/test/bookmarks/bookmark-link-test.js
@@ -4,15 +4,11 @@
 Y.applyConfig({fetchCSS:true});
 Y.use('console', 'test', "node-event-simulate", function(Y) {
     
-    bookmarkLinkTestCase = new Y.Test.Case({
+    var bookmarkLinkTestCase = new Y.Test.Case({
         
         name : "BookmarkLink Test Case",
         
-        link : null,
-        
-        setUp : function() {
-            this.link = Y.lane.BookmarkLink;
-        },
+        link : Y.lane.BookmarkLink,
     
         testExists : function() {
             Y.Assert.isTrue(Y.Lang.isObject(this.link));
@@ -20,6 +16,11 @@ Y.use('console', 'test', "node-event-simulate", function(Y) {
         
         testDontShowAlreadyBookmarked : function() {
             Y.one("#alreadyBookmarked").simulate("mouseover");
+            Y.Assert.areEqual(0, this.link.get("status"));
+        },
+        
+        testAnotherDontShowAlreadyBookmarked : function() {
+            Y.one("#anotherAlreadyBookmarked").simulate("mouseover");
             Y.Assert.areEqual(0, this.link.get("status"));
         }
         
