1d886193655c954e020ff907fa56ae65714597d3
Charles Yates <ceyates@stanford.edu>
2012-09-16 16:55:06 +0000

    add BaseProxyURLDataBinder
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@104484 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/BaseProxyURLDataBinder.java
@@ -0,0 +1,45 @@
+package edu.stanford.irt.laneweb.servlet.binding;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import edu.stanford.irt.laneweb.ipgroup.IPGroup;
+import edu.stanford.irt.laneweb.model.Model;
+import edu.stanford.irt.laneweb.model.ModelUtil;
+import edu.stanford.irt.laneweb.proxy.Ticket;
+
+/**
+ * A DataBinder that binds the base proxy url to the Model.  It requires that the sunetid,
+ * ticket, proxyLink, ipgroup and basePath, if present, have already been put in the model.
+ */
+public class BaseProxyURLDataBinder implements DataBinder {
+
+    private static final String EZPROXY_LINK = "http://laneproxy.stanford.edu/login?user=";
+
+    private static final String TICKET = "&ticket=";
+
+    private static final String URL = "&url=";
+
+    private static final String WEBAUTH_LINK = "/secure/apps/proxy/credential?url=";
+
+    @Override
+    public void bind(final Map<String, Object> model, final HttpServletRequest request) {
+        if (model.get(Model.PROXY_LINKS).equals(Boolean.TRUE)) {
+            StringBuilder baseProxyURL = new StringBuilder();
+            IPGroup ipgroup = ModelUtil.getObject(model, Model.IPGROUP, IPGroup.class);
+            if (IPGroup.SHC.equals(ipgroup) || IPGroup.LPCH.equals(ipgroup)) {
+                baseProxyURL.append(EZPROXY_LINK);
+            } else {
+                String sunetid = ModelUtil.getString(model, Model.SUNETID);
+                Ticket ticket = ModelUtil.getObject(model, Model.TICKET, Ticket.class);
+                if (ticket == null || sunetid == null) {
+                    baseProxyURL.append(ModelUtil.getString(model, Model.BASE_PATH)).append(WEBAUTH_LINK);
+                } else {
+                    baseProxyURL.append(EZPROXY_LINK).append(sunetid).append(TICKET).append(ticket).append(URL);
+                }
+            }
+            model.put(Model.BASE_PROXY_URL, baseProxyURL.toString());
+        }
+    }
+}
