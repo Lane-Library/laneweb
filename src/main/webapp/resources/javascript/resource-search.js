6f0492d1a492e0333a8cb0f4d1d4a5a49b9aadd0
Charles Yates <ceyates@stanford.edu>
2012-09-24 21:47:29 +0000

    add querymap-controller.js and ResourceSearchView class
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@104712 c440208b-5602-0410-a095-92527a996296

--- src/main/webapp/resources/javascript/resource-search.js
+++ src/main/webapp/resources/javascript/resource-search.js
@@ -5,6 +5,7 @@
     var lane = Y.lane,
         model = lane.Model,
         basePath = model.get(model.BASE_PATH) || "",
+        SEARCH_INTERVAL = 2000,
     
     ResourceSearch = {
 
@@ -16,17 +17,35 @@
             Y.io(basePath + "/apps/search/json?q=" + query + resourcesParams, {
                 on : {
                     success : function(id, o) {
-                        this.fire("success", Y.JSON.parse(o.responseText));
+                    	var result = Y.JSON.parse(o.responseText);
+                        this.fire("update", result);
+                        if (result.status === "running") {
+                        	Y.later(SEARCH_INTERVAL, window, function() {
+                        		ResourceSearch.search(query, resources);
+                        	});
+                        }
                     }
                 },
                 context : this
             });
         }
     
+    },
+    
+    ResourceSearchView = function(id, fn) {
+    	this._handle = ResourceSearch.on("update", function(result) {
+    		var resourceResult = result.resources[id];
+			fn.call(resourceResult);
+    		if (resourceResult.status) {
+    			this._handle.detach();
+    		}
+    	}, this);
     };
 
     Y.augment(ResourceSearch, Y.EventTarget);
     
     lane.ResourceSearch = ResourceSearch;
+    
+    lane.ResourceSearchView = ResourceSearchView;
 	
 })();
