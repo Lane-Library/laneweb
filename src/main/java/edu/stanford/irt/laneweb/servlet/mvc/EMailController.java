1ad9eb508fd53a1892f47fb7ba0111aa4dca9aec
Charles Yates <ceyates@stanford.edu>
2012-09-26 01:12:59 +0000

    add EMailController case 76276 switch from using formmail to spring mail
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@104782 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/EMailController.java
@@ -0,0 +1,28 @@
+package edu.stanford.irt.laneweb.servlet.mvc;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EMailController {
+
+    @RequestMapping(value = "/apps/mail")
+    public Map<String, ?> sendMail(final Model model) {
+        return model.asMap();
+    }
+
+    @ModelAttribute
+    protected void getParameters(final HttpServletRequest request, final Model model) {
+        Map<String, String[]> map = request.getParameterMap();
+        for (Entry<String, String[]> entry : map.entrySet()) {
+            model.addAttribute(entry.getKey(), entry.getValue()[0]);
+        }
+    }
+}
