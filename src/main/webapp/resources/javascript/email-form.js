0d435b189fde1cf4ed12ef01c002a4dd5a86ab0b
Charles Yates <ceyates@stanford.edu>
2012-11-29 21:28:01 +0000

    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@106636 c440208b-5602-0410-a095-92527a996296

---
+++ src/main/webapp/resources/javascript/email-form.js
@@ -0,0 +1,9 @@
+(function(){
+	
+    var EmailForm = function(config) {
+        EmailForm.superclass.constructor.apply(this, arguments);
+    };
+    
+    Y.extend(EmailForm, Y.Base, {}, {});
+	
+})();
