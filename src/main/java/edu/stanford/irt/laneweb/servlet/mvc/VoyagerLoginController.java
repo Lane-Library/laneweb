1c984e712c8a8f0e73af2739d8b59135547a6fb5
Ryan Max Steinberg <ryanmax@stanford.edu>
2013-01-22 23:48:31 +0000

    remove logging used to resolve case 80483
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@108154 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/VoyagerLoginController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/VoyagerLoginController.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,8 +26,6 @@ public class VoyagerLoginController {
     @Autowired
     private LDAPDataBinder ldapDataBinder;
 
-    private final Logger log = LoggerFactory.getLogger(VoyagerLoginController.class);
-
     @Autowired
     private SunetIdAndTicketDataBinder sunetidTicketDataBinder;
 
@@ -38,11 +34,8 @@ public class VoyagerLoginController {
 
     @ModelAttribute(Model.UNIVID)
     public void getUnivid(final HttpServletRequest request, final org.springframework.ui.Model model) {
-        this.log.error("model before sunetidTicketDataBinder: " + model);
         this.sunetidTicketDataBinder.bind(model.asMap(), request);
-        this.log.error("model before ldapDataBinder: " + model);
         this.ldapDataBinder.bind(model.asMap(), request);
-        this.log.error("model after binding: " + model);
     }
 
     @RequestMapping(value = "/secure/voyager/{db}")
