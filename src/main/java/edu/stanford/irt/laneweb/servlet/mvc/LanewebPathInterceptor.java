e2ef331aa2e223a52b3d77343712134f79e393c0
Charles Yates <ceyates@stanford.edu>
2013-02-25 20:59:14 +0000

    remove personal afs space directories as content sources
    git-svn-id: https://irt-svn.stanford.edu/repos/irt/lane/laneweb/trunk@109712 c440208b-5602-0410-a095-92527a996296

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/LanewebPathInterceptor.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/LanewebPathInterceptor.java
@@ -2,8 +2,6 @@ package edu.stanford.irt.laneweb.servlet.mvc;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,25 +13,9 @@ import edu.stanford.irt.laneweb.model.Model;
 
 public class LanewebPathInterceptor extends HandlerInterceptorAdapter {
 
-    private static final String[][] BASE_MAPPINGS = new String[][] {
-            { "/alainb", "file:/afs/ir.stanford.edu/users/a/l/alainb/laneweb" },
-            { "/ceyates", "file:/afs/ir.stanford.edu/users/c/e/ceyates/laneweb" },
-            { "/olgary", "file:/afs/ir.stanford.edu/users/o/l/olgary/laneweb" },
-            { "/ryanmax", "file:/afs/ir.stanford.edu/users/r/y/ryanmax/laneweb" },
-            { "/ajchrist", "file:/afs/ir.stanford.edu/users/a/j/ajchrist/laneweb" } };
-
-    private Map<String, URL> baseMappings;
-
     private URL defaultContentBase;
 
     private URL stageBase;
-
-    public LanewebPathInterceptor() throws MalformedURLException {
-        this.baseMappings = new HashMap<String, URL>(BASE_MAPPINGS.length);
-        for (String[] element : BASE_MAPPINGS) {
-            this.baseMappings.put(element[0], new URL(element[1]));
-        }
-    }
 
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
@@ -69,22 +51,12 @@ public class LanewebPathInterceptor extends HandlerInterceptorAdapter {
         if (servletPath.indexOf("/stage") == 0) {
             return contextPath + "/stage";
         }
-        for (String key : this.baseMappings.keySet()) {
-            if (servletPath.indexOf(key) == 0) {
-                return contextPath + key;
-            }
-        }
         return contextPath;
     }
 
     private URL getContentBase(final String servletPath) throws MalformedURLException {
         if (servletPath.indexOf("/stage") == 0) {
             return this.stageBase;
-        }
-        for (String key : this.baseMappings.keySet()) {
-            if (servletPath.indexOf(key) == 0) {
-                return new URL(this.baseMappings.get(key) + "/content");
-            }
         }
         return this.defaultContentBase;
     }
--- src/test/java/edu/stanford/irt/laneweb/servlet/mvc/LanewebPathInterceptorTest.java
+++ src/test/java/edu/stanford/irt/laneweb/servlet/mvc/LanewebPathInterceptorTest.java
@@ -17,8 +17,6 @@ import edu.stanford.irt.laneweb.model.Model;
 
 public class LanewebPathInterceptorTest {
 
-    private URL ceyatesContent;
-
     private URL defaultContentBase;
 
     private LanewebPathInterceptor interceptor;
@@ -32,23 +30,9 @@ public class LanewebPathInterceptorTest {
         this.interceptor = new LanewebPathInterceptor();
         this.defaultContentBase = new URL("file:/afs/ir.stanford.edu/groups/lane/beta2/live");
         this.stageBase = new URL("file:/afs/ir.stanford.edu/groups/lane/beta2/stage");
-        this.ceyatesContent = new URL("file:/afs/ir.stanford.edu/users/c/e/ceyates/laneweb/content");
         this.interceptor.setDefaultContentBase(this.defaultContentBase);
         this.interceptor.setStageBase(this.stageBase);
         this.request = createMock(HttpServletRequest.class);
-    }
-
-    @Test
-    public void testAFS() throws Exception {
-        expect(this.request.getRequestURI()).andReturn("/ceyates/index.html");
-        expect(this.request.getContextPath()).andReturn("");
-        this.request.setAttribute(Model.BASE_PATH, "/ceyates");
-        expect(this.request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE)).andReturn("/ceyates/index.html");
-        this.request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, "/index.html");
-        this.request.setAttribute(Model.CONTENT_BASE, this.ceyatesContent);
-        replay(this.request);
-        this.interceptor.preHandle(this.request, null, null);
-        verify(this.request);
     }
 
     @Test
@@ -57,19 +41,6 @@ public class LanewebPathInterceptorTest {
         expect(this.request.getContextPath()).andReturn("/laneweb");
         this.request.setAttribute(Model.BASE_PATH, "/laneweb");
         this.request.setAttribute(Model.CONTENT_BASE, this.defaultContentBase);
-        replay(this.request);
-        this.interceptor.preHandle(this.request, null, null);
-        verify(this.request);
-    }
-
-    @Test
-    public void testContextPathAndAFS() throws Exception {
-        expect(this.request.getRequestURI()).andReturn("/laneweb/ceyates/index.html");
-        expect(this.request.getContextPath()).andReturn("/laneweb");
-        this.request.setAttribute(Model.BASE_PATH, "/laneweb/ceyates");
-        expect(this.request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE)).andReturn("/ceyates/index.html");
-        this.request.setAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE, "/index.html");
-        this.request.setAttribute(Model.CONTENT_BASE, this.ceyatesContent);
         replay(this.request);
         this.interceptor.preHandle(this.request, null, null);
         verify(this.request);
