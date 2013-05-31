0455feeb784306dc2d65bc448ab6194124bcdbb5
Charles Yates <ceyates@stanford.edu>
2013-05-31 08:55:39 -0700

    changes related to irt-cocoon changes

--- src/main/java/edu/stanford/irt/laneweb/cocoon/XIncludeExceptionListenerImpl.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/XIncludeExceptionListenerImpl.java
@@ -1,9 +1,8 @@
 package edu.stanford.irt.laneweb.cocoon;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.Locator;
 
 import edu.stanford.irt.cocoon.xml.XIncludeExceptionListener;
 
@@ -11,13 +10,17 @@ public class XIncludeExceptionListenerImpl implements XIncludeExceptionListener 
 
     private Logger log = LoggerFactory.getLogger(XIncludeExceptionListener.class);
 
-    private Map<String, Object> model;
-
-    public void exception(final Exception e) {
-        this.log.error(this.model.toString(), e);
+    public void exception(Locator locator, final Exception e) {
+        this.log.error(createMessage(locator), e);
     }
-
-    public void setModel(final Map<String, Object> model) {
-        this.model = model;
+    
+    private String createMessage(Locator locator) {
+        return new StringBuilder("XInclude failed: ")
+        .append(locator.getSystemId())
+        .append(" line:")
+        .append(locator.getLineNumber())
+        .append(" column:")
+        .append(locator.getColumnNumber())
+        .toString();
     }
 }
