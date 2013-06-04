7049e75f391f6c4d271f8ebb4c1f0a25b1c4fea3
Charles Yates <ceyates@stanford.edu>
2013-06-04 13:40:30 -0700

    /error.html not cacheable, request-uri and referrer put into result document by laneweb.xsl

--- src/test/java/edu/stanford/irt/laneweb/cocoon/CacheableSelectorTest.java
+++ src/test/java/edu/stanford/irt/laneweb/cocoon/CacheableSelectorTest.java
@@ -1,7 +1,7 @@
 package edu.stanford.irt.laneweb.cocoon;
 
-import static org.junit.Assert.*;
-import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,12 +11,11 @@ import org.junit.Test;
 
 import edu.stanford.irt.laneweb.model.Model;
 
-
 public class CacheableSelectorTest {
-    
-    private CacheableSelector selector;
-    
+
     private Map<String, Object> model;
+
+    private CacheableSelector selector;
 
     @Before
     public void setUp() throws Exception {
@@ -25,21 +24,9 @@ public class CacheableSelectorTest {
     }
 
     @Test
-    public void testSunetid() {
-        this.model.put(Model.SUNETID, "ditenus");
-        assertFalse(this.selector.select(null, this.model, null));
-    }
-
-    @Test
-    public void testQuery() {
-        this.model.put(Model.QUERY, "query");
-        assertFalse(this.selector.select(null, this.model, null));
-    }
-
-    @Test
-    public void testEmrid() {
-        this.model.put(Model.EMRID, "emrid");
-        assertFalse(this.selector.select(null, this.model, null));
+    public void testBasePathNotStage() {
+        this.model.put(Model.BASE_PATH, "/");
+        assertTrue(this.selector.select(null, this.model, null));
     }
 
     @Test
@@ -55,8 +42,26 @@ public class CacheableSelectorTest {
     }
 
     @Test
-    public void testBasePathNotStage() {
-        this.model.put(Model.BASE_PATH, "/");
-        assertTrue(this.selector.select(null, this.model, null));
+    public void testEmrid() {
+        this.model.put(Model.EMRID, "emrid");
+        assertFalse(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testErrorDotHtml() {
+        this.model.put(Model.SITEMAP_URI, "/error.html");
+        assertFalse(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testQuery() {
+        this.model.put(Model.QUERY, "query");
+        assertFalse(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testSunetid() {
+        this.model.put(Model.SUNETID, "ditenus");
+        assertFalse(this.selector.select(null, this.model, null));
     }
 }
