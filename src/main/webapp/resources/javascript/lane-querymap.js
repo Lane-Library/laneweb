629c4f0cf0e325aeafca1656da8539e1b99ff8ce
Charles Yates <ceyates@stanford.edu>
2013-06-18 09:23:00 -0700

    global reference not required

--- src/main/webapp/resources/javascript/lane-querymap.js
+++ src/main/webapp/resources/javascript/lane-querymap.js
@@ -2,33 +2,33 @@
 //check if there is a query
 (function() {
 	
-	var model = Y.lane.Model,
-	    query = model.get(model.QUERY),
-	    basePath = model.get(model.BASE_PATH) || "",
-	    encodedQuery = model.get(model.URL_ENCODED_QUERY),
+	var Model = Y.lane.Model,
+	    query = Model.get(Model.QUERY),
+	    basePath = Model.get(Model.BASE_PATH) || "",
+	    encodedQuery = Model.get(Model.URL_ENCODED_QUERY),
 	    queryMapping = Y.one('#queryMapping');
         if (query && queryMapping) {
             Y.io(basePath + '/apps/querymap/json?q=' + encodedQuery, {
                 on:{
                 success: function(id, o) {
-                    var anchor, span, i;
-                    LANE.search.querymap = Y.JSON.parse(o.responseText);
-                    if (LANE.search.querymap.resourceMap) {
-                        LANE.search.querymap.getResourcesString = function() {
+                    var anchor, span, i,
+                        queryMap = Y.JSON.parse(o.responseText);
+                    if (queryMap.resourceMap) {
+                        queryMap.getResourcesString = function() {
                             var i, separator = "; ", string = "";
-                            for (i = 0; i < LANE.search.querymap.resourceMap.resources.length; i++) {
-                                string += LANE.search.querymap.resourceMap.resources[i].label;
-                                if(i != LANE.search.querymap.resourceMap.resources.length-1){
+                            for (i = 0; i < queryMap.resourceMap.resources.length; i++) {
+                                string += queryMap.resourceMap.resources[i].label;
+                                if(i != queryMap.resourceMap.resources.length-1){
                                     string += separator;
                                 }
                             }
                             return string;
                         };
-                        LANE.search.querymap.getResultCounts = function() {
+                        queryMap.getResultCounts = function() {
                             var url = basePath + '/apps/search/json?q=' + encodedQuery, i;
-                            for (i = 0; i < LANE.search.querymap.resourceMap.resources.length; i++) {
-                                if (!LANE.search.querymap.resourceMap.resources[i].status) {
-                                    url += '&r=' + LANE.search.querymap.resourceMap.resources[i].id;
+                            for (i = 0; i < queryMap.resourceMap.resources.length; i++) {
+                                if (!queryMap.resourceMap.resources[i].status) {
+                                    url += '&r=' + queryMap.resourceMap.resources[i].id;
                                 }
                             }
                             url += '&rd=' + Math.random();
@@ -36,7 +36,7 @@
                                 on:{
                                     success: function(id, o) {
                                             var results = Y.JSON.parse(o.responseText),
-                                                rs = LANE.search.querymap.resourceMap.resources,
+                                                rs = queryMap.resourceMap.resources,
                                                 i, needMore = false, result;
                                             for (i = 0; i < rs.length; i++) {
                                                 if (!rs[i].status) {
@@ -56,7 +56,7 @@
                                                 }
                                             }
                                             if (needMore) {
-                                                setTimeout(LANE.search.querymap.getResultCounts, 2000);
+                                                setTimeout(queryMap.getResultCounts, 2000);
                                             }
                                             //queryMapping.style.display = 'inline';
                                             Y.fire('lane:popin', queryMapping);
@@ -64,25 +64,25 @@
                                     }
                                 });
                             };
-                            for (i = 0; i < LANE.search.querymap.resourceMap.resources.length; i++) {
-                                LANE.search.querymap.resourceMap.resources[i].status = '';
+                            for (i = 0; i < queryMap.resourceMap.resources.length; i++) {
+                                queryMap.resourceMap.resources[i].status = '';
                                 span = document.createElement('span');
                                 anchor = document.createElement('a');
-                                anchor.title = 'QueryMapping: ' + LANE.search.querymap.resourceMap.resources[i].label;
+                                anchor.title = 'QueryMapping: ' + queryMap.resourceMap.resources[i].label;
                                 span.appendChild(anchor);
-                                anchor.appendChild(document.createTextNode(LANE.search.querymap.resourceMap.resources[i].label));
+                                anchor.appendChild(document.createTextNode(queryMap.resourceMap.resources[i].label));
                                 queryMapping.append(span);
-                                LANE.search.querymap.resourceMap.resources[i].anchor = anchor;
+                                queryMap.resourceMap.resources[i].anchor = anchor;
                             }
                             if (document.getElementById('queryMappingDescriptor')) {
-                                document.getElementById('queryMappingDescriptor').appendChild(document.createTextNode(LANE.search.querymap.resourceMap.descriptor));
+                                document.getElementById('queryMappingDescriptor').appendChild(document.createTextNode(queryMap.resourceMap.descriptor));
                             }
-                            LANE.search.querymap.getResultCounts();
+                            queryMap.getResultCounts();
                             // track mapped term, descriptor, and resources
                             Y.fire("lane:trackableEvent", {
                                 category: "lane:queryMapping",
-                                action: "query=" + query + "; descriptor=" + LANE.search.querymap.resourceMap.descriptor.descriptorName,
-                                label: "resources=" + LANE.search.querymap.getResourcesString()
+                                action: "query=" + query + "; descriptor=" + queryMap.resourceMap.descriptor.descriptorName,
+                                label: "resources=" + queryMap.getResourcesString()
                             });
                         }
                     }
