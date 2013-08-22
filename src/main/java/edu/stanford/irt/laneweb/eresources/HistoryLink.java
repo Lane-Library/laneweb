4d6cf1847e0f55f82240341a00770cb8fac674db
Charles Yates <ceyates@stanford.edu>
2013-08-22 13:33:31 -0700

    accommodate lack of additional_text column in history data

--- src/main/java/edu/stanford/irt/laneweb/eresources/HistoryLink.java
+++ src/main/java/edu/stanford/irt/laneweb/eresources/HistoryLink.java
@@ -1,9 +1,14 @@
 package edu.stanford.irt.laneweb.eresources;
 
 /**
- * A temporary Link subclass to be used until the LINK_TEXT column is added to the history H_LINK table.
+ * A temporary Link subclass to be used until the LINK_TEXT column is added to the history H_LINK table. And until the
+ * ADDITIONAL_TEXT is added to that table.
  */
 public class HistoryLink extends Link {
+
+    private String additionalText;
+
+    private Boolean isFirstLink;
 
     private String linkText;
 
@@ -12,32 +17,80 @@ public class HistoryLink extends Link {
     }
 
     @Override
-    public String getLinkText() {
-        if (this.linkText == null) {
+    public String getAdditionalText() {
+        if (this.additionalText == null) {
             StringBuilder sb = new StringBuilder();
-            Version version = getVersion();
-            String summaryHoldings = version.getSummaryHoldings();
-            if (summaryHoldings != null && version.getLinks().size() == 1) {
-                sb.append(summaryHoldings);
-                String dates = version.getDates();
-                if (dates != null) {
-                    sb.append(", ").append(dates);
+            if (isFirstLink()) {
+                sb.append(getVersion().getPrimaryAdditionalText());
+                if (sb.length() == 1 && getLabel() != null) {
+                    sb.append(getLabel());
+                }
+                if (getInstruction() != null) {
+                    if (sb.length() > 1) {
+                        sb.append(", ");
+                    }
+                    sb.append(getInstruction());
+                }
+                if (sb.length() > 1) {
+                    sb.append(" ");
                 }
             } else {
-                String label = getLabel();
-                if (label != null) {
-                    sb.append(label);
+                if (getInstruction() != null) {
+                    sb.append(" ").append(getInstruction());
+                }
+                if (getVersion().getPublisher() != null) {
+                    sb.append(" ").append(getVersion().getPublisher());
                 }
             }
-            if (sb.length() == 0) {
-                sb.append(getUrl());
+            this.additionalText = sb.toString();
+        }
+        return this.additionalText;
+    }
+
+    @Override
+    public String getLinkText() {
+        if (this.linkText == null) {
+            if (isFirstLink()) {
+                this.linkText = getVersion().getEresource().getTitle();
+            } else {
+                StringBuilder sb = new StringBuilder();
+                Version version = getVersion();
+                String summaryHoldings = version.getSummaryHoldings();
+                if (summaryHoldings != null && version.getLinks().size() == 1) {
+                    sb.append(summaryHoldings);
+                    String dates = version.getDates();
+                    if (dates != null) {
+                        sb.append(", ").append(dates);
+                    }
+                } else {
+                    String label = getLabel();
+                    if (label != null) {
+                        sb.append(label);
+                    }
+                }
+                if (sb.length() == 0) {
+                    sb.append(getUrl());
+                }
+                String description = version.getDescription();
+                if (description != null) {
+                    sb.append(" ").append(description);
+                }
+                this.linkText = sb.toString();
             }
-            String description = version.getDescription();
-            if (description != null) {
-                sb.append(" ").append(description);
-            }
-            this.linkText = sb.toString();
         }
         return this.linkText;
+    }
+
+    private boolean isFirstLink() {
+        if (this.isFirstLink == null) {
+            Version version = getVersion();
+            if (equals(version.getLinks().iterator().next())
+                    && version.equals(version.getEresource().getVersions().iterator().next())) {
+                this.isFirstLink = Boolean.TRUE;
+            } else {
+                this.isFirstLink = Boolean.FALSE;
+            }
+        }
+        return Boolean.valueOf(this.isFirstLink);
     }
 }
