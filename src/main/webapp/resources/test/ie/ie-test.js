65851c530ef51d1490f171e476b167caa7dfe2f7
Charles Yates <ceyates@stanford.edu>
2013-11-04 14:54:51 -0800

    more javascript test coverage

--- src/main/webapp/resources/test/ie/ie-test.js
+++ src/main/webapp/resources/test/ie/ie-test.js
@@ -5,7 +5,56 @@ Y.applyConfig({fetchCSS:true});
 Y.use('node-event-simulate', 'console', 'test', function(Y){
 
     var ieTestCase = new Y.Test.Case({
-        name: 'Lane IE Test Case'
+        name: 'Lane IE Test Case',
+        
+        testNavParentHasClassHover: function() {
+            var menu = Y.one("#laneNavMenu");
+            menu.simulate("mouseover");
+            Y.Assert.isTrue(menu.hasClass("hover"));
+            menu.simulate("mouseout");
+            Y.Assert.isFalse(menu.hasClass("hover"));
+        },
+        
+        testFavoritesClassHover: function() {
+            var menu = Y.one("#favorites");
+            menu.simulate("mouseover");
+            Y.Assert.isTrue(menu.hasClass("hover"));
+            menu.simulate("mouseout");
+            Y.Assert.isFalse(menu.hasClass("hover"));
+        },
+        
+        testLibraryContextHasNoBorder: function() {
+            var node = Y.one("#libraryContact").one("li");
+            Y.Assert.areEqual("none", node.getStyle("borderLeft"));
+        },
+        
+        testType1DetailsDivHasClassChild: function() {
+            Y.Assert.isTrue(Y.one(".type1 .details > div").hasClass("child"));
+        },
+        
+        testFeedbackLinkHasClassIe6Feedback: function() {
+            Y.Assert.isTrue(Y.one("#feedbackLink").hasClass("ie6Feedback"));
+        },
+        
+        testSpanAfterLastInModule: function() {
+            var node = Y.one(".module");
+            Y.Assert.isTrue(node.get("lastChild").hasClass("after"));
+        },
+        
+        testSpanAfterNextTopResources: function() {
+            var node = Y.one("#topResources");
+            Y.Assert.isTrue(node.next().hasClass("after"));
+        },
+        
+        testBookmarkletNotIEDisplayNone: function() {
+            Y.Assert.areEqual("none", Y.one("#bookmarkletNotIE").getStyle("display"));
+        },
+        
+        testBookmarkletIEDisplayBlock: function() {
+            Y.Assert.areEqual("block", Y.one("#bookmarkletIE").getStyle("display"));
+        }
+        
+           
     });
     
     Y.one('body').addClass('yui3-skin-sam');
--- src/main/webapp/resources/test/popin/popin-test.js
+++ src/main/webapp/resources/test/popin/popin-test.js
@@ -5,7 +5,30 @@ Y.applyConfig({fetchCSS:true});
 Y.use('node-event-simulate', 'console', 'test', function(Y){
 
     var popinTestCase = new Y.Test.Case({
-        name: 'Lane Popin Test Case'
+        name: 'Lane Popin Test Case',
+        
+        testPopinEvent: function() {
+            var node = Y.one("#spellCheck");
+            node.setStyle("display", "inline");
+            Y.fire("lane:popin", node);
+            Y.Assert.isTrue(node.hasClass("active"));
+            Y.Assert.isTrue(Y.one("#popin").hasClass("active"));
+            Y.Assert.areEqual("0px", Y.one("#searchResults").getStyle("margin-top"));
+            Y.Assert.areEqual("7px", Y.one(".rightSearchTips").getStyle("margin-top"));
+            Y.Assert.areEqual("-28px", Y.one("#searchFacets").getStyle("margin-top"));
+        },
+        
+        testPopinEventIE8: function() {
+            Y.UA.ie = 8;
+            var node = Y.one("#queryMapping");
+            node.setStyle("display", "inline");
+            Y.fire("lane:popin", node);
+            Y.Assert.isTrue(node.hasClass("active"));
+            Y.Assert.isTrue(Y.one("#popin").hasClass("active"));
+            Y.Assert.areEqual("-3px", Y.one("#searchResults").getStyle("margin-top"));
+            Y.Assert.areEqual("7px", Y.one(".rightSearchTips").getStyle("margin-top"));
+            Y.Assert.areEqual("-28px", Y.one("#searchFacets").getStyle("margin-top"));
+        }
     });
     
     Y.one('body').addClass('yui3-skin-sam');
