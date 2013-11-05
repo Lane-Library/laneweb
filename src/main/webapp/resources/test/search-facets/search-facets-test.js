1014707ab03fdb8a9b578542d48379e577502832
Charles Yates <ceyates@stanford.edu>
2013-11-04 17:06:36 -0800

    more javascript test coverage

--- src/main/webapp/resources/test/search-facets/search-facets-test.js
+++ src/main/webapp/resources/test/search-facets/search-facets-test.js
@@ -5,7 +5,22 @@ Y.applyConfig({fetchCSS:true});
 Y.use('node-event-simulate', 'console', 'test', function(Y){
 
     var searchFacetsTestCase = new Y.Test.Case({
-        name: 'Lane Search Facets Test Case' 
+        name: 'Lane Search Facets Test Case',
+        
+        testFacetClick: function() {
+            Y.one("#b-bFacet").simulate("click");
+            Y.Assert.areEqual("/plain/search/b/b-b.html?source=b-b&amp;q=undefined", Y.one("#searchResults").get("innerHTML"));
+        },
+        
+        testAnotherFacetClick: function() {
+            Y.one("#c-cFacet").simulate("click");
+            Y.Assert.areEqual("/plain/search/c/c-c.html?source=c-c&amp;q=undefined", Y.one("#searchResults").get("innerHTML"));
+        },
+        
+        testYetAnotherFacetClick: function() {
+            Y.one("#a-aFacet").simulate("click");
+            Y.Assert.areEqual("a-a results", Y.one("#searchResults").get("innerHTML"));
+        }
     });
 
     
