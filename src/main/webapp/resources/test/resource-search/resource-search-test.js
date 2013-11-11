7bfc8e2cfb156fdf70fe89db02b6340650df0015
Charles Yates <ceyates@stanford.edu>
2013-11-11 11:09:30 -0800

    more javascript test coverage

--- src/main/webapp/resources/test/resource-search/resource-search-test.js
+++ src/main/webapp/resources/test/resource-search/resource-search-test.js
@@ -4,9 +4,25 @@
 
 Y.applyConfig({fetchCSS:true});
 Y.use('console', 'test', function(Y){
+    
+    Y.io = function(url, config) {
+        config.on.success.apply(this, [0,{responseText:'{"status":"successful"}'}]);
+    };
 
     var resourceSearchTestCase = new Y.Test.Case({
-        name: 'Lane ResourceSearch Test Case'
+        name: 'Lane ResourceSearch Test Case',
+        
+        search: Y.lane.ResourceSearch,
+        
+        view: new Y.lane.ResourceResultView(0, this.foo),
+        
+        foo: function(id, result) {
+            this.result = result;
+        },
+        
+        testSearch: function() {
+            this.search.search("query", ["pubmed","scopus"]);
+        }
     });
     
     Y.one('body').addClass('yui3-skin-sam');
