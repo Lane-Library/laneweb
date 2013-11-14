045870b7f3123f42fe3501d7c8c8d76648d06dad
Charles Yates <ceyates@stanford.edu>
2013-11-14 13:42:21 -0800

    fix problem with test involving cookies

--- src/main/webapp/resources/test/persistent-login/persistent-login-test.js
+++ src/main/webapp/resources/test/persistent-login/persistent-login-test.js
@@ -1,4 +1,14 @@
 (function(){
+    
+    Y.lane.Location.on("hrefChange", function(event) {
+        event.preventDefault();
+        persistentLoginTestCase.href = event.newVal;
+    });
+    
+    Y.all("a").on("click", function(event) {
+        event.preventDefault();
+        persistentLoginTestCase.path = event.target.get("pathname");
+    });
 
     Y.io = function(url, config) {
         config.on.success.apply(this, [0,{responseText:'<div><a id="yes-persistent-login">yes</a><a id="no-persistent-login">no</a><input type="checkbox" id="dont-ask-again"/></div>'}]);
@@ -6,45 +16,65 @@
     var persistentLoginTestCase = new Y.Test.Case({
         name: 'persistent-login Test Case',
         
+        cookie: Y.Cookie.get("persistent-preference"),
+        
+        href: null,
+        
+        path: null,
+        
         tearDown: function() {
             Y.lane.Lightbox.hide();
             Y.lane.Lightbox.setContent("");
+            this.href = null;
+            this.path = null;
         },
         
         testLoginClickYesClick: function() {
             Y.one("#login").simulate("click");
-            var yes = Y.one("#yes-persistent-login");
-            var handle = yes.on("click", function(event) {
-                event.preventDefault();
-            });
-            yes.simulate("click");
-            handle.detach();
-            //TODO: why &url=, not ?url=
-            Y.Assert.isTrue(yes.get("href").indexOf("/secure/persistentLogin.html&url=") > 0);
+            if (this.cookie === "denied") {
+                Y.Assert.areSame("/secure/persistentLogin.html?pl=false&ur", this.href.substring(0, 40));
+            } else {
+                var yes = Y.one("#yes-persistent-login");
+                var handle = yes.on("click", function(event) {
+                    event.preventDefault();
+                });
+                yes.simulate("click");
+                handle.detach();
+                //TODO: why &url=, not ?url=
+                Y.Assert.isTrue(yes.get("href").indexOf("/secure/persistentLogin.html&url=") > 0);
+            }
         },
         
         testLoginClickNoClick: function() {
             Y.one("#login").simulate("click");
-            var no = Y.one("#no-persistent-login");
-            var handle = no.on("click", function(event) {
-                event.preventDefault();
-            });
-            no.simulate("click");
-            handle.detach();
-            //TODO: why &url=, not ?url=
-            Y.Assert.isTrue(no.get("href").indexOf("/secure/persistentLogin.html&url=") > 0);
+            if (this.cookie === "denied") {
+                Y.Assert.areSame("/secure/persistentLogin.html?pl=false&ur", this.href.substring(0, 40));
+            } else {
+                var no = Y.one("#no-persistent-login");
+                var handle = no.on("click", function(event) {
+                    event.preventDefault();
+                });
+                no.simulate("click");
+                handle.detach();
+                //TODO: why &url=, not ?url=
+                Y.Assert.isTrue(no.get("href").indexOf("/secure/persistentLogin.html&url=") > 0);
+            }
         },
         
         testProxyLoginClick: function() {
             Y.one("#proxylogin").simulate("click");
-            var yes = Y.one("#yes-persistent-login");
-            var handle = yes.on("click", function(event) {
-                event.preventDefault();
-            });
-            yes.simulate("click");
-            handle.detach();
-            //TODO: why &url=, not ?url=
-            Y.Assert.isTrue(yes.get("href").indexOf("/secure/persistentLogin.html&url=") > 0);
+            if (this.cookie === "denied") {
+                Y.Assert.isTrue(-1 < this.path.indexOf("secure/apps/proxy/credential") < 2);
+            } else {
+                var yes = Y.one("#yes-persistent-login");
+                var handle = yes.on("click", function(event) {
+                    event.preventDefault();
+                });
+                yes.simulate("click");
+                handle.detach();
+                //TODO: why &url=, not ?url=
+                Y.Assert.isTrue(yes.get("href").indexOf("/secure/persistentLogin.html&url=") > 0);
+            }
         }
     });
 
@@ -58,5 +88,4 @@
     Y.Test.Runner.add(persistentLoginTestCase);
     Y.Test.Runner.masterSuite.name = "persistent-login.js";
     Y.Test.Runner.run();
-    
 })();
