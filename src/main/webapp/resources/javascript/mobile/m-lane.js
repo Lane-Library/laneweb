8e86148513214ab645bb54de8559dfaec6b41505
Ryan Max Steinberg <ryanmax@stanford.edu>
2014-01-28 14:16:19 -0800

    $.mobile.minScrollBack is deprecated in jqm 1.4 and jqm scrolling has a timer of 150ms

--- src/main/webapp/resources/javascript/mobile/m-lane.js
+++ src/main/webapp/resources/javascript/mobile/m-lane.js
@@ -17,19 +17,19 @@ if (typeof ($.LANE) === "undefined") {
  * @param activePage
  */
 $.LANE.scrollPage = function(activePage){
-    var delay = 50, // wait for jqm to finish its scrolling. TODO: verify this is long enough on slower devices
+    var delay = 160, // wait for jqm to finish its scrolling. TODO: verify this is long enough on slower devices
     hideSearchScroll = 100,
     hideHeaderScroll = 50;
-    if(activePage.find(".results").size() || activePage.find(".absInfo").size()){ // search results and abstract/info pages
+    if(activePage.find(".results li").size() > 1 || activePage.find(".absInfo").size()){ // search results and abstract/info pages
         setTimeout(function(){
-            if(window.pageYOffset < $.mobile.minScrollBack){
+            if(window.pageYOffset < hideSearchScroll){
                 window.scrollTo(0, hideSearchScroll);
             }
         },delay);
     }
     else if(activePage.attr('id') !== '_home' && activePage.find(".resourceList").size()){ // resource list pages
         setTimeout(function(){
-            if(window.pageYOffset < $.mobile.minScrollBack){
+            if(window.pageYOffset < hideHeaderScroll){
                 window.scrollTo(0, hideHeaderScroll);
             }
         },delay);
