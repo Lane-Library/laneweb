1e220280e2083cfdf5d9f96d77623eaf00bad22f
Charles Yates <ceyates@stanford.edu>
2014-02-12 08:34:59 -0800

    inject contentBase as Resource instead of URL

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ContentBaseAwareHttpRequestHandler.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/ContentBaseAwareHttpRequestHandler.java
@@ -1,13 +1,10 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
@@ -15,10 +12,10 @@ import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.ResourceNotFoundException;
 
 public class ContentBaseAwareHttpRequestHandler extends ResourceHttpRequestHandler {
-    
-    private URL contentBase;
 
-    public ContentBaseAwareHttpRequestHandler(final URL contentBase) {
+    private Resource contentBase;
+
+    public ContentBaseAwareHttpRequestHandler(final Resource contentBase) {
         this.contentBase = contentBase;
     }
 
@@ -27,25 +24,19 @@ public class ContentBaseAwareHttpRequestHandler extends ResourceHttpRequestHandl
         Resource resource = null;
         String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
         if (path == null) {
-            throw new IllegalStateException("Required request attribute '" + HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
-                    + "' is not set");
-        }
-        Resource contentBase;
-        try {
-            contentBase = new UrlResource(this.contentBase.toString() + "/");
-        } catch (MalformedURLException e) {
-            throw new IllegalArgumentException(e.getMessage(), e);
+            throw new IllegalStateException("Required request attribute '"
+                    + HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE + "' is not set");
         }
         try {
-            resource = contentBase.createRelative(path);
-            if (resource.exists() && resource.isReadable()) {
-                return resource;
+            resource = this.contentBase.createRelative(path);
+            if (!resource.exists() || !resource.isReadable()) {
+                resource = super.getResource(request);
             }
         } catch (IOException e) {
             throw new LanewebException(e);
         }
-        resource = super.getResource(request);
         if (resource == null) {
+            // TODO: return null if don't need error.html page presented (eg images, etc)
             throw new ResourceNotFoundException(path + " not found");
         }
         return resource;
