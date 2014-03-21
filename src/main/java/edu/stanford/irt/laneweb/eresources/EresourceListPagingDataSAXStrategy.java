1799b3c63bba1b0a46273ece768cc604025a51e0
Charles Yates <ceyates@stanford.edu>
2014-03-21 12:17:40 -0700

    move AbstractXHTMLSAXStrategy to resources package

--- src/main/java/edu/stanford/irt/laneweb/eresources/EresourceListPagingDataSAXStrategy.java
+++ src/main/java/edu/stanford/irt/laneweb/eresources/EresourceListPagingDataSAXStrategy.java
@@ -7,6 +7,7 @@ import org.xml.sax.SAXException;
 
 import edu.stanford.irt.cocoon.xml.XMLConsumer;
 import edu.stanford.irt.laneweb.LanewebException;
+import edu.stanford.irt.laneweb.resource.AbstractXHTMLSAXStrategy;
 import edu.stanford.irt.laneweb.util.XMLUtils;
 
 public class EresourceListPagingDataSAXStrategy extends AbstractXHTMLSAXStrategy<EresourceListPagingData> {
