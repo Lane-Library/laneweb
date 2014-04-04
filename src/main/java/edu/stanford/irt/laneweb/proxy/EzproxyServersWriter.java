0a03faf6dc5ba39a2da19d74f7118c76273c7574
Charles Yates <ceyates@stanford.edu>
2014-04-04 10:15:23 -0700

    externalize SQL query text

--- src/main/java/edu/stanford/irt/laneweb/proxy/EzproxyServersWriter.java
+++ src/main/java/edu/stanford/irt/laneweb/proxy/EzproxyServersWriter.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -16,38 +17,6 @@ import edu.stanford.irt.laneweb.util.JdbcUtils;
 public class EzproxyServersWriter {
 
     private static final byte[] HJ = { 'H', 'J', ' ' };
-
-    private static final String UNION = "union ";
-
-    private static final String SQL = "WITH HOSTS AS " + "  ( SELECT DISTINCT LINK AS HOST "
-            + "  FROM LMLDB.ELINK_INDEX " + "  WHERE ELINK_INDEX.RECORD_TYPE = 'A' "
-            + "  AND URL_HOST NOT LIKE '%.stanford.edu%' "
-            + UNION
-            + "  SELECT DISTINCT LINK AS HOST "
-            + "  FROM LMLDB.ELINK_INDEX, "
-            + "    LMLDB.MFHD_MASTER "
-            + "  WHERE ELINK_INDEX.RECORD_ID = MFHD_MASTER.MFHD_ID "
-            + "  AND ELINK_INDEX.RECORD_TYPE = 'M' "
-            + "  AND URL_HOST NOT LIKE '%.stanford.edu%' "
-            + "  AND MFHD_MASTER.MFHD_ID NOT IN "
-            + "    (SELECT MFHD_ID "
-            + "    FROM LMLDB.MFHD_DATA "
-            + "    WHERE LOWER(RECORD_SEGMENT) LIKE '%, noproxy%' "
-            + "    ) "
-            + "  ) "
-            + "SELECT SUBSTR(HOST, 0, instr(HOST,'/',1,3)-1) "
-            + "FROM HOSTS "
-            + "WHERE HOST LIKE 'https://%' "
-            + "AND instr(HOST,'/',1,3) > 0 "
-            + UNION
-            + "SELECT HOST FROM HOSTS WHERE HOST LIKE 'https://%' AND instr(HOST,'/',1,3) = 0 "
-            + UNION
-            + "SELECT SUBSTR(HOST, 0, instr(HOST,'/',1,3)-1) "
-            + "FROM HOSTS "
-            + "WHERE HOST LIKE 'http://%' "
-            + "AND instr(HOST,'/',1,3) > 0 "
-            + UNION
-            + "SELECT HOST FROM HOSTS WHERE HOST LIKE 'http://%' AND INSTR(HOST,'/',1,3) = 0";
 
     private static final byte[] SUL;
 
@@ -63,9 +32,12 @@ public class EzproxyServersWriter {
     }
 
     private DataSource dataSource;
+    
+    private String sql;
 
-    public EzproxyServersWriter(final DataSource dataSource) {
+    public EzproxyServersWriter(final DataSource dataSource, final Properties sql) {
         this.dataSource = dataSource;
+        this.sql = sql.getProperty("ezproxy-servers.query");
     }
 
     public void write(final OutputStream outputStream) throws IOException {
@@ -78,7 +50,7 @@ public class EzproxyServersWriter {
         try {
             conn = this.dataSource.getConnection();
             stmt = conn.createStatement();
-            rs = stmt.executeQuery(SQL);
+            rs = stmt.executeQuery(this.sql);
             while (rs.next()) {
                 String host = rs.getString(1);
                 outputStream.write(T);
