7e069f95f4e2388701f7ef8e0a06e6cb5da76c58
Charles Yates <ceyates@stanford.edu>
2014-04-10 06:59:32 -0700

    add unit test

---
+++ src/test/java/edu/stanford/irt/laneweb/servlet/binding/TemplateDataBinderTest.java
@@ -0,0 +1,54 @@
+package edu.stanford.irt.laneweb.servlet.binding;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.stanford.irt.laneweb.model.Model;
+
+public class TemplateDataBinderTest {
+
+    private TemplateDataBinder binder;
+
+    private TemplateChooser chooser;
+
+    private Map<String, Object> model;
+
+    private HttpServletRequest request;
+
+    @Before
+    public void setUp() throws Exception {
+        this.chooser = createMock(TemplateChooser.class);
+        this.binder = new TemplateDataBinder(this.chooser);
+        this.model = new HashMap<String, Object>();
+        this.request = createMock(HttpServletRequest.class);
+    }
+
+    @Test
+    public void testBind() {
+        expect(this.chooser.getTemplate(this.request)).andReturn("template");
+        replay(this.chooser, this.request);
+        this.binder.bind(this.model, this.request);
+        assertEquals("template", this.model.get(Model.TEMPLATE));
+        verify(this.chooser, this.request);
+    }
+
+    @Test
+    public void testBindDebug() {
+        this.model.put(Model.DEBUG, Boolean.TRUE);
+        replay(this.chooser, this.request);
+        this.binder.bind(this.model, this.request);
+        assertEquals("debug", this.model.get(Model.TEMPLATE));
+        verify(this.chooser, this.request);
+    }
+}
