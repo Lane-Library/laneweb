fe84dd8b7c3564d5875926949ecd48b1d3ffc966
Charles Yates <ceyates@stanford.edu>
2014-05-22 13:20:14 -0700

    add unit tests, use constructor injection

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/bookmarks/HTMLBookmarkController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/bookmarks/HTMLBookmarkController.java
@@ -2,6 +2,7 @@ package edu.stanford.irt.laneweb.servlet.mvc.bookmarks;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.stanford.irt.laneweb.bookmarks.Bookmark;
+import edu.stanford.irt.laneweb.bookmarks.BookmarkDAO;
 import edu.stanford.irt.laneweb.model.Model;
+import edu.stanford.irt.laneweb.servlet.binding.BookmarkDataBinder;
+import edu.stanford.irt.laneweb.servlet.binding.SunetIdAndTicketDataBinder;
 
 @Controller
 @RequestMapping(value = "/bookmarks", method = RequestMethod.POST, consumes = "application/x-www-form-urlencoded")
 public class HTMLBookmarkController extends BookmarkController {
 
     private String redirectURI = "redirect:/favorites.html";
+
+    @Autowired
+    public HTMLBookmarkController(BookmarkDAO bookmarkDAO, BookmarkDataBinder bookmarkDataBinder,
+            SunetIdAndTicketDataBinder sunetidTicketDataBinder) {
+        super(bookmarkDAO, bookmarkDataBinder, sunetidTicketDataBinder);
+    }
 
     @RequestMapping(params = "action=add")
     public String addBookmark(
