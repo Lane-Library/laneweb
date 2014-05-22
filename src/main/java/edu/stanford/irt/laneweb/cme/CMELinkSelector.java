cb048979913725894a4a3937d647ee8a81320bf0
Ryan Max Steinberg <ryanmax@stanford.edu>
2014-05-22 11:43:13 -0700

    don't rewrite UTD/cme links when hospital IP but no emrid

--- src/main/java/edu/stanford/irt/laneweb/cme/CMELinkSelector.java
+++ src/main/java/edu/stanford/irt/laneweb/cme/CMELinkSelector.java
@@ -3,6 +3,7 @@ package edu.stanford.irt.laneweb.cme;
 import java.util.Map;
 
 import edu.stanford.irt.cocoon.sitemap.select.Selector;
+import edu.stanford.irt.laneweb.ipgroup.IPGroup;
 import edu.stanford.irt.laneweb.model.Model;
 import edu.stanford.irt.laneweb.model.ModelUtil;
 
@@ -13,7 +14,9 @@ public class CMELinkSelector implements Selector {
     @Override
     public boolean select(final String expression, final Map<String, Object> objectModel,
             final Map<String, String> parameters) {
-        return ModelUtil.getObject(objectModel, Model.PROXY_LINKS, Boolean.class, Boolean.FALSE).booleanValue()
-                || !EMPTY_STRING.equals(ModelUtil.getString(objectModel, Model.EMRID, EMPTY_STRING));
+        IPGroup ipGroup = ModelUtil.getObject(objectModel, Model.IPGROUP, IPGroup.class, IPGroup.ERR);
+        boolean isHospital = (IPGroup.SHC.equals(ipGroup) || IPGroup.LPCH.equals(ipGroup));
+        return (!EMPTY_STRING.equals(ModelUtil.getString(objectModel, Model.EMRID, EMPTY_STRING)) || (ModelUtil
+                .getObject(objectModel, Model.PROXY_LINKS, Boolean.class, Boolean.FALSE).booleanValue() && !isHospital));
     }
 }
--- src/test/java/edu/stanford/irt/laneweb/cme/CMELinkSelectorTest.java
+++ src/test/java/edu/stanford/irt/laneweb/cme/CMELinkSelectorTest.java
@@ -3,12 +3,14 @@ package edu.stanford.irt.laneweb.cme;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.stanford.irt.laneweb.ipgroup.IPGroup;
 import edu.stanford.irt.laneweb.model.Model;
 
 public class CMELinkSelectorTest {
@@ -24,15 +26,35 @@ public class CMELinkSelectorTest {
     }
 
     @Test
-    public void testSelectEmptyEmrid() {
+    public void testSelectEmrid() {
+        this.model.put(Model.EMRID, "emrid");
+        assertTrue(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testSelectEmridEmpty() {
         this.model.put(Model.EMRID, "");
         assertFalse(this.selector.select(null, this.model, null));
     }
 
     @Test
-    public void testSelectEmrid() {
+    public void testSelectEmridNull() {
+        assertFalse(this.selector.select(null, Collections.<String, Object> emptyMap(), null));
+    }
+
+    @Test
+    public void testSelectLPCHEmrid() {
+        this.model.put(Model.PROXY_LINKS, true);
+        this.model.put(Model.IPGROUP, IPGroup.LPCH);
         this.model.put(Model.EMRID, "emrid");
         assertTrue(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testSelectLPCHNoEmrid() {
+        this.model.put(Model.PROXY_LINKS, true);
+        this.model.put(Model.IPGROUP, IPGroup.LPCH);
+        assertFalse(this.selector.select(null, this.model, null));
     }
 
     @Test
@@ -45,5 +67,20 @@ public class CMELinkSelectorTest {
     public void testSelectProxyLinks() {
         this.model.put(Model.PROXY_LINKS, true);
         assertTrue(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testSelectSHCEmrid() {
+        this.model.put(Model.PROXY_LINKS, true);
+        this.model.put(Model.IPGROUP, IPGroup.SHC);
+        this.model.put(Model.EMRID, "emrid");
+        assertTrue(this.selector.select(null, this.model, null));
+    }
+
+    @Test
+    public void testSelectSHCNoEmrid() {
+        this.model.put(Model.PROXY_LINKS, true);
+        this.model.put(Model.IPGROUP, IPGroup.SHC);
+        assertFalse(this.selector.select(null, this.model, null));
     }
 }
