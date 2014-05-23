5584b1c3d21614de7162c779d8b74a23016c5d8c
Charles Yates <ceyates@stanford.edu>
2014-05-23 10:06:27 -0700

    use File instead of String in constructor

--- src/test/java/edu/stanford/irt/laneweb/hours/TodaysHoursTest.java
+++ src/test/java/edu/stanford/irt/laneweb/hours/TodaysHoursTest.java
@@ -1,12 +1,12 @@
 /**
- * 
+ *
  */
 package edu.stanford.irt.laneweb.hours;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,23 +23,19 @@ public class TodaysHoursTest {
 
     private TodaysHours todaysHours;
 
-    /**
-     * @throws java.lang.Exception
-     */
     @Before
-    public void setUp() throws Exception {
-        this.todaysHours = new TodaysHours(getClass().getResource("").toExternalForm(), "hours.xml");
+    public void setUp() {
+        this.todaysHours = new TodaysHours(new File("target/test-classes/edu/stanford/irt/laneweb/hours/hours.xml"));
     }
 
     @Test
     public final void test() throws ParseException {
-        assertTrue(this.todaysHours.toString().length() > 0);
         assertEquals("Just an average Monday", this.todaysHours.toString(this.dateFormat.parse("1/30/2012")));
         assertEquals("8 am - 10 pm", this.todaysHours.toString(this.dateFormat.parse("1/31/2012")));
         assertEquals("2/9 hours", this.todaysHours.toString(this.dateFormat.parse("2/09/2012")));
         assertEquals("A special someone's birthday?", this.todaysHours.toString(this.dateFormat.parse("2/10/2012")));
         assertFalse(this.todaysHours.getHours().isEmpty());
-        this.todaysHours = new TodaysHours(getClass().getResource("").toExternalForm(), "empty-file");
+        this.todaysHours = new TodaysHours(new File("target/test-classes/edu/stanford/irt/laneweb/hours/empty-file"));
         assertEquals("??", this.todaysHours.toString(this.dateFormat.parse("1/30/2012")));
     }
 }
