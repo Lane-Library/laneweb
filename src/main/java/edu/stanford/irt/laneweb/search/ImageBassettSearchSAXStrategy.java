ab549a2094d2178ecbf20d4b38935781f89efb56
alain <alainb@stanford.edu>
2014-06-06 10:42:10 -0700

    to add popup window for the site that use the data from sorl

--- src/main/java/edu/stanford/irt/laneweb/search/ImageBassettSearchSAXStrategy.java
+++ src/main/java/edu/stanford/irt/laneweb/search/ImageBassettSearchSAXStrategy.java
@@ -1,55 +1,78 @@
 package edu.stanford.irt.laneweb.search;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
 
 import edu.stanford.irt.cocoon.xml.XMLConsumer;
 import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.bassett.BassettImage;
+import edu.stanford.irt.laneweb.util.XMLUtils;
 
-public class ImageBassettSearchSAXStrategy extends AbstractImageSearchSAXStrategy<Map<String, Object>> {
+public class ImageBassettSearchSAXStrategy extends AbstractImageSearchSAXStrategy<HashMap<String, Object>> {
 
-    private static final String BASSETT_ICON_SRC_URI = "http://elane.stanford.edu/public/L254573/small/";
+	private final int MAX_BASSETT_RESULT = 12;
+	private static final String BASSETT_PAGE_PATH = "/biomed-resources/bassett/bassettView.html?bn=";
+	private static final String BASSETT_ICON_SRC_URI = "http://elane.stanford.edu/public/L254573/small/";
+	private static final String BASSETT_SEARCH_URL = "http://lane.stanford.edu/search.html?source=bassett&q=";
+	private static final String BASSETT_MEDIUM_IMAGE_SRC = "http://elane.stanford.edu/public/L254573/medium/";
+	
+	private static final String CDATA = "CDATA";
+	private static final String DIV = "div";
+	private static final String CLASS = "class";
+	private static final String XHTML_NS = "http://www.w3.org/1999/xhtml";
 
-    private static final String BASSETT_PAGE_PATH = "/biomed-resources/bassett/bassettView.html?bn=";
+	
+	@SuppressWarnings("unchecked")
+	public void toSAX(HashMap<String, Object> result, XMLConsumer xmlConsumer) {
+		try {
+			String query = (String) result.get(ImageSearchGenerator.SEARCH_TERM);
+			List<BassettImage> bassettResult = (List<BassettImage>) result.get(ImageSearchGenerator.BASSETT_RESULT);
+			int bassettMaxResult = ((bassettResult.size() > MAX_BASSETT_RESULT) ? MAX_BASSETT_RESULT : bassettResult.size());
+			
+			if (bassettResult.size() != 0) {
+				startDiv(xmlConsumer);
+				createTitle(xmlConsumer, "bassett", "Bassett", String.valueOf(bassettMaxResult), String.valueOf(bassettResult.size()), BASSETT_SEARCH_URL.concat(query));
+				startElementWithId(xmlConsumer, UL, "imageList");
+				for (BassettImage bassettImage : bassettResult) {
+					String titleAndSubtitle = bassettImage.getTitle();
+					String bassettTitle = "";
+					if (titleAndSubtitle != null) {
+						String[] title = titleAndSubtitle.split("\\.");
+						bassettTitle = title[0];
+					}
+					generateImages(xmlConsumer,bassettImage.getBassettNumber() , bassettTitle, BASSETT_PAGE_PATH.concat(bassettImage.getBassettNumber()),
+							BASSETT_ICON_SRC_URI.concat(bassettImage.getImage()), bassettImage.getImage());
+					if (--bassettMaxResult == 0)
+						break;
+				}
+				endUl(xmlConsumer);
+				endDiv(xmlConsumer);
+				generateTooltips( xmlConsumer, bassettResult);
+			}
+		} catch (SAXException | ParseException e) {
+			throw new LanewebException(e);
+		}
+	}
 
-    private static final String BASSETT_SEARCH_URL = "http://lane.stanford.edu/search.html?source=bassett&q=";
-
-    private static final int MAX_BASSETT_RESULT = 12;
-
-    @SuppressWarnings("unchecked")
-    public void toSAX(final Map<String, Object> result, final XMLConsumer xmlConsumer) {
-        try {
-            String query = (String) result.get(ImageSearchGenerator.SEARCH_TERM);
-            List<BassettImage> bassettResult = (List<BassettImage>) result.get(ImageSearchGenerator.BASSETT_RESULT);
-            int bassettMaxResult = bassettResult.size() > ImageBassettSearchSAXStrategy.MAX_BASSETT_RESULT ? ImageBassettSearchSAXStrategy.MAX_BASSETT_RESULT
-                    : bassettResult.size();
-            if (!bassettResult.isEmpty()) {
-                startDiv(xmlConsumer);
-                createTitle(xmlConsumer, "bassett", "Bassett", String.valueOf(bassettMaxResult),
-                        String.valueOf(bassettResult.size()), BASSETT_SEARCH_URL.concat(query));
-                startElementWithId(xmlConsumer, UL, "imageList");
-                for (BassettImage bassettImage : bassettResult) {
-                    String titleAndSubtitle = bassettImage.getTitle();
-                    String bassettTitle = "";
-                    if (titleAndSubtitle != null) {
-                        String[] title = titleAndSubtitle.split("\\.");
-                        bassettTitle = title[0];
-                    }
-                    generateImages(xmlConsumer, bassettTitle,
-                            BASSETT_PAGE_PATH.concat(bassettImage.getBassettNumber()),
-                            BASSETT_ICON_SRC_URI.concat(bassettImage.getImage()));
-                    if (--bassettMaxResult == 0) {
-                        break;
-                    }
-                }
-                endUl(xmlConsumer);
-                endDiv(xmlConsumer);
-            }
-        } catch (SAXException e) {
-            throw new LanewebException(e);
-        }
-    }
+	protected void generateTooltips(XMLConsumer xmlConsumer, List<BassettImage>  bassettImages) throws SAXException, ParseException {
+		AttributesImpl atts = new AttributesImpl();
+	    atts.addAttribute(XHTML_NS, "style", "style", CDATA, "display:none" );
+	    atts.addAttribute(XHTML_NS, CLASS , CLASS, CDATA, "tooltips");
+	    XMLUtils.startElement(xmlConsumer, XHTML_NS, DIV, atts);
+	    int bassettMaxResult = ((bassettImages.size() > MAX_BASSETT_RESULT) ? MAX_BASSETT_RESULT : bassettImages.size());
+		
+	 	for (BassettImage bassettImage  : bassettImages) {
+	 		generateTooltipsImage(xmlConsumer, bassettImage.getBassettNumber(), BASSETT_MEDIUM_IMAGE_SRC.concat(bassettImage.getImage()));
+	 		if (--bassettMaxResult == 0)
+				break;
+		}
+		endDiv(xmlConsumer);
+	}
+	
 }
