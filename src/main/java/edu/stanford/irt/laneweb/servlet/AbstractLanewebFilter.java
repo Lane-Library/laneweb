25ac2924d88677e29d6b76587a2514794a92cd61
Charles Yates <ceyates@stanford.edu>
2014-06-22 08:24:11 -0700

    create new unit tests

--- src/main/java/edu/stanford/irt/laneweb/servlet/AbstractLanewebFilter.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/AbstractLanewebFilter.java
@@ -14,31 +14,22 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * An abstract implementation of Filter that does the casting to
  * HttpServletRequest and HttpServletResponse so that the subclasses don't have
- * to. If the instanceof checks fail, which will never happen, the processing is
- * passed to the chain.
+ * to.
  */
 public abstract class AbstractLanewebFilter implements Filter {
 
-    private FilterConfig filterConfig;
-
+    @Override
     public void destroy() {
     }
 
+    @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException,
             ServletException {
-        if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
-            internalDoFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
-        } else {
-            chain.doFilter(request, response);
-        }
+        internalDoFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
     }
 
+    @Override
     public void init(final FilterConfig filterConfig) {
-        this.filterConfig = filterConfig;
-    }
-
-    protected FilterConfig getConfiguration() {
-        return this.filterConfig;
     }
 
     protected abstract void internalDoFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
---
+++ src/test/java/edu/stanford/irt/laneweb/servlet/LanewebHTTPSFilterTest.java
@@ -0,0 +1,82 @@
+package edu.stanford.irt.laneweb.servlet;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LanewebHTTPSFilterTest {
+
+    private FilterChain chain;
+
+    private LanewebHTTPSFilter filter;
+
+    private HttpServletRequest request;
+
+    private HttpServletResponse response;
+
+    @Before
+    public void setUp() {
+        this.filter = new LanewebHTTPSFilter();
+        this.chain = createMock(FilterChain.class);
+        this.response = createMock(HttpServletResponse.class);
+        this.request = createMock(HttpServletRequest.class);
+    }
+
+    @Test
+    public void testInternalDoFilter() throws IOException, ServletException {
+        expect(this.request.getQueryString()).andReturn(null);
+        expect(this.request.getRequestURL()).andReturn(new StringBuffer("https://lane.stanford.edu/secure/index.html"));
+        expect(this.request.getHeader("gohttps")).andReturn(null);
+        expect(this.request.getScheme()).andReturn("https");
+        this.chain.doFilter(this.request, this.response);
+        replay(this.chain, this.response, this.request);
+        this.filter.internalDoFilter(this.request, this.response, this.chain);
+        verify(this.chain, this.response, this.request);
+    }
+
+    @Test
+    public void testInternalDoFilterGoHttps() throws IOException, ServletException {
+        expect(this.request.getQueryString()).andReturn(null);
+        expect(this.request.getRequestURL()).andReturn(new StringBuffer("https://lane.stanford.edu/secure/index.html"));
+        expect(this.request.getHeader("gohttps")).andReturn("1");
+        this.chain.doFilter(this.request, this.response);
+        replay(this.chain, this.response, this.request);
+        this.filter.internalDoFilter(this.request, this.response, this.chain);
+        verify(this.chain, this.response, this.request);
+    }
+
+    @Test
+    public void testInternalDoFilterHttp() throws IOException, ServletException {
+        expect(this.request.getQueryString()).andReturn(null);
+        expect(this.request.getRequestURL()).andReturn(new StringBuffer("http://lane.stanford.edu/secure/index.html"));
+        expect(this.request.getHeader("gohttps")).andReturn(null);
+        expect(this.request.getScheme()).andReturn("http");
+        this.response.sendRedirect("https://lane.stanford.edu/secure/index.html");
+        replay(this.chain, this.response, this.request);
+        this.filter.internalDoFilter(this.request, this.response, this.chain);
+        verify(this.chain, this.response, this.request);
+    }
+
+    @Test
+    public void testInternalDoFilterHttpQuery() throws IOException, ServletException {
+        expect(this.request.getQueryString()).andReturn("query");
+        expect(this.request.getRequestURL()).andReturn(new StringBuffer("http://lane.stanford.edu/secure/index.html"));
+        expect(this.request.getHeader("gohttps")).andReturn(null);
+        expect(this.request.getScheme()).andReturn("http");
+        this.response.sendRedirect("https://lane.stanford.edu/secure/index.html?query");
+        replay(this.chain, this.response, this.request);
+        this.filter.internalDoFilter(this.request, this.response, this.chain);
+        verify(this.chain, this.response, this.request);
+    }
+}
