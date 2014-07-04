b02c1224c1baa54e362f4f02ca6f496265b7a2be
Ryan Max Steinberg <ryanmax@stanford.edu>
2014-07-03 21:48:03 -0700

    integration test for relevance ranking

--- src/test/java/edu/stanford/irt/laneweb/integration/LanewebIT.java
+++ src/test/java/edu/stanford/irt/laneweb/integration/LanewebIT.java
@@ -2,7 +2,13 @@ package edu.stanford.irt.laneweb.integration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -41,6 +47,18 @@ public class LanewebIT {
     @Resource
     private WebApplicationContext webApplicationContext;
 
+    private boolean pubmedIsReachable () {
+        boolean reachable = false;
+        try {
+            if (InetAddress.getByName("www.ncbi.nlm.nih.gov") != null ) {
+                reachable = true;
+            }
+        } catch (UnknownHostException e) {
+            //
+        }
+        return reachable;
+    }
+    
     @Before
     public void setupFixture() {
         this.mockMvc = webAppContextSetup(this.webApplicationContext).build();
@@ -54,6 +72,16 @@ public class LanewebIT {
     @Test
     public void testBioresearchSearch() throws Exception {
         this.mockMvc.perform(get("/search.html?source=bioresearch-all&q=test")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPubmedSearch() throws Exception {
+        if (pubmedIsReachable()) {
+            Map<String, String> ns = new HashMap<String, String>();
+            ns.put("h", "http://www.w3.org/1999/xhtml");
+            // query term must appear within <strong> in first three results
+            this.mockMvc.perform(get("/apps/search/content/html/pubmed?q=test")).andExpect(xpath("//h:li[position() <= 3]//h:a[@class='primaryLink']/h:strong", ns, new Object()).exists());
+        }
     }
     
     @Test
