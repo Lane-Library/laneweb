f43e1673ad8c78cb791e17ca39ea4e68039fe6a6
Charles Yates <ceyates@stanford.edu>
2014-07-22 13:57:10 -0700

    case 98509 create mechanism to set boxes to the same height as the tallest one

---
+++ src/main/webapp/resources/javascript/same-height.js
@@ -0,0 +1,43 @@
+(function() {
+    
+    /* javascript to set the height of a group of boxes to the that of the tallest one.
+     * The boxes are grouped using the same-height-n class where n is a number 0-9 */
+
+    var maxHeight,
+        // get all the nodes with same-height-n
+        nodes = Y.all("*[class^='same-height-'], *[class*=' same-height-']"),
+        size = nodes.size(),
+        height,
+        i,
+        j,
+        classes = {},
+        regex = /.*(same-height-\d).*/,
+        matches;
+
+    // find the class for each group
+    for (i = 0; i < size; i++) {
+        matches = regex.exec(nodes.item(i).getAttribute("class"));
+        if (matches.length === 2) {
+            classes[matches[1]] = "." + matches[1];
+        }
+    }
+
+    for (j in classes) {
+        // for each group find the tallest
+        maxHeight = 0;
+        nodes = Y.all(classes[j]);
+        size = nodes.size();
+        for (i = 0; i < size; i++) {
+            height = nodes.item(i).get("clientHeight");
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
+        }
+        // set all of the group to the tallest's height
+        for (i = 0; i < size; i++) {
+            nodes.item(i).setStyle("height", maxHeight + "px");
+        }
+    }
+
+
+})();
