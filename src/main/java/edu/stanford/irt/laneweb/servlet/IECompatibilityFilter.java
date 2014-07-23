816ee703a919f2cbeb6e770544cf8918cdad8168
Charles Yates <ceyates@stanford.edu>
2014-07-23 15:26:20 -0700

    case 98565 Error displaying homepage from Hospital Computer

---
+++ src/main/java/edu/stanford/irt/laneweb/servlet/IECompatibilityFilter.java
@@ -0,0 +1,25 @@
+package edu.stanford.irt.laneweb.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A Filter that adds the X-UA-Compatible header with value IE=edge to cause IE browsers to use standards mode in
+ * rendering html
+ */
+public class IECompatibilityFilter extends AbstractLanewebFilter {
+
+    @Override
+    protected void internalDoFilter(final HttpServletRequest request, final HttpServletResponse response,
+            final FilterChain chain) throws IOException, ServletException {
+        String userAgent = request.getHeader("user-agent");
+        if (userAgent != null && userAgent.indexOf("MSIE") > 0) {
+            response.setHeader("X-UA-Compatible", "IE=edge");
+        }
+        chain.doFilter(request, response);
+    }
+}
