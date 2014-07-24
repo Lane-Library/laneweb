bba2effb604d35ac1334889291d7a46b7513efcc
Ryan Max Steinberg <ryanmax@stanford.edu>
2014-07-24 11:48:06 -0700

    controller for solr search facets

---
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/SearchFacetsController.java
@@ -0,0 +1,62 @@
+package edu.stanford.irt.laneweb.servlet.mvc;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.solr.core.query.result.FacetFieldEntry;
+import org.springframework.data.solr.core.query.result.FacetPage;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.stanford.irt.laneweb.solr.SolrRepository;
+
+@Controller
+public class SearchFacetsController {
+
+    private static final String FACETS = "facets";
+
+    @Autowired
+    SolrRepository repository;
+
+    @RequestMapping(value = "/apps/search/facets")
+    @ResponseBody
+    public Map<String, Map<String, Map<String, Long>>> facetByField(final Model model,
+            @RequestParam(value = "q", required = true) final String query, final HttpServletRequest request) {
+        Map<String, Map<String, Long>> facetsMap = new HashMap<String, Map<String, Long>>();
+        Map<String, Long> countMap = new LinkedHashMap<String, Long>();
+        FacetPage<?> fps = this.repository.searchFacetByManyFields(query, new PageRequest(0, 1));
+        long total = fps.getTotalElements();
+        countMap.put("total", Long.valueOf(total));
+        facetsMap.put("all", countMap);
+        if (0 == total) {
+            return Collections.singletonMap(FACETS, facetsMap);
+        }
+        for (Page<FacetFieldEntry> page : fps.getFacetResultPages()) {
+            if (!page.hasContent()) {
+                continue;
+            }
+            Map<String, Long> resultsMap = new LinkedHashMap<String, Long>();
+            String fieldName = null;
+            for (FacetFieldEntry entry : page) {
+                if (fieldName == null) {
+                    fieldName = entry.getField().getName();
+                }
+                String facetName = entry.getValue();
+                Long facetValue = Long.valueOf(entry.getValueCount());
+                resultsMap.put(facetName, facetValue);
+            }
+            facetsMap.put(fieldName, resultsMap);
+        }
+        return Collections.singletonMap(FACETS, facetsMap);
+    }
+}
