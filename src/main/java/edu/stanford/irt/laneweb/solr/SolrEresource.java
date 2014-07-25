346c45a0bfa33ec5b5d7c553cf8b6794018366f7
Ryan Max Steinberg <ryanmax@stanford.edu>
2014-07-24 23:11:09 -0700

    unused method

--- src/main/java/edu/stanford/irt/laneweb/solr/SolrEresource.java
+++ src/main/java/edu/stanford/irt/laneweb/solr/SolrEresource.java
@@ -63,10 +63,6 @@ public class SolrEresource {
         return this.id;
     }
 
-    public int getIntergerId() {
-        return this.id.hashCode();
-    }
-
     public List<Link> getLinks() {
         if (this.linksList == null) {
             setLinkList();
