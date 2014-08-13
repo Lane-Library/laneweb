b1e55c9a922c618191aaf7c47faee8f771907ce7
Ryan Max Steinberg <ryanmax@stanford.edu>
2014-08-13 14:03:19 -0700

    case 98878: use placeholder attribute for input hints where supported

--- src/main/webapp/resources/javascript/lane-textinputs.js
+++ src/main/webapp/resources/javascript/lane-textinputs.js
@@ -1,62 +1,91 @@
 /**
- * candy for input[type="text"]
- *  assumes uses title and value attributes used for help text
- *     applies hintStyle only to default help text
- *  clears default help text on focus and adds back on blur
+ * set input placeholder attribute in capable browsers
+ * for browsers without placeholder functionality (IE <=9) set, clear and style input value
  */
 (function() {
 
     Y.lane.TextInput = function(input, hintText) {
         var hintStyle = "inputHint",
             _hintText = hintText || '',
+            placeholderCapable = 'placeholder' in Y.Node.getDOMNode(input),
+            _destroy =function() {
+                input.set('placeholder','');
+            },
+            _getValue = function() {
+                return input.get('value');
+            },
             _reset = function(input) {
-                input.addClass(hintStyle);
-                input.set('value', _hintText);
+                input.set('value', '');
             },
-            focusHandle = input.on('focus', function(event) {
-                if (event.target.get('value') === _hintText) {
-                    event.target.set('value', '');
-                    event.target.removeClass(hintStyle);
-                }
-            }),
-            blurHandle = input.on('blur', function(event) {
-                if (event.target.get('value') === '') {
-                    _reset(event.target);
-                }
-            });
-        if (input.get('value') === '' || input.get('value') === _hintText) {
-            _reset(input);
-        }
-        return {
-            getValue: function() {
-                var value = input.get('value');
-                return value === _hintText ? '' : value;
+            _setHintText = function(hintText) {
+                input.set('placeholder',hintText);
             },
-            setValue: function(value) {
+            _setValue = function(value) {
                 input.set('value', value);
-                input.removeClass("inputHint");
+            };
+            input.set('placeholder',_hintText);
+            if (!placeholderCapable) {
+                _destroy =function() {
+                    input.detach(focusHandle);
+                    input.detach(blurHandle);
+                    input.removeClass('inputHint');
+                    if (input.get('value') === _hintText) {
+                        input.set('value', '');
+                    }
+                },
+                _getValue = function() {
+                    var value = input.get('value');
+                    return value === _hintText ? '' : value;
+                },
+                _reset = function(input) {
+                    input.addClass(hintStyle);
+                    input.set('value', _hintText);
+                },
+                _setHintText = function(hintText) {
+                    var oldHintText = _hintText;
+                    _hintText = hintText;
+                    if (input.get('value') === '' || input.get('value') === oldHintText) {
+                        _reset(input);
+                    }
+                },
+                _setValue = function(value) {
+                    input.set('value', value);
+                    input.removeClass("inputHint");
+                },
+                focusHandle = input.on('focus', function(event) {
+                    if (event.target.get('value') === _hintText) {
+                        event.target.set('value', '');
+                        event.target.removeClass(hintStyle);
+                    }
+                }),
+                blurHandle = input.on('blur', function(event) {
+                    if (event.target.get('value') === '') {
+                        _reset(event.target);
+                    }
+                });
+                if (input.get('value') === '' || input.get('value') === _hintText) {
+                    _reset(input);
+                }
+            }
+        return {
+            destroy: function() {
+                _destroy();
+            },
+            getValue: function() {
+                return _getValue();
             },
             getInput: function() {
                 return input;
-            },
-            setHintText: function(hintText) {
-                var oldHintText = _hintText;
-                _hintText = hintText;
-                if (input.get('value') === '' || input.get('value') === oldHintText) {
-                    _reset(input);
-                }
             },
             //added reset function so BookmarkEditor can reset
             reset : function() {
                 _reset(input);
             },
-            destroy: function() {
-                input.detach(focusHandle);
-                input.detach(blurHandle);
-                input.removeClass('inputHint');
-                if (input.get('value') === _hintText) {
-                    input.set('value', '');
-                }
+            setHintText: function(hintText) {
+                _setHintText(hintText);
+            },
+            setValue: function(value) {
+                _setValue(value);
             }
         };
     };
--- src/main/webapp/resources/test/textinputs/textinputs-test.js
+++ src/main/webapp/resources/test/textinputs/textinputs-test.js
@@ -4,6 +4,7 @@
 Y.applyConfig({fetchCSS:true});
 Y.use("attribute", "node-event-simulate", "console", "test", function(Y){
 
+    var placeholderCapable = 'placeholder' in Y.Node.getDOMNode(Y.one("#input1"));
     var textinputsTestCase = new Y.Test.Case({
         name: "Lane TextInputs Test Case",
         testConstructorHintText : function() {
@@ -11,50 +12,71 @@ Y.use("attribute", "node-event-simulate", "console", "test", function(Y){
             Y.Assert.areEqual("", input.get("value"));
             var textInput = new Y.lane.TextInput(input, "hint text");
             Y.Assert.areEqual("", textInput.getValue());
-            Y.Assert.areEqual("hint text", input.get("value"));
-            Y.Assert.isTrue(input.hasClass("inputHint"));
+            if (placeholderCapable) {
+                Y.Assert.areEqual("hint text", input.get("placeholder"));
+            } else {
+                Y.Assert.areEqual("hint text", input.get("value"));
+                Y.Assert.isTrue(input.hasClass("inputHint"));
+            }
         },
         testSetHintText: function() {
             var input = Y.one("#input2");
             var textInput = new Y.lane.TextInput(input, "hint text");
-            Y.Assert.areEqual("hint text", input.get("value"));
             textInput.setHintText("new hint text");
-            Y.Assert.areEqual("new hint text", input.get("value"));
-            Y.Assert.isTrue(input.hasClass("inputHint"));
+            if (placeholderCapable) {
+                Y.Assert.areEqual("new hint text", input.get("placeholder"));
+            } else {
+                Y.Assert.areEqual("new hint text", input.get("value"));
+                Y.Assert.isTrue(input.hasClass("inputHint"));
+            }
         },
         testFocus: function() {
             var input = Y.one("#input3");
             var textInput = new Y.lane.TextInput(input, "hint text");
-            Y.Assert.isTrue(input.hasClass("inputHint"), "doesn\"t have class inputHint");
-            //have to do input.after("focus") safari value is not set on return from simulate.
-            var focusHandle = input.after("focus", function() {
-                input.detach(focusHandle);
+            if (placeholderCapable) {
+                input.simulate("focus");
                 Y.Assert.areEqual("", input.get("value"), "value = " + input.get("value"));
-                Y.Assert.isFalse(input.hasClass("inputHint"), "has class inputHint");
-            });
-            input.simulate("focus");
+            } else {
+                Y.Assert.isTrue(input.hasClass("inputHint"), "doesn\"t have class inputHint");
+                //have to do input.after("focus") safari value is not set on return from simulate.
+                var focusHandle = input.after("focus", function() {
+                    input.detach(focusHandle);
+                    Y.Assert.areEqual("", input.get("value"), "value = " + input.get("value"));
+                    Y.Assert.isFalse(input.hasClass("inputHint"), "has class inputHint");
+                });
+                input.simulate("focus");
+            }
         },
         testBlur: function() {
             var input = Y.one("#input4");
             var textInput = new Y.lane.TextInput(input, "hint text");
-            Y.Assert.isTrue(input.hasClass("inputHint"), "doesn\"t have class inputHint");
-            //have to do input.after("focus") safari value is not set on return from simulate.
-            var blurHandle = input.after("blur", function() {
-                input.detach(blurHandle);
-                Y.Assert.areEqual("hint text", input.get("value"));
-                Y.Assert.isTrue(input.hasClass("inputHint"));
-            });
-            var focusHandle = input.after("focus", function() {
-                input.detach(focusHandle);
+            if (placeholderCapable) {
                 input.simulate("blur");
-            });
-            input.simulate("focus");
+                Y.Assert.areEqual("hint text", input.get("placeholder"));
+            } else {
+                Y.Assert.isTrue(input.hasClass("inputHint"), "doesn\"t have class inputHint");
+                //have to do input.after("focus") safari value is not set on return from simulate.
+                var blurHandle = input.after("blur", function() {
+                    input.detach(blurHandle);
+                    Y.Assert.areEqual("hint text", input.get("value"));
+                    Y.Assert.isTrue(input.hasClass("inputHint"));
+                });
+                var focusHandle = input.after("focus", function() {
+                    input.detach(focusHandle);
+                    input.simulate("blur");
+                });
+                input.simulate("focus");
+            }
         },
         testNoHintText: function() {
             var input = Y.one("#input5");
             var textInput = new Y.lane.TextInput(input);
-            Y.Assert.areEqual(input.get("value"), "");
-            Y.Assert.isTrue(input.hasClass("inputHint"));
+            if (placeholderCapable) {
+                Y.Assert.areEqual(input.get("placeholder"), "");
+            } else {
+                Y.Assert.areEqual(input.get("value"), "");
+                Y.Assert.isTrue(input.hasClass("inputHint"));
+            }
         }
     });
 
