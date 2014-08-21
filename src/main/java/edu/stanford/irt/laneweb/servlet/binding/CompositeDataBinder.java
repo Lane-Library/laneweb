13e81d3f5164a5c2aa15668d62f60b722047df54
Charles Yates <ceyates@stanford.edu>
2014-08-21 14:37:52 -0700

    add unit test

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/CompositeDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/CompositeDataBinder.java
@@ -1,6 +1,5 @@
 package edu.stanford.irt.laneweb.servlet.binding;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -10,8 +9,8 @@ public class CompositeDataBinder implements DataBinder {
 
     private List<DataBinder> dataBinders;
     
-    public CompositeDataBinder(final DataBinder... dataBinders) {
-        this.dataBinders = Arrays.asList(dataBinders);
+    public CompositeDataBinder(final List<DataBinder> dataBinders) {
+        this.dataBinders = dataBinders;
     }
 
     public void bind(final Map<String, Object> model, final HttpServletRequest request) {
