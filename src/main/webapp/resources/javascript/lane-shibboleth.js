378f9ce25d461ee6a0c94627803abc78a099e86a
alain <alainb@stanford.edu>
2014-10-09 10:18:57 -0700

    to add a message if redirected

--- src/main/webapp/resources/javascript/lane-shibboleth.js
+++ src/main/webapp/resources/javascript/lane-shibboleth.js
@@ -4,8 +4,10 @@
 	
 		var SHIBBOLETH_COOKIE_NAME = 'shibboleth-organization',
 			SLEEPING_TIME = 10000,
+			MESSAGE_CONTENT = "<h2>You will be a redirected</h2>",
+			
 		shibbolethAnchors = Y.all('#shibboleth-links a'),
-		id, value, expdate, href; 	
+		id, value, expdate, href, message; 	
 	 	Y.on("click",function(event) {
 	        id = event.target.get('id');
 	        expdate= new Date();
@@ -15,6 +17,8 @@
 	
 		value = Y.Cookie.get(SHIBBOLETH_COOKIE_NAME);
 		if(value && Y.one('#'+value)){
+			message_contnair = Y.one('#message-contnair');
+			message_contnair.set('innerHTML', MESSAGE_CONTENT);
 			href = Y.one('#'+value).get('href');
 			setTimeout(function(){
 				window.location = href;
