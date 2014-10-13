984cb09d99008771b003a44ef1cd81fa2e68dfd7
Charles Yates <ceyates@stanford.edu>
2014-10-13 14:46:17 +0200

    refactor PersistentLoginController, temporarily reduce duration for testing purposes

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/PersistentLoginController.java
@@ -2,9 +2,9 @@ package edu.stanford.irt.laneweb.servlet.mvc;
 
 /**
  * This class will add three cookies the persistent-expired-date, persistent-preference and user. The user cookie will
- * have the userid, name, email , the userAgent and the expired date appended and encrypted. The persistent-preference have the
- * expired date minus 3 days only pl=true have to have the secure in the path but not the other But if pl=renew the
- * status of the user is looked up see it is active or not. Before to delete the cookie, we check if the
+ * have the userid, name, email , the userAgent and the expired date appended and encrypted. The persistent-preference
+ * have the expired date minus 3 days only pl=true have to have the secure in the path but not the other But if pl=renew
+ * the status of the user is looked up see it is active or not. Before to delete the cookie, we check if the
  * persistent-preference value is not equals to denied because if it is equals denied the persistent window will never
  * appear.
  */
@@ -20,9 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.codec.PersistentLoginToken;
 import edu.stanford.irt.laneweb.codec.UserCookieCodec;
 import edu.stanford.irt.laneweb.model.Model;
@@ -34,6 +34,12 @@ import edu.stanford.irt.laneweb.user.User.Status;
 public class PersistentLoginController {
 
     public static final String PERSISTENT_LOGIN_PREFERENCE = "persistent-preference";
+
+    // grace period is three days
+    private static final int GRACE_PERIOD = 3600 * 24 * 3;
+
+    // login duration is two weeks:
+    private static final int PERSISTENT_LOGIN_DURATION = GRACE_PERIOD + 1000;//3600 * 24 * 7 * 2;
 
     private static final String UTF8 = "UTF-8";
 
@@ -47,31 +53,38 @@ public class PersistentLoginController {
         this.codec = codec;
     }
 
-    @RequestMapping(value = "/secure/persistentLogin.html", params = { "pl=true" })
-    public View createCookie(@ModelAttribute(Model.USER) final User user, final String url,
-            final HttpServletRequest request, final HttpServletResponse response) throws UnsupportedEncodingException {
-        checkUserIdAndSetCookies(user, request, response);
-        return setView(url, response);
+    @RequestMapping(value = { "/secure/persistentLogin.html", "/persistentLogin.html" }, params = { "pl=false" })
+    public String disablePersistentLogin(final RedirectAttributes redirectAttrs,
+            @ModelAttribute(Model.USER) final User user,
+            final String url,
+            final HttpServletRequest request,
+            final HttpServletResponse response) {
+        removeCookies(request, response);
+        return getRedirectURL(url);
     }
 
-    @RequestMapping(value = { "/secure/persistentLogin.html", "/persistentLogin.html" }, params = { "pl=false" })
-    public View removeCookieAndView(@ModelAttribute(Model.USER) final User user, final String url,
-            final HttpServletRequest request, final HttpServletResponse response) throws UnsupportedEncodingException {
-        removeCookies(request, response);
-        return setView(url, response);
+    @RequestMapping(value = "/secure/persistentLogin.html", params = { "pl=true" })
+    public String enablePersistentLogin(final RedirectAttributes redirectAttrs,
+            @ModelAttribute(Model.USER) final User user,
+            final String url,
+            final HttpServletRequest request,
+            final HttpServletResponse response) {
+        checkUserIdAndSetCookies(user, request, response);
+        return getRedirectURL(url);
     }
 
     @RequestMapping(value = { "/secure/persistentLogin.html", "/persistentLogin.html" }, params = { "url", "pl=renew" })
-    public View renewCookieAndRedirect(@ModelAttribute(Model.USER) final User user, final String url,
-            final HttpServletRequest request, final HttpServletResponse response) throws UnsupportedEncodingException {
+    public String renewPersistentLogin(final RedirectAttributes redirectAttrs,
+            @ModelAttribute(Model.USER) final User user,
+            final String url,
+            final HttpServletRequest request,
+            final HttpServletResponse response) {
         if (user.isStanfordUser() && user.getStatus() == Status.ACTIVE) {
             checkUserIdAndSetCookies(user, request, response);
         } else {
             resetCookies(request, response);
         }
-        RedirectView view = new RedirectView(URLDecoder.decode(url, UTF8), true, true);
-        view.setExpandUriTemplateVariables(false);
-        return view;
+        return getRedirectURL(url);
     }
 
     @ModelAttribute
@@ -89,6 +102,20 @@ public class PersistentLoginController {
         } else {
             resetCookies(request, response);
         }
+    }
+
+    private String getRedirectURL(final String url) {
+        StringBuilder sb = new StringBuilder("redirect:");
+        if (null == url) {
+            sb.append("/myaccounts.html");
+        } else {
+            try {
+                sb.append(URLDecoder.decode(url, UTF8));
+            } catch (UnsupportedEncodingException e) {
+                throw new LanewebException("won't happen", e);
+            }
+        }
+        return sb.toString();
     }
 
     private void removeCookies(final HttpServletRequest request, final HttpServletResponse response) {
@@ -134,40 +161,22 @@ public class PersistentLoginController {
     private void setCookies(final HttpServletRequest request, final HttpServletResponse response, final User user) {
         String userAgent = request.getHeader("User-Agent");
         if (null != userAgent && null != user) {
-            int twoWeeks = 3600 * 24 * 7 * 2;
-            // gracePeriod is three days
-            int gracePeriod = 3600 * 24 * 3;
             PersistentLoginToken token = this.codec.createLoginToken(user, userAgent.hashCode());
             Cookie cookie = new Cookie(UserCookieCodec.LANE_COOKIE_NAME, token.getEncryptedValue());
             cookie.setPath("/");
-            // cookie is available for 2 weeks
-            cookie.setMaxAge(twoWeeks);
+            cookie.setMaxAge(PERSISTENT_LOGIN_DURATION);
             response.addCookie(cookie);
-            Calendar gc = Calendar.getInstance();
-            gc.add(Calendar.SECOND, twoWeeks);
-            cookie = new Cookie(Model.PERSISTENT_LOGIN_EXPIRATION_DATE, String.valueOf(gc.getTime().getTime()));
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.SECOND, PERSISTENT_LOGIN_DURATION);
+            cookie = new Cookie(Model.PERSISTENT_LOGIN_EXPIRATION_DATE, String.valueOf(calendar.getTime().getTime()));
             cookie.setPath("/");
-            // cookie is available for 2 weeks
-            cookie.setMaxAge(twoWeeks);
+            cookie.setMaxAge(PERSISTENT_LOGIN_DURATION);
             response.addCookie(cookie);
-            gc.add(Calendar.SECOND, -gracePeriod);
-            cookie = new Cookie(PERSISTENT_LOGIN_PREFERENCE, String.valueOf(gc.getTime().getTime()));
+            calendar.add(Calendar.SECOND, -GRACE_PERIOD);
+            cookie = new Cookie(PERSISTENT_LOGIN_PREFERENCE, String.valueOf(calendar.getTime().getTime()));
             cookie.setPath("/");
-            // cookie is available for 2 weeks
-            cookie.setMaxAge(twoWeeks);
+            cookie.setMaxAge(PERSISTENT_LOGIN_DURATION);
             response.addCookie(cookie);
         }
-    }
-
-    private View setView(final String url, final HttpServletResponse response) throws UnsupportedEncodingException {
-        RedirectView view = null;
-        if (null == url) {
-            response.setCharacterEncoding(UTF8);
-            view = new RedirectView("/myaccounts.html", true, true);
-        } else {
-            view = new RedirectView(URLDecoder.decode(url, UTF8), true, true);
-        }
-        view.setExpandUriTemplateVariables(false);
-        return view;
     }
 }
