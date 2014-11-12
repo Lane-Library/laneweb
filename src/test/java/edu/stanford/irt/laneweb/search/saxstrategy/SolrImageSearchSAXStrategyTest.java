8f2e3413e559bd47ce0573451404d2622df58308
alain <alainb@stanford.edu>
2014-11-12 15:03:42 -0800

    cleanup

--- src/test/java/edu/stanford/irt/laneweb/search/saxstrategy/SolrImageSearchSAXStrategyTest.java
+++ src/test/java/edu/stanford/irt/laneweb/search/saxstrategy/SolrImageSearchSAXStrategyTest.java
@@ -1,6 +1,10 @@
 package edu.stanford.irt.laneweb.search.saxstrategy;
 
 import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -11,13 +15,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.data.domain.Page;
 
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.verify;
-import static org.junit.Assert.assertEquals;
 import edu.stanford.irt.laneweb.TestXMLConsumer;
 import edu.stanford.irt.laneweb.model.Model;
 import edu.stanford.irt.solr.Image;
