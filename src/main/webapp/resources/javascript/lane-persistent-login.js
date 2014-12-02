38541833efafaff278e28f2a5f399e1ddcd167b6
alain <alainb@stanford.edu>
2014-12-02 11:10:56 -0800

    to add persistent-exntesion popup back

--- src/main/webapp/resources/javascript/lane-persistent-login.js
+++ src/main/webapp/resources/javascript/lane-persistent-login.js
@@ -1,13 +1,57 @@
 (function() {
 
-	var model = Y.lane.Model,
+	var model = Y.lane.Model, redirectUrl ,
 	basePath = model.get(model.BASE_PATH)|| "",  
 	location = Y.lane.Location,
 	// isStanfordActive == true only if user is from stanford and is active in the LDAP
 	// See UserDataBinder.java
-	isStanfordActive = model.get(model.IS_ACTIVE_SUNETID);
+	isStanfordActive = model.get(model.IS_ACTIVE_SUNETID)
+	basePath = model.get(model.BASE_PATH)|| "", 
+	drMode = model.get(model.DISASTER_MODE);
+	
+	// if someone click on a proxied link and he is from stanford so he will
+	// have the possibility to extend his persistent login
+	Y.on("click", function(event) {extensionPersistentLoginPopup(event);}, "a[href*=/redirect/cme]");
+	Y.on("click", function(event) {extensionPersistentLoginPopup(event);}, "a[href*=laneproxy.stanford.edu/login]");
+	
+	var extensionPersistentLoginPopup = function(event){
+		var link = event.target, href = link.get('href');
+		if (isStanfordActive && !drMode && persistentStatusCookie && now.getTime() > persistentStatusCookie) {
+			event.preventDefault();
+			link.set('rel', 'persistentLogin');
+			redirectUrl = encodeURIComponent(event.target.get('href'));
+			getPopup(basePath + '/plain/persistent-extension-popup.html');
+		}
+	};
 
+	
+	getPopup = function(urlPage) {
+		Y.io(urlPage, {
+			on : {
+				success : popupWindow
+			}
+		});
+	};
 
+	
+	// The popup window for expension
+	var popupWindow = function(id, o) {
+		var lightbox = Y.lane.Lightbox, shibbolethAnchors, href, node;
+		lightbox.setContent(o.responseText);
+		lightbox.show();
+		Y.once("click", function(event) {
+			var node = event.currentTarget, href;
+		     
+			if (!redirectUrl) {
+				redirectUrl = "/index.html";
+			}
+				href =  basePath+ '/secure/persistentLogin.html?pl=renew&url='+ encodeURIComponent(redirectUrl);
+			node.set('href', href);
+		}, "");
+	};
+	// END POPUP
+
+	//To display the persistent checkbox in the discoverypage  
 	if(Y.one('#persistent')){
 		var div = Y.one('#persistent div.header'),
 		messageDiv = Y.one('#persistent .message'),
