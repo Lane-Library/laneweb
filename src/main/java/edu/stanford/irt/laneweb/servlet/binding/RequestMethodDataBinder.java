7a3a00d1524da89702f00ee56d2a2a45e8f80cc2
Charles Yates <ceyates@stanford.edu>
2014-12-03 17:06:54 +0200

    see case 100633 proxy-links can be set to false even when a user is authenticated

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/RequestMethodDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/RequestMethodDataBinder.java
@@ -1,6 +1,7 @@
 package edu.stanford.irt.laneweb.servlet.binding;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -8,10 +9,13 @@ import edu.stanford.irt.laneweb.model.Model;
 
 public class RequestMethodDataBinder implements DataBinder {
 
+    private static final Pattern PROXY_LINKS = Pattern.compile("(&|)proxy-links=(true|false)");
+
     public void bind(final Map<String, Object> model, final HttpServletRequest request) {
         String value = request.getQueryString();
         if (value != null) {
-            model.put(Model.QUERY_STRING, value);
+            //remove the proxy-links parameter
+            model.put(Model.QUERY_STRING, PROXY_LINKS.matcher(value).replaceAll(""));
         }
         value = request.getRequestURI();
         if (value != null) {
