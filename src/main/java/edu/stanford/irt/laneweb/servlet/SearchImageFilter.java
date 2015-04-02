0fdbd6f05378d6a0eaac10cc6eb9852a9365778c
alainb <alainb@stanford.edu>
2015-04-02 16:05:09 -0700

    to add auto parameter to be able to switch of tab if the user clik on it

--- src/main/java/edu/stanford/irt/laneweb/servlet/SearchImageFilter.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/SearchImageFilter.java
@@ -38,7 +38,8 @@ public class SearchImageFilter  extends AbstractLanewebFilter{
 	
 	 protected void internalDoFilter(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
 		String sourceOri = request.getParameter("source");
-		if (sourceOri.indexOf("images-all") > -1) {
+		String auto = request.getParameter("auto");
+		if (sourceOri.indexOf("images-all") > -1 && !"no".equals(auto)){
 			String query = request.getParameter("q");
 			Map<String, Long> copyrighToValue = getTabValuesFromSolr(query);
 			Object[] keys = copyrighToValue.keySet().toArray();
