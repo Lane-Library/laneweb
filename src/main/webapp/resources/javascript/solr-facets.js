35eecc6ceb14a62ba1f2417c067cafc16dc73d33
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-06-02 11:24:27 -0700

    hide h3 Limits when no facets returned

--- src/main/webapp/resources/javascript/solr-facets.js
+++ src/main/webapp/resources/javascript/solr-facets.js
@@ -53,14 +53,16 @@
             Y.io(basePath + '/apps/search/facets/html' + locationSearch, {
                 on: {
                     success:function(id, o) {
-                        facetsContainer.append(o.responseText);
-                        Y.all('.facetHeader.openOnInit').each(function(node){toggleHeader(node)});
-                        Y.all('.facetHeader').on('click',toggleHeader);
-                        // fade in facets container
-                        new Y.Anim({
-                            node: facetsContainer,
-                            to:{opacity:1}
-                        }).run();
+                        if (o.responseText.indexOf('solrFacet') > -1) {
+                            facetsContainer.append(o.responseText);
+                            Y.all('.facetHeader.openOnInit').each(function(node){toggleHeader(node)});
+                            Y.all('.facetHeader').on('click',toggleHeader);
+                            // fade in facets container
+                            new Y.Anim({
+                                node: facetsContainer,
+                                to:{opacity:1}
+                            }).run();
+                        }
                     }
                 }
             });
