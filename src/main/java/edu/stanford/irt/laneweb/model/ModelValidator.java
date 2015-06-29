ca0e724c156e891dc0236cee8a399127bbb9126b
Charles Yates <ceyates@stanford.edu>
2015-06-29 08:05:40 +0200

    Revert "oopsie, valid by default"
    This reverts commit fd552d81bfbd22875654d7a16d61b2ef34890a20.

--- src/main/java/edu/stanford/irt/laneweb/model/ModelValidator.java
+++ src/main/java/edu/stanford/irt/laneweb/model/ModelValidator.java
@@ -3,6 +3,6 @@ package edu.stanford.irt.laneweb.model;
 public class ModelValidator {
 
     public boolean isValid(final String key, final Object value) {
-        return true;
+        return false;
     }
 }
