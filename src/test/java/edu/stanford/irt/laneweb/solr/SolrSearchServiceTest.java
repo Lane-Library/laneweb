356a8622f12739484ef19a47bb022b9b963b8913
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-09-23 10:25:52 -0700

    more tests

--- src/test/java/edu/stanford/irt/laneweb/solr/SolrSearchServiceTest.java
+++ src/test/java/edu/stanford/irt/laneweb/solr/SolrSearchServiceTest.java
@@ -1,9 +1,11 @@
 package edu.stanford.irt.laneweb.solr;
 
+import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
 
@@ -19,7 +21,9 @@ import org.junit.Test;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.FacetOptions.FacetSort;
 import org.springframework.data.solr.core.query.result.FacetFieldEntry;
+import org.springframework.data.solr.core.query.result.FacetPage;
 import org.springframework.data.solr.core.query.result.SolrResultPage;
 
 import edu.stanford.irt.laneweb.eresources.Eresource;
@@ -46,6 +50,34 @@ public class SolrSearchServiceTest {
         this.repository = createMock(SolrRepository.class);
         this.template = createMock(SolrTemplate.class);
         this.searchService = new TestSolrSearchService(this.template, this.repository);
+    }
+
+    @Test
+    public final void testFacetByField() {
+        FacetPage<Object> fpage = createMock(FacetPage.class);
+        expect(this.template.queryForFacetPage(anyObject(), anyObject())).andReturn(fpage);
+        replay(this.template, fpage);
+        this.searchService.facetByField("query", "filters", "field", 0, 10, 1, FacetSort.COUNT);
+        verify(this.template, fpage);
+        reset(this.template, fpage);
+        expect(this.template.queryForFacetPage(anyObject(), anyObject())).andReturn(fpage);
+        replay(this.template, fpage);
+        this.searchService.facetByField("query", "", "field", 0, 10, 1, FacetSort.COUNT);
+        verify(this.template, fpage);
+    }
+
+    @Test
+    public final void testFacetByManyFields() {
+        FacetPage<Object> fpage = createMock(FacetPage.class);
+        expect(this.template.queryForFacetPage(anyObject(), anyObject())).andReturn(fpage);
+        replay(this.template, fpage);
+        this.searchService.facetByManyFields("query", "filters", 1);
+        verify(this.template, fpage);
+        reset(this.template, fpage);
+        expect(this.template.queryForFacetPage(anyObject(), anyObject())).andReturn(fpage);
+        replay(this.template, fpage);
+        this.searchService.facetByManyFields("query", "", 1);
+        verify(this.template, fpage);
     }
 
     @Test
