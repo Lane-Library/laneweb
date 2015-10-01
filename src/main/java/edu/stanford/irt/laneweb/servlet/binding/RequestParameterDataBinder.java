6cb1ad047ff13b72b4e78c1d7ca5e062af305605
alainb <alainb@stanford.edu>
2015-10-01 12:54:30 -0700

    to be able to filter calendar by email

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/RequestParameterDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/RequestParameterDataBinder.java
@@ -26,7 +26,7 @@ public class RequestParameterDataBinder implements DataBinder {
         { "remove-pl", Model.REMOVE_PERSISTENT_LOGIN } };
 
     private static final String[] PARAMETER_SAME_AS_MODEL = { Model.ACTION, Model.BANNER, Model.CATEGORY,
-        Model.CLASS_ID, Model.TIMEOUT, Model.RESOURCE_ID, Model.PAGE, Model.TITLE, Model.SELECTION,
+        Model.CLASS_ID, Model.EMAIL, Model.TIMEOUT, Model.RESOURCE_ID, Model.PAGE, Model.TITLE, Model.SELECTION,
             Model.BASSETT_NUMBER, Model.URL, Model.CALLBACK, Model.PASSWORD, Model.RELEASE, Model.HOST, Model.SOURCEID,
             Model.SOURCE, Model.ID, Model.TEXT, Model.RETURN , Model.LIMIT };
 
