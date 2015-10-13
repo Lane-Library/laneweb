0c15f5347ba48edaeb557a707577a43de9d92911
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-10-13 16:58:00 -0700

    sonar: trailing whitespace

--- src/main/webapp/resources/javascript/solr-pagination.js
+++ src/main/webapp/resources/javascript/solr-pagination.js
@@ -1,9 +1,9 @@
 (function() {
         Y.all('.s-pagination form[name=pagination]').on('submit', function (e) {
-            var form = e.target, 
-            p = form.get('page'), 
-            parent = form.get('parentNode'), 
-            page = Number(p.get('value').replace(/[^\d]/g,'')), 
+            var form = e.target,
+            p = form.get('page'),
+            parent = form.get('parentNode'),
+            page = Number(p.get('value').replace(/[^\d]/g,'')),
             pages = form.get('pages');
             p.set('value',page);
             if (page < 1 || page > Number(pages.get('value'))) {
