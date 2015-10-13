cef031d31e5901aa8285d09663e8c9df85b41977
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-10-13 16:03:32 -0700

    more converage

--- src/main/webapp/resources/test/solr-pagination/solr-pagination-test.js
+++ src/main/webapp/resources/test/solr-pagination/solr-pagination-test.js
@@ -1,5 +1,5 @@
 /**
- * @author ceyates
+ * @author ryanmax
  */
 Y.applyConfig({fetchCSS:true});
 Y.use('node-event-simulate', 'console', 'test', function(Y){
@@ -8,22 +8,28 @@ Y.use('node-event-simulate', 'console', 'test', function(Y){
         name: "Solr Pagination TestCase",
         testSubmitInvalid1: function() {
             var form = Y.one("form"),
-            parentDiv = Y.one(".s-pagination"),
-            pages = form.get('pages');
+            parentDiv = Y.one(".s-pagination");
             form.one("input[name=page]").set('value','999');
             form.simulate("submit");
             Y.Assert.areNotEqual(null, parentDiv.one(".error"));
-            form.append(pages);
             parentDiv.one(".error").remove();
         },
         testSubmitInvalid2: function() {
             var form = Y.one("form"),
-            parentDiv = Y.one(".s-pagination"),
-            pages = form.get('pages');
+            parentDiv = Y.one(".s-pagination");
             form.one("input[name=page]").set('value','0');
             form.simulate("submit");
             Y.Assert.areNotEqual(null, parentDiv.one(".error"));
-            form.append(pages);
+            parentDiv.one(".error").remove();
+        },
+        testSubmitInvalidTwice: function() {
+            var form = Y.one("form"),
+            parentDiv = Y.one(".s-pagination");
+            form.one("input[name=page]").set('value','0');
+            form.simulate("submit");
+            Y.Assert.areNotEqual(null, parentDiv.one(".error"));
+            form.simulate("submit");
+            Y.Assert.areEqual(1, parentDiv.all(".error").size());
             parentDiv.one(".error").remove();
         },
         testSubmitValid: function() {
