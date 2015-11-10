3a030e97b24840dd6c764636334d890ea1feb74c
Charles Yates <ceyates@stanford.edu>
2015-11-10 08:05:17 +0200

    case 112705 create an animated arrow to point to the solr search ad

--- src/main/webapp/resources/javascript/solr-ad.js
+++ src/main/webapp/resources/javascript/solr-ad.js
@@ -1,9 +1,18 @@
 // solr beta advert
 (function() {
     var advert,
-        login = Y.one("#login");
+        login = Y.one("#login"),
+        anim
     if(login){
-        advert = Y.Node.create('<li style="font-weight:bold;"><span style="color:#9a0003">NEW! </span><a style="color:#007c92" href="http://lane-beta.stanford.edu/lanesearch.html">Try Lane Search beta <i class="fa fa-arrow-right" style="color:#007c92"></i></a></li>');
+        advert = Y.Node.create('<li style="font-weight:bold;"><i class="fa fa-arrow-right" id="betaAddArrow" style="font-size:24px;position:relative;right:100px;top:3px;opacity:0;color:#9a0003"></i> <span style="color:#9a0003">NEW! </span><a style="color:#007c92" href="http://lane-beta.stanford.edu/lanesearch.html">Try Lane Search beta <i class="fa fa-arrow-right" style="color:#007c92"></i></a></li>');
         login.prepend(advert);
+        anim = new Y.Anim({
+            node: "#betaAddArrow",
+            to: {
+                opacity: 1,
+                right:0},
+            duration: 1
+        });
+        anim.run();
     }
 })();
