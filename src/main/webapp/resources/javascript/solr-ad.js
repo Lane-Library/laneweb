88f731a48bd0dff7a5fe939c24f9cd618245f6e3
Charles Yates <ceyates@stanford.edu>
2015-11-10 08:06:53 +0200

    forgot semicolon

--- src/main/webapp/resources/javascript/solr-ad.js
+++ src/main/webapp/resources/javascript/solr-ad.js
@@ -2,7 +2,7 @@
 (function() {
     var advert,
         login = Y.one("#login"),
-        anim
+        anim;
     if(login){
         advert = Y.Node.create('<li style="font-weight:bold;"><i class="fa fa-arrow-right" id="betaAdArrow" style="font-size:24px;position:relative;right:100px;top:3px;opacity:0;color:#9a0003"></i> <span style="color:#9a0003">NEW! </span><a style="color:#007c92" href="http://lane-beta.stanford.edu/lanesearch.html">Try Lane Search beta <i class="fa fa-arrow-right" style="color:#007c92"></i></a></li>');
         login.prepend(advert);
