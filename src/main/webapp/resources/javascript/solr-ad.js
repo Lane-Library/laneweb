1eb0816bc1afa5fa16b3937f6c07f0fc7369a208
Charles Yates <ceyates@stanford.edu>
2015-11-10 08:17:15 +0200

    correct position

--- src/main/webapp/resources/javascript/solr-ad.js
+++ src/main/webapp/resources/javascript/solr-ad.js
@@ -4,13 +4,13 @@
         login = Y.one("#login"),
         anim;
     if(login){
-        advert = Y.Node.create('<li style="font-weight:bold;"><i class="fa fa-arrow-right" id="betaAdArrow" style="font-size:24px;position:relative;right:100px;top:3px;opacity:0;color:#9a0003"></i> <span style="color:#9a0003">NEW! </span><a style="color:#007c92" href="http://lane-beta.stanford.edu/lanesearch.html">Try Lane Search beta <i class="fa fa-arrow-right" style="color:#007c92"></i></a></li>');
+        advert = Y.Node.create('<li style="font-weight:bold;position:relative"><i class="fa fa-arrow-right" id="betaAdArrow" style="font-size:24px;position:absolute;left:-100px;top:-5px;opacity:0;color:#9a0003"></i> <span style="color:#9a0003">NEW! </span><a style="color:#007c92" href="http://lane-beta.stanford.edu/lanesearch.html">Try Lane Search beta <i class="fa fa-arrow-right" style="color:#007c92"></i></a></li>');
         login.prepend(advert);
         anim = new Y.Anim({
             node: "#betaAdArrow",
             to: {
                 opacity: 1,
-                right:0},
+                left:-10},
             duration: 1
         });
         anim.run();
