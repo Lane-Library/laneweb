341c007ff3d415137f4755cac895d1d21af4dcc3
alainb <alainb@stanford.edu>
2015-12-02 09:17:23 -0800

    to refactorize the javascript

--- src/main/webapp/resources/javascript/lane-bassett.js
+++ src/main/webapp/resources/javascript/lane-bassett.js
@@ -1,170 +1,154 @@
 (function() {
 
+	var bassettContent = Y.one('#bassettContent'), model = Y.lane.Model, 
+	basePath = model.get(model.BASE_PATH)|| "", 
+	diagramDisplay = false,
+	accordion, history, registerLinksContainer, subRegionToShow = 4, prevRegion, prevSubRegion, 
+	seeAll, region, subRegion, anchor,
+	formatAjaxUrl = function(
+			href) {
+		var url;
+		href = href.replace("search.html",
+				"/biomed-resources/bassett/bassettsView.html");
+		href = href.substr(href.indexOf("/bassett/") + 8);
+		href = href.split("?");
+		if (href.length === 1) {
+			url = href[0];
+		}
+		if (href.length > 1) {
+			url = href[0] + "?" + href[1];
+		}
+		if (diagramDisplay) {
+			url = url + "&t=diagram";
+		}
+		return url;
+	},
 
-        var bassettContent = Y.one('#bassettContent'),
-            model = Y.lane.Model,
-            basePath = model.get(model.BASE_PATH) || "",
-            diagramDisplay = false,
-            accordion,
-            history,
-            registerLinksContainer,
-            subRegionToShow = 4,
-
-            formatAjaxUrl = function(href) {
-                var url;
-                href = href.replace("search.html", "/biomed-resources/bassett/bassettsView.html");
-                href = href.substr(href.indexOf("/bassett/") + 8);
-                href = href.split("?");
-                if (href.length === 1) {
-                    url = href[0];
-                }
-                if (href.length > 1) {
-                    url = href[0] + "?" + href[1];
-                }
-                if (diagramDisplay) {
-                    url = url + "&t=diagram";
-                }
-                return url;
-            },
-
-            loadContent = function(url) {
-                url = basePath + "/plain/biomed-resources/bassett/raw".concat(url);
-                function successHandler(id, o) {
-                    var content = Y.Node.create(o.responseText),
-                        container = Y.one('#bassettContent');
-                    container.setContent(content);
-                    registerLinksContainer(container);
-                }
-                Y.io(url, {on : {success : successHandler}});
-            },
-
-            handleClick = function(ev) {
-                var url;
-                if (this.get('id') === "diagram-choice") {
-                    diagramDisplay = true;
-                }
-                if (this.get('id') === "photo-choice") {
-                    diagramDisplay = false;
-                }
-                url = formatAjaxUrl(this.get('href'));
-                try {
-                    history.addValue("bassett",url);
-                } catch (e) {
-                    loadContent(url);
-                }
-                ev.preventDefault();
-            },
-
-        initializeHistory = function() {
-            history = new Y.HistoryHash();
-            if(history.get('bassett')){
-                loadContent(history.get('bassett'));
-            }
-            history.on("bassettChange",function(e) {
-                loadContent(e.newVal);
-            });
-            history.on("bassettRemove",function() {
-                loadContent(formatAjaxUrl(Y.lane.Location.get("href")));
-            });
-        };
-
-        registerLinksContainer = function(container) {
-            var anchor, i;
-            if (container) {
-                anchor = container.all('a');
-                for (i = 0; i < anchor.size(); i++) {
-                    if (anchor.item(i).get('rel') === null || anchor.item(i).get('rel') === "" ||  anchor.item(i).get('rel') === "propagation") {
-                        anchor.item(i).on('click', handleClick);
-                    }
-                }
-            }
-        };
-
-        
-       //For the bassett menu
-        
-
-	    expandSubRegion = function(event) {
-			var region = event.currentTarget.ancestor('ul'),
-			seeAllContent,
-			seeAll = region.one('.see-all');
-			if(seeAll){
-				seeAllContent = seeAll.getHTML();
+	loadContent = function(url) {
+		url = basePath + "/plain/biomed-resources/bassett/raw".concat(url);
+		function successHandler(id, o) {
+			var content = Y.Node.create(o.responseText), container = Y
+					.one('#bassettContent');
+			container.setContent(content);
+			registerLinksContainer(container);
+		}
+		Y.io(url, {
+			on : {
+				success : successHandler
 			}
-			hideRegions();
-			resetSubregion();
-			if ( "see all" == seeAllContent) {
-				seeAll.setHTML('hide');
-				var subRegions = region.all('li');
-				for (i = subRegionToShow + 1; i < subRegions.size(); i++) {
-					subRegions.item(i).show();
-				}
-			}
-	    }
-        
-        surlineSubRegion = function(event){
-        	resetSubregion();
-        	var anchor = event.currentTarget;
-        	selectedSubRegion = anchor.ancestor();
-        	selectedSubRegion.addClass('enabled');
-        	var i = selectedSubRegion.one('i');
-        	i.removeClass('fa-square-o');
-        	i.addClass('fa-check-circle-o');
-        }
+		});
+	},
 
-        resetSubregion = function(){
-        	var liList = Y.all('.region li:not(:first-child)');
-        	for(i= 0; i< liList.size(); i++){
-        		var li = liList.item(i);
-        		li.removeClass('enabled');
-        		var iElement = li.one('i');
-        		if(iElement != null){
-	        		iElement.addClass('fa-square-o');
-	            	iElement.removeClass('fa-check-circle-o');
-        		}
-            }
-        }
-        
-        hideRegions = function() {
-			var region = Y.all('.region');
-			for (i = 0; i < region.size(); i++) {
-				hideSubRegion( region.item(i));
-			}
-			
-        }
-        
+	handleClick = function(ev) {
+		var url;
+		if (this.get('id') === "diagram-choice") {
+			diagramDisplay = true;
+		}
+		if (this.get('id') === "photo-choice") {
+			diagramDisplay = false;
+		}
+		url = formatAjaxUrl(this.get('href'));
+		try {
+			history.addValue("bassett", url);
+		} catch (e) {
+			loadContent(url);
+		}
+		ev.preventDefault();
+	},
 
-	    hideSubRegion = function(region) {
-			var seeAll = region.one('.see-all');
-			if (seeAll) {
-				seeAll.setHTML('see all');
-				var subRegion = region.all('li');
-				for (j = 0; j < subRegion.size(); j++) {
-					if (j > subRegionToShow) {
-						subRegion.item(j).hide();
-					}
+	initializeHistory = function() {
+		history = new Y.HistoryHash();
+		if (history.get('bassett')) {
+			loadContent(history.get('bassett'));
+		}
+		history.on("bassettChange", function(e) {
+			loadContent(e.newVal);
+		});
+		history.on("bassettRemove", function() {
+			loadContent(formatAjaxUrl(Y.lane.Location.get("href")));
+		});
+	};
+
+	registerLinksContainer = function(container) {
+		anchor;
+		if (container) {
+			anchor = container.all('a');
+			for (var i = 0; i < anchor.size(); i++) {
+				if (anchor.item(i).get('rel') === null
+						|| anchor.item(i).get('rel') === ""
+						|| anchor.item(i).get('rel') === "propagation") {
+					anchor.item(i).on('click', handleClick);
 				}
 			}
 		}
-        
-        if (bassettContent) {
-            accordion = Y.one('#bassett-menu');
-            // not if largerView.html
-            if (accordion) {
-            	hideRegions();
-                registerLinksContainer(accordion);
-                registerLinksContainer(Y.one('#bassettContent'));
-                var seeAll = Y.all('.see-all');
-                for (i = 0; i < seeAll.size(); i++) {
-                    seeAll.item(i).on('click', expandSubRegion);
-                }
-                var li = Y.all('.region li:not(:first-child) a');
-                for (i = 0; i < li.size(); i++) {
-                    li.item(i).on('click', surlineSubRegion);
-                }
-                initializeHistory();
-            }
-        }
-        
-        
+	};
+
+	// For the bassett menu
+
+	expandSubRegion = function(event) {
+		region = event.currentTarget.ancestor('ul');
+		var display = region.all('li').item(subRegionToShow+1).getStyle('display');
+		hideSubRegions(region);
+		resetSubRegion();
+		if (display == 'none') {
+			region.one('.see-all').setHTML('hide');
+			subRegion = region.all('li');
+			for (i = subRegionToShow + 1; i < subRegion.size(); i++) {
+				subRegion.item(i).setStyle('display', 'block');
+			}
+		}
+	}
+
+	hideSubRegions = function(region) {
+		if (prevRegion && prevRegion.one('.see-all')) {
+			prevRegion.one('.see-all').setHTML('see all');
+			subRegion = prevRegion.all('li');
+			for (j = subRegionToShow; j < subRegion.size(); j++) {
+				subRegion.item(j).hide();
+			}
+		}
+		prevRegion = region;
+	}
+
+	
+	surlineSubRegion = function(event) {
+		anchor = event.currentTarget;
+		subRegion = anchor.ancestor();
+		resetSubRegion(subRegion);
+		subRegion.addClass('enabled');
+		var i = subRegion.one('i');
+		i.removeClass('fa-square-o');
+		i.addClass('fa-check-circle-o');
+	}
+
+	resetSubRegion = function(subRegion) {
+		if (prevSubRegion && prevSubRegion.one('i')) {
+			prevSubRegion.removeClass('enabled');
+			var iElement = prevSubRegion.one('i');
+			iElement.addClass('fa-square-o');
+			iElement.removeClass('fa-check-circle-o');
+		}
+		prevSubRegion = subRegion;
+	}
+
+	
+
+	if (bassettContent) {
+		accordion = Y.one('#bassett-menu');
+		// not if largerView.html
+		if (accordion) {
+			registerLinksContainer(accordion);
+			registerLinksContainer(Y.one('#bassettContent'));
+			seeAll = Y.all('.see-all');
+			for (i = 0; i < seeAll.size(); i++) {
+				seeAll.item(i).on('click', expandSubRegion);
+			}
+			var lis = Y.all('.region li:not(:first-child) a');
+			for (i = 0; i < lis.size(); i++) {
+				lis.item(i).on('click', surlineSubRegion);
+			}
+			initializeHistory();
+		}
+	}
+
 })();
