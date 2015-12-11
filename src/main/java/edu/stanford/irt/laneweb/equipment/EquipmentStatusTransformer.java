9fc1cebc26741ee11da7bf2486150181f603d862
Charles Yates <ceyates@stanford.edu>
2015-12-11 11:17:31 +0200

    cleanup

--- src/main/java/edu/stanford/irt/laneweb/equipment/EquipmentStatusTransformer.java
+++ src/main/java/edu/stanford/irt/laneweb/equipment/EquipmentStatusTransformer.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.sql.DataSource;
 
@@ -19,24 +17,29 @@ import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.util.XMLUtils;
 
 public class EquipmentStatusTransformer implements Transformer {
-    
+
     private static final String AVAILABLE_QUERY = "SELECT bi.bib_id, COUNT(*) FROM lmldb.bib_item bi, "
-        + "  lmldb.item_status item_status_1 LEFT OUTER JOIN lmldb.item_status item_status_2 "
-        + "ON (item_status_1.item_id          = item_status_2.item_id "
-        + "AND item_status_1.item_status_date < item_status_2.item_status_date) "
-        + "WHERE item_status_2.item_id       IS NULL "
-        + "AND bi.item_id                     = item_status_1.item_id "
-        + "AND item_status_1.item_status      = 1 "
-        + "AND bi.bib_id in (select regexp_substr(?,'[^,]+', 1, level) from dual connect by regexp_substr(?, '[^,]+', 1, level) is not null) "
-        + "GROUP BY bi.bib_id";
+            + "  lmldb.item_status item_status_1 LEFT OUTER JOIN lmldb.item_status item_status_2 "
+            + "ON (item_status_1.item_id          = item_status_2.item_id "
+            + "AND item_status_1.item_status_date < item_status_2.item_status_date) "
+            + "WHERE item_status_2.item_id       IS NULL "
+            + "AND bi.item_id                     = item_status_1.item_id " + "AND item_status_1.item_status      = 1 "
+            + "AND bi.bib_id in (select regexp_substr(?,'[^,]+', 1, level) from dual connect by regexp_substr(?, '[^,]+', 1, level) is not null) "
+            + "GROUP BY bi.bib_id";
+
+    private static final String DIV = "div";
+
+    private static final String SPAN = "span";
+
+    private static final String XHTML_NAMESPACE = "http://www.w3.org/1999/xhtml";
+
+    private DataSource dataSource;
+
+    private StringBuilder ids = new StringBuilder();
 
     private XMLConsumer xmlConsumer;
-    
-    private StringBuilder ids = new StringBuilder();
-    
-    public DataSource dataSource;
-    
-    public EquipmentStatusTransformer(DataSource dataSource) {
+
+    public EquipmentStatusTransformer(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
@@ -141,25 +144,23 @@ public class EquipmentStatusTransformer implements Transformer {
     public void startPrefixMapping(final String prefix, final String uri) throws SAXException {
         this.xmlConsumer.startPrefixMapping(prefix, uri);
     }
-    
-    private void generateStatus() throws SAXException {
 
+    private void generateStatus() throws SAXException {
         try (Connection conn = this.dataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(AVAILABLE_QUERY)) {
+                PreparedStatement stmt = conn.prepareStatement(AVAILABLE_QUERY)) {
             // remove leading comma
             this.ids.deleteCharAt(0);
-            String idList = ids.toString();
+            String idList = this.ids.toString();
             stmt.setString(1, idList);
             stmt.setString(2, idList);
             try (ResultSet rs = stmt.executeQuery()) {
-                while(rs.next()) {
-                    XMLUtils.startElement(xmlConsumer, "http://www.w3.org/1999/xhtml", "div");
-                    XMLUtils.createElementNS(this.xmlConsumer, "http://www.w3.org/1999/xhtml", "span", rs.getString(1));
-                    XMLUtils.createElementNS(this.xmlConsumer, "http://www.w3.org/1999/xhtml", "span", rs.getString(2));
-                    XMLUtils.endElement(xmlConsumer, "http://www.w3.org/1999/xhtml", "div");
+                while (rs.next()) {
+                    XMLUtils.startElement(this.xmlConsumer, XHTML_NAMESPACE, DIV);
+                    XMLUtils.createElementNS(this.xmlConsumer, XHTML_NAMESPACE, SPAN, rs.getString(1));
+                    XMLUtils.createElementNS(this.xmlConsumer, XHTML_NAMESPACE, SPAN, rs.getString(2));
+                    XMLUtils.endElement(this.xmlConsumer, XHTML_NAMESPACE, DIV);
                 }
             }
-            
         } catch (SQLException e) {
             throw new LanewebException(e);
         }
