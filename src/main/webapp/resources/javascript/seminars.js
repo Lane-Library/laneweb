d3e10e03cd7701193bc5cc9dd09b90554f934187
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-12-16 12:17:26 -0800

    sonar

--- src/main/webapp/resources/javascript/seminars.js
+++ src/main/webapp/resources/javascript/seminars.js
@@ -4,9 +4,9 @@
         var currentYear = new Date().getFullYear(),
         visibleSeminars = seminars.size();
         Y.all('.seminar').each(
-            function(n) {
-                var month = this.one('.month').get('text'), 
-                day = this.one('.day').get('text'), 
+            function() {
+                var month = this.one('.month').get('text'),
+                day = this.one('.day').get('text'),
                 endTime = this.one('.time').get('text'), date;
                 if (month && day && endTime) {
                     endTime = endTime.toString().replace(/.* - (\d{1,2}:\d{2} .m)$/, "$1");
