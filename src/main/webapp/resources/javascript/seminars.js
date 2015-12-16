d0b168e3ddb62c6015ef0ebe0c10b38e2741339d
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-12-15 17:28:50 -0800

    case 99709: don't show grand round events after event has occurred

---
+++ src/main/webapp/resources/javascript/seminars.js
@@ -0,0 +1,18 @@
+(function() {
+    var currentYear = new Date().getFullYear();
+    Y.all('.seminar').each(
+            function(n) {
+                var month = this.one('.month').get('text'), 
+                day = this.one('.day').get('text'), 
+                endTime = this.one('.time').get('text'), date;
+                if (month && day && endTime) {
+                    endTime = endTime.toString().replace(/.* - (\d{1,2}:\d{2} .m)$/, "$1");
+                    date = month + " " + day + ", " + currentYear;
+                    if (Date.parse(date + " " + endTime) < Date.now()) {
+                        this.setStyles({
+                            display : "none"
+                        });
+                    }
+                }
+            });
+})();
