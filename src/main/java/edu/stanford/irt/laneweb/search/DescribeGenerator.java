f19340e68b55422ebcc278405d1bb7378f495314
alainb <alainb@stanford.edu>
2016-02-26 11:04:46 -0800

    The query can be null for search-template.xml or search-describe The search monitor was broken

--- src/main/java/edu/stanford/irt/laneweb/search/DescribeGenerator.java
+++ src/main/java/edu/stanford/irt/laneweb/search/DescribeGenerator.java
@@ -1,11 +1,15 @@
 package edu.stanford.irt.laneweb.search;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Objects;
 
 import edu.stanford.irt.cocoon.cache.Validity;
 import edu.stanford.irt.cocoon.cache.validity.AlwaysValid;
 import edu.stanford.irt.cocoon.pipeline.CacheablePipelineComponent;
 import edu.stanford.irt.cocoon.xml.SAXStrategy;
+import edu.stanford.irt.laneweb.model.Model;
+import edu.stanford.irt.laneweb.model.ModelUtil;
 import edu.stanford.irt.search.impl.MetaSearchManager;
 import edu.stanford.irt.search.impl.Result;
 import edu.stanford.irt.search.impl.SimpleQuery;
@@ -37,4 +41,11 @@ public class DescribeGenerator extends AbstractMetasearchGenerator<Result> imple
     protected Result doSearch(final String query) {
         return describe(new SimpleQuery(""), null);
     }
+    
+    @Override
+    public void setModel(final Map<String, Object> model) {
+        model.put(Model.QUERY, "");
+        super.setModel(model);
+    }
+    
 }
