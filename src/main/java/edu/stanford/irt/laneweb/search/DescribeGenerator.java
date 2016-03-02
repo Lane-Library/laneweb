97d6faa5cfe7bdd29deee7ef10a53effd77680a1
Charles Yates <ceyates@stanford.edu>
2016-03-02 09:43:09 +0200

    remove DescribeGenerator from AbstractSearchGenerator hierarchy

--- src/main/java/edu/stanford/irt/laneweb/search/DescribeGenerator.java
+++ src/main/java/edu/stanford/irt/laneweb/search/DescribeGenerator.java
@@ -1,25 +1,31 @@
 package edu.stanford.irt.laneweb.search;
 
 import java.io.Serializable;
-import java.util.Map;
-import java.util.Objects;
 
 import edu.stanford.irt.cocoon.cache.Validity;
 import edu.stanford.irt.cocoon.cache.validity.AlwaysValid;
 import edu.stanford.irt.cocoon.pipeline.CacheablePipelineComponent;
+import edu.stanford.irt.cocoon.pipeline.generate.AbstractGenerator;
 import edu.stanford.irt.cocoon.xml.SAXStrategy;
-import edu.stanford.irt.laneweb.model.Model;
-import edu.stanford.irt.laneweb.model.ModelUtil;
+import edu.stanford.irt.cocoon.xml.XMLConsumer;
+import edu.stanford.irt.search.Query;
 import edu.stanford.irt.search.impl.MetaSearchManager;
 import edu.stanford.irt.search.impl.Result;
 import edu.stanford.irt.search.impl.SimpleQuery;
 
-public class DescribeGenerator extends AbstractMetasearchGenerator<Result> implements CacheablePipelineComponent {
+public class DescribeGenerator extends AbstractGenerator implements CacheablePipelineComponent {
+
+    private static final Query EMPTY_QUERY = new SimpleQuery("");
 
     private static final String TYPE = "describe";
 
+    private MetaSearchManager metaSearchManager;
+
+    private SAXStrategy<Result> saxStrategy;
+
     public DescribeGenerator(final MetaSearchManager metaSearchManager, final SAXStrategy<Result> saxStrategy) {
-        super(metaSearchManager, saxStrategy);
+        this.metaSearchManager = metaSearchManager;
+        this.saxStrategy = saxStrategy;
     }
 
     @Override
@@ -38,14 +44,7 @@ public class DescribeGenerator extends AbstractMetasearchGenerator<Result> imple
     }
 
     @Override
-    protected Result doSearch(final String query) {
-        return describe(new SimpleQuery(""), null);
+    protected void doGenerate(final XMLConsumer xmlConsumer) {
+        this.saxStrategy.toSAX(this.metaSearchManager.describe(EMPTY_QUERY, null), xmlConsumer);
     }
-    
-    @Override
-    public void setModel(final Map<String, Object> model) {
-        model.put(Model.QUERY, "");
-        super.setModel(model);
-    }
-    
 }
