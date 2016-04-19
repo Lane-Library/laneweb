ca0c449b294e9eb95a32434a23aa88d09e0da922
Charles Yates <ceyates@stanford.edu>
2016-04-19 08:56:43 +0200

    return full data model

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/UserDataController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/UserDataController.java
@@ -1,41 +1,30 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
-import java.util.Collections;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import edu.stanford.irt.laneweb.model.Model;
-import edu.stanford.irt.laneweb.servlet.binding.UserDataBinder;
-import edu.stanford.irt.laneweb.user.User;
+import edu.stanford.irt.laneweb.servlet.binding.CompositeDataBinder;
 
 @Controller
 public class UserDataController {
 
-    private UserDataBinder binder;
+    private CompositeDataBinder binder;
 
     @Autowired
-    public UserDataController(final UserDataBinder binder) {
+    public UserDataController(final CompositeDataBinder binder) {
         this.binder = binder;
     }
 
     @RequestMapping(value = "/apps/userData")
     @ResponseBody
-    public Map<String, Object> getUserData(@ModelAttribute(Model.USER) final User user) {
-        return Collections.singletonMap(Model.USER, user);
-    }
-
-    @ModelAttribute
-    protected void bind(final HttpServletRequest request, final org.springframework.ui.Model model) {
+    public Map<String, Object> getUserData(final HttpServletRequest request, final org.springframework.ui.Model model) {
         this.binder.bind(model.asMap(), request);
-        if (!model.containsAttribute(edu.stanford.irt.laneweb.model.Model.USER)) {
-            model.addAttribute(edu.stanford.irt.laneweb.model.Model.USER, null);
-        }
+        return model.asMap();
     }
 }
