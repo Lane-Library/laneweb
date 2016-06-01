0f5a461d18a5c6974d14bad241fc8e64d0f147ec
Charles Yates <ceyates@stanford.edu>
2016-06-01 15:30:54 +0200

    case 121937 update to nekohtml-1.9.22 and work around its empty namespace problem

--- src/main/java/edu/stanford/irt/laneweb/cocoon/HTMLSAXParser.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/HTMLSAXParser.java
@@ -3,15 +3,44 @@ package edu.stanford.irt.laneweb.cocoon;
 import java.io.IOException;
 
 import org.apache.xerces.parsers.AbstractSAXParser;
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import edu.stanford.irt.cocoon.source.Source;
+import edu.stanford.irt.cocoon.xml.ContentHandlerWrapper;
 import edu.stanford.irt.cocoon.xml.SAXParser;
 import edu.stanford.irt.cocoon.xml.XMLConsumer;
 import edu.stanford.irt.laneweb.LanewebException;
 
 public class HTMLSAXParser extends AbstractSAXParser implements SAXParser {
+
+    // nekohtml doesn't provide namespaces for the elements it creates, breaking saxon
+    private static class MissingNamespaceProvider extends ContentHandlerWrapper {
+
+        public MissingNamespaceProvider(final XMLConsumer xmlConsumer) {
+            super(xmlConsumer, null);
+        }
+
+        @Override
+        public void endElement(final String uri, final String localName, final String qName) throws SAXException {
+            String ns = uri;
+            if (uri == null || "".equals(uri)) {
+                ns = "http://www.w3.org/1999/xhtml";
+            }
+            super.endElement(ns, localName, qName);
+        }
+
+        @Override
+        public void startElement(final String uri, final String localName, final String qName, final Attributes atts)
+                throws SAXException {
+            String ns = uri;
+            if (uri == null || "".equals(uri)) {
+                ns = "http://www.w3.org/1999/xhtml";
+            }
+            super.startElement(ns, localName, qName, atts);
+        }
+    }
 
     public HTMLSAXParser(final NekoHTMLConfiguration config) {
         super(config);
@@ -20,7 +49,7 @@ public class HTMLSAXParser extends AbstractSAXParser implements SAXParser {
     @Override
     public void parse(final Source source, final XMLConsumer xmlConsumer) {
         try {
-            setContentHandler(xmlConsumer);
+            setContentHandler(new MissingNamespaceProvider(xmlConsumer));
             // case 79097 processing instructions with joost transformations
             // nekohtml doesn't send endDTD which confuses joost. fix by not setting lexicalHandler
             InputSource inputSource = new InputSource();
