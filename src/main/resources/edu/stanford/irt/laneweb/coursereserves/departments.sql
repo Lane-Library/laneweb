3eefaf2593bac639afc4feecd13b1e4d37343243
Charles Yates <ceyates@stanford.edu>
2016-06-28 08:41:19 -0700

    case 122542 Exclude 2 categories from the course reserves

--- src/main/resources/edu/stanford/irt/laneweb/coursereserves/departments.sql
+++ src/main/resources/edu/stanford/irt/laneweb/coursereserves/departments.sql
@@ -4,6 +4,8 @@ FROM lmldb.department,
   lmldb.reserve_list,
   lmldb.reserve_list_courses
 WHERE department.department_id  != 22
+AND department.department_id    != 11
+AND department.department_id    != 8
 AND reserve_list.reserve_list_id = reserve_list_courses.reserve_list_id
 AND department.department_id     = reserve_list_courses.department_id
 AND expire_date                  > SYSDATE
