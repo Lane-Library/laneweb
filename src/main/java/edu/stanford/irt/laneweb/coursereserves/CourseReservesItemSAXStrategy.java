08304944faa469aae61430faa33641e0b3687e60
Charles Yates <ceyates@stanford.edu>
2016-07-12 10:12:42 -0700

    add bookcovers

--- src/main/java/edu/stanford/irt/laneweb/coursereserves/CourseReservesItemSAXStrategy.java
+++ src/main/java/edu/stanford/irt/laneweb/coursereserves/CourseReservesItemSAXStrategy.java
@@ -1,11 +1,13 @@
 package edu.stanford.irt.laneweb.coursereserves;
 
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
 
 import edu.stanford.irt.cocoon.xml.XMLConsumer;
 import edu.stanford.irt.coursereserves.CourseReservesItem;
 import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.resource.AbstractXHTMLSAXStrategy;
+import edu.stanford.irt.laneweb.util.XMLUtils;
 
 public class CourseReservesItemSAXStrategy extends AbstractXHTMLSAXStrategy<CourseReservesItem> {
 
@@ -13,8 +15,13 @@ public class CourseReservesItemSAXStrategy extends AbstractXHTMLSAXStrategy<Cour
     public void toSAX(final CourseReservesItem item, final XMLConsumer xmlConsumer) {
         try {
             startLi(xmlConsumer);
-            createAnchor(xmlConsumer, "http://lmldb.stanford.edu/cgi-bin/Pwebrecon.cgi?BBID=" + item.getId(),
-                    item.getTitle());
+            String id = Integer.toString(item.getId());
+            AttributesImpl atts = new AttributesImpl();
+            atts.addAttribute("", "class", "class", "CDATA", "bookcover");
+            atts.addAttribute("", "data-bibid", "data-bibid", "CDATA", id);
+            XMLUtils.startElement(xmlConsumer, XHTML_NS, "img", atts);
+            XMLUtils.endElement(xmlConsumer, XHTML_NS, "img");
+            createAnchor(xmlConsumer, "http://lmldb.stanford.edu/cgi-bin/Pwebrecon.cgi?BBID=" + id, item.getTitle());
             if (item.getAuthor() != null) {
                 createElement(xmlConsumer, "div", item.getAuthor());
             }
