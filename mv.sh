44eae0d212bce4fcd3bd76a1c875941338a787ad
Xu Wang <xuwang@gmail.com>
2016-07-30 20:18:59 -0700

    add oracle jdbc,ucp,ons as maven dependency, make the war ready for stock tomcat image [skip ci]

--- mv.sh
+++ mv.sh
@@ -1,6 +1,7 @@
 # tomcat sidecar cmd
 
-# copy laneweb.war to the emptyDir volume path, i.e. /app
-cp /laneweb.war /app
+# copy laneweb.war to the emptyDir volume path, i.e. /tomcat/webapps
+mkdir -p /tomcat/webapps
+cp /laneweb.war /tomcat/webapps
 # hold the container with a tail forever
 tail -f /dev/null
