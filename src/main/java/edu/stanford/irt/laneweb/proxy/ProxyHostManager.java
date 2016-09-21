6b8ce73f3c791e267486fff67fcf6b58a9161c63
Ryan Max Steinberg <ryanmax@stanford.edu>
2016-09-21 10:33:47 -0700

    exclude suppressed bibs from proxy host queries

--- src/main/java/edu/stanford/irt/laneweb/proxy/ProxyHostManager.java
+++ src/main/java/edu/stanford/irt/laneweb/proxy/ProxyHostManager.java
@@ -38,8 +38,12 @@ public class ProxyHostManager {
                 "UNION " +
                 "SELECT DISTINCT URL_HOST AS HOST " +
                 "FROM LMLDB.ELINK_INDEX, " +
-                "  LMLDB.MFHD_MASTER " +
+                "  LMLDB.MFHD_MASTER, " +
+                "  LMLDB.BIB_MFHD, " +
+                "  LMLDB.BIB_MASTER " +
                 "WHERE ELINK_INDEX.RECORD_ID    = MFHD_MASTER.MFHD_ID " +
+                "AND MFHD_MASTER.MFHD_ID = BIB_MFHD.MFHD_ID " +
+                "AND BIB_MFHD.BIB_ID = BIB_MASTER.BIB_ID " +
                 "AND LINK_SUBTYPE              IN ('HTTP','HTTPS') " +
                 "AND ELINK_INDEX.RECORD_TYPE    = 'M' " +
                 "AND ELINK_INDEX.RECORD_ID NOT IN " +
@@ -48,7 +52,8 @@ public class ProxyHostManager {
                 "  WHERE LOWER(RECORD_SEGMENT) LIKE '%, noproxy%' " +
                 "  ) " +
                 "AND URL_HOST NOT LIKE '%.stanford.edu' " +
-                "AND MFHD_MASTER.SUPPRESS_IN_OPAC != 'Y'";
+                "AND MFHD_MASTER.SUPPRESS_IN_OPAC != 'Y' " +
+                "AND BIB_MASTER.SUPPRESS_IN_OPAC != 'Y'";
 
         DatabaseProxyHostSet(final DataSource dataSource) {
             Connection conn = null;
