80956a9bcecd0d4b59d83ad2d296267e693f124f
Charles Yates <ceyates@stanford.edu>
2016-10-21 11:55:09 -0700

    private visibility for name

--- src/main/java/edu/stanford/irt/laneweb/servlet/CookieName.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/CookieName.java
@@ -1,9 +1,13 @@
 package edu.stanford.irt.laneweb.servlet;
 
 public enum CookieName {
-    EXPIRATION("lane-login-expiration-date"), EZPROXY("ezproxy"), IS_PERSISTENT("isPersistent"), USER("lane_user");
 
-    String name;
+    EXPIRATION("lane-login-expiration-date"),
+    EZPROXY("ezproxy"),
+    IS_PERSISTENT("isPersistent"),
+    USER("lane_user");
+
+    private String name;
 
     CookieName(final String name) {
         this.name = name;
