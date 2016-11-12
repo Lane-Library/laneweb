c4c4a4471ef954ad2c8d6f41754dae5995122b8b
Charles Yates <ceyates@stanford.edu>
2016-11-12 06:42:37 -0800

    minor cleanup

--- src/main/java/edu/stanford/irt/laneweb/servlet/binding/LDAPDataBinder.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/binding/LDAPDataBinder.java
@@ -22,9 +22,11 @@ public class LDAPDataBinder implements DataBinder {
     public void bind(final Map<String, Object> model, final HttpServletRequest request) {
         String userid = (String) model.get(Model.USER_ID);
         if (userid != null) {
-            String sunetid = userid.substring(0, userid.indexOf('@'));
-            String name, univid, email;
+            String name;
+            String univid;
+            String email;
             Boolean isActive;
+            String sunetid = userid.substring(0, userid.indexOf('@'));
             HttpSession session = request.getSession();
             synchronized (session) {
                 name = (String) session.getAttribute(Model.NAME);
