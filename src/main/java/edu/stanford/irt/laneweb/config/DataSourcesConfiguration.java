73bd9c7ad32f98b6049e7c92a89df0869dd1d01f
Charles Yates <ceyates@stanford.edu>
2017-01-09 15:49:04 -0800

    use default initial and min pool sizes for datasources, both are 0

--- src/main/java/edu/stanford/irt/laneweb/config/DataSourcesConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/DataSourcesConfiguration.java
@@ -18,9 +18,7 @@ public class DataSourcesConfiguration {
     public DataSource eresourcesDataSource(@Value("%{edu.stanford.irt.laneweb.db.eresources.url}") final String url,
             @Value("%{edu.stanford.irt.laneweb.db.eresources.user}") final String user,
             @Value("%{edu.stanford.irt.laneweb.db.eresources.password}") final String password,
-            @Value("%{edu.stanford.irt.laneweb.db.eresources.initialPoolSize}") final int initialPoolSize,
             @Value("%{edu.stanford.irt.laneweb.db.eresources.maxPoolSize}") final int maxPoolSize,
-            @Value("%{edu.stanford.irt.laneweb.db.eresources.minPoolSize}") final int minPoolSize,
             @Value("%{edu.stanford.irt.laneweb.db.eresources.onsConfiguration}") final String onsConfiguration,
             @Value("%{edu.stanford.irt.laneweb.db.eresources.fastConnectionFailoverEnabled}") final boolean failoverEnabled)
             throws SQLException {
@@ -29,9 +27,7 @@ public class DataSourcesConfiguration {
         dataSource.setURL(url);
         dataSource.setUser(user);
         dataSource.setPassword(password);
-        dataSource.setInitialPoolSize(initialPoolSize);
         dataSource.setMaxPoolSize(maxPoolSize);
-        dataSource.setMinPoolSize(minPoolSize);
         dataSource.setONSConfiguration(onsConfiguration);
         dataSource.setFastConnectionFailoverEnabled(failoverEnabled);
         return dataSource;
@@ -41,18 +37,14 @@ public class DataSourcesConfiguration {
     public DataSource grandroundsDataSource(@Value("%{edu.stanford.irt.laneweb.db.grandrounds.url}") final String url,
             @Value("%{edu.stanford.irt.laneweb.db.grandrounds.user}") final String user,
             @Value("%{edu.stanford.irt.laneweb.db.grandrounds.password}") final String password,
-            @Value("%{edu.stanford.irt.laneweb.db.grandrounds.initialPoolSize}") final int initialPoolSize,
-            @Value("%{edu.stanford.irt.laneweb.db.grandrounds.maxPoolSize}") final int maxPoolSize,
-            @Value("%{edu.stanford.irt.laneweb.db.grandrounds.minPoolSize}") final int minPoolSize)
+            @Value("%{edu.stanford.irt.laneweb.db.grandrounds.maxPoolSize}") final int maxPoolSize)
             throws SQLException {
         PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
         dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
         dataSource.setURL(url);
         dataSource.setUser(user);
         dataSource.setPassword(password);
-        dataSource.setInitialPoolSize(initialPoolSize);
         dataSource.setMaxPoolSize(maxPoolSize);
-        dataSource.setMinPoolSize(minPoolSize);
         return dataSource;
     }
 
@@ -60,17 +52,13 @@ public class DataSourcesConfiguration {
     public DataSource voyagerDataSource(@Value("%{edu.stanford.irt.laneweb.db.voyager.url}") final String url,
             @Value("%{edu.stanford.irt.laneweb.db.voyager.user}") final String user,
             @Value("%{edu.stanford.irt.laneweb.db.voyager.password}") final String password,
-            @Value("%{edu.stanford.irt.laneweb.db.voyager.initialPoolSize}") final int initialPoolSize,
-            @Value("%{edu.stanford.irt.laneweb.db.voyager.maxPoolSize}") final int maxPoolSize,
-            @Value("%{edu.stanford.irt.laneweb.db.voyager.minPoolSize}") final int minPoolSize) throws SQLException {
+            @Value("%{edu.stanford.irt.laneweb.db.voyager.maxPoolSize}") final int maxPoolSize) throws SQLException {
         PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
         dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
         dataSource.setURL(url);
         dataSource.setUser(user);
         dataSource.setPassword(password);
-        dataSource.setInitialPoolSize(initialPoolSize);
         dataSource.setMaxPoolSize(maxPoolSize);
-        dataSource.setMinPoolSize(minPoolSize);
         return dataSource;
     }
 }
