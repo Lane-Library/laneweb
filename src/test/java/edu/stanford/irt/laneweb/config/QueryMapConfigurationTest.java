450bd21a660f4ed56a0fb39ef972fc67017dcb10
Charles Yates <ceyates@stanford.edu>
2017-05-02 13:34:05 -0700

    use the SolrTemplate from eresources for querymap

--- src/test/java/edu/stanford/irt/laneweb/config/QueryMapConfigurationTest.java
+++ src/test/java/edu/stanford/irt/laneweb/config/QueryMapConfigurationTest.java
@@ -3,29 +3,24 @@ package edu.stanford.irt.laneweb.config;
 import static org.easymock.EasyMock.createMock;
 import static org.junit.Assert.assertNotNull;
 
-import org.apache.solr.client.solrj.SolrClient;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.data.solr.core.SolrTemplate;
 
 public class QueryMapConfigurationTest {
 
     private QueryMapConfiguration configuration;
 
-    private SolrClient solrClient;
+    private SolrTemplate solrClient;
 
     @Before
     public void setUp() {
-        this.solrClient = createMock(SolrClient.class);
+        this.solrClient = createMock(SolrTemplate.class);
         this.configuration = new QueryMapConfiguration(this.solrClient);
     }
 
     @Test
     public void testQueryMapper() {
         assertNotNull(this.configuration.queryMapper());
-    }
-
-    @Test
-    public void testQueryMapperSolrTemplate() {
-        assertNotNull(this.configuration.queryMapperSolrTemplate());
     }
 }
