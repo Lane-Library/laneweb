5df35d7493f115d31a7672d67891da56adab9e06
Charles Yates <ceyates@stanford.edu>
2017-05-17 12:48:04 -0700

    case 133525 Feedback-bug: Bookmark page has a bug

--- src/main/webapp/resources/javascript/lane-ie.js
+++ src/main/webapp/resources/javascript/lane-ie.js
@@ -6,14 +6,27 @@
     if (Y.UA.ie) {
 
         //toggle bookmarklet instructions for IE on favorites page
-        node = document.querySelector("#bookmarkletNotIE");
-        if (node) {
-            node.style.display = "none";
-        }
-        node = document.querySelector("#bookmarkletIE");
-        if (node) {
-            node.style.display = "block";
-        }
+        [].forEach.call(document.querySelectorAll('#bookmarkletNotIE, .bookmarklet-not-ie'), function(node) {
+            node.style.display = 'none';
+        });
+
+        [].forEach.call(document.querySelectorAll('#bookmarkletIE, .bookmarklet-ie'), function(node) {
+            node.style.display = 'block';
+        });
+
+    }
+
+    if (/Edge/.test(window.navigator.userAgent)) {
+
+        //toggle bookmarklet instructions for IE on favorites page
+        [].forEach.call(document.querySelectorAll('#bookmarkletNotIE, .bookmarklet-not-ie'), function(node) {
+            node.style.display = 'none';
+        });
+
+        [].forEach.call(document.querySelectorAll('.bookmarklet-edge'), function(node) {
+            node.style.display = 'block';
+        });
+
     }
 
 })();
