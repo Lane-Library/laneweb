11d93bbbd727695767d870b18eb37d08414c559b
Charles Yates <ceyates@stanford.edu>
2017-06-15 14:22:27 -0700

    remove authentication pieces, won't work at the moment

--- src/main/java/edu/stanford/irt/laneweb/config/ClassesServiceConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/ClassesServiceConfiguration.java
@@ -14,9 +14,8 @@ public class ClassesServiceConfiguration {
     public URI catalogServiceURI(@Value("${edu.stanford.irt.laneweb.classes-service.scheme}") final String scheme,
             @Value("${edu.stanford.irt.laneweb.classes-service.host}") final String host,
             @Value("${edu.stanford.irt.laneweb.classes-service.port}") final int port,
-            @Value("${edu.stanford.irt.laneweb.classes-service.path}") final String path,
-            @Value("${edu.stanford.irt.laneweb.classes-service.userInfo}") final String userInfo)
+            @Value("${edu.stanford.irt.laneweb.classes-service.path}") final String path)
             throws URISyntaxException {
-        return new URI(scheme, userInfo, host, port, path, null, null);
+        return new URI(scheme, null, host, port, path, null, null);
     }
 }
