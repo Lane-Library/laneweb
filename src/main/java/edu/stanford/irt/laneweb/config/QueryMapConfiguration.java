25ab56c88c1989a76e1d1861cbacc96e39bd0044
Charles Yates <ceyates@stanford.edu>
2017-06-30 19:04:45 -0700

    use querymap-2.0.0-SNAPSHOT, uses json from solr server

--- src/main/java/edu/stanford/irt/laneweb/config/QueryMapConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/QueryMapConfiguration.java
@@ -1,23 +1,33 @@
 package edu.stanford.irt.laneweb.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.solr.core.SolrTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.stanford.irt.querymap.DescriptorManager;
+import edu.stanford.irt.querymap.DescriptorWeightMap;
+import edu.stanford.irt.querymap.JsonSource;
 import edu.stanford.irt.querymap.QueryMapper;
-import edu.stanford.irt.querymap.SolrQueryToDescriptor;
+import edu.stanford.irt.querymap.QueryToDescriptor;
+import edu.stanford.irt.querymap.StreamResourceMapping;
 
 @Configuration
 public class QueryMapConfiguration {
 
+    private static final String ENDPOINT = "/lane-facet?q=%s&qt=%%2Flane-facet&facet=true&facet.mincount=1&facet.limit=10&facet.field=mesh&wt=json";
+
     @Bean
-    public QueryMapper queryMapper(@Qualifier("laneSearchSolrTemplate") final SolrTemplate solrTemplate) {
-        QueryMapper queryMapper = new QueryMapper();
+    public QueryMapper queryMapper(final ObjectMapper objectMapper,
+            @Value("${edu.stanford.irt.laneweb.solr-url-laneSearch}") final String solrServerUrl) {
         DescriptorManager descriptorManager = new DescriptorManager();
-        queryMapper.setDescriptorManager(descriptorManager);
-        queryMapper.setQueryToDescriptor(new SolrQueryToDescriptor(descriptorManager, solrTemplate));
+        QueryMapper queryMapper = new QueryMapper(
+                new StreamResourceMapping(QueryMapper.class.getResourceAsStream("resource-maps.xml")),
+                descriptorManager,
+                new QueryToDescriptor(descriptorManager,
+                        new DescriptorWeightMap(QueryMapper.class.getResourceAsStream("descriptor-weights.xml")),
+                        objectMapper, new JsonSource(solrServerUrl + ENDPOINT)));
         return queryMapper;
     }
 }
