e6561b7b89679d59548d0e174d6ba4389fb76764
Charles Yates <ceyates@stanford.edu>
2017-07-04 12:50:14 -0700

    add an ExceptionHandler to QueryMapController

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/QueryMapController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/QueryMapController.java
@@ -1,17 +1,27 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import edu.stanford.irt.laneweb.eresources.SolrQueryParser;
+import edu.stanford.irt.querymap.QueryMapException;
 import edu.stanford.irt.querymap.QueryMapper;
 import edu.stanford.irt.querymap.ResourceMap;
 
 @Controller
 public class QueryMapController {
+
+    private static final String ERROR_MESSAGE_FORMAT = "QueryMapper failed to get resource map: %s";
+
+    private static final Logger log = LoggerFactory.getLogger(QueryMapController.class);
 
     private SolrQueryParser parser;
 
@@ -27,5 +37,14 @@ public class QueryMapController {
     @ResponseBody
     public ResourceMap getJSONResourceMap(@RequestParam final String q) {
         return this.queryMapper.getResourceMap(this.parser.parse(q));
+    }
+
+    @ExceptionHandler(QueryMapException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(final QueryMapException exception) {
+        String message = String.format(ERROR_MESSAGE_FORMAT, exception);
+        log.error(message);
+        return message;
     }
 }
