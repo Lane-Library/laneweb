fc61b60412f4575107b739edbc88d01a8e612bca
Charles Yates <ceyates@stanford.edu>
2017-07-13 12:44:32 -0700

    return HTTP 204 no content for requests like *.php

--- src/main/java/edu/stanford/irt/laneweb/servlet/NoContentServlet.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/NoContentServlet.java
@@ -9,16 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * A servlet that sends an empty 404 response. To be used for .php requests, which will always be attempts to spam or
+ * A servlet that sends an empty 204 response. To be used for .php requests, which will always be attempts to spam or
  * vulnerability probes. And also used for /rss/browse and /rss/mesh requests that crawlers persist in requesting.
  */
-public class NotFoundServlet extends HttpServlet {
+public class NoContentServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException, ServletException {
-        response.sendError(HttpURLConnection.HTTP_NOT_FOUND);
+        response.sendError(HttpURLConnection.HTTP_NO_CONTENT);
     }
 }
