a6962767f3d31bbebc94693e938bf66a81eba846
Charles Yates <ceyates@stanford.edu>
2017-08-01 08:20:54 -0700

    proxy redis session related class to take care of problem resulting from latest javamelody

--- src/main/java/edu/stanford/irt/laneweb/config/SessionConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/SessionConfiguration.java
@@ -1,9 +1,14 @@
 package edu.stanford.irt.laneweb.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
 @Configuration
@@ -15,5 +20,14 @@ public class SessionConfiguration extends RedisHttpSessionConfiguration {
         JedisConnectionFactory factory = new JedisConnectionFactory();
         factory.setHostName(hostName);
         return factory;
+    }
+
+    @Override
+    @Bean
+    @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public RedisOperationsSessionRepository sessionRepository(
+            final RedisOperations<Object, Object> sessionRedisTemplate,
+            final ApplicationEventPublisher applicationEventPublisher) {
+        return super.sessionRepository(sessionRedisTemplate, applicationEventPublisher);
     }
 }
