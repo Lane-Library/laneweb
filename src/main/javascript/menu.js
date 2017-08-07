ea0de041a890dade5bf99c277617b4346fd5b951
Charles Yates <ceyates@stanford.edu>
2017-08-07 15:35:10 -0700

    case 134458 submenu for the left section nav

---
+++ src/main/javascript/menu.js
@@ -0,0 +1,19 @@
+(function() {
+    [].forEach.call(document.querySelectorAll('.menu ul'), function(submenu) {
+        if (submenu.querySelector('.menuitem-active')) {
+            submenu.className = 'submenu submenu-active';
+            submenu.parentNode.querySelector('a').innerHTML += ' <i class="fa fa-chevron-up"></i>';
+        } else {
+            submenu.className = 'submenu';
+            submenu.parentNode.querySelector('a').innerHTML += ' <i class="fa fa-chevron-down"></i>';
+        }
+        submenu.parentNode.addEventListener('click', function(event) {
+            if (event.target === this.querySelector('.fa')) {
+                event.preventDefault();
+                this.querySelector('ul').classList.toggle('submenu-active');
+                event.target.classList.toggle('fa-chevron-down');
+                event.target.classList.toggle('fa-chevron-up');
+            }
+        });
+    });
+})();
