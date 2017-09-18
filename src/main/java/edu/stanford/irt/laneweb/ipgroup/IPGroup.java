1af31e7f381f78b385dbab348a91f224798b471a
Ryan Max Steinberg <ryanmax@stanford.edu>
2017-09-18 15:51:07 -0700

    case 136060: update LPCH IP ranges: decommission STC data center

--- src/main/java/edu/stanford/irt/laneweb/ipgroup/IPGroup.java
+++ src/main/java/edu/stanford/irt/laneweb/ipgroup/IPGroup.java
@@ -38,7 +38,6 @@ public final class IPGroup implements Serializable {
         ROOT.addSubrange(new CIDRRange("199.68.152.135/32", LPCH));
         ROOT.addSubrange(new CIDRRange("199.68.152.137/32", LPCH));
         ROOT.addSubrange(new CIDRRange("199.68.152.139/32", LPCH));
-        ROOT.addSubrange(new CIDRRange("209.11.191.1/32", LPCH));
         ROOT.addSubrange(new CIDRRange("10.238.0.0/19", LPCH));
         ROOT.addSubrange(new CIDRRange("10.252.1.1/32", LPCH));
         ROOT.addSubrange(new CIDRRange("10.252.1.2/32", LPCH));
