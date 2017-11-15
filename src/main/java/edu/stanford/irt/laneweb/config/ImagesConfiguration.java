87e7c830080e10335079ffbc26888a81559a3104
alainb <alainb@stanford.edu>
2017-11-14 16:03:26 -0800

    clean-up

--- src/main/java/edu/stanford/irt/laneweb/config/ImagesConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/ImagesConfiguration.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
