0420837f50b16d12319a82773281e537449b48fb
Charles Yates <ceyates@stanford.edu>
2017-12-12 16:32:18 -0800

    merge AbstractGrandRoundsService into HTTPGrandRoundsService

--- src/main/java/edu/stanford/irt/laneweb/catalog/grandrounds/HTTPGrandRoundsService.java
+++ src/main/java/edu/stanford/irt/laneweb/catalog/grandrounds/HTTPGrandRoundsService.java
@@ -6,13 +6,27 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.stanford.irt.grandrounds.GrandRoundsException;
+import edu.stanford.irt.grandrounds.Link;
+import edu.stanford.irt.grandrounds.Presentation;
 import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.util.IOUtils;
+import edu.stanford.lane.catalog.CatalogSQLException;
+import edu.stanford.lane.catalog.Record;
+import edu.stanford.lane.catalog.RecordCollection;
 
-public class HTTPGrandRoundsService extends AbstractGrandRoundsService {
+public class HTTPGrandRoundsService implements GrandRoundsService {
 
     private static final String ENDPOINT_PATH_FORMAT = "grandrounds?department=%s&year=%s";
+
+    private static final Logger LOG = LoggerFactory.getLogger(GrandRoundsService.class);
 
     private static final String UTF8 = StandardCharsets.UTF_8.name();
 
@@ -23,6 +37,22 @@ public class HTTPGrandRoundsService extends AbstractGrandRoundsService {
     }
 
     @Override
+    public List<Presentation> getGrandRounds(final String department, final String year) {
+        List<Presentation> presentations = new ArrayList<>();
+        try (InputStream input = getInputStream(department, year)) {
+            RecordCollection collection = new RecordCollection(input);
+            for (Record record : collection) {
+                addPresentationIfValid(new Presentation(record), presentations);
+            }
+            return presentations.stream()
+                    .sorted((final Presentation p1, final Presentation p2)
+                            -> p2.getDate().compareTo(p1.getDate()))
+                    .collect(Collectors.toList());
+        } catch (CatalogSQLException | IOException e) {
+            throw new LanewebException(e);
+        }
+    }
+
     protected InputStream getInputStream(final String department, final String year) {
         try {
             String endpointPath = String.format(ENDPOINT_PATH_FORMAT, URLEncoder.encode(department, UTF8),
@@ -30,6 +60,17 @@ public class HTTPGrandRoundsService extends AbstractGrandRoundsService {
             return IOUtils.getStream(new URL(this.catalogServiceURI.toURL(), endpointPath));
         } catch (IOException e) {
             throw new LanewebException(e);
+        }
+    }
+
+    private void addPresentationIfValid(final Presentation presentation, final List<Presentation> presentations) {
+        int recordId = presentation.getId();
+        try {
+            presentation.getDate();
+            presentation.getLinks().stream().forEach(Link::getURI);
+            presentations.add(presentation);
+        } catch (GrandRoundsException e) {
+            LOG.error(recordId + " not valid", e);
         }
     }
 }
---
+++ src/test/java/edu/stanford/irt/laneweb/catalog/grandrounds/HTTPGrandRoundsServiceTest.java
@@ -0,0 +1,83 @@
+package edu.stanford.irt.laneweb.catalog.grandrounds;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.stanford.irt.laneweb.LanewebException;
+
+public class JDBCGrandRoundsServiceTest {
+
+    private CallableStatement callable;
+
+    private Clob clob;
+
+    private Connection connection;
+
+    private DataSource dataSource;
+
+    private GrandRoundsService manager;
+
+    @Before
+    public void setUp() throws IOException {
+        this.dataSource = createMock(DataSource.class);
+        this.manager = new JDBCGrandRoundsService(this.dataSource, "presentationsSQL");
+        this.connection = createMock(Connection.class);
+        this.callable = createMock(CallableStatement.class);
+        this.clob = createMock(Clob.class);
+    }
+
+    @Test
+    public void testGetGrandRounds() throws SQLException {
+        expect(this.dataSource.getConnection()).andReturn(this.connection);
+        expect(this.connection.prepareCall(isA(String.class))).andReturn(this.callable);
+        this.callable.setString(1, "MEDICINE");
+        this.callable.setString(2, "year");
+        this.callable.registerOutParameter(3, Types.CLOB);
+        expect(this.callable.execute()).andReturn(true);
+        expect(this.callable.getClob(3)).andReturn(this.clob);
+        expect(this.clob.getAsciiStream()).andReturn(getClass().getResourceAsStream("presentation.mrc"));
+        this.clob.free();
+        this.callable.close();
+        this.connection.close();
+        replay(this.callable, this.clob, this.connection, this.dataSource);
+        assertEquals(1, this.manager.getGrandRounds("medicine", "year").size());
+        verify(this.callable, this.clob, this.connection, this.dataSource);
+    }
+
+    @Test
+    public void testGetGrandRoundsThrowsException() throws SQLException {
+        expect(this.dataSource.getConnection()).andReturn(this.connection);
+        expect(this.connection.prepareCall(isA(String.class))).andReturn(this.callable);
+        this.callable.setString(1, "MEDICINE");
+        this.callable.setString(2, "year");
+        this.callable.registerOutParameter(3, Types.CLOB);
+        expect(this.callable.execute()).andReturn(true);
+        expect(this.callable.getClob(3)).andReturn(this.clob);
+        expect(this.clob.getAsciiStream()).andThrow(new SQLException());
+        this.clob.free();
+        this.callable.close();
+        this.connection.close();
+        replay(this.callable, this.clob, this.connection, this.dataSource);
+        try {
+            this.manager.getGrandRounds("medicine", "year");
+        } catch (LanewebException e) {
+        }
+        verify(this.callable, this.clob, this.connection, this.dataSource);
+    }
+}
