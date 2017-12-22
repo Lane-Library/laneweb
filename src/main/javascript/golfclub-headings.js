b2034c541a042ec6d213d0c3ea804918d264c8f8
Charles Yates <ceyates@stanford.edu>
2017-12-22 13:49:21 -0800

    remove debugging statement

--- src/main/javascript/golfclub-headings.js
+++ src/main/javascript/golfclub-headings.js
@@ -2,7 +2,6 @@
 
     "use strict";
 
-    console.log(typeof document.querySelectorAll(".golfclub"));
     document.querySelectorAll(".golfclub").forEach(function(node) {
         node.innerHTML = '<span><span class="golfclub-left"></span>' + node.innerHTML + '<span class="golfclub-right"></span></span>';
     });
