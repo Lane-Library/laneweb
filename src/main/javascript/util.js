5302f64f50e1b5589325abf197512275fed8e39d
Charles Yates <ceyates@stanford.edu>
2017-12-30 07:58:40 -0800

    cleanup

--- src/main/javascript/util.js
+++ src/main/javascript/util.js
@@ -27,7 +27,7 @@
             }
         }
         return result.nodeType === 1 ? result : null;
-    }
+    };
 
     /*
      * polyfill for NodeList.prototype.forEach() from
@@ -54,17 +54,17 @@
      * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
      */
     if (!Element.prototype.matches) {
-        Element.prototype.matches = 
-            Element.prototype.matchesSelector || 
+        Element.prototype.matches =
+            Element.prototype.matchesSelector ||
             Element.prototype.mozMatchesSelector ||
-            Element.prototype.msMatchesSelector || 
-            Element.prototype.oMatchesSelector || 
+            Element.prototype.msMatchesSelector ||
+            Element.prototype.oMatchesSelector ||
             Element.prototype.webkitMatchesSelector ||
             function(s) {
                 var matches = (this.document || this.ownerDocument).querySelectorAll(s),
                     i = matches.length;
                 while (--i >= 0 && matches.item(i) !== this) {}
-                return i > -1;            
+                return i > -1;
             };
     }
 })();
