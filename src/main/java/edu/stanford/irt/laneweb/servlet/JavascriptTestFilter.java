47399046c7c42100d1055b713b6e4b3850f6b4d0
Charles Yates <ceyates@stanford.edu>
2018-01-04 12:37:55 -0800

    make javascript tests visible via web application
    at /resources/tests/index.html

---
+++ src/main/java/edu/stanford/irt/laneweb/servlet/JavascriptTestFilter.java
@@ -0,0 +1,21 @@
+package edu.stanford.irt.laneweb.servlet;
+
+import java.util.regex.Pattern;
+
+import javax.servlet.FilterChain;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter({ "/main/*", })
+public class JavascriptTestFilter extends AbstractLanewebFilter {
+
+    private static final Pattern MAIN = Pattern.compile("(.*)main(.*)");
+
+    @Override
+    protected void internalDoFilter(final HttpServletRequest request, final HttpServletResponse response,
+            final FilterChain chain) {
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        response.addHeader("Location", MAIN.matcher(request.getRequestURI()).replaceAll("$1resources$2"));
+    }
+}
