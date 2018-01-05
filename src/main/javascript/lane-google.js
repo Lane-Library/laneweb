df40bcc253e78ed94d6caad2f54441ca600c08c2
Charles Yates <ceyates@stanford.edu>
2018-01-05 09:03:57 -0800

    refactor to reduce complexity

--- src/main/javascript/lane-google.js
+++ src/main/javascript/lane-google.js
@@ -2,57 +2,66 @@
 
     "use strict";
 
-    var gaPageTracker,
-        gaJsHost = (("https:" === location.protocol) ? "https://ssl." : "http://www.");
+    var gaJsHost = (("https:" === location.protocol) ? "https://ssl." : "http://www."),
+        noopTracker = {
+            _setDomainName: function() {},
+            _setVar: function() {},
+            _setCustomVar: function() {},
+            _trackPageview: function() {},
+            _trackEvent: function() {}
+        },
+        createPageTracker = function(gat) {
+            var host = location.host,
+                pageTracker;
+            if (!gat) {
+                pageTracker = noopTracker;
+            } else if (host.match("lane.stanford.edu")) {
+                pageTracker = gat._createTracker("UA-3202241-2","gaPageTracker");
+            } else if (host.match("lane-beta.stanford.edu")) {
+                pageTracker = gat._createTracker("UA-3203486-9","gaPageTracker");
+            } else {
+                pageTracker = gat._createTracker("UA-3203486-2","gaPageTracker");
+            }
+            return pageTracker;
+        },
+        gaPageTracker = noopTracker;
+
     Y.Get.script(gaJsHost + "google-analytics.com/ga.js", {
         onSuccess: function() {
-            var host = location.host,
-                model = L.Model,
+            var model = L.Model,
                 ipgroup = model.get(model.IPGROUP),
                 auth = model.get(model.AUTH);
-            if (window._gat !== undefined) {
-                if (host.match("lane.stanford.edu")) {
-                    gaPageTracker = window._gat._createTracker("UA-3202241-2","gaPageTracker");
-                } else if (host.match("lane-beta.stanford.edu")) {
-                    gaPageTracker = window._gat._createTracker("UA-3203486-9","gaPageTracker");
-                } else {
-                    gaPageTracker = window._gat._createTracker("UA-3203486-2","gaPageTracker");
-                }
-                if (gaPageTracker) {
-                    //you can call _setLocalServerMode on gaPageTracker for testing/debugging
-                    gaPageTracker._setDomainName(".stanford.edu");
-                    if (ipgroup) {
-                        gaPageTracker._setVar(ipgroup);
-                        gaPageTracker._setCustomVar(1,'ipGroup',ipgroup ,2);
-                    }
-                    if (auth) {
-                        gaPageTracker._setCustomVar(2,'authenticatedSession',auth,2);
-                        if (L.BookmarksWidget && L.BookmarksWidget.get("bookmarks").size() > 0) {
-                            gaPageTracker._setCustomVar(3,'bookmarkEnabledSession',auth,2);
-                        }
-                    }
-                    gaPageTracker._trackPageview();
+            gaPageTracker = createPageTracker(window._gat);
+            //you can call _setLocalServerMode on gaPageTracker for testing/debugging
+            gaPageTracker._setDomainName(".stanford.edu");
+            if (ipgroup) {
+                gaPageTracker._setVar(ipgroup);
+                gaPageTracker._setCustomVar(1,'ipGroup',ipgroup ,2);
+            }
+            if (auth) {
+                gaPageTracker._setCustomVar(2,'authenticatedSession',auth,2);
+                if (L.BookmarksWidget && L.BookmarksWidget.get("bookmarks").size() > 0) {
+                    gaPageTracker._setCustomVar(3,'bookmarkEnabledSession',auth,2);
                 }
             }
+            gaPageTracker._trackPageview();
         }
     });
+
     L.on("tracker:trackableEvent",  function(event) {
-        if (gaPageTracker !== undefined) {
-            gaPageTracker._trackEvent(event.category, event.action, event.label, event.value);
-        }
+        gaPageTracker._trackEvent(event.category, event.action, event.label, event.value);
     });
+
     L.on("tracker:trackablePageview",  function(event) {
-        if (gaPageTracker !== undefined) {
-            if (event.external) {
-                if(event.query !== undefined && event.query !== '' ){
-                    gaPageTracker._trackEvent('lane:offsite', "/OFFSITE-CLICK-EVENT/"+encodeURIComponent(event.title) ,event.host+event.path+event.query);
-                }else{
-                    gaPageTracker._trackEvent('lane:offsite', "/OFFSITE-CLICK-EVENT/"+encodeURIComponent(event.title) ,event.host+event.path);
-                }
-                gaPageTracker._trackPageview('/OFFSITE/' + encodeURIComponent(event.title));
-            } else {
-                gaPageTracker._trackPageview('/ONSITE/' + encodeURIComponent(event.title) + '/' + event.path);
+        if (event.external) {
+            if(event.query !== undefined && event.query !== '' ){
+                gaPageTracker._trackEvent('lane:offsite', "/OFFSITE-CLICK-EVENT/"+encodeURIComponent(event.title) ,event.host+event.path+event.query);
+            }else{
+                gaPageTracker._trackEvent('lane:offsite', "/OFFSITE-CLICK-EVENT/"+encodeURIComponent(event.title) ,event.host+event.path);
             }
+            gaPageTracker._trackPageview('/OFFSITE/' + encodeURIComponent(event.title));
+        } else {
+            gaPageTracker._trackPageview('/ONSITE/' + encodeURIComponent(event.title) + '/' + event.path);
         }
     });
 })();
