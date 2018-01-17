dd79232bf5478707d50c4cb430c95bb723e5c69e
Charles Yates <ceyates@stanford.edu>
2018-01-16 20:53:08 -0800

    don't wait for DOMContentLoaded, may have already happened

--- src/main/javascript/search-form-scroll.js
+++ src/main/javascript/search-form-scroll.js
@@ -4,26 +4,24 @@
 
     "use strict";
 
-    if (/\/search\.html/.test(location.pathname)) {
-        window.addEventListener("DOMContentLoaded", function() {
-            var i = 0,
-                offset = document.forms[0].offsetTop + 50,
-                // Edge has scrollY initially set to 0 regardless of previous position
-                // this polls value every 100ms for 1s
-                edgeDelay = function() {
-                    if (window.scrollY === 0 && i < 10) {
-                        i++;
-                        window.setTimeout(edgeDelay, 100);
-                    } else if (window.scrollY < offset) {
-                        window.scroll(0, offset);
-                    }
-                }
-            if (/Edge/.test(window.navigator.userAgent)) {
-                edgeDelay();
-            } else if (window.scrollY < offset) {
+    if (/.*\/search\.html/.test(location.pathname)) {
+        var i = 0,
+            offset = document.forms[0].offsetTop + 50,
+        // Edge has scrollY initially set to 0 regardless of previous position
+        // this polls value every 100ms for 1s
+        edgeDelay = function() {
+            if (window.pageYOffset === 0 && i < 10) {
+                i++;
+                window.setTimeout(edgeDelay, 100);
+            } else if (window.pageYOffset < offset) {
                 window.scroll(0, offset);
             }
-        });
+        }
+        if (/Edge/.test(window.navigator.userAgent)) {
+            edgeDelay();
+        } else if (window.pageYOffset < offset) {
+            window.scroll(0, offset);
+        }
     }
 
 })();
