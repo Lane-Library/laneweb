8b834890067586ea005ba44b231cd88c5da534b7
Charles Yates <ceyates@stanford.edu>
2018-01-31 14:20:02 -0800

    preparations for merging gce on on-premise code bases

--- src/main/java/edu/stanford/irt/laneweb/config/BookmarksConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/BookmarksConfiguration.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
