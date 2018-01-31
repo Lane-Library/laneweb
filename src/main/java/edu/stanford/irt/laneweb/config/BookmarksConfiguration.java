5c2a06b66d959c3a5c25dbdbdf1cc97d5f938302
Charles Yates <ceyates@stanford.edu>
2018-01-31 12:19:33 -0800

    add dummyBookmarkService

--- src/main/java/edu/stanford/irt/laneweb/config/BookmarksConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/BookmarksConfiguration.java
@@ -1,15 +1,19 @@
 package edu.stanford.irt.laneweb.config;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.oxm.Marshaller;
 
 import edu.stanford.irt.cocoon.pipeline.Generator;
 import edu.stanford.irt.cocoon.sitemap.select.Selector;
+import edu.stanford.irt.laneweb.bookmarks.Bookmark;
 import edu.stanford.irt.laneweb.bookmarks.BookmarkGenerator;
 import edu.stanford.irt.laneweb.bookmarks.BookmarkService;
 import edu.stanford.irt.laneweb.bookmarks.JDBCBookmarkService;
@@ -37,7 +41,30 @@ public class BookmarksConfiguration {
     }
 
     @Bean(name = "edu.stanford.irt.laneweb.bookmarks.BookmarkService")
+    @Profile("!gce")
     public BookmarkService bookmarkService(@Qualifier("javax.sql.DataSource/bookmarks") final DataSource dataSource) {
         return new StanfordDomainStrippingBookmarkService(new JDBCBookmarkService(dataSource));
+    }
+
+    @Bean(name = "edu.stanford.irt.laneweb.bookmarks.BookmarkService")
+    @Profile("gce")
+    public BookmarkService dummyBookmarkService() {
+        return new BookmarkService() {
+
+            @Override
+            public List<Bookmark> getLinks(final String userid) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int getRowCount() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void saveLinks(final String userid, final List<Bookmark> links) {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 }
