be22020ab465e94ad3c1fe6bd02aba84ae65667d
Charles Yates <ceyates@stanford.edu>
2018-02-01 06:08:40 -0800

    add tests

--- src/test/java/edu/stanford/irt/laneweb/config/BookmarksConfigurationTest.java
+++ src/test/java/edu/stanford/irt/laneweb/config/BookmarksConfigurationTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.stanford.irt.laneweb.bookmarks.BookmarkService;
+
 public class BookmarksConfigurationTest {
 
     private BookmarksConfiguration configuration;
@@ -32,5 +34,25 @@ public class BookmarksConfigurationTest {
     @Test
     public void testBookmarkService() {
         assertNotNull(this.configuration.bookmarkService(null));
+    }
+
+    @Test
+    public void testDummyBookmarkService() {
+        BookmarkService service = this.configuration.dummyBookmarkService();
+        try {
+            service.getLinks(null);
+        } catch (UnsupportedOperationException e) {
+            assertNotNull(e);
+        }
+        try {
+            service.getRowCount();
+        } catch (UnsupportedOperationException e) {
+            assertNotNull(e);
+        }
+        try {
+            service.saveLinks(null, null);
+        } catch (UnsupportedOperationException e) {
+            assertNotNull(e);
+        }
     }
 }
---
+++ src/test/java/edu/stanford/irt/laneweb/config/NotGCESessionConfigurationTest.java
@@ -0,0 +1,13 @@
+package edu.stanford.irt.laneweb.config;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class NotGCESessionConfigurationTest {
+
+    @Test
+    public void testSpringSessionRepositoryFilter() {
+        assertNotNull(new NotGCESessionConfiguration().springSessionRepositoryFilter());
+    }
+}
