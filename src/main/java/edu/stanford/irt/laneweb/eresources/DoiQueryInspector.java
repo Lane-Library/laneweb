068d22d7e712de1a354cb812c5fcb68d0d9aa326
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-03-21 10:17:36 -0700

    case 139640: poor results for some DOI searches
    add quotes to search them as phrases

--- src/main/java/edu/stanford/irt/laneweb/eresources/DoiQueryInspector.java
+++ src/main/java/edu/stanford/irt/laneweb/eresources/DoiQueryInspector.java
@@ -9,10 +9,12 @@ import java.util.regex.Pattern;
  */
 public final class DoiQueryInspector implements QueryInspector {
 
-    private static final Pattern DOI_PATTERN = Pattern.compile("\\b10\\.\\d+\\b");
+    private static final Pattern DOI_PATTERN = Pattern.compile("(\\b10\\.[^ ]+\\b)");
 
     private static final Pattern PREFIX_PATTERN = Pattern
             .compile("(?:https?:\\/\\/)?(?:dx\\.)?doi\\.org/(10\\.\\w+)\\b", Pattern.CASE_INSENSITIVE);
+
+    private static final String QUOTE = "\"";
 
     @Override
     public boolean combinable() {
@@ -22,8 +24,10 @@ public final class DoiQueryInspector implements QueryInspector {
     @Override
     public String inspect(final String query) {
         String parsed = PREFIX_PATTERN.matcher(query).replaceAll("$1");
-        // remove slashes in DOIs because they give edismax parser trouble
         if (DOI_PATTERN.matcher(parsed).find()) {
+            // add quotes to DOIs to improve precision
+            parsed = DOI_PATTERN.matcher(parsed).replaceAll(QUOTE + "$1" + QUOTE);
+            // remove slashes in DOIs because they give edismax parser trouble
             parsed = parsed.replace('/', ' ');
         }
         return parsed;
--- src/test/java/edu/stanford/irt/laneweb/eresources/SolrQueryParserTest.java
+++ src/test/java/edu/stanford/irt/laneweb/eresources/SolrQueryParserTest.java
@@ -53,18 +53,19 @@ public class SolrQueryParserTest {
         assertEquals("recordId:12345", this.parser.parse("12345"));
         assertEquals("12345 54321", this.parser.parse("12345 54321"));
         assertEquals("12345\\-54321", this.parser.parse("12345-54321"));
-        assertEquals("10.1016 j.it.2015.02.003", this.parser.parse("http://dx.doi.org/10.1016/j.it.2015.02.003"));
-        assertEquals("10.1016 j.it.2015.02.003", this.parser.parse("dx.doi.org/10.1016/j.it.2015.02.003"));
-        assertEquals("10.1016 j.it.2015.02.003", this.parser.parse("doi.org/10.1016/j.it.2015.02.003"));
-        assertEquals("BMJ 2015; 351 doi\\: 10.1136 bmj.h5942",
+        assertEquals("\"10.1016 j.it.2015.02.003\"", this.parser.parse("10.1016/j.it.2015.02.003"));
+        assertEquals("\"10.1016 j.it.2015.02.003\"", this.parser.parse("http://dx.doi.org/10.1016/j.it.2015.02.003"));
+        assertEquals("\"10.1016 j.it.2015.02.003\"", this.parser.parse("dx.doi.org/10.1016/j.it.2015.02.003"));
+        assertEquals("\"10.1016 j.it.2015.02.003\"", this.parser.parse("doi.org/10.1016/j.it.2015.02.003"));
+        assertEquals("BMJ 2015; 351 doi\\: \"10.1136 bmj.h5942\"",
                 this.parser.parse("BMJ 2015; 351 doi: http://dx.doi.org/10.1136/bmj.h5942"));
-        assertEquals("10.1016 j.it.2015.02.003 10.1136 bmj.h5942",
+        assertEquals("\"10.1016 j.it.2015.02.003\" \"10.1136 bmj.h5942\"",
                 this.parser.parse("doi.org/10.1016/j.it.2015.02.003 http://dx.doi.org/10.1136/bmj.h5942"));
         assertEquals("(id:12345 OR id:123456) OR (id:12345 OR id:123456)", this.parser.parse("id:12345 OR id:123456"));
         assertEquals("(recordId:12345 OR pmid\\:123456) OR (recordId:12345 OR pmid\\:123456)",
                 this.parser.parse("recordId:12345 OR pmid:123456"));
         assertEquals(
-                "Journal of Antimicrobial Chemotherapy, Volume 72, Issue 4, 1 April 2017, 1147–1151, 10.1093 jac dkw537",
+                "Journal of Antimicrobial Chemotherapy, Volume 72, Issue 4, 1 April 2017, 1147–1151, \"10.1093 jac dkw537\"",
                 this.parser.parse(
                         "Journal of Antimicrobial Chemotherapy, Volume 72, Issue 4, 1 April 2017, 1147–1151, https://doi.org/10.1093/jac/dkw537"));
     }
