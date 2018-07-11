e0a588cd67605868895a5a33575e06b4f7f733cb
Charles Yates <ceyates@stanford.edu>
2018-07-11 12:39:58 -0700

    case 143703 use Spring RestTemplate where possible
    remove HTTPEquipmentService in favor of RESTEquipmentService

--- src/main/java/edu/stanford/irt/laneweb/catalog/equipment/RESTEquipmentService.java
+++ src/main/java/edu/stanford/irt/laneweb/catalog/equipment/RESTEquipmentService.java
@@ -4,34 +4,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.List;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import edu.stanford.irt.laneweb.LanewebException;
+import edu.stanford.irt.laneweb.rest.RESTService;
+import edu.stanford.irt.laneweb.rest.TypeReference;
 import edu.stanford.irt.laneweb.util.ServiceURIResolver;
 
-public class HTTPEquipmentService implements EquipmentService {
+
+public class RESTEquipmentService implements EquipmentService {
+
+    private static final TypeReference<List<Map<String, String>>> TYPE = new TypeReference<List<Map<String, String>>>() {
+    };
 
     private static final String RECORDS_ENDPOINT_PATH = "equipment/records";
 
     private static final String STATUS_ENDPOINT_PATH_FORMAT = "equipment/status?idList=%s";
 
     private URI catalogServiceURI;
-
-    private ObjectMapper objectMapper;
+    private RESTService restService;
 
     private ServiceURIResolver uriResolver;
 
-    public HTTPEquipmentService(final ObjectMapper objectMapper, final URI catalogServiceURI,
-            final ServiceURIResolver uriResolver) {
-        this.objectMapper = objectMapper;
+    public RESTEquipmentService(URI catalogServiceURI, RESTService restService, ServiceURIResolver uriResolver) {
         this.catalogServiceURI = catalogServiceURI;
+        this.restService = restService;
         this.uriResolver = uriResolver;
     }
 
     @Override
-    public InputStream getRecords(final List<String> params) {
+    public InputStream getRecords(List<String> params) {
         try {
             return this.uriResolver.getInputStream(this.catalogServiceURI.resolve(RECORDS_ENDPOINT_PATH));
         } catch (IOException e) {
@@ -40,13 +43,11 @@ public class HTTPEquipmentService implements EquipmentService {
     }
 
     @Override
-    public List<EquipmentStatus> getStatus(final String idList) {
+    public List<EquipmentStatus> getStatus(String idList) {
         String pathWithIDListParam = String.format(STATUS_ENDPOINT_PATH_FORMAT, idList);
-        try (InputStream input = this.uriResolver.getInputStream(this.catalogServiceURI.resolve(pathWithIDListParam))) {
-            return this.objectMapper.readValue(input, new TypeReference<List<EquipmentStatus>>() {
-            });
-        } catch (IOException e) {
-            throw new LanewebException(e);
-        }
+        List<Map<String, String>> list = this.restService.getObject(this.catalogServiceURI.resolve(pathWithIDListParam), TYPE);
+        return list.stream()
+                .map(m -> new EquipmentStatus(m.get("bibID"), m.get("count")))
+                .collect(Collectors.toList());
     }
 }
