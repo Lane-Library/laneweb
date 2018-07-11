4ab8cdb1e7270c08828af06402f2c04acb58c3c4
Charles Yates <ceyates@stanford.edu>
2018-07-11 16:49:04 -0700

    fix broken /eresources/ezproxy-servers.txt

--- src/main/java/edu/stanford/irt/laneweb/proxy/RESTProxyServersService.java
+++ src/main/java/edu/stanford/irt/laneweb/proxy/RESTProxyServersService.java
@@ -1,15 +1,21 @@
 package edu.stanford.irt.laneweb.proxy;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.util.Set;
 
+import edu.stanford.irt.laneweb.LanewebException;
 import edu.stanford.irt.laneweb.rest.RESTService;
 
 public class RESTProxyServersService implements ProxyServersService {
 
+    private static final int BYTE_BUFFER_SIZE = 4096;
+
     private static final String HOSTS_ENDPOINT = "proxy/hosts";
+
+    private static final String WRITE_ENDPOINT = "proxy/write";
 
     private URI catalogServiceURI;
 
@@ -28,6 +34,18 @@ public class RESTProxyServersService implements ProxyServersService {
 
     @Override
     public void write(final OutputStream outputStream) throws IOException {
-        throw new UnsupportedOperationException();
+        try (InputStream input = this.restservice.getInputStream(this.catalogServiceURI.resolve(WRITE_ENDPOINT))) {
+            byte[] buffer = new byte[BYTE_BUFFER_SIZE];
+            int i = 0;
+            while (true) {
+                i = input.read(buffer);
+                if (i == -1) {
+                    break;
+                }
+                outputStream.write(buffer, 0, i);
+            }
+        } catch (IOException e) {
+            throw new LanewebException(e);
+        }
     }
 }
--- src/test/java/edu/stanford/irt/laneweb/proxy/RESTProxyServersServiceTest.java
+++ src/test/java/edu/stanford/irt/laneweb/proxy/RESTProxyServersServiceTest.java
@@ -4,9 +4,13 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.mock;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertSame;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
@@ -20,6 +24,8 @@ import edu.stanford.irt.laneweb.rest.RESTService;
 
 public class RESTProxyServersServiceTest {
 
+    private InputStream inputStream;
+
     private RESTService restService;
 
     private RESTProxyServersService service;
@@ -31,6 +37,7 @@ public class RESTProxyServersServiceTest {
         this.uri = new URI("/");
         this.restService = mock(RESTService.class);
         this.service = new RESTProxyServersService(this.uri, this.restService);
+        this.inputStream = new ByteArrayInputStream(new byte[0]);
     }
 
     @Test
@@ -41,8 +48,13 @@ public class RESTProxyServersServiceTest {
         verify(this.restService);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void testWrite() throws IOException {
-        this.service.write(null);
+    @Test
+    public void testWrite() throws IOException, URISyntaxException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        expect(this.restService.getInputStream(new URI("/proxy/write"))).andReturn(this.inputStream);
+        replay(this.restService);
+        this.service.write(baos);
+        assertArrayEquals(new byte[0], baos.toByteArray());
+        verify(this.restService);
     }
 }
