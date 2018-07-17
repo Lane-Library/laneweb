139699003176c447a97c315bf6e3b64ae2942891
Charles Yates <ceyates@stanford.edu>
2018-07-16 19:52:28 -0700

    disable spring boot /error mapping

--- src/main/java/edu/stanford/irt/laneweb/Laneweb.java
+++ src/main/java/edu/stanford/irt/laneweb/Laneweb.java
@@ -4,22 +4,23 @@ import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
 @ServletComponentScan
 public class Laneweb {
 
     public static void main(final String[] args) {
         SpringApplication.run(Laneweb.class, args);
     }
-    
+
     @Bean
     public ConfigurableServletWebServerFactory webServerFactory(
-            @Value("${edu.stanford.irt.laneweb.ajpPort:8009}") int ajpPort) {
+            @Value("${edu.stanford.irt.laneweb.ajpPort:8009}") final int ajpPort) {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         Connector ajpConnector = new Connector("AJP/1.3");
         ajpConnector.setPort(ajpPort);
