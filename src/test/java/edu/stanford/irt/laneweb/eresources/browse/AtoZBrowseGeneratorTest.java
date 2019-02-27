8b7ad9837743e5579b43435427aa8934e935f093
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-02-27 12:35:00 -0800

    more test coverage

--- src/test/java/edu/stanford/irt/laneweb/eresources/browse/AtoZBrowseGeneratorTest.java
+++ src/test/java/edu/stanford/irt/laneweb/eresources/browse/AtoZBrowseGeneratorTest.java
@@ -51,16 +51,27 @@ public class AtoZBrowseGeneratorTest {
         FacetPage<Eresource> facetpage = mock(FacetPage.class);
         Collection<Page<FacetFieldEntry>> facetResultPages = mock(Collection.class);
         Iterator<Page<FacetFieldEntry>> it1 = mock(Iterator.class);
+        Iterator<FacetFieldEntry> it2 = mock(Iterator.class);
+        Page<FacetFieldEntry> page1 = mock(Page.class);
+        FacetFieldEntry ffe = mock(FacetFieldEntry.class);
         this.generator.setParameters(Collections.singletonMap(Model.TYPE, "foo"));
         this.saxStrategy.toSAX(isA(List.class), eq(this.xmlConsumer));
         expect(this.solrService.facetByField("advanced:true recordType:bib AND (isRecent:1 OR isLaneConnex:1)",
                 "type:foo", "title_starts", 0, 200, 0, FacetSort.INDEX)).andReturn(facetpage);
         expect(facetpage.getFacetResultPages()).andReturn(facetResultPages);
         expect(facetResultPages.iterator()).andReturn(it1);
+        expect(it1.hasNext()).andReturn(true);
+        expect(it1.next()).andReturn(page1);
+        expect(page1.iterator()).andReturn(it2);
+        expect(it2.hasNext()).andReturn(true);
+        expect(it2.next()).andReturn(ffe);
+        expect(ffe.getValue()).andReturn("ertlswa");
+        expect(ffe.getValueCount()).andReturn(new Long(4));
+        expect(it2.hasNext()).andReturn(false);
         expect(it1.hasNext()).andReturn(false);
-        replay(this.solrService, this.saxStrategy, this.xmlConsumer, facetpage, facetResultPages, it1);
+        replay(this.solrService, this.saxStrategy, this.xmlConsumer, facetpage, facetResultPages, it1, it2, page1, ffe);
         this.generator.doGenerate(this.xmlConsumer);
-        verify(this.solrService, this.saxStrategy, this.xmlConsumer, facetpage, facetResultPages, it1);
+        verify(this.solrService, this.saxStrategy, this.xmlConsumer, facetpage, facetResultPages, it1, it2, page1, ffe);
     }
 
     @Test
