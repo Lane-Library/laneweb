dfae39358568caa50b104812735766e9068f64fb
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-02-28 22:17:35 -0800

    fix unit test

--- src/test/java/edu/stanford/irt/laneweb/eresources/browse/BrowseLetterTest.java
+++ src/test/java/edu/stanford/irt/laneweb/eresources/browse/BrowseLetterTest.java
@@ -8,19 +8,19 @@ public class BrowseLetterTest {
 
     @Test
     public final void testGetCount() {
-        BrowseLetter letter = new BrowseLetter("requestUri", "a", 10);
+        BrowseLetter letter = new BrowseLetter("base", "a", 10);
         assertEquals(10, letter.getCount());
     }
 
     @Test
     public final void testGetLetter() {
-        BrowseLetter letter = new BrowseLetter("requestUri", "a", 10);
+        BrowseLetter letter = new BrowseLetter("base", "a", 10);
         assertEquals("a", letter.getLetter());
     }
 
     @Test
     public final void testGetUrl() {
-        assertEquals("requestUri?a=a", (new BrowseLetter("requestUri", "a", 10)).getUrl());
-        assertEquals("requestUri?a=%23", (new BrowseLetter("requestUri", "1", 10)).getUrl());
+        assertEquals("base.html?a=a", (new BrowseLetter("base", "a", 10)).getUrl());
+        assertEquals("base.html?a=%23", (new BrowseLetter("base", "1", 10)).getUrl());
     }
 }
