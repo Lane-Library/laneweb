601495db02b090e6820745002261b5f7c9b3bb2d
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-03-26 17:48:33 -0700

    more testing for Case 150284:
    Prompted to login to access Lane resources from Stanford Health Library

--- src/main/java/edu/stanford/irt/laneweb/ipgroup/IPGroup.java
+++ src/main/java/edu/stanford/irt/laneweb/ipgroup/IPGroup.java
@@ -32,7 +32,8 @@ public final class IPGroup implements Serializable {
     private static final long serialVersionUID = 1L;
 
     static {
-        ROOT.addSubrange(new CIDRRange("207.254.56.75/32", SHC));
+        ROOT.addSubrange(new CIDRRange("127.0.0.1/32", SHC));
+        ROOT.addSubrange(new CIDRRange("13.56.32.141/32", SHC));
         ROOT.addSubrange(new CIDRRange("10.252.0.0/14", SHC));
         ROOT.addSubrange(new CIDRRange("10.0.0.0/8", SU));
         ROOT.addSubrange(new CIDRRange("45.42.34.136/32", LPCH));
