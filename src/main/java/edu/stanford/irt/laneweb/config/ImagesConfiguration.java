97d0819c2bd9e3cfa4d10aec10646572954e6e9f
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-08-01 12:17:32 -0700

    code cleanup

--- src/main/java/edu/stanford/irt/laneweb/config/ImagesConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/ImagesConfiguration.java
@@ -49,7 +49,7 @@ public class ImagesConfiguration {
     public Generator bassettGenerator() {
         return new BassettImageGenerator(this.solrImageService, pageSAXStrategy());
     }
-    
+
     @Bean
     public SAXStrategy<FacetPage<BassettImage>> countSAXStrategy() {
         return new BassettCountSAXStrategy();
@@ -77,13 +77,6 @@ public class ImagesConfiguration {
         return new SolrImageSearchGenerator(this.solrImageService, solrImageSearchSAXStrategy());
     }
 
-    @Bean(name = "edu.stanford.irt.cocoon.pipeline.Generator/search-image-preview")
-    @Scope("prototype")
-    public Generator sorlImageSearchPreviewGenerator(final Marshaller marshaller) {
-        return new SorlImageSearchPreviewGenerator( marshaller, this.solrImageService);
-    }
-
-    
     @Bean
     public SAXStrategy<SolrImageSearchResult> solrImageSearchSAXStrategy() {
         return new SolrImageSearchSAXStrategy(websiteIdMapping());
@@ -93,6 +86,12 @@ public class ImagesConfiguration {
     @Scope("prototype")
     public Generator solrImageSearchTabGenerator(final Marshaller marshaller) {
         return new SolrImageSearchTabGenerator(this.solrImageService, marshaller);
+    }
+
+    @Bean(name = "edu.stanford.irt.cocoon.pipeline.Generator/search-image-preview")
+    @Scope("prototype")
+    public Generator sorlImageSearchPreviewGenerator(final Marshaller marshaller) {
+        return new SorlImageSearchPreviewGenerator(marshaller, this.solrImageService);
     }
 
     @Bean
