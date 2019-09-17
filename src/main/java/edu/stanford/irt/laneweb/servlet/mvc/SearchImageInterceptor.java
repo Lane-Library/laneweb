5ffc09ec63a333ca2b94a705e6768abd3f7af376
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-09-17 16:57:19 -0700

    code cleanup and sonar: Remove this unused method parameter "sourceOri"

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/SearchImageInterceptor.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/SearchImageInterceptor.java
@@ -25,58 +25,56 @@ import edu.stanford.irt.solr.service.SolrImageService;
 
 public class SearchImageInterceptor extends HandlerInterceptorAdapter {
 
-	private static final String ALL_IMAGES = "images-all";
+    private static final String ALL_IMAGES = "images-all";
 
-	private static final String CC_IMAGES = "cc-images-all";
+    private static final String CC_IMAGES = "cc-images-all";
 
-	private static final String PMC_IMAGES = "pmc-images-all";
+    private static final String PMC_IMAGES = "pmc-images-all";
 
-	private static final String RL_IMAGES = "rl-images-all";
+    private static final String RL_IMAGES = "rl-images-all";
 
-	private Map<String, String> copyrightMapping;
+    private Map<String, String> copyrightMapping;
 
-	private SolrImageService service;
+    private SolrImageService service;
 
-	public SearchImageInterceptor(final SolrImageService service) {
-		this.service = service;
-		this.copyrightMapping = new HashMap<>();
-		this.copyrightMapping.put("0", ALL_IMAGES);
-		this.copyrightMapping.put("10", CC_IMAGES);
-		this.copyrightMapping.put("15", PMC_IMAGES);
-		this.copyrightMapping.put("20", RL_IMAGES);
-	}
+    public SearchImageInterceptor(final SolrImageService service) {
+        this.service = service;
+        this.copyrightMapping = new HashMap<>();
+        this.copyrightMapping.put("0", ALL_IMAGES);
+        this.copyrightMapping.put("10", CC_IMAGES);
+        this.copyrightMapping.put("15", PMC_IMAGES);
+        this.copyrightMapping.put("20", RL_IMAGES);
+    }
 
-	@Override
-	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
-			throws IOException {
-		String sourceOri = request.getParameter("source");
-		String auto = request.getParameter("auto");
-		if (sourceOri != null && sourceOri.indexOf(ALL_IMAGES) > -1 && !"no".equals(auto)) {
-			String query = request.getParameter("q");
-			String copyrightName = this.getTabValuesFromSolr(query, sourceOri);
-			String source = copyrightMapping.get(copyrightName);
-			if (null != copyrightName && null != source && !source.equals(sourceOri)) {
-				String url = request.getRequestURL() + "?" + request.getQueryString();
-				response.sendRedirect(url.replace(sourceOri, source).concat("&auto=no"));
-				return false;
-			}
-		}
-		return true;
-	}
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
+            throws IOException {
+        String sourceOri = request.getParameter("source");
+        String auto = request.getParameter("auto");
+        if (sourceOri != null && sourceOri.indexOf(ALL_IMAGES) > -1 && !"no".equals(auto)) {
+            String query = request.getParameter("q");
+            String copyrightName = this.getTabValuesFromSolr(query);
+            String source = this.copyrightMapping.get(copyrightName);
+            if (null != copyrightName && null != source && !source.equals(sourceOri)) {
+                String url = request.getRequestURL() + "?" + request.getQueryString();
+                response.sendRedirect(url.replace(sourceOri, source).concat("&auto=no"));
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private String getTabValuesFromSolr(final String queryTerm, String sourceOri) {
-		FacetPage<Image> facetPage = this.service.facetOnCopyright(queryTerm);
-		Page<FacetFieldEntry> page = facetPage.getFacetResultPage("copyright");
-		List<FacetFieldEntry> facetList = new ArrayList<>( page.getContent());
-		Collections.sort(facetList, new FacetFieldComparator() );
-		for (FacetFieldEntry facet : facetList) {
-			long copyrightValue = facet.getValueCount();
-			if (copyrightValue > 0) {
-				return facet.getValue();
-			}
-		}
-		return null;
-	}
-
-
+    private String getTabValuesFromSolr(final String queryTerm) {
+        FacetPage<Image> facetPage = this.service.facetOnCopyright(queryTerm);
+        Page<FacetFieldEntry> page = facetPage.getFacetResultPage("copyright");
+        List<FacetFieldEntry> facetList = new ArrayList<>(page.getContent());
+        Collections.sort(facetList, new FacetFieldComparator());
+        for (FacetFieldEntry facet : facetList) {
+            long copyrightValue = facet.getValueCount();
+            if (copyrightValue > 0) {
+                return facet.getValue();
+            }
+        }
+        return null;
+    }
 }
