37728f9c9d9651f5ff2e61e1956d70a5af0aae29
Alain <aboussard@stanfordhealthcare.org>
2020-06-02 11:23:12 -0700

    for force the the response to https if the request was done in https https://medjira.stanford.edu/projects/LANEWEB/issues/LANEWEB-10573?

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/RedirectHandlerInterceptor.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/RedirectHandlerInterceptor.java
@@ -1,5 +1,6 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
+
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,8 @@ public class RedirectHandlerInterceptor extends HandlerInterceptorAdapter {
 
     private RedirectProcessor redirectProcessor;
 
+    private static final String HTTPS = "https";
+
     public RedirectHandlerInterceptor(final RedirectProcessor redirectProcessor) {
         this.redirectProcessor = redirectProcessor;
     }
@@ -25,6 +28,13 @@ public class RedirectHandlerInterceptor extends HandlerInterceptorAdapter {
         String uri = requestURI.substring(basePath.length());
         String redirectURL = this.redirectProcessor.getRedirectURL(uri, basePath, request.getQueryString());
         if (redirectURL != null) {
+            // LANEWEB-10573
+            if (request.getHeader("gohttps") != null || HTTPS.equals(request.getHeader("x-forwarded-proto"))) {
+                StringBuilder httpsRedirectURL = new StringBuilder(HTTPS + "://");
+                httpsRedirectURL.append(request.getLocalName());
+                httpsRedirectURL.append(redirectURL);
+                redirectURL = httpsRedirectURL.toString();
+            }
             response.sendRedirect(redirectURL);
             return false;
         }
