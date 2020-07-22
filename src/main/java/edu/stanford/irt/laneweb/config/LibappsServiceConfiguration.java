0ac60d1be9072264161368396cebc2913afbec10
Alain <aboussard@stanfordhealthcare.org>
2020-07-21 17:11:27 -0700

    to rename the method

--- src/main/java/edu/stanford/irt/laneweb/config/LibappsServiceConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/LibappsServiceConfiguration.java
@@ -11,14 +11,14 @@ import org.springframework.context.annotation.Configuration;
 public class LibappsServiceConfiguration {
 
     @Bean("java.net.URI/libguide-service")
-    public URI classesServiceURI(@Value("${edu.stanford.irt.laneweb.libguide-service.scheme}") String scheme,
+    public URI guideServiceURI(@Value("${edu.stanford.irt.laneweb.libguide-service.scheme}") String scheme,
             @Value("${edu.stanford.irt.laneweb.libguide-service.host}") String host,
             @Value("${edu.stanford.irt.laneweb.libguide-service.port}") int port) throws URISyntaxException {
         return new URI(scheme, null, host, port, null, null, null);
     }
 
     @Bean("java.net.URI/libguide-api-service")
-    public URI classesApiServiceURI(@Value("${edu.stanford.irt.laneweb.libguide-api-service.scheme}") String scheme,
+    public URI guideApiServiceURI(@Value("${edu.stanford.irt.laneweb.libguide-api-service.scheme}") String scheme,
             @Value("${edu.stanford.irt.laneweb.libguide-api-service.host}") String host,
             @Value("${edu.stanford.irt.laneweb.libguide-api-service.port}") int port,
             @Value("${edu.stanford.irt.laneweb.libguide-api-service.path}") String path,
