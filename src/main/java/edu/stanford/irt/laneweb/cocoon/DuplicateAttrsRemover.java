6f4d7942a51a550f4181a054a35ab7510ca269c0
Alain <aboussard@stanfordhealthcare.org>
2020-10-21 11:51:13 -0700

    To remove duplicate if the attributes length is superior to 8

--- src/main/java/edu/stanford/irt/laneweb/cocoon/DuplicateAttrsRemover.java
+++ src/main/java/edu/stanford/irt/laneweb/cocoon/DuplicateAttrsRemover.java
@@ -8,21 +8,38 @@ import org.apache.xerces.xni.QName;
 import org.apache.xerces.xni.XMLAttributes;
 import org.cyberneko.html.filters.DefaultFilter;
 
+/*
+ * This class remove the double attributes if the list attributes size if bigger than 8
+ * net.sf.saxon.om.LargeAttributeMap check for double attributes and only if the size is superior to 8
+ * net.sf.saxon.om.SmallAttributeMap.LIMIT = 8
+ */
 
-public class DuplicateAttrsRemover extends DefaultFilter{
-    
-  
+public class DuplicateAttrsRemover extends DefaultFilter {
+
+    private static final int LIMIT = 8;
+
+    @Override
+    public void startElement(QName element, XMLAttributes attributes, Augmentations augs) {
+        removeDuplicateAttributes(attributes);
+        super.startElement(element, attributes, augs);
+    }
+
     @Override
     public void emptyElement(QName element, XMLAttributes attributes, Augmentations augs) {
-        List<String> attributeNames = new ArrayList<>();
-        for (int i = 0; i < attributes.getLength(); i++) {
-            String name = attributes.getQName(i);
-            if (attributeNames.contains(name)) {
-                attributes.removeAttributeAt(i);
-            }
-            attributeNames.add(name);
-        }
+        removeDuplicateAttributes(attributes);
         super.emptyElement(element, attributes, augs);
     }
 
+    private void removeDuplicateAttributes(XMLAttributes attributes) {
+        if (attributes.getLength() > LIMIT) {
+            List<String> attributeNames = new ArrayList<>();
+            for (int i = 0; i < attributes.getLength(); i++) {
+                String name = attributes.getQName(i);
+                if (attributeNames.contains(name)) {
+                    attributes.removeAttributeAt(i);
+                }
+                attributeNames.add(name);
+            }
+        }
+    }
 }
