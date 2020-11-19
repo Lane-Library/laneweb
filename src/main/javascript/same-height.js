5666ea704ef184db0fb47a10b6b88851e60388e3
alainb <alainb@stanford.edu>
2020-11-19 15:57:55 -0800

    Responsive ui

--- src/main/javascript/same-height.js
+++ src/main/javascript/same-height.js
@@ -13,31 +13,36 @@
         clazz,
         classes = {},
         regex = /.*(same-height-\d).*/,
-        matches;
+        matches,
+        smallmedia = window.matchMedia("(max-width: 738px)")
+        ;
 
-    // find the class for each group
-    for (i = 0; i < nodes.length; i++) {
-        matches = regex.exec(nodes.item(i).className);
-        if (matches.length === 2) {
-            classes[matches[1]] = "." + matches[1];
-        }
-    }
-
-    for (clazz in classes) {
-        if (classes.hasOwnProperty(clazz)) {
-            // for each group find the tallest
-            nodes = document.querySelectorAll(classes[clazz]);
-            heights = [];
-            nodes.forEach(function(node) {
-                heights.push(node.clientHeight);
-            });
-            maxHeight = Math.max.apply(null, heights);
-            // set all of the group to the tallest's height
-            nodes.forEach(function(node) {
-                node.style.minHeight = maxHeight + "px";
-            });
-        }
-    }
-
+    
+    if( ! smallmedia){
+		 // find the class for each group
+		    
+	    for (i = 0; i < nodes.length; i++) {
+	        matches = regex.exec(nodes.item(i).className);
+	        if (matches.length === 2) {
+	            classes[matches[1]] = "." + matches[1];
+	        }
+	    }
+	
+	    for (clazz in classes) {
+	        if (classes.hasOwnProperty(clazz)) {
+	            // for each group find the tallest
+	            nodes = document.querySelectorAll(classes[clazz]);
+	            heights = [];
+	            nodes.forEach(function(node) {
+	                heights.push(node.clientHeight);
+	            });
+	            maxHeight = Math.max.apply(null, heights);
+	            // set all of the group to the tallest's height
+	            nodes.forEach(function(node) {
+	                node.style.minHeight = maxHeight + "px";
+	            });
+	        }
+	    }
+	}
 
 })();
