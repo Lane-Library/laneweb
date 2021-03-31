4e828f8a9bd4ddc9f2a7e2fc11e9ad735fb8072d
Alain Boussard <alainb@stanford.edu>
2021-03-31 09:48:24 -0700

    https://medjira.stanford.edu/browse/LANEWEB-10696

--- src/main/java/edu/stanford/irt/laneweb/seminars/SeminarsGenerator.java
+++ src/main/java/edu/stanford/irt/laneweb/seminars/SeminarsGenerator.java
@@ -26,7 +26,7 @@ public class SeminarsGenerator extends AbstractGenerator {
     private static final String SEMINARS_NS = "http://lane.stanford.edu/seminars/ns";
 
     private static final MessageFormat URL_FORMAT = new MessageFormat(
-            "https://med.stanford.edu/seminars/validatecmecalendar.do?filter=true&selMonth={0}&selDay={1}&selYear={2}&futureNumberDays=60&departmentId=0&seminarLocation=0&keyword=&courseType=gran");
+            "https://seminars.med.stanford.edu/seminars/validatecmecalendar.do?filter=true&selMonth={0}&selDay={1}&selYear={2}&futureNumberDays=60&departmentId=0&seminarLocation=0&keyword=&courseType=gran");
 
     private final DateTimeFormatter dayFormat = DateTimeFormatter.ofPattern("dd");
 
