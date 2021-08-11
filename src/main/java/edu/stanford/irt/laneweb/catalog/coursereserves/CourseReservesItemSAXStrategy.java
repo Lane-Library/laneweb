f929596a15492666ceb83b53a4072ce27c21cf9a
Ryan Max Steinberg <ryanmax@stanford.edu>
2021-08-11 16:37:52 -0700

    LANEWEB-10768: course reserves improvement
    point to Lane Search instead of Voyager
    add Request button selectively

--- src/main/java/edu/stanford/irt/laneweb/catalog/coursereserves/CourseReservesItemSAXStrategy.java
+++ src/main/java/edu/stanford/irt/laneweb/catalog/coursereserves/CourseReservesItemSAXStrategy.java
@@ -43,7 +43,7 @@ public class CourseReservesItemSAXStrategy extends AbstractXHTMLSAXStrategy<Cour
             if (isDigital && url != null) {
                 href = url;
             } else {
-                href = "http://lmldb.stanford.edu/cgi-bin/Pwebrecon.cgi?BBID=" + id;
+                href = "https://lane.stanford.edu/view/bib/" + id;
             }
             createAnchor(xmlConsumer, href, item.getTitle());
             String author = item.getAuthor();
@@ -63,7 +63,7 @@ public class CourseReservesItemSAXStrategy extends AbstractXHTMLSAXStrategy<Cour
             }
             endDiv(xmlConsumer);
             if (type == ItemType.PRINT_BOOK) {
-                availableCount(xmlConsumer, item.getAvailableCount());
+                availableCount(xmlConsumer, item.getAvailableCount(), id);
                 callNumber(xmlConsumer, item.getCallNumber());
             }
             endDiv(xmlConsumer);
@@ -74,7 +74,8 @@ public class CourseReservesItemSAXStrategy extends AbstractXHTMLSAXStrategy<Cour
         }
     }
 
-    private void availableCount(final XMLConsumer xmlConsumer, final Integer availableCount) throws SAXException {
+    private void availableCount(final XMLConsumer xmlConsumer, final Integer availableCount, final String id)
+            throws SAXException {
         if (availableCount != null) {
             startDiv(xmlConsumer);
             createStrong(xmlConsumer, "Status: ");
@@ -83,6 +84,17 @@ public class CourseReservesItemSAXStrategy extends AbstractXHTMLSAXStrategy<Cour
             } else {
                 XMLUtils.data(xmlConsumer, "Available ");
                 createStrong(xmlConsumer, availableCount.toString());
+                startElementWithClass(xmlConsumer, "span", "requestIt");
+                AttributesImpl atts = new AttributesImpl();
+                atts.addAttribute(EMPTY_NS, CLASS, CLASS, CDATA, "btn alt");
+                atts.addAttribute(EMPTY_NS, "title", "title", CDATA, "Request this item");
+                atts.addAttribute(EMPTY_NS, "href", "href", CDATA,
+                        "https://lmldb.stanford.edu/cgi-bin/Pwebrecon.cgi?BBID=" + id + "&lw.req=true");
+                atts.addAttribute(EMPTY_NS, "rel", "rel", CDATA, "popup console 1020 800");
+                XMLUtils.startElement(xmlConsumer, XHTML_NS, "a", atts);
+                XMLUtils.data(xmlConsumer, "Request");
+                XMLUtils.endElement(xmlConsumer, XHTML_NS, "a");
+                XMLUtils.endElement(xmlConsumer, XHTML_NS, "span");
             }
             endDiv(xmlConsumer);
         }
