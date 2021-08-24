f152f40dbfc35f5cf0d1422c7e0f77675d3a86b3
Ryan Max Steinberg <ryanmax@stanford.edu>
2021-08-24 11:59:50 -0700

    LANEWEB-10789: no need to throttle browzine api requests

--- src/test/javascript/browzine/browzine-test.js
+++ src/test/javascript/browzine/browzine-test.js
@@ -6,43 +6,21 @@ YUI({fetchCSS:false}).use("test", "test-console", function(Y) {
 
         name: "Lane Browzine Testcase",
 
-        "test viewport:init": function() {
-            var inview = 0;
-            L.fire("viewport:init", {
-                viewport: {
-                    inView: function() {
-                        return inview++ === 0;
-                    }
-                }
-            });
+        "test 1": function() {
             var links = document.querySelectorAll("li[data-doi='1'] a"),
                 lastLink = links[links.length - 1];
             Y.Assert.areEqual("fulltext-url", lastLink.getAttribute("href"))
             Y.Assert.areEqual(true, lastLink.isTrackableAsEvent)
         },
 
-        "test viewport:scrolled 1": function() {
-            L.fire("viewport:scrolled", {
-                viewport: {
-                    inView: function() {
-                        return true;
-                    }
-                }
-            });
+        "test 2": function() {
             var links = document.querySelectorAll("li[data-doi='2'] a"),
                 lastLink = links[links.length - 1];
             Y.Assert.areEqual("fulltext-url", lastLink.getAttribute("href"))
             Y.Assert.areEqual(true, lastLink.isTrackableAsEvent)
         },
         
-        "test viewport:scrolled 2": function() {
-            L.fire("viewport:scrolled", {
-                viewport: {
-                    inView: function() {
-                        return true;
-                    }
-                }
-            });
+        "test no doi": function() {
             Y.Assert.areEqual("old", document.querySelector("li:not([data-doi]) a").getAttribute("href"))
             Y.Assert.areEqual(1, document.querySelectorAll("li:not([data-doi]) a").length);
         }
