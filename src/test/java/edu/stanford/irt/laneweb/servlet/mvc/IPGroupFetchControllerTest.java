24ad90bc13d4d53aa0e75d15b53eb5fdeddb78f5
Ryan Max Steinberg <ryanmax@stanford.edu>
2021-09-13 14:40:06 -0700

    sonar says: Replace these 3 tests with a single Parameterized one.
    less clear than no params?

--- src/test/java/edu/stanford/irt/laneweb/servlet/mvc/IPGroupFetchControllerTest.java
+++ src/test/java/edu/stanford/irt/laneweb/servlet/mvc/IPGroupFetchControllerTest.java
@@ -6,26 +6,55 @@ import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.ui.Model;
 
 import edu.stanford.irt.laneweb.ipgroup.IPGroup;
 import edu.stanford.irt.laneweb.servlet.binding.RemoteProxyIPDataBinder;
 
+@RunWith(Parameterized.class)
 public class IPGroupFetchControllerTest {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                // get ip with callback
+                { IPGroup.OTHER, "callback", "callback('OTHER');" },
+                // malicious callback
+                { IPGroup.OTHER, "LANE.Tracker.setIpGroup71907%3balert(1)%2f%2f734", "OTHER" },
+                // no callback
+                { IPGroup.OTHER, null, "OTHER" } });
+    }
 
     private RemoteProxyIPDataBinder binder;
 
+    private String callback;
+
     private IPGroupFetchController controller;
+
+    private String expected;
+
+    private IPGroup ipGroup;
 
     private Model model;
 
     private HttpServletRequest request;
+
+    public IPGroupFetchControllerTest(final IPGroup ipGroup, final String callback, final String expected) {
+        this.ipGroup = ipGroup;
+        this.callback = callback;
+        this.expected = expected;
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -36,33 +65,19 @@ public class IPGroupFetchControllerTest {
     }
 
     @Test
+    public void shouldReturnExpectedString() {
+        replay(this.binder, this.model, this.request);
+        String actual = this.controller.getIPGroup(this.ipGroup, this.callback);
+        assertEquals(actual, this.expected);
+        verify(this.binder, this.model, this.request);
+    }
+
+    @Test
     public void testBind() {
         expect(this.model.asMap()).andReturn(Collections.emptyMap());
         this.binder.bind(Collections.emptyMap(), this.request);
         replay(this.binder, this.model, this.request);
         this.controller.bind(this.request, this.model);
-        verify(this.binder, this.model, this.request);
-    }
-
-    @Test
-    public void testGetIPGroup() {
-        replay(this.binder, this.model, this.request);
-        assertEquals("callback('OTHER');", this.controller.getIPGroup(IPGroup.OTHER, "callback"));
-        verify(this.binder, this.model, this.request);
-    }
-
-    @Test
-    public void testGetIPGroupMaliciousCallback() {
-        replay(this.binder, this.model, this.request);
-        assertEquals("OTHER",
-                this.controller.getIPGroup(IPGroup.OTHER, "LANE.Tracker.setIpGroup71907%3balert(1)%2f%2f734"));
-        verify(this.binder, this.model, this.request);
-    }
-
-    @Test
-    public void testGetIPGroupNoCallback() {
-        replay(this.binder, this.model, this.request);
-        assertEquals("OTHER", this.controller.getIPGroup(IPGroup.OTHER, null));
         verify(this.binder, this.model, this.request);
     }
 }
