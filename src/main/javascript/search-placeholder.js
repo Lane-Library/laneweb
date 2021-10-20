3a06064b73ee5e7ad8a69fc37b0347484067d98f
aboussard <alainb@stanford.edu>
2021-10-20 10:11:55 -0700

    new search-form

--- src/main/javascript/search-placeholder.js
+++ src/main/javascript/search-placeholder.js
@@ -1,50 +1,19 @@
-if (document.querySelector(".search-form"))  {
 
 (function() {
 
-    "use strict";
+	"use strict";
 
-    var PLACEHOLDER = "placeholder",
 
-        inputNode = document.querySelector(".search-form input[name=q]"),
+	var dropdown = document.querySelector(".search-dropdown");
 
-        model = function(def, current) {
-            return {
-                def: def,
-                current: current
-            };
-          }(inputNode.dataset[PLACEHOLDER],
-              document.querySelector(".search-tab-active").dataset[PLACEHOLDER]),
-
-        view = function() {
-
-            return {
-                    setPlaceholder: function(placeholder) {
-                        inputNode.placeholder = placeholder;
-                    }
-                };
-
-        }(),
-
-        controller = function() {
-
-            return {
-                activeChange: function(event) {
-                    view.setPlaceholder(event.active ? model.current : model.def);
-                },
-
-                tabChange: function(event) {
-                    var newVal = event.newVal;
-                    model.current = newVal[newVal.source].placeholder;
-                    view.setPlaceholder(model.current);
-                }
-            };
-        }();
-
-    L.on("searchTabs:change", controller.tabChange);
-    L.on("search:activeChange", controller.activeChange);
+	if (dropdown) {
+		dropdown.addEventListener("change", function(e) {
+			var el = e.target,
+				placeholdervalue = el.options[el.selectedIndex].dataset.placeholder;
+			document.querySelector(".search-form input[name=q]").placeholder = placeholdervalue;
+		});
+	}
 
 
 })();
 
-}
