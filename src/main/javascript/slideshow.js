312db7269ff8b72cad9b1563fe3afa28149af602
aboussard <alainb@stanford.edu>
2022-02-07 09:12:18 -0800

    to fix staff dir

--- src/main/javascript/slideshow.js
+++ src/main/javascript/slideshow.js
@@ -1,67 +1,69 @@
 (function() {
+
 	"use strict";
 
-	var slides = document.querySelectorAll(".slide"),
-		container = document.getElementById("slide-show"),
-		nextButton = document.getElementById("next-slide"),
-		previousButton = document.getElementById("previous-slide"),
-		padding = 24,
+	if (document.querySelectorAll(".slide") !== undefined && document.querySelectorAll(".slide").length > 0) {
 
 
+		var container = document.getElementById("slide-show"),
+			nextButton = document.getElementById("next-slide"),
+			previousButton = document.getElementById("previous-slide"),
+			padding = 24,
 
-		model = function(slides) {
+
+		 model = function(slides) {
 
 			var m = {
 				slides: slides,
 				index: 0,
 				maxPreviousClick: slides.length - 1
-			};
+			}
 			return m;
 
-		}(slides),
+		}(document.querySelectorAll(".slide")),
 
-		view = function() {
-			nextButton.className = "disable";
-			return {
-				updateButton: function(index, slidesLength) {
-					if (index >= slidesLength) {
-						previousButton.className = "disable";
-					} else {
-						previousButton.className = "";
-					}
-					if (index <= 0) {
-						nextButton.className = "disable";
-					} else {
-						nextButton.className = "";
+			view = function() {
+				nextButton.className = "disable";
+				return {
+					updateButton: function(index, slidesLength) {
+						if (index >= slidesLength) {
+							previousButton.className = "disable";
+						} else {
+							previousButton.className = "";
+						}
+						if (index <= 0) {
+							nextButton.className = "disable";
+						} else {
+							nextButton.className = "";
+						}
 					}
 				}
-			}
-		}(),
+			}(),
 
-		controller = function() {
+			controller = function() {
 
-			return {
-				next: function() {
-					model.index--;
-					model.slides[model.index].className = "slide";
-					view.updateButton(model.index, model.maxPreviousClick);
-				},
-				previous: function() {
-					model.slides[model.index].className = "slide desactive-next";
-					model.index++;
-					view.updateButton(model.index, model.maxPreviousClick);
-				},
-				maxPreviousClicks: function() {
-					var imageDiv = model.slides[model.slides.length - 1];
-					var t = parseFloat(container.offsetWidth) / (parseFloat(imageDiv.offsetWidth + padding));
-					model.maxPreviousClick = model.slides.length - parseInt(t);
+				return {
+					next: function() {
+						model.index--;
+						model.slides[model.index].className = "slide";
+						view.updateButton(model.index, model.maxPreviousClick);
+					},
+					previous: function() {
+						model.slides[model.index].className = "slide desactive-next";
+						model.index++;
+						view.updateButton(model.index, model.maxPreviousClick);
+					},
+					maxPreviousClicks: function() {
+						var imageDiv = model.slides[model.slides.length - 1];
+						var t = parseFloat(container.offsetWidth) / (parseFloat(imageDiv.offsetWidth + padding));
+						model.maxPreviousClick = model.slides.length - parseInt(t);
+					}
 				}
-			}
-		}();
+			}();
 
 
 
-	if (slides !== undefined && slides.length > 0) {
+
 		controller.maxPreviousClicks();
 		previousButton.addEventListener(
 			"click", function(e) {
