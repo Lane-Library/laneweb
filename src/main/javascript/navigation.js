f7ebd826f4e98e519cce2db9e993d802dc3a2c1c
aboussard <alainb@stanford.edu>
2022-02-14 11:54:55 -0800

    Update navigation

--- src/main/javascript/navigation.js
+++ src/main/javascript/navigation.js
@@ -2,51 +2,21 @@
 
 	"use strict";
 
-	window.model = window.model || {};
-
-
-
-	var smallmedia = window.matchMedia("(min-width: 1100px)");
 
 	if (document.querySelectorAll(".nav-menu") !== undefined) {
 
-
-		var resetNav = function() {
-			smallmedia = window.matchMedia("(min-width: 1100px)");
-			document.querySelectorAll(".nav-menu").forEach(function(node) {
-					node.classList.remove('nav-menu-active');						
-					if(node.querySelector(".nav-menu-content") != null){
-						node.querySelector(".nav-menu-content").style.display = null;
-					}										
-				});
-		}
-
-
 		document.querySelectorAll(".nav-menu").forEach(function(node) {
 			node.addEventListener("click", function(event) {
+				var smallmedia = window.matchMedia("(min-width: 1100px)"),
+					nav = event.currentTarget, navContent;
 				if (!smallmedia.matches) {
-					var nav = event.currentTarget,
-						navContent = nav.querySelector(".nav-menu-content"),
-						display = navContent.style.display;
-					if (display == '') {
-						nav.classList.add('nav-menu-active');
-						navContent.style.display = "block";
-					} else {
-						navContent.style.display = null;
-						nav.classList.remove('nav-menu-active');
-					}
+					navContent = nav.querySelector('.nav-menu-content');
+					nav.classList.toggle('nav-menu-active-on-click');
+					navContent.classList.toggle('dropdown-content-on-click');
 				}
-
 			});
 		});
-		
-		window.addEventListener('resize', resetNav);
 	}
-
-
-
-
-
 
 })();
 
