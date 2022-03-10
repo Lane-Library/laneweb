5212c49aa51ac6fafff3b562f215828f4aab4f12
Ryan Max Steinberg <ryanmax@stanford.edu>
2022-03-09 21:11:32 -0800

    if the click target has an href, don't toggle the nav menu
    solves problem of closing the menu before user leaves the page

--- src/main/javascript/navigation.js
+++ src/main/javascript/navigation.js
@@ -7,8 +7,10 @@
         document.querySelectorAll(".nav-menu").forEach(function(node) {
             node.addEventListener("click", function(event) {
                 var smallmedia = window.matchMedia("(min-width: 1100px)"),
-                    nav = event.currentTarget, navContent;
-                if (!smallmedia.matches) {
+                    nav = event.currentTarget, 
+                    clickTarget = event.target,
+                    navContent;
+                if (!smallmedia.matches && !clickTarget.href) {
                     navContent = nav.querySelector('.dropdown-content');
                     nav.classList.toggle('nav-menu-active-on-click');
                     navContent.classList.toggle('dropdown-content-on-click');
