68b6dfa4f0c9a5b56bcdbc3828a0a58ca584d7d3
Ryan Max Steinberg <ryanmax@stanford.edu>
2022-03-09 20:53:19 -0800

    source format

--- src/main/javascript/navigation.js
+++ src/main/javascript/navigation.js
@@ -1,31 +1,27 @@
 (function() {
 
-	"use strict";
+    "use strict";
 
+    if (document.querySelectorAll(".nav-menu") !== undefined) {
 
-	if (document.querySelectorAll(".nav-menu") !== undefined) {
+        document.querySelectorAll(".nav-menu").forEach(function(node) {
+            node.addEventListener("click", function(event) {
+                var smallmedia = window.matchMedia("(min-width: 1100px)"),
+                    nav = event.currentTarget, navContent;
+                if (!smallmedia.matches) {
+                    navContent = nav.querySelector('.dropdown-content');
+                    nav.classList.toggle('nav-menu-active-on-click');
+                    navContent.classList.toggle('dropdown-content-on-click');
+                }
+            });
+        });
 
-		
+        document.querySelector(".menu-overlay").addEventListener(
+            "click", function(e) {
+                window.location.hash = "#";
+            }, false);
 
-		document.querySelectorAll(".nav-menu").forEach(function(node) {
-			node.addEventListener("click", function(event) {
-				var smallmedia = window.matchMedia("(min-width: 1100px)"),
-					nav = event.currentTarget, navContent;
-				if (!smallmedia.matches) {
-					navContent = nav.querySelector('.dropdown-content');
-					nav.classList.toggle('nav-menu-active-on-click');
-					navContent.classList.toggle('dropdown-content-on-click');
-				}
-			});
-		});
-			
-			
-		document.querySelector(".menu-overlay").addEventListener(
-			"click", function(e) {
-				window.location.hash = "#";				
-			}, false);
-
-	}
+    }
 
 })();
 
