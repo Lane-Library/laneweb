9d06188ac3a28f20703f51d21b9f1d672e04df7c
aboussard <alainb@stanford.edu>
2022-05-07 18:08:06 -0700

    fix javascript on phone

--- src/main/javascript/staff-directory.js
+++ src/main/javascript/staff-directory.js
@@ -4,12 +4,13 @@
 
     if (document.querySelectorAll(".staff-list .slide") !== undefined && document.querySelectorAll(".staff-list .slide").length > 0) {
 
-        document.querySelectorAll(".staff-list .slide").forEach(function(slide) {
+        document.querySelectorAll('div[class ="staff-list"] div[class ="slide"]').forEach(function(slide) {
             slide.addEventListener("click", function(event) {
-                var nodeName = event.target.tagName;
-                if ("A" != nodeName && slide.querySelector(".overlay") !== undefined) {
-                    slide.querySelector(".overlay").classList.toggle("on");
-                    slide.querySelector(".staff-info").classList.toggle("on");
+                var nodeName = event.target.tagName,
+                    currentNode = event.currentTarget;
+                if ("A" != nodeName && node.querySelector(".overlay") !== undefined) {
+                    currentNode.querySelector(".overlay").classList.toggle("on");
+                    currentNode.querySelector(".staff-info").classList.toggle("on");
                     event.stopPropagation();
                     event.preventDefault();
                 }
