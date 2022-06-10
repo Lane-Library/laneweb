5c50fdae63cd49a85d01b49f7fd3be041a2fc8dd
aboussard <alainb@stanford.edu>
2022-06-10 15:40:30 -0700

    rewrite javascript with MVC

--- src/main/javascript/guides.js
+++ src/main/javascript/guides.js
@@ -6,60 +6,89 @@
     if (document.querySelector(".guide")) {
 
 
-        window.addEventListener("load", function() {
-            var target = window.location.hash;
-            if (!target) {
-                target = "#all-guides"
+
+        var model = function(id) {
+            if (!id)
+                id = '#all-guides'
+            var m = {
+                target: id,
             }
-            document.querySelector(target).classList.add('active');
-            document.querySelectorAll('.menu-guide ul li a').forEach(function(anchor) {
-                if (anchor.href.endsWith(target)) {
-                    anchor.classList.add('menuitem-active');
+            return m;
+        }(window.location.hash),
+
+            view = function() {
+                return {
+
+                    clearMenu: function() {
+                        document.querySelectorAll('.menu-guide ul li a.menuitem-active').forEach(function(anchor) {
+                            anchor.classList.remove('menuitem-active');
+
+                        })
+                    },
+                    activeMenu: function(target) {
+                        document.querySelectorAll('.menu-guide ul li a').forEach(function(anchor) {
+                            if (anchor.href.endsWith(target)) {
+                                anchor.classList.add('menuitem-active');
+                            }
+                        })
+                    }
+                    ,
+                    openGuide: function(target) {
+                        document.querySelector(target).classList.add('active');
+                    },
+                    closesAllGuides: function() {
+                        document.querySelectorAll('.libguides div.active').forEach(function(div) {
+                            div.classList.remove('active');
+                        })
+                    }
                 }
-            })
+            }(),
 
-        }, false);
+            controller = function() {
+                return {
+                    load: function() {
+                        view.clearMenu();
+                        view.activeMenu(model.target);
+                        view.openGuide(model.target);
+                    },
+                    reload: function() {
+                        view.clearMenu();
+                        view.activeMenu(model.target);
+                        view.closesAllGuides();
+                        view.openGuide(model.target);
+                    },
+                    unactiveAll: function() {
+                        view.clearMenu();
+                        view.closesAllGuides();
+                    }
+                }
+            }();
 
-        // Manage click the menu in the left
-        document.querySelectorAll('.menu-guide ul li a').forEach(function(anchor) {
-            anchor.addEventListener('click', function(event) {
-                var menu = event.target,
-                    id = menu.href.substring(menu.href.indexOf('#'));
-                document.querySelectorAll('.menu-guide ul li a').forEach(function(link) {
-                    link.classList.remove('menuitem-active');
-                })
-                anchor.classList.add('menuitem-active');
-                document.querySelectorAll('.libguides div.active').forEach(function(div) {
-                    div.classList.remove('active');
+
+
+        window.addEventListener("load", function() {
+            controller.load();
+            document.querySelectorAll('.menu-guide ul li a').forEach(function(anchor) {
+                anchor.addEventListener('click', function(event) {
+                    model.target = event.target.href.substring(event.target.href.indexOf('#'));
+                    controller.reload();
                 });
-                document.querySelector(id).classList.add('active');
             });
-        });
 
 
-
-
-        // Manage click the toogle  menu on eahc guides
-        document.querySelectorAll('.guide-menu-toggle').forEach(function(menubutton) {
-            menubutton.addEventListener('click', function(event) {
-                var button = event.target,
-                    target = '#' + button.dataset.target,
-                    guideDiv = document.querySelector(target);
-                guideDiv.classList.toggle('active');
-                document.querySelectorAll('.libguides div.active').forEach(function(div) {
-                    if ("#" + div.id != target) {
-                        div.classList.remove('active');
-                    }
+            document.querySelectorAll('.guide-menu-toggle.on').forEach(function(menubutton) {
+                menubutton.addEventListener('click', function(event) {
+                    model.target = "#" + event.target.dataset.target;
+                    controller.reload();
                 });
-                document.querySelectorAll('.menu-guide ul li a').forEach(function(anchor) {
-                    anchor.classList.remove('menuitem-active');
-                    if (anchor.href.endsWith(target) && !button.href.endsWith('#off')) {
-                        anchor.classList.add('menuitem-active');
-                    }
+            });
+
+            document.querySelectorAll('.guide-menu-toggle.off').forEach(function(menubutton) {
+                menubutton.addEventListener('click', function(event) {
+                    controller.unactiveAll();
+
                 });
-            })
-        });
-
-
+            });
+        }, false);
     }
 })();
