6e1ae233a3d73f4ee228a82771aede3f77688f5a
Ryan Max Steinberg <ryanmax@stanford.edu>
2023-01-06 12:54:19 -0800

    LANEWEB-11068: basic Zotero support (DOI only)

---
+++ src/main/javascript/zotero.js
@@ -0,0 +1,22 @@
+(function() {
+
+    "use strict";
+
+    /**
+     * LANEWEB-11068: Basic Zotero support for DOIs
+     * Zotero does a decent job of scraping free-text DOIs and expanding them to complete citations.
+     * For more comprehensive Zotero support, consider storing metadata objects (COinS?) in Solr at index time.
+     */
+    var dois = [];
+    document.querySelectorAll("li[data-doi]").forEach(function(node) {
+        var doi = node.dataset.doi;
+        if (!dois.includes(doi)) {
+            dois.push(doi);
+        }
+    })
+    if (dois.length) {
+        document.body.insertAdjacentHTML("beforeend",
+            '<span class="zotero-metadata">doi:' + dois.join(' doi:') + '</span>');
+    }
+
+})();
