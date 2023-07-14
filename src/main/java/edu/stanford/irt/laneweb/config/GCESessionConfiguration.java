127ac41e56452867ee1cf50587a9295e4001863c
aboussard <alainb@stanford.edu>
2023-07-14 13:30:25 -0700

    To exclude RedisAutoConfiguration.class

--- src/main/java/edu/stanford/irt/laneweb/config/GCESessionConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/GCESessionConfiguration.java
@@ -1,8 +1,11 @@
 package edu.stanford.irt.laneweb.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
@@ -11,16 +14,18 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 @EnableRedisHttpSession
 @Profile("gce")
+@Import({ RedisAutoConfiguration.class })
 public class GCESessionConfiguration {
  
     @Bean
-    public static ConfigureRedisAction configureRedisAction() {
+    static ConfigureRedisAction configureRedisAction() {
         return ConfigureRedisAction.NO_OP;
     }
 
+    @Primary
     @Bean
-    public LettuceConnectionFactory connectionFactory(
-            @Value("${edu.stanford.irt.laneweb.redis.host}") final String host,
+    LettuceConnectionFactory connectionFactory(
+             @Value("${edu.stanford.irt.laneweb.redis.host}") final String host,
             @Value("${edu.stanford.irt.laneweb.redis.port}") final int port) {
         return new LettuceConnectionFactory(host, port);
     }
