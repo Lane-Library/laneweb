ae7aa1820d71afd6dd1443294c938c0492d42537
aboussard <alainb@stanford.edu>
2023-09-27 12:07:50 -0700

    To remove warning during  mvn package added * @type {Object}

--- src/main/javascript/suggest.js
+++ src/main/javascript/suggest.js
@@ -13,11 +13,12 @@
     /**
      * A class that provides an autocomplete widget for inputs.
      * @class Suggest
+     * @type {Object}
      * @requires EventTarget
 	 * @constructor
      * @param input {Node} the input node.
      * @param limit {String} the limit parameter for the request.
-	 * @param sourceBase {String}.
+	 * @param sourceBase {String} .
      */
     Suggest = function(input,  minQueryLength, sourceBase) {
         var yuiinput = input._node ? input : new Y.Node(input);
