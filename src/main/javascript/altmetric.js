3a7027d4a39a3c61a7ea36bc29eb533bad2105b2
Ryan Max Steinberg <ryanmax@stanford.edu>
2023-11-20 13:31:12 -0800

    help label/link not needed since using default (not donut) display

--- src/main/javascript/altmetric.js
+++ src/main/javascript/altmetric.js
@@ -7,15 +7,6 @@
     if(Model.get(Model.AUTH) && document.querySelector(".lwSearchResults")){
         L.Get.script("https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js");
         L.Get.script("https://badge.dimensions.ai/badge.js");
-        /* add help link/labl to donut
-        document.querySelectorAll(".altmetric-embed").forEach(function(badgeNode) {
-            badgeNode.addEventListener("altmetric:show", function() {
-                badgeNode.insertAdjacentHTML("beforeend",
-                    '<a title="What is Altmetric?" href="https://help.altmetric.com/support/solutions/articles/6000232837-what-is-altmetric-and-what-does-it-provide-">Altmetric</a>'
-                )
-            })
-        });
-        */
     }
 
 })();
