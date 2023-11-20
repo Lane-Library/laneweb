fd2cf3fc9ca6a8af15894d9cad5b5ed7d709adbe
Ryan Max Steinberg <ryanmax@stanford.edu>
2023-11-20 14:17:46 -0800

    update comment

--- src/main/javascript/altmetric.js
+++ src/main/javascript/altmetric.js
@@ -3,7 +3,7 @@
     "use strict";
     let Model = L.Model;
 
-    // show altmetric widgets for authenticated users
+    // show altmetric widgets on search/browse pages for authenticated users
     if(Model.get(Model.AUTH) && document.querySelector(".lwSearchResults")){
         L.Get.script("https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js");
         L.Get.script("https://badge.dimensions.ai/badge.js");
