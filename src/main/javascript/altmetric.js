a7153d9c3077cd2f47a8ae0013f67271fa984fde
Ryan Max Steinberg <ryanmax@stanford.edu>
2023-11-21 09:26:52 -0800

    LANEWEB-11247
    show dimensions badges on public pages
    altmetric badges require authentication

--- src/main/javascript/altmetric.js
+++ src/main/javascript/altmetric.js
@@ -3,10 +3,14 @@
     "use strict";
     let Model = L.Model;
 
-    // show altmetric widgets on search/browse pages for authenticated users
-    if(Model.get(Model.AUTH) && document.querySelector(".lwSearchResults")){
-        L.Get.script("https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js");
+    // LANEWEB-11247: show altmetric/dimensions widgets on search/browse pages
+    // show dimensions badges on public pages
+    // altmetric badges require authentication
+    if(document.querySelector(".lwSearchResults")){
         L.Get.script("https://badge.dimensions.ai/badge.js");
+        if(Model.get(Model.AUTH)){
+            L.Get.script("https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js");
+        }
     }
 
 })();
