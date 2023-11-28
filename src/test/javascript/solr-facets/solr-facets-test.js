5dd65c2972f3a610309d091eebe6ad17805c7d63
aboussard <alainb@stanford.edu>
2023-11-27 16:29:28 -0800

    to replace all <i to svg in test javascript

--- src/test/javascript/solr-facets/solr-facets-test.js
+++ src/test/javascript/solr-facets/solr-facets-test.js
@@ -8,7 +8,7 @@ YUI({fetchCSS:false}).use("test", "test-console", function(Y) {
 L.Model.set(L.Model.URL_ENCODED_QUERY, "foo");
 
 L.io = function(url, config) {
-    config.on.success.apply(config.context, [0, {responseText:'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><body><div class="bd"><h3>Filter Results</h3><ul><li class="solrFacet facetHeader">Results from <span id="sources"><i class="fa fa-info-circle fa-lg"></i></span></li><li class="enabled"><a href="?source=all-all&amp;q=foo" title="remove"><i class="fa fa-check-square fa-lg"></i></a><span class="facetLabel">Lane Catalog</span><span class="facetCount">9</span></li></ul></div></body></html>'}, {}]);
+    config.on.success.apply(config.context, [0, {responseText:'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><body><div class="bd"><h3>Filter Results</h3><ul><li class="solrFacet facetHeader">Results from <span id="sources"><svg class="fa-lg"><use xlink:href="/resources/svg/regular.svg#info-circle"></use></svg></i></span></li><li class="enabled"><a href="?source=all-all&amp;q=foo" title="remove"><svg class="fa-lg"><use xlink:href="/resources/svg/regular.svg#check-square "></use></svg></a><span class="facetLabel">Lane Catalog</span><span class="facetCount">9</span></li></ul></div></body></html>'}, {}]);
 };
 
 var solrFacetsTestCase = new Y.Test.Case({
