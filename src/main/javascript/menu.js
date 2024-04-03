e795af719204c4b13242c299b066ee19f73396d5
aboussard <alainb@stanford.edu>
2024-04-03 12:18:54 -0700

    To update css and javascript for the who-can-use-lane page

--- src/main/javascript/menu.js
+++ src/main/javascript/menu.js
@@ -4,7 +4,7 @@
 
 
     //For regular mobile menu 
-    document.querySelectorAll('.menu-container.mobile h2').forEach(function(menu) {
+    document.querySelectorAll('.menu-container.mobile h2, .menu-container.phone h2').forEach(function(menu) {
         menu.addEventListener('click', function(event) {
             event.preventDefault();
             menu.closest(".menu-container").classList.toggle('active');
