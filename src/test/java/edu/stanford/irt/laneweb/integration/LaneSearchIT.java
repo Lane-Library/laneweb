cbce53b64f5fe26217dcd6e67fc1412cd5e0cc4b
aboussard <alainb@stanford.edu>
2024-04-08 14:28:36 -0700

    To add @TestPropertySource to be able to read the properties file And use @MockBean for the JavaMailSender bean generated by spring boot

--- src/test/java/edu/stanford/irt/laneweb/integration/LaneSearchIT.java
+++ src/test/java/edu/stanford/irt/laneweb/integration/LaneSearchIT.java
@@ -15,8 +15,11 @@ import jakarta.annotation.Resource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,12 +28,16 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
+@TestPropertySource(locations = "classpath:config/application.properties")
 @ContextConfiguration(classes = edu.stanford.irt.laneweb.config.LanewebConfiguration.class)
 public class LaneSearchIT {
 
     private static final MediaType TEXT_HTML = new MediaType("text", "html", StandardCharsets.UTF_8);
 
     private MockMvc mockMvc;
+    
+    @MockBean
+    JavaMailSender JavaMailSender;
 
     private Map<String, String> ns;
 
