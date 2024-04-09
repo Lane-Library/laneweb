f7957a9f036cbcf4af90f4355a4a29e81a801520
aboussard <alainb@stanford.edu>
2024-04-09 14:30:10 -0700

    Clean up and format

--- src/main/java/edu/stanford/irt/laneweb/config/EmailConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/EmailConfiguration.java
@@ -3,10 +3,8 @@ package edu.stanford.irt.laneweb.config;
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import edu.stanford.irt.laneweb.email.EMailSender;
