ceec4e2bd5a7e9ad584528c65be4fd86d202a4a3
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-04-10 16:59:12 -0700

    add unit test for new-ish BasicAuthRESTService

--- src/test/java/edu/stanford/irt/laneweb/rest/RESTClientTestConfiguration.java
+++ src/test/java/edu/stanford/irt/laneweb/rest/RESTClientTestConfiguration.java
@@ -8,12 +8,17 @@ import org.springframework.web.client.RestClient;
 public class RESTClientTestConfiguration {
 
     @Bean
-    RestClient restClient(RestClient.Builder builder) {
+    BasicAuthRESTService basicAuthRestService(final RestClient restClient) {
+        return new BasicAuthRESTService(restClient, "userInfo");
+    }
+
+    @Bean
+    RestClient restClient(final RestClient.Builder builder) {
         return builder.build();
     }
 
     @Bean
-    RESTService restService(RestClient restClient) {
+    RESTService restService(final RestClient restClient) {
         return new RESTService(restClient);
     }
 }
