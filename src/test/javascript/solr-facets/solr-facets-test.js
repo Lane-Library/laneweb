c25f5f26fdbe41595cc7933e9dc94d6b299e40b3
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-05-13 12:56:50 -0700

    fix broken JS unit tests

--- src/test/javascript/solr-facets/solr-facets-test.js
+++ src/test/javascript/solr-facets/solr-facets-test.js
@@ -1,54 +1,51 @@
 /**
  * @author ryanmax
  */
-YUI({fetchCSS:false}).use("test", "test-console", function(Y) {
+YUI({ fetchCSS: false }).use("test", "test-console", function (Y) {
 
     "use strict";
 
-L.Model.set(L.Model.URL_ENCODED_QUERY, "foo");
+    L.Model.set(L.Model.URL_ENCODED_QUERY, "foo");
 
- var dateSolrForm = document.querySelector("#solr-date-form"),
-    startYearInput = document.querySelector(".date.start"),
-    endYearInput = document.querySelector(".date.end"),
-    errorMessage  = document.querySelector("#facet-error-message"),
-    btn = document.querySelector("#submit")  ,
-    submit = document.createEvent("Event");
-     
-          
-     
+    let dateSolrForm = document.querySelector("#solr-date-form"),
+        startYearInput = document.querySelector(".date.start"),
+        endYearInput = document.querySelector(".date.end"),
+        errorMessage = document.querySelector("#facet-error-message"),
+        submit = document.createEvent("Event");
 
+    let solrFacetsTestCase = new Y.Test.Case({
 
-var solrFacetsTestCase = new Y.Test.Case({
+        name: "Solr Facets Test Case",
 
-      name: "Check Min Start Date",
-        testCheckStartValue: function() {
+        // Check Min Start Date
+        testCheckStartValue: function () {
             startYearInput.value = 100;
-           submit.initEvent("submit", true, true);
+            submit = new Event("submit", { bubbles: true, cancelable: true });
             dateSolrForm.dispatchEvent(submit);
-           Y.Assert.areEqual("Value must be greater than or equal to 1400.", errorMessage.textContent);  
-    },
-    
-    name: "Check Null Start Date",
-        testCheckNullValue: function() {
+            Y.Assert.areEqual("Please select a value that is no less than 1400.", errorMessage.textContent);
+        },
+
+        // Check Null Start Date
+        testCheckNullValue: function () {
             startYearInput.value = '';
-           submit.initEvent("submit", true, true);
+            submit = new Event("submit", { bubbles: true, cancelable: true });
             dateSolrForm.dispatchEvent(submit);
-           Y.Assert.areEqual("Please fill out this field.", errorMessage.textContent);  
-    },
-    
-    name: "Check if start Date bigger than than end Data",
-        testCheckEndDatesmallerThanStartDate: function() {
+            Y.Assert.areEqual("Please enter a number.", errorMessage.textContent);
+        },
+
+        // Check if start Date bigger than than end Data
+        testCheckEndDatesmallerThanStartDate: function () {
             startYearInput.value = 1963;
             endYearInput.value = 1900;
             dateSolrForm.dispatchEvent(submit);
-           Y.Assert.areEqual("The start year should be smaller than the end year", errorMessage.textContent);  
-    }
-});
+            Y.Assert.areEqual("The start year should be smaller than the end year", errorMessage.textContent);
+        }
+    });
 
-new Y.Test.Console().render();
+    new Y.Test.Console().render();
 
-Y.Test.Runner.add(solrFacetsTestCase);
-Y.Test.Runner.masterSuite.name = "solr-facets-test.js";
-Y.Test.Runner.run();
+    Y.Test.Runner.add(solrFacetsTestCase);
+    Y.Test.Runner.masterSuite.name = "solr-facets-test.js";
+    Y.Test.Runner.run();
 
 });
