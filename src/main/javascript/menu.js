67074e07264ada2397671bebd6f5d785528da981
Alain Boussard <elainb@stanford.edu>
2024-08-09 10:36:54 -0700

    Refactor CSS to improve button styling and navigation menu appearance

--- src/main/javascript/menu.js
+++ src/main/javascript/menu.js
@@ -1,35 +1,47 @@
-(function() {
+(function () {
 
     "use strict";
 
 
     //For regular mobile menu 
-    document.querySelectorAll('.menu-container.mobile h2, .menu-container.phone h2').forEach(function(menu) {
-        menu.addEventListener('click', function(event) {
+    document.querySelectorAll('.menu-container.mobile h2, .menu-container.phone h2').forEach(function (menu) {
+        menu.addEventListener('click', function (event) {
             event.preventDefault();
             menu.closest(".menu-container").classList.toggle('active');
         })
     });
 
 
-    document.querySelectorAll('.menu-container ul li a[href^="#"]').forEach(function(link) {
-        link.addEventListener('click', function(event) {
-           let clickTarget = event.target;
-           clickTarget.closest('ul').querySelectorAll('li a').forEach(function(anchor) {
+    document.querySelectorAll('.menu-container ul li a[href^="#"]').forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            let clickTarget = event.target;
+            clickTarget.closest('ul').querySelectorAll('li a').forEach(function (anchor) {
                 anchor.classList.remove('menuitem-active');
             });
-            clickTarget.classList.add('menuitem-active');    
+            clickTarget.classList.add('menuitem-active');
         })
     });
 
-    
-    window.addEventListener("load", function() {
-    let hash = window.location.hash;
-    document.querySelectorAll('.menu-container ul li a[href^="#"]').forEach(function(link) {
-        if(hash === link.hash){
-            link.classList.add('menuitem-active');
-        }       
+    // to select menu  from the header 
+    document.querySelectorAll("li.dropdown.nav-menu span").forEach(function (span) {
+        span.parentElement.querySelectorAll("a").forEach(function (link) {
+            if (link.pathname == window.location.pathname && link.hash == window.location.hash) {
+                span.classList.add("red");
+                span.classList.add("btm-brdr-red");
+            }
+        });
     });
-    });
+
+    // to select menu  from the content 
+    window.addEventListener("load", function () {
+        let hash = window.location.hash
+        document.querySelectorAll('.menu-container ul li a[href^="#"]').forEach(function (link) {
+            if (hash === link.hash) {
+                link.classList.add('menuitem-active');
+            }
+        });
+    })
+
+
 
 })();
