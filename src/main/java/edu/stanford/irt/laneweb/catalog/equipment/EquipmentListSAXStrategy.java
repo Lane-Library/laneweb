0a16bb875f87a493bdd516e344d02d3d374dcd06
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-08-19 12:34:28 -0700

    LANEWEB-11354: add icons for charger, key; fix magnifying glass
    default to grey instead of black

--- src/main/java/edu/stanford/irt/laneweb/catalog/equipment/EquipmentListSAXStrategy.java
+++ src/main/java/edu/stanford/irt/laneweb/catalog/equipment/EquipmentListSAXStrategy.java
@@ -67,29 +67,34 @@ public class EquipmentListSAXStrategy extends AbstractXHTMLSAXStrategy<List<Equi
     }
 
     private String iconClass(final String title) {
+        String normedString = title.toLowerCase();
         StringBuilder sb = new StringBuilder("fa-4x ");
-        if (title.contains("iPad")) {
+        if (normedString.contains("ipad")) {
             sb.append("fa-solid fa-tablet-screen-button");
-        } else if (title.contains("Apple")) {
+        } else if (normedString.contains("apple")) {
             sb.append("fa-brands fa-apple");
-        } else if (title.contains("Android")) {
+        } else if (normedString.contains("android")) {
             sb.append("fa-brands fa-android");
-        } else if (title.contains("Keyboard")) {
+        } else if (normedString.contains("keyboard")) {
             sb.append("fa-solid fa-keyboard");
-        } else if (title.contains("Headphones")) {
+        } else if (normedString.contains("headphones")) {
             sb.append("fa-solid fa-headphones");
-        } else if (title.contains("Tablet")) {
+        } else if (normedString.contains("tablet")) {
             sb.append("fa-solid fa-tablet-screen-button");
-        } else if (title.contains("USB")) {
+        } else if (normedString.contains("usb")) {
             sb.append("fa-brands fa-usb");
-        } else if (title.contains("Cable")) {
+        } else if (normedString.contains("charger")) {
+            sb.append("fa-regular fa-battery-bolt");
+        } else if (normedString.contains("cable")) {
             sb.append("fa-brands fa-gg");
-        } else if (title.contains("Recorder")) {
+        } else if (normedString.contains("recorder")) {
             sb.append("fa-solid fa-microphone");
-        } else if (title.contains("Polling")) {
+        } else if (normedString.contains("polling")) {
             sb.append("fa-solid fa-users");
-        } else if (title.contains("magnifying")) {
+        } else if (normedString.contains("magnifying")) {
             sb.append("fa-solid fa-magnifying-glass");
+        } else if (normedString.contains("key")) {
+            sb.append("fa-regular fa-key");
         }
         return sb.toString();
     }
