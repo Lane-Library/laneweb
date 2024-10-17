7794f4edde80dcc13f47fe6eb53fc347b4a067b2
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-10-16 20:35:13 -0700

    PubMed metasearch is failing, so skip integration test for now

--- src/test/java/edu/stanford/irt/laneweb/integration/LanewebIT.java
+++ src/test/java/edu/stanford/irt/laneweb/integration/LanewebIT.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -106,6 +107,7 @@ public class LanewebIT {
                                 .andExpect(status().isNotFound());
         }
 
+        @Ignore("metasearch is failing for PubMed")
         @Test
         public void testPubmedSearch() throws Exception {
                 Map<String, String> ns = new HashMap<>();
