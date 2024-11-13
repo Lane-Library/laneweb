b9d3e146e928b6289f95b6a742d019e9734eb8dd
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-11-13 14:24:57 -0800

    formatting

--- src/test/javascript-e2e/cypress/e2e/back-to-top.spec.cy.js
+++ src/test/javascript-e2e/cypress/e2e/back-to-top.spec.cy.js
@@ -1,17 +1,17 @@
 describe('Back to Top', () => {
-  it('can selectively see the back to top button', () => {
-    cy.viewport(1101, 660);
-    cy.visit('/about/about.html');
-    cy.get('.back-to-top').as('backToTop');
-    cy.get('@backToTop').should('not.be.visible');
+    it('can selectively see the back to top button', () => {
+        cy.viewport(1101, 660);
+        cy.visit('/about/about.html');
+        cy.get('.back-to-top').as('backToTop');
+        cy.get('@backToTop').should('not.be.visible');
 
-    cy.scrollTo('bottom');
-    cy.get('@backToTop').should('be.visible');
-    cy.window().its('scrollY').should('not.equal', 0);
+        cy.scrollTo('bottom');
+        cy.get('@backToTop').should('be.visible');
+        cy.window().its('scrollY').should('not.equal', 0);
 
-    cy.get('@backToTop').click();
+        cy.get('@backToTop').click();
 
-    cy.window().its('scrollY').should('equal', 0);
+        cy.window().its('scrollY').should('equal', 0);
 
-  })
+    })
 })
