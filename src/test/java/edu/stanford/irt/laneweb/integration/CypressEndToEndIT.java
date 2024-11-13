e9059110c349c24c30d86e5e555ea26b061093de
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-11-13 14:26:42 -0800

    fighting too much against wimdeblauwe's testcontainers-cypress
    now using testcontainers directly
    the wimdeblauwe project did some mochawesome report parsing that's handy but
    not worth the trouble of the dependency

--- src/test/java/edu/stanford/irt/laneweb/integration/CypressEndToEndIT.java
+++ src/test/java/edu/stanford/irt/laneweb/integration/CypressEndToEndIT.java
@@ -2,51 +2,69 @@ package edu.stanford.irt.laneweb.integration;
 
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.OutputFrame;
 
 import edu.stanford.irt.laneweb.Laneweb;
-import io.github.wimdeblauwe.testcontainers.cypress.CypressContainer;
-import io.github.wimdeblauwe.testcontainers.cypress.CypressTestResults;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Laneweb.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CypressEndToEndIT {
 
+    private static final String DOCKER_IMAGE = "cypress/included:13.15.2";
+
+    private static final int MAX_TOTAL_TEST_TIME_IN_MINUTES = 5;
+
     @LocalServerPort
     private int port;
 
     @Test
-    public void runCypressTests() throws InterruptedException, IOException, TimeoutException {
+    public void runCypressTests() throws InterruptedException {
         Testcontainers.exposeHostPorts(this.port);
-        try (CypressContainer container = new CypressContainer()) {
-            container.withLocalServerPort(this.port);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        try (GenericContainer<?> container = createCypressContainer()) {
             container.start();
-            CypressTestResults testResults = container.getTestResults();
-            StringBuilder details = new StringBuilder();
-            details.append(testResults.toString());
-            details.append("\nDetails:");
-            details.append("\n=========================================");
-            testResults.getSuites().forEach(suite -> {
-                details.append("\n");
-                details.append("\nSuite: " + suite.getTitle());
-                suite.getTests().forEach(test -> {
-                    String status = test.isSuccess() ? "SUCCESS" : "FAILURE";
-                    details.append("\n[TEST] " + test.getDescription() + ": " + status);
-                });
+            container.followOutput(new Consumer<OutputFrame>() {
+
+                @Override
+                public void accept(final OutputFrame outputFrame) {
+                    // output can be very verbose, only print non-empty lines
+                    if (!outputFrame.getUtf8String().equals("\n")) {
+                        System.out.println(outputFrame.getUtf8String());
+                    }
+                    // brittle test to determine if tests have failed
+                    if (outputFrame.getUtf8String().contains(" failed (")) {
+                        countDownLatch.countDown();
+                        fail("Failure while running Cypress tests");
+                    } else if (outputFrame.getUtf8String().contains("All specs passed!")) {
+                        countDownLatch.countDown();
+                    }
+                }
             });
-            if (testResults.getNumberOfFailingTests() > 0) {
-                fail("Failure while running Cypress tests:\n" + details.toString() + "\n");
-            } else {
-                System.out.println("Cypress tests ran successfully:\n" + details.toString() + "\n");
-            }
+            countDownLatch.await(MAX_TOTAL_TEST_TIME_IN_MINUTES, TimeUnit.MINUTES);
         }
+    }
+
+    @NotNull
+    private GenericContainer<?> createCypressContainer() {
+        GenericContainer<?> result = new GenericContainer<>(DOCKER_IMAGE);
+        result.withClasspathResourceMapping("e2e", "/e2e", BindMode.READ_WRITE);
+        result.withCreateContainerCmdModifier(cmd -> cmd.withEntrypoint("bash", "-c",
+                "rm -rf cypress/reports/mochawesome && npm install && chmod a+wx -R node_modules && cypress run --headless"));
+        result.setWorkingDirectory("/e2e");
+        result.addEnv("CYPRESS_baseUrl", "http://host.testcontainers.internal:" + this.port);
+        return result;
     }
 }
