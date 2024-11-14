c2101669625ae4a460e130242059187ac8d4ff2b
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-11-13 16:26:28 -0800

    convert the rest of the bassett unit tests

--- src/test/javascript-e2e/cypress/e2e/bassett.spec.cy.js
+++ src/test/javascript-e2e/cypress/e2e/bassett.spec.cy.js
@@ -29,5 +29,91 @@ describe('Bassett', () => {
         cy.get('#abdomen li:visible').should('have.length.lt', 10);
     })
 
-    // more tests needed
+    it('shows diagrams instead of images', () => {
+        cy.viewport(1101, 750);
+        cy.visit('/biomed-resources/bassett/index.html');
+
+        cy.get('#abdomen a.see-all').click();
+
+        // images should be displayed by default
+        cy.get('#thumbnail img').each(($img) => {
+            expect($img).to.have.attr('src').match(/.*L.jpg/);
+        });
+
+        cy.get('#diagram-choice').click();
+        cy.get('#thumbnail img').each(($img) => {
+            expect($img).to.have.attr('src').match(/.*D.jpg/);
+        });
+
+        cy.get('#photo-choice').click();
+        cy.get('#thumbnail img').each(($img) => {
+            expect($img).to.have.attr('src').match(/.*L.jpg/);
+        });
+    })
+
+    it('pagination form contains appropriate page data', () => {
+        cy.viewport(1101, 750);
+        cy.visit('/biomed-resources/bassett/index.html');
+
+        cy.get('#abdomen a.see-all').click();
+
+        cy.get('.pagingForm').first().as('paginationForm');
+
+        cy.get('@paginationForm').find('input[name=pages]').as('pagesInput');
+
+        // testSubmitRemovesPages
+        cy.get('@pagesInput').should('have.value', '5');
+        cy.intercept('GET', '/biomed-resources/bassett/bassettsView.html?r=Abdomen&page=1', (req) => {
+            expect(req.body).to.not.have.property('pages');
+        }).as('formSubmit');
+        cy.get('@paginationForm').submit();
+        cy.wait('@formSubmit');
+
+    })
+
+    it('pagination form handles error values', () => {
+        cy.viewport(1101, 750);
+        cy.visit('/biomed-resources/bassett/index.html');
+
+        cy.get('#abdomen a.see-all').click();
+
+        cy.get('.pagingForm').first().as('paginationForm');
+
+        cy.get('@paginationForm').find('input[name=page]').as('pageInput');
+
+        // set pages to foo and submit
+        cy.get('@pageInput').type('foo');
+        cy.get('@paginationForm').submit();
+        cy.get('.bassett-error').should('be.visible');
+        cy.get('.bassett-error').should('contain', 'ERROR');
+
+        // testPageLessThanOne
+        cy.get('@pageInput').clear().type('0');
+        cy.get('@paginationForm').submit();
+        cy.get('.bassett-error').should('be.visible');
+        cy.get('.bassett-error').should('contain', 'ERROR');
+
+        // testPageMoreThanPages
+        cy.get('@pageInput').clear().type('10');
+        cy.get('@paginationForm').submit();
+        cy.get('.bassett-error').should('be.visible');
+        cy.get('.bassett-error').should('contain', 'ERROR');
+    })
+
+    it('pagination form handles valid values', () => {
+        cy.viewport(1101, 750);
+        cy.visit('/biomed-resources/bassett/index.html');
+
+        cy.get('#abdomen a.see-all').click();
+
+        cy.get('.pagingForm').first().as('paginationForm');
+
+        cy.get('@paginationForm').find('input[name=page]').as('pageInput');
+
+        cy.get('@pageInput').clear().type('3');
+        cy.intercept('GET', '/biomed-resources/bassett/bassettsView.html?r=Abdomen&page=3').as('formSubmit');
+        cy.get('@paginationForm').submit();
+        cy.wait('@formSubmit');
+    })
+
 })
