c6107907465377359fd711efa040f736590ea442
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-11-14 11:20:04 -0800

    add bookcovers test (demo of fixtures)

---
+++ src/test/javascript-e2e/cypress/e2e/bookcovers.spec.cy.js
@@ -0,0 +1,17 @@
+describe('Book Covers', () => {
+
+    it('displays bookcover images after a search', () => {
+        cy.viewport(1101, 750);
+
+        cy.intercept('/apps/bookcovers*', { fixture: 'bookcovers.json' }).as('bookcoversReq');
+
+        cy.visit('/search.html?q=id%3Abib-12+OR+id%3Abib-13+OR+id%3Abib-17&source=all-all&facets=recordType%3A"bib"&sort=title_sort+asc,year+desc');
+
+        cy.get('li[data-sid=bib-17] .bookcover img').should('exist');
+        cy.get('li[data-sid=bib-17] .bookcover img').should('have.attr', 'src').and('eq', '//fake/17.png');
+
+        cy.scrollTo(0, 100);
+        cy.wait('@bookcoversReq');
+    })
+
+})
