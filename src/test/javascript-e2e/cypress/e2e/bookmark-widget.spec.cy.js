bd816a2789de2040d28fe0ce14a1c502dc131bb2
Alain Boussard <alainb@stanford.edu>
2024-12-05 11:52:51 -0800

    Add end-to-end tests for bookmark widget functionality

---
+++ src/test/javascript-e2e/cypress/e2e/bookmark-widget.spec.cy.js
@@ -0,0 +1,31 @@
+//most the tests was done in bookmark-links.spec.cy.js
+
+describe('Bookmark Widget', () => {
+
+    beforeEach(() => {
+        cy.viewport(1101, 1750);
+        cy.visit('/test/test-bookmarks.html?template=none');
+        cy.get('#long-bookmarking').as('long-bookmarking');
+    })
+
+    it('add bookmark and tes the li text length', () => {
+        cy.intercept(
+            'POST',
+            '/bookmarks',
+            {
+                statusCode: 200
+            }).as('addBookmark');
+        cy.get('#bookmarks li').should('have.length', 7);
+        cy.get('@long-bookmarking').trigger('mouseover');
+        cy.get('@long-bookmarking').trigger('mouseleave');
+        cy.get('.bookmark-link').trigger('mouseover');
+        cy.get('.bookmark-link').click();
+        cy.get('.favorites .fa.fa-bookmark').should('have.class', 'shake');
+        cy.wait('@addBookmark');
+        cy.get('.favorites .fa.fa-bookmark').should('not.have.class', 'shake');
+        cy.get('#bookmarks li').should('have.length', 8);
+        cy.get('#bookmarks li').first().should('have.text', 'This is a long Bookmarking to te...');
+    })
+
+
+})
