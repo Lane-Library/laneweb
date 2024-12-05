f6f537c87accdb0b6372fb0656cd6c34e2075259
Alain Boussard <alainb@stanford.edu>
2024-12-05 13:43:20 -0800

    Add end-to-end tests for Lightbox functionality

---
+++ src/test/javascript-e2e/cypress/e2e/lightbox.spec.cy.js
@@ -0,0 +1,30 @@
+describe('lightbox', () => {
+
+    beforeEach(() => {
+        cy.viewport(1101, 1050);
+        cy.visit('/index.html');
+    })
+
+    it('test lightbox show', () => {
+        cy.window().its('L.Lightbox').invoke('setContent', 'test');
+        cy.window().its('L.Lightbox').invoke('show');
+        cy.get('.lightbox').should('be.visible');
+        cy.get('.lightboxbg').should('be.visible');
+    })
+
+
+    it('test lightbox hide', () => {
+        cy.window().its('L.Lightbox').invoke('setContent', 'test');
+        cy.window().its('L.Lightbox').invoke('show');
+        cy.window().its('L.Lightbox').invoke('hide');
+        cy.get('.lightbox').should('be.not.visible');
+        cy.get('.lightboxbg').should('be.not.visible');
+    })
+
+
+    it('test lightbox content', () => {
+        cy.window().its('L.Lightbox').invoke('setContent', 'Hree is the content');
+        cy.window().its('L.Lightbox').invoke('show');
+        cy.get('.lightbox').should('have.text', 'Hree is the content');
+    })
+});
