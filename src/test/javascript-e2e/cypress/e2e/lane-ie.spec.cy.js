862e2d15c452b6ee2c62516f59c9fb9944a4b59b
Alain Boussard <alainb@stanford.edu>
2025-01-15 10:36:27 -0800

    add: implement Cypress tests for lane bookmarklet selector

---
+++ src/test/javascript-e2e/cypress/e2e/lane-ie.spec.cy.js
@@ -0,0 +1,35 @@
+describe('lane bookmarklet selector', () => {
+
+
+    it('test not ie and not edge', () => {
+        cy.visit('/cypress-test/favorites.html');
+        cy.get('#bookmarkletNotIE').should('be.visible');
+        cy.get('#bookmarkletIE').should('not.be.visible');
+        cy.get('.bookmarklet-edge').should('not.be.visible');
+    });
+
+
+    //** The tests below don't work because the headers are not set as the cypress documentation suggest **/
+
+    // it('test ie ', () => {
+    //     cy.visit('/cypress-test/favorites.html', {
+    //         headers: {
+    //             'user-agent': 'MSIE',
+    //         }
+    //     });
+    //     cy.get('#bookmarkletNotIE').should('not.be.visible');
+    //     cy.get('#bookmarkletIE').should('not.visible');
+    //     cy.get('.bookmarklet-edge').should('not.be.visible');
+    // })
+
+    // it('test edge browser', () => {
+    //     cy.visit('/cypress-test/favorites.html', {
+    //         headers: {
+    //             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/12.246',
+    //         }
+    //     });
+    //     cy.get('#bookmarkletNotIE').should('not.be.visible');
+    //     cy.get('#bookmarkletIE').should('not.be.visible');
+    //     cy.get('.bookmarklet-edge').should('be.visible');
+    //})
+});
