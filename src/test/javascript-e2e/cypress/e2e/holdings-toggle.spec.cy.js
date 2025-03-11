41e5d16e747bf98b42b4d8978aa740eba3463cac
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-03-11 16:46:19 -0700

    test is flaky and intermittently fails: break into separate tests

--- src/test/javascript-e2e/cypress/e2e/holdings-toggle.spec.cy.js
+++ src/test/javascript-e2e/cypress/e2e/holdings-toggle.spec.cy.js
@@ -1,23 +1,43 @@
 describe('Holdings Toggle', () => {
 
-    it('should show/hide holdings info and report the clicks to GA', () => {
-
+    beforeEach(() => {
         cy.visit('/cypress-test/search.html?q=12&source=all-all&facets=recordType:"bib"');
 
-        // intercept the GA request and count the number of click events
-        cy.intercept('POST', 'https://www.google-analytics.com/g/collect*', (req) => {
-            const holdingsTriggerCount = (req.body.match(/hldgsTrigger/g) || []).length;
-            req.reply('OK');
-            expect(holdingsTriggerCount).to.eq(2);
+        cy.intercept('/apps/*', {
+            statusCode: 200,
+            body: '{}'
         });
+
+        cy.intercept('https://www.google-analytics.com/g/collect*', (req) => {
+            if (req.body.match(/hldgsTrigger.*close/g)) {
+                req.alias = 'gaCollectClose';
+            }
+            if (req.body.match(/hldgsTrigger.*open/g)) {
+                req.alias = 'gaCollectOpen';
+            }
+            req.reply('OK');
+        });
+
+    })
+
+    it('should hide holdings info and report the clicks to GA', () => {
 
         cy.get('.hldgsTrigger').first().as('hldgsTrigger');
         cy.get('.table-main').first().as('tableMain');
         expect(cy.get('@tableMain').should('be.visible'));
         cy.get('@hldgsTrigger').click();
         expect(cy.get('@tableMain').should('not.be.visible'));
-        cy.get('@hldgsTrigger').click();
-        expect(cy.get('@tableMain').should('be.visible'));
+        cy.wait('@gaCollectClose');
     })
 
+    it('should show and then hide holdings info and report the clicks to GA', () => {
+
+        cy.get('.hldgsTrigger').first().as('hldgsTrigger');
+        cy.get('.table-main').first().as('tableMain');
+        expect(cy.get('@tableMain').should('be.visible'));
+        cy.get('@hldgsTrigger').click();
+        cy.get('@hldgsTrigger').click();
+        expect(cy.get('@tableMain').should('be.visible'));
+        cy.wait('@gaCollectOpen');
+    })
 })
