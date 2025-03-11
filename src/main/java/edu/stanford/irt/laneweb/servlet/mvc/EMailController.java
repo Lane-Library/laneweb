091293ad5113359e1f0897f83e414063d9fd7fac
Alain Boussard <alainb@stanford.edu>
2025-03-11 09:25:05 -0700

    LANEWEB-11455 Refactor EMailController to remove file upload handling and related methods

--- src/main/java/edu/stanford/irt/laneweb/servlet/mvc/EMailController.java
+++ src/main/java/edu/stanford/irt/laneweb/servlet/mvc/EMailController.java
@@ -1,9 +1,5 @@
 package edu.stanford.irt.laneweb.servlet.mvc;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -15,8 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.stanford.irt.laneweb.LanewebException;
@@ -24,12 +18,9 @@ import edu.stanford.irt.laneweb.email.EMailSender;
 import edu.stanford.irt.laneweb.servlet.binding.DataBinder;
 import edu.stanford.irt.laneweb.spam.SpamService;
 
-
 @Controller
 @RequestMapping(value = "/apps/mail/")
 public class EMailController {
-
-    public static final long MAX_UPLOAD_SIZE = 4194304;
 
     private static final String ASKUS_ADDRESS = "LaneAskUs@stanford.edu";
 
@@ -37,31 +28,19 @@ public class EMailController {
 
     private static final String ASKUS_PORTAL = "laneaskus";
 
-    private static final String CONFIRMATION_PAGE_EJP = "redirect:/contacts/ejp-confirmation.html";
-
     private static final String DOCXPRESS_ADDRESS = "docxpress@lists.stanford.edu";
 
     private static final String DOCXPRESS_PATH = "docxpress";
 
     private static final String DOCXPRESS_PORTAL = "docxpress";
 
-    private static final String EJP_ADDRESS = "ejproblem@lists.stanford.edu";
-
-    private static final String EJP_PATH = "ejp";
-
-    private static final String EJP_PORTAL = "ejp";
-
     private static final String ERROR_PAGE = "redirect:/error.html";
 
     private static final String FORM_MIME_TYPE = "application/x-www-form-urlencoded";
 
-    private static final String MULTIPART_MIME_TYPE = "multipart/form-data";
-
     private static final String QUESTION = "question";
 
     private static final String SUBJECT = "subject";
-
-    private static final String UPLOAD_ERROR_PAGE = "redirect:/error_upload_file.html";
 
     private EMailSender sender;
 
@@ -86,24 +65,6 @@ public class EMailController {
         }
         sendEmail(DOCXPRESS_ADDRESS, map);
         return getRedirectTo(map);
-    }
-
-    @PostMapping(value = EJP_PATH, consumes = MULTIPART_MIME_TYPE)
-    public String formSubmitEJP(final Model model, @RequestParam("attachment") final MultipartFile[] files,
-            final RedirectAttributes atts) throws IllegalStateException, IOException {
-        Map<String, Object> map = model.asMap();
-        if (!validateForm(map, EJP_PORTAL)) {
-            return ERROR_PAGE;
-        }
-        File[] attachments = validateFileMultipartFiles(files);
-        if (attachments == null) {
-            return UPLOAD_ERROR_PAGE;
-        }
-        StringBuilder subject = new StringBuilder((String) map.get(SUBJECT));
-        subject.append(" ").append(map.get("title"));
-        map.put(SUBJECT, subject.toString());
-        sendEmail(EJP_ADDRESS, map, attachments);
-        return CONFIRMATION_PAGE_EJP;
     }
 
     // Form from the error 404 page
@@ -138,69 +99,6 @@ public class EMailController {
     private void sendEmail(final String recipient, final Map<String, Object> data) {
         data.put("recipient", recipient);
         this.sender.sendEmail(data);
-    }
-
-    private void sendEmail(final String recipient, final Map<String, Object> data, final File[] files)
-            throws IOException {
-        try {
-            data.put("recipient", recipient);
-            this.sender.sendEmail(data, files);
-        } catch (Exception e) {
-            throw new LanewebException(e);
-        } finally {
-            if (null != files && files.length > 0) {
-                for (File file : files) {
-                    if (file != null) {
-                        Files.delete(file.toPath());
-                    }
-                }
-            }
-        }
-    }
-
-    private File[] validateFileMultipartFiles(final MultipartFile[] attachments)
-            throws IllegalStateException, IOException {
-        File[] files = new File[attachments.length];
-        for (int i = 0; i < attachments.length; i++) {
-            if (attachments[i] != null && !attachments[i].isEmpty()) {
-                File checkedFile = validateFileMultipartFile(attachments[i]);
-                if (checkedFile != null) {
-                    files[i] = checkedFile;
-                } else {
-                    return null;
-                }
-            }
-        }
-        return files;
-    }
-
-    private File validateFileMultipartFile(final MultipartFile attachment) throws IllegalStateException, IOException {
-        File file = null;
-        if (attachment != null && !attachment.isEmpty()) {
-            String contentType = attachment.getContentType();
-            if (contentType == null || !contentType.startsWith("image/")) {
-                return null;
-            }
-            file = new File("/tmp/" + attachment.getOriginalFilename());
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                fos.write(attachment.getBytes());
-                if (file.length() > MAX_UPLOAD_SIZE) {
-                    Files.delete(file.toPath());
-                    return null;
-                }
-            } catch (IOException e) {
-                throw new LanewebException(e);
-            }
-        }
-        return file;
-    }
-
-    private boolean validateForm(final Map<String, Object> feedback, final String portal) {
-        if (this.spamService.isSpam(portal, feedback)) {
-            return false;
-        }
-        return !(feedback.get("email") == null || feedback.get("email").toString().isEmpty()
-                || feedback.get("name") == null || feedback.get("name").toString().isEmpty());
     }
 
     @ModelAttribute
