ed0f5aa3a31d130af1dd40bf00272394cd15c551
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-08-20 16:29:38 -0700

    refactor w/ assistance from Gemini
    improvements:
    - will work even if forms are dynamically added to a page
    - one event listener for entire doc instead if each submit button (likely minimal improvement for us, since we don't have lots of form-rich pages)

--- src/main/javascript/validation.js
+++ src/main/javascript/validation.js
@@ -1,14 +1,19 @@
-(function() {
+(() => {
 
     "use strict";
 
-    document.querySelectorAll('[type="submit"]').forEach(function(node) {
-        node.addEventListener("click", function(event) {
-            let form = event.target.form;
-            if (form) {
-                form.classList.add("submitted");
-            }
-        });
+    document.addEventListener("click", event => {
+        // is the click on a submit button?
+        if (!event.target.matches('[type="submit"]')) {
+            return;
+        }
+
+        // event.target is the submit button, so find the parent form
+        const form = event.target.form;
+
+        if (form) {
+            form.classList.add("submitted");
+        }
     });
 
 })();
