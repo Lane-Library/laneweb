aa073cf26de0d1fdc9176cc14241b9970d8e3401
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-09-02 16:13:43 -0700

    Cypress test failing now that script uses clipboard API

--- src/test/javascript-e2e/cypress/e2e/permalink-toggle.spec.cy.js
+++ src/test/javascript-e2e/cypress/e2e/permalink-toggle.spec.cy.js
@@ -1,38 +1,38 @@
 describe('Permalink Toggle Test', () => {
-    it('should toggle permalinks correctly', () => {
-        cy.viewport(1101, 750);
-
-        // intercept the GA POST and make sure "permalinkCopied" is present in the body (below)
-        cy.intercept('POST', 'https://www.google-analytics.com/g/collect*', (req) => {
-            if (req.body.includes('permalinkCopied')) {
-                req.reply('OK');
-                req.alias = 'gaCollect';
-            }
+    it('should toggle permalinks correctly and fire a tracking event', () => {
+        cy.visit('/cypress-test/search.html?source=all-all&q=12&facets=recordType:%22bib%22', {
+            // Stub the clipboard API before the page loads
+            onBeforeLoad(win) {
+                cy.stub(win.navigator.clipboard, 'writeText').resolves();
+            },
         });
 
-        cy.visit('/cypress-test/search.html?source=all-all&q=12&facets=recordType:%22bib%22');
+        // Intercept the GA event
+        cy.intercept('POST', '**/collect*').as('gaCollect');
 
-        //hover over the more-detail-container container to make the permalink visible
+        cy.viewport(1101, 750);
+
         cy.get('.more-detail-container').trigger('mouseover');
         cy.get('.more-detail-container .sourceInfo').invoke('show');
 
-        // intentionally use the containing div instead of the anchor
-        // because clicking temporarily changes the innerHTML (in permalink-toggle.js)
-        cy.get('.permalink').as('permalink');
-
-        cy.get('@permalink').invoke('text').should('contain', 'Get Shareable Link');
+        cy.get('.permalink').as('permalink')
+            .should('contain.text', 'Get Shareable Link');
 
         cy.get('@permalink').click();
 
-        cy.get('@permalink').invoke('text').should('contain', 'Link copied');
-
-        cy.wait(2100);
-
-        cy.get('.permalink').invoke('text').should('contain', 'Get Shareable Link');
-
-        cy.wait('@gaCollect').then((interception) => {
-            expect(interception.request.body).to.include('lane%3ApermalinkCopied');
+        // Assert that the stubbed function was called.
+        // Cypress will automatically retry this assertion, effectively pausing
+        // the test until the click handler calls `writeText`.
+        cy.window().then((win) => {
+            expect(win.navigator.clipboard.writeText).to.have.been.calledOnce;
         });
 
+        cy.get('@permalink').should('contain.text', 'Link copied');
+
+        cy.wait('@gaCollect').its('request.body').should('include', 'lane%3ApermalinkCopied');
+
+        // Assert the text reverts
+        cy.get('@permalink', { timeout: 2100 })
+            .should('contain.text', 'Get Shareable Link');
     });
 });
