9f91786043a6ed4a959bae15ed497d02165c734f
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-09-09 11:52:03 -0700

    refactor w/ assistance from Gemini
    improvements:
    - use ES6 getters and setters
    - restructuring in _handleValueChange method
    - more descriptive error messages
    - template literal in toString method

--- src/main/javascript/bookmark.js
+++ src/main/javascript/bookmark.js
@@ -1,50 +1,46 @@
-(function () {
+(() => {
 
     "use strict";
-
 
     class Bookmark {
 
         constructor(label, url) {
+            this._label = undefined;
+            this._url = undefined;
             L.addEventTarget(this, {
                 prefix: 'bookmark'
             });
-            this.on("valueChange", (e) => this._valueChange(e));
+            this.on("valueChange", this._handleValueChange);
             this.setValues(label, url);
         };
-
-
 
         /**
          * The default changeEvent handler
          *
-         * @method _valueChange
          * @private
          * @param event
          *            {CustomEvent} the valueChange event
          */
-        _valueChange(event) {
-            this._label = event.newLabel;
-            this._url = event.newUrl;
+        _handleValueChange = ({ newLabel, newUrl }) => {
+            this._label = newLabel;
+            this._url = newUrl;
         }
 
         /**
          * getter for the label
          *
-         * @method getLabel
          * @returns {string} the label
          */
-        getLabel() {
+        get label() {
             return this._label;
         }
 
         /**
          * getter for the url
          *
-         * @method getUrl
          * @returns {string} the url
          */
-        getUrl() {
+        get url() {
             return this._url;
         }
 
@@ -52,30 +48,26 @@
          * setter for the label, delegates to setValues with the current url as
          * the url value.
          *
-         * @method setLabel
          * @param newLabel
          *            {string}
          */
-        setLabel(newLabel) {
+        set label(newLabel) {
             this.setValues(newLabel, this._url);
         }
 
         /**
          * setter for the url, delegates to setValues with the current
          *
-         * @method setUrl label as the label value.
-         *
          * @param newUrl
          *            {string}
          */
-        setUrl(newUrl) {
+        set url(newUrl) {
             this.setValues(this._label, newUrl);
         }
 
         /**
          * Set both the label and url then fire a changed event
          *
-         * @method setValues
          * @param newLabel
          *            {string}
          * @param newUrl
@@ -83,33 +75,30 @@
          */
         setValues(newLabel, newUrl) {
             if (!newLabel) {
-                throw ("null or empty newLabel");
+                throw new Error("Bookmark label cannot be null or empty.");
             }
             if (!newUrl) {
-                throw ("null or empty newUrl");
+                throw new Error("Bookmark URL cannot be null or empty.");
             }
-            let changed = newLabel !== this._label || newUrl !== this._url;
-            if (changed) {
+            const hasChanged = newLabel !== this._label || newUrl !== this._url;
+            if (hasChanged) {
                 this.fire("valueChange", {
                     prevLabel: this._label,
                     prevUrl: this._url,
-                    newLabel: newLabel,
-                    newUrl: newUrl,
+                    newLabel,
+                    newUrl,
                     target: this
                 });
             }
         }
 
         /**
-         * @method toString
          * @return {string} a string with the label and url values
          */
         toString() {
-            return "Bookmark{label:" + this._label + ",url:" + this._url + "}";
+            return `Bookmark{label:${this._label},url:${this._url}}`;
         }
     };
-
-
 
     // make the Bookmark constructor globally accessible
     L.Bookmark = Bookmark;
