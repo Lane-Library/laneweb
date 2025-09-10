ac1079a18893a9f67982adc4228831bda260794d
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-09-09 17:46:25 -0700

    refactor w/ assistance from Gemini
    improvements:
    - _createInitialEditors now uses Array.from(items).map(...) to cleanly transform a list of <li> elements into an array of BookmarkEditor instances
    - _getSerializedEditors now uses filter instead of forEach
    - event handler methods use arrow functions automatically binding `this` and eliminating the need for .bind(this)
    - repeated code now has own method: _createEditor
    - _handleDragOver simplified to use before() and after()

--- src/main/javascript/bookmarks-editor.js
+++ src/main/javascript/bookmarks-editor.js
@@ -1,64 +1,85 @@
-(function () {
+(() => {
 
     "use strict";
 
+    const BookmarkEditor = L.BookmarkEditor;
+    const editorsNode = document.querySelector("#bookmarks-editor");
 
-    let BookmarkEditor = L.BookmarkEditor,
-        editorsNode = document.querySelector("#bookmarks-editor");
-
+    if (!editorsNode) {
+        return;
+    }
 
     /**
      * The BookmarksEditor.
      * Contains one BookmarkEditor for each bookmark.
      */
     class BookmarksEditor {
-        constructor(args) {
-            this.srcNode = args.srcNode;
-            this.bookmarks = args.bookmarks;
+        constructor({ srcNode, bookmarks }) {
+            this.srcNode = srcNode;
+            this.bookmarks = bookmarks;
+            this.editors = [];
             this.editing = false;
-            this.bindUI();
-            this.syncUI();
-        }
+            this._listElement = this.srcNode.querySelector("ul");
 
+            // Instance properties for drag state
+            this.from = -1; // The original index from dragStart
+            this.to = -1;   // The destination index, updated on dragOver
+            this.dragged_source = null; // The actual DOM node being dragged
 
-
-        bindUI() {
-            //Add EventTarget attributes to the Bookmarks prototype
-            L.addEventTarget(this, {
-                prefix: 'bookmarksEditor'
-            });
-
-            let bookmarks = this.bookmarks,
-                buttons = this.srcNode.querySelectorAll("fieldset button");
-            buttons.forEach(button => {
-                button.addEventListener("click", (event) => this._handleButtonClick(event));
-            });
-            bookmarks.after("removeSync", (e) => this._handleBookmarksRemove(e));
-            bookmarks.after("addSync", (e) => this._handleBookmarkAdd(e));
-            bookmarks.after("updateSync", (e) => this._handleBookmarkUpdate(e));
-            bookmarks.after("moveSync", (e) => this._handleBookmarkMove(e));
+            this._bindEvents();
+            this._createInitialEditors();
         }
 
         /**
-         * Creates the BookmarkEditors.
-         * @method syncUI
+         * Centralized method for creating a BookmarkEditor instance and binding its events.
+         * @private
          */
-        syncUI() {
-            let editor, editors = [], i,
-                srcNode = this.srcNode,
-                items = srcNode.querySelectorAll("li"),
-                bookmarks = this.bookmarks;
-            for (i = 0; i < items.length; i++) {
-                editor = new BookmarkEditor({ srcNode: items.item(i), bookmark: bookmarks.getBookmark(i), position: i });
-                editor.on("destroy", (e) => this._handleDestroyEditor(e));
-                editor.on("dragOver", (e) => this._handleDragOver(e));
-                editor.on("dragStart", (e) => this._handleDragStart(e));
-                editor.on("dragEnd", (e) => this._handleDragEnd(e));
-                editors.push(editor);
-            }
-            this.editors = editors;
-            // show add bookmarks if no bookmarks present
-            if (0 == items.length) {
+        _createEditor({ srcNode, bookmark, position }) {
+            const editor = new BookmarkEditor({ srcNode, bookmark, position });
+
+            editor.on("destroy", event => this._handleDestroyEditor(event));
+            editor.on("dragOver", event => this._handleDragOver(event));
+            editor.on("dragStart", event => this._handleDragStart(event));
+            editor.on("dragEnd", event => this._handleDragEnd(event));
+
+            return editor;
+        }
+
+        /**
+         * Sets up event handlers for the main editor and the bookmarks model.
+         * @private
+         */
+        _bindEvents = () => {
+            // Add EventTarget attributes to the Bookmarks prototype
+            L.addEventTarget(this, { prefix: 'bookmarksEditor' });
+
+            this.srcNode.querySelectorAll("fieldset button").forEach(button => {
+                button.addEventListener("click", this._handleButtonClick);
+            });
+
+            this.bookmarks.after("removeSync", this._handleBookmarksRemove);
+            this.bookmarks.after("addSync", this._handleBookmarkAdd);
+            this.bookmarks.after("updateSync", this._handleBookmarkUpdate);
+            this.bookmarks.after("moveSync", this._handleBookmarkMove);
+        }
+
+        /**
+         * Creates the initial set of BookmarkEditors from the existing DOM.
+         * @private
+         */
+        _createInitialEditors = () => {
+            const items = this._listElement.querySelectorAll("li");
+
+            this.editors = Array.from(items).map((item, index) =>
+                this._createEditor({
+                    srcNode: item,
+                    bookmark: this.bookmarks.getBookmark(index),
+                    position: index
+                })
+            );
+
+            // Show 'add' UI if no bookmarks are present.
+            if (this.editors.length === 0) {
                 this.add();
             }
         }
@@ -69,29 +90,25 @@
          * @method add
          */
         add() {
-            let items = this.srcNode.querySelector("ul"),
-                item = document.createElement('li'),
-                addBookmarkContainer = document.querySelector(".addBookmarkContainer"),
-                editors = this.editors,
-                adding = editors.length && editors[0].editing,
-                editor;
-            item.innerHTML += "<a href=''></a>";
+            const addBookmarkContainer = document.querySelector(".addBookmarkContainer");
+
             // toggle add bookmark button
-            if (addBookmarkContainer) {
-                addBookmarkContainer.classList.toggle("active");
-            }
+            addBookmarkContainer?.classList.toggle("active");
+
+            // check if the first editor is an unsaved "add" editor.
+            const isAlreadyAdding = this.editors.length > 0 && !this.editors[0].bookmark;
+
             // destroy add bookmark editor if already present/open
             // otherwise, create a new add bookmark editor
-            if (adding) {
-                editors[0].destroy();
+            if (isAlreadyAdding) {
+                this.editors[0].destroy();
             } else {
-                items.prepend(item);
-                editor = new BookmarkEditor({ srcNode: item, position: 0 });
-                editor.on("destroy", (e) => this._handleDestroyEditor(e));
-                editor.on("dragOver", (e) => this._handleDragOver(e));
-                editor.after("dragStart", (e) => this._handleDragStart(e));
-                editor.on("dragEnd", (e) => this._handleDragEnd(e));
-                editors.unshift(editor);
+                const item = document.createElement('li');
+                item.innerHTML = "<a href=''></a>";
+                this._listElement.prepend(item);
+
+                const editor = this._createEditor({ srcNode: item, position: 0 });
+                this.editors.unshift(editor);
                 editor.setEditing(true);
             }
         }
@@ -99,172 +116,171 @@
         /**
          * Return an Array of BookmarkEditors that have been written to disk (have an a/href]).
          * LANEWEB-10988: returning unsaved "add" editor causes many bugs
-         * @method _getSerializedEditors
          * @private
          * @returns {Array}
          */
         _getSerializedEditors() {
-            let filteredEditors = [];
-            this.editors.forEach(function (editor) {
-                if (editor.srcNode) {
-                    filteredEditors.push(editor);
-                }
-            });
-            return filteredEditors;
+            return this.editors.filter(editor => editor.bookmark);
         }
+
+        // --- Event Handlers (defined as class properties for auto-binding) ---
 
         /**
          * Responds to the bookmarks:addSync event, call update() on the appropriate BookmarkEditor.
-         * @method _handleBookmarkAdd
          * @private
-         * @param event {CustomEvent}
          */
-        _handleBookmarkAdd(event) {
+        _handleBookmarkAdd = () => {
             this.editors[0].update();
-            this._syncPosition();
+            this._syncPositions();
         }
 
         /**
          * Responds to the bookmarks:moveSync event, rearranges the editors appropriately
-         * @method _handleBookmarkMove
          * @private
-         * @param event {CustomEvent}
          */
-        _handleBookmarkMove(event) {
-            this.editors.splice(this.to, 0, this.editors.splice(this.from, 1)[0]);
-            this._syncPosition();
+        _handleBookmarkMove = (event) => { 
+            this.editors.splice(event.to, 0, this.editors.splice(event.from, 1)[0]);
+            this._syncPositions();
         }
 
 
         /**
-     * Responds to the bookmarks:removeSync event, calls destroy on each BookmarkEditor
-     * associated with removed bookmarks.
-     * @method _handleBookmarksRemove
-     * @private
-     * @param event {CustomEvent}
-     */
-        _handleBookmarksRemove(event) {
-            let i, editors = this._getSerializedEditors();
-            for (i = event.positions.length - 1; i >= 0; --i) {
-                editors[event.positions[i]].destroy(true);
+         * Responds to the bookmarks:removeSync event, calls destroy on each BookmarkEditor
+         * associated with removed bookmarks.
+         * @private
+         * @param event {CustomEvent}
+         */
+        _handleBookmarksRemove = (event) => {
+            const savedEditors = this._getSerializedEditors();
+            // iterate backwards to safely destroy editors without messing up indices
+            for (let i = event.positions.length - 1; i >= 0; i--) {
+                const position = event.positions[i];
+                savedEditors[position]?.destroy(true);
             }
-            this._syncPosition();
+            this._syncPositions();
         }
 
         /**
          * Responds to the bookmarks:updateSync event, calls update() on the appropriate BookmarkEditor.
-         * @method _handleBookmarkUpdate
          * @private
          * @param event {CustomEvent}
          */
-        _handleBookmarkUpdate(event) {
-            let editors = this._getSerializedEditors();
-            editors[event.position].update();
+        _handleBookmarkUpdate = (event) => {
+            const editors = this._getSerializedEditors();
+            editors[event.position]?.update();
         }
 
         /**
          * The click handler for buttons, delegates to the function named the same as the buttons value.
-         * @method _handleButtonClick
          * @private
          * @param event {CustomEvent}
          */
-        _handleButtonClick(event) {
+        _handleButtonClick = (event) => {
             event.preventDefault();
-            //see case 67695
-            //pressing return generates a click on the add button for some reason
-            //pageX is 0 in that situation
-            if (event.pageX !== 0) {
-                let fn = this[event.currentTarget.getAttribute("value")];
-                if (fn) {
-                    fn.call(this);
-                }
-            }
+            // see case 67695
+            // pressing return generates a click on the add button for some reason
+            // pageX is 0 in that situation
+            if (event.pageX === 0) return;
+
+            const action = event.currentTarget.value;
+            this[action]?.();
         }
 
         /**
          * Removes a destroyed editor from the backing Array.
-         * @method _handleDestroyEditor
          * @private
          * @param event {CustomEvent}
          */
-        _handleDestroyEditor(event) {
-            let editors = this.editors,
-                position = editors.indexOf(event.editor);
-            editors.splice(position, 1);
-            this._syncPosition();
+        _handleDestroyEditor = (event) => {
+            const position = this.editors.indexOf(event.editor);
+            if (position > -1) {
+                this.editors.splice(position, 1);
+            }
+            this._syncPositions();
         }
 
+        // --- Drag and Drop Handlers ---
 
-
-        _handleDragStart(e) {
-            this.dragged_source = e.target;
-            this.from = e.position;
+        _handleDragStart = (event) => {
+            this.dragged_source = event.target;
+            this.from = event.position;
         }
 
-        _handleDragOver(event) {
-            this.to = this.getNodeIndex(event.target);
+        /**
+         * Fired repeatedly as a dragged item is moved over other child editors.
+         * @private
+         */
+        _handleDragOver = (event) => {
+            // <li> being hovered over
+            const dropTarget = event.target;
 
+            // set the 'to' index for the final drop. This gets updated on every hover.
+            this.to = this._getNodeIndex(dropTarget);
 
-            let ul = this.srcNode.querySelector("ul"),
-                lis = ul.children,
-                index_from = this.getNodeIndex(this.dragged_source),
-                drag = lis[index_from],
-                drop = lis[this.to];
-            if (drop && drop.classList.contains('bookmark-editor-content')) {
-                if (this.to > index_from) {
-                    if (this.to + 1 == lis.length) {
-                        ul.appendChild(drag);
-                    }
-                    else {
-                        drop = lis[this.to + 1];
-                    }
-                }
-                if (this.to + 1 != lis.length) {
-                    drop.parentNode.insertBefore(drag, drop);
-                }
+            // recalculate the dragged item's current index
+            const currentIndexFrom = this._getNodeIndex(this.dragged_source);
+
+            // guard against invalid states or no-op moves
+            if (currentIndexFrom === -1 || this.to === -1 || currentIndexFrom === this.to) {
+                return;
+            }
+
+            if (currentIndexFrom < this.to) {
+                // Moving DOWN: Insert the dragged item *after* the drop target.
+                dropTarget.after(this.dragged_source);
+            } else {
+                // Moving UP: Insert the dragged item *before* the drop target.
+                dropTarget.before(this.dragged_source);
             }
         }
 
-        getNodeIndex(node) {
-            let lis = this.srcNode.querySelector("ul").children;
-            for (let i = 0; i < lis.length; i++) {
-                if (lis[i] === node) {
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-
-        _handleDragEnd(event) {
-            if (this.from != this.to) {
+        /**
+         * Fired when the drag operation ends. Finalizes the move.
+         * @private
+         */
+        _handleDragEnd = () => {
+            // Only fire the event if the original position and final position are different.
+            // `this.from` was set at the start, `this.to` was set by the last dragOver.
+            if (this.from !== this.to && this.to !== -1) {
                 this.bookmarks.moveBookmark(this.to, this.from);
             }
+
+            // clean up state
+            this.dragged_source = null;
+            this.from = -1;
+            this.to = -1;
         }
 
+        /**
+         * Retrieves the index of a given node among its sibling elements.
+         * @param {HTMLElement} node - The DOM element whose index is to be determined.
+         * @returns {number} The zero-based index of the node among its siblings, or -1 if not found.
+         */
+        _getNodeIndex = (node) => {
+            return Array.from(this._listElement.children).indexOf(node);
+        }
 
-        _syncPosition() {
-            let i = 0;
-            this.editors.forEach(function (editor) {
-                editor.position = i++;
+        /**
+         * Updates the `position` property of all editors to match their current order.
+         * @private
+         */
+        _syncPositions = () => {
+            this.editors.forEach((editor, index) => {
+                editor.position = index;
             });
-        };
+        }
 
     }
-    //Create a new BookmarksEditor
-    if (editorsNode) {
-        if (L.BookmarksWidget) {
-            L.BookmarksEditor = new BookmarksEditor({
-                srcNode: document.querySelector("#bookmarks-editor"),
-                bookmarks: L.BookmarksWidget.bookmarks
-            });
-        } else {
-            // case 141805 bookmark edit buttons fail if bookmarks editor not initialized
-            editorsNode.querySelectorAll("button").forEach(function (node) {
-                node.addEventListener("click", function (event) {
-                    event.preventDefault();
-                });
-            });
-        }
+    // Create a new BookmarksEditor
+    if (L.BookmarksWidget) {
+        L.BookmarksEditor = new BookmarksEditor({
+            srcNode: editorsNode,
+            bookmarks: L.BookmarksWidget.bookmarks
+        });
+    } else {
+        // case 141805 bookmark edit buttons fail if bookmarks editor not initialized
+        editorsNode.querySelectorAll("button").forEach(node => {
+            node.addEventListener("click", event => event.preventDefault());
+        });
     }
 })();
