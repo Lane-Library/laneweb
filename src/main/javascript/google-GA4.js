2fbcab9b9156acdd91ed4bc59d21a3bfd6701e66
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-09-12 09:18:05 -0700

    refactor w/ assistance from Gemini
    improvements:
    - data-driven way to determine tracker ID
    - arrow functions, template literals, const, etc.
    - Before (Bug): The original code passed a Map object directly into the gtag 'config' call. The gtag library expects a plain JavaScript object { key: value, ... }, not a Map instance. This would likely fail silently.
      After (Fix): We use Object.fromEntries(dimensions) to correctly convert the Map into the plain object that gtag requires. This is a crucial functional fix and a modern ES2019 feature.

--- src/main/javascript/google-GA4.js
+++ src/main/javascript/google-GA4.js
@@ -1,94 +1,84 @@
-(function () {
+(() => {
 
     "use strict";
 
-    let getTrackerId = function () {
-        let host = location.host,
-            trackerId;
-        if (host.match("lane.stanford.edu")) {
-            trackerId = "G-Y6KGXN1JXT";
-        } else if (host.match("lane-beta.stanford.edu")) {
-            trackerId = "G-RF4JWB6KG5";
-        } else {
-            trackerId = "G-CRPQYN7JFT";
-        }
-        return trackerId;
-    },
+    const DEFAULT_TRACKER_ID = 'G-CRPQYN7JFT';
+    const TRACKER_CONFIG = new Map([
+        ['lane.stanford.edu', 'G-Y6KGXN1JXT'],
+        ['lane-beta.stanford.edu', 'G-RF4JWB6KG5'],
+    ]);
+    const GA_MEASUREMENT_ID = TRACKER_CONFIG.get(location.host) || DEFAULT_TRACKER_ID;
 
-        GA_MEASUREMENT_ID = getTrackerId(),
-
-        // custom dimension indexes must be configured in the GA admin interface for each property
-        // https://support.google.com/analytics/answer/2709829?hl=en&topic=2709827&ctx=topic
-        LANEWEB_DIMENSION = 'laneweb_dimensions',
-        IP_GROUP_DIMENSION = 'dimension1',
-        AUTHENTICATED_SESSION_DIMENSION = 'dimension2',
-        BOOKMARK_ENABLED_SESSION_DIMENSION = 'dimension3';
+    // Custom dimension indexes must be configured in the GA admin interface for each property
+    // https://support.google.com/analytics/answer/2709829?hl=en&topic=2709827&ctx=topic
+    const LANEWEB_DIMENSION = "laneweb_dimensions";
+    const IP_GROUP_DIMENSION = "dimension1";
+    const AUTHENTICATED_SESSION_DIMENSION = "dimension2";
+    const BOOKMARK_ENABLED_SESSION_DIMENSION = "dimension3";
 
     // load analytics.js and add the ga object
     // https://developers.google.com/analytics/devguides/collection/analyticsjs/#the_javascript_measurement_snippet
-    let script = document.createElement('script');
-    script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_MEASUREMENT_ID;
-    script.onload = function () {
-        let model = L.Model, bookmark,
-            ipGroup = model.get(model.IPGROUP),
-            auth = model.get(model.AUTH),
-            dimensions = new Map();
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    script.onload = () => {
+        const { get, IPGROUP, AUTH } = L.Model;
+        const ipGroup   = get(IPGROUP);
+        const auth      = get(AUTH);
+        let bookmark;
 
-        window.dataLayer = window.dataLayer || [];
-        window.gtag = window.gtag || function () {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
+        const dimensions = new Map();
 
         if (ipGroup) {
             dimensions.set(IP_GROUP_DIMENSION, 'ipGroup');
         }
         if (auth) {
             dimensions.set(AUTHENTICATED_SESSION_DIMENSION, 'auth');
-            if (L.BookmarksWidget && L.BookmarksWidget.bookmarks.length > 0) {
+            if (L.BookmarksWidget?.bookmarks?.length > 0) {
                 dimensions.set(BOOKMARK_ENABLED_SESSION_DIMENSION, 'bookmark');
                 bookmark = auth;
             }
         }
 
+        window.dataLayer = window.dataLayer || [];
+        // do not use arrow function here, it breaks gtag which expects an "array-like" arguments object
+        window.gtag = window.gtag || function () {
+            dataLayer.push(arguments);
+        }
+
+        // Initialize gtag
+        gtag('js', new Date());
+
         gtag('config', GA_MEASUREMENT_ID, {
-            'custom_map': dimensions
+            // gtag expects a plain object, not a Map, so convert it
+            'custom_map': Object.fromEntries(dimensions)
         });
 
-        gtag('event', LANEWEB_DIMENSION, { 'ipGroup': ipGroup, 'auth': auth, 'bookmark': bookmark });
+        gtag('event', LANEWEB_DIMENSION, { ipGroup, auth, bookmark });
     };
     document.head.appendChild(script);
 
-
-
-    L.on("tracker:trackableEvent", function (event) {
+    L.on("tracker:trackableEvent", ({ category, action, label, value }) => {
         if (window.gtag) {
-            window.gtag('event', event.category, {
-                'event_action': event.action,
-                'event_label': event.label,
-                'event_value': event.value
+            window.gtag('event', category, {
+                'event_action': action,
+                'event_label': label,
+                'event_value': value
             });
         }
     });
 
-    L.on("tracker:trackablePageview", function (event) {
-        if (window.gtag && event.external) {
-            if (event.query !== undefined && event.query !== '') {
-                window.gtag('event', 'lane:offsite', {
-                    'event_action': "/OFFSITE-CLICK-EVENT/" + encodeURIComponent(event.title),
-                    'event_label': event.host + event.path + event.query
-                });
-
-            } else {
-                window.gtag('event', 'lane:offsite', {
-                    'event_action': "/OFFSITE-CLICK-EVENT/" + encodeURIComponent(event.title),
-                    'event_label': event.host + event.path
-                });
-            }
+    L.on("tracker:trackablePageview", (event) => {
+        const { external, query, title, host, path } = event;
+        if (window.gtag && external) {
+            window.gtag('event', 'lane:offsite', {
+                'event_action': `/OFFSITE-CLICK-EVENT/${encodeURIComponent(title)}`,
+                'event_label': `${host}${path}${query || ''}`
+            });
         } else if (window.gtag) {
             window.gtag('event', 'page_view', {
-                'page_location': '/ONSITE/' + encodeURIComponent(event.title) + '/' + event.path
+                'page_location': `/ONSITE/${encodeURIComponent(title)}/${path}`
             })
         }
     });
+
 })();
