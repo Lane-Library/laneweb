fe320ebb883e6f9f08d8be509a2cdaeada6b3cb1
Alain Boussard <alainb@stanford.edu>
2025-09-12 13:09:27 -0700

    feat: add BasicAuthConfiguration for test profile with REST service beans

--- src/test/java/edu/stanford/irt/laneweb/rest/BaiscAuthConfiguration.java
+++ src/test/java/edu/stanford/irt/laneweb/rest/BaiscAuthConfiguration.java
@@ -5,9 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestClient;
 
-@Configuration
-@Profile("test")
-public class RESTClientTestConfiguration {
+@Configuration @Profile("test")
+public class BaiscAuthConfiguration {
 
     @Bean
     BasicAuthRESTService basicAuthRestService(final RestClient restClient) {
