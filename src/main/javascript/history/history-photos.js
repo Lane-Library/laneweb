dc2b4dc407b6220a9a4e24ba7da17c825af48dfd
Ryan Max Steinberg <ryanmax@stanford.edu>
2025-09-19 17:22:08 -0700

    refactoring w/ assistance from Gemini
    improvements:
    - descriptive constants instead of magic numbers
    - reduce nested-ness by removing photoService and just using standalone functions
    - use async/await with fetch instead of XMLHttpRequest
    - replace verbose for loops with map() and forEach()
    - nested setTimeout()s should be replaced but bumped into display issues when extensively refactoring

--- src/main/javascript/history/history-photos.js
+++ src/main/javascript/history/history-photos.js
@@ -1,107 +1,118 @@
-(function() {
+(() => {
 
     "use strict";
 
-    let images = [],
+    const historyPhotosContainer = document.querySelector(".history-photos");
 
-        historyPhotos = document.querySelector(".history-photos"),
+    if (!historyPhotosContainer) {
+        return;
+    }
 
-        links = historyPhotos ? historyPhotos.querySelectorAll("a") : {},
+    let images = [];
+    const links = historyPhotosContainer.querySelectorAll("a");
+    const TOTAL_PHOTOS = 6;
+    const ROTATION_INTERVAL_MS = 15000;
+    const FADE_DURATION_MS = 1000;
+    const TARGET_ROW_WIDTH = 989;
+    const TARGET_IMAGE_HEIGHT = 200;
 
-        photoService = {
+    const getFactor = (imagesToFactor) => {
+        let width = 0;
+        const relevantImages = imagesToFactor.slice(0, TOTAL_PHOTOS);
+        if (relevantImages.length === 0) return 1;
 
-            getFactor: function(imagesToFactor) {
-                let width = 0,
-                    factor = 1000,
-                    i = 0;
+        for (const image of relevantImages) {
+            width += image.width;
+        }
+        return TARGET_ROW_WIDTH / width;
+    }
 
-                while (i < 6 && factor > 1) {
-                    width += imagesToFactor[i++].width;
-                    factor = 989 / width;
+    const resizeImage = (image) => {
+        if (image.height > 0) {
+            const aspect = image.width / image.height;
+            image.height = TARGET_IMAGE_HEIGHT;
+            image.width = TARGET_IMAGE_HEIGHT * aspect;
+        }
+    }
+
+    const fetchPhotos = () => {
+        fetch("../apps/getHistoryPhotoList")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status} ${response.statusText}`);
                 }
-
-                return factor;
-            },
-
-            resize: function(image) {
-                let aspect = image.width / image.height;
-                image.height = 200;
-                image.width = 200 * aspect;
-            },
-
-            getPhotos: function() {
-                let request = new XMLHttpRequest();
-                request.open("GET", "../apps/getHistoryPhotoList", true);
-                request.onload = function() {
-                    if (request.status >= 200 && request.status < 400) {
-                        let photos = JSON.parse(request.responseText);
-                        for(let i = 0; i < 6; i++) {
-                            photos[i].image = new Image();
-                            photos[i].image.src = photos[i].thumbnail;
-                            photos[i].image.alt = photos[i].title;
-                        }
-                        setTimeout(function() {
-                            historyPhotos.style.opacity = 0;
-                            setTimeout(function() {
-                                let factor, j;
-                                images = [];
-                                for (j = 0; j < 6; j++) {
-                                    photoService.resize(photos[j].image);
-                                    images.push(photos[j].image);
-                                }
-                                factor = photoService.getFactor(images);
-                                for (j = 0; j < 6; j++) {
-                                    links.item(j).style.width = Math.round(photos[j].image.width * factor) + "px";
-                                    links.item(j).href = photos[j].page;
-                                    links.item(j).replaceChild(photos[j].image, links.item(j).firstChild);
-                                }
-                                historyPhotos.style.opacity = 1;
-                            }, 1000);
-                            photoService.getPhotos();
-                        }, 15000);
-                    }
-                };
-                request.send();
-            }
-        },
-
-        imagesComplete = function() {
-            let complete = true;
-            if (links.length !== images.length) {
-                complete = false;
-            } else {
-                images.forEach(function(image) {
-                    if (!image.complete) {
-                        complete = false;
-                    }
+                return response.json();
+            })
+            .then(photos => {
+                photos.slice(0, TOTAL_PHOTOS).forEach(photo => {
+                    photo.image = new Image();
+                    photo.image.src = photo.thumbnail;
+                    photo.image.alt = photo.title;
                 });
-            }
-            return complete;
-        },
+                setTimeout(() => {
+                    historyPhotosContainer.style.opacity = 0;
+                    setTimeout(() => {
+                        let factor;
+                        images = [];
+                        images = photos.slice(0, TOTAL_PHOTOS).map(photo => {
+                            resizeImage(photo.image);
+                            return photo.image;
+                        });
+                        factor = getFactor(images);
+                        photos.slice(0, TOTAL_PHOTOS).forEach((photo, j) => {
+                            const link = links[j];
+                            if (link) {
+                                link.style.width = `${Math.round(photo.image.width * factor)}px`;
+                                link.href = photo.page;
+                                link.replaceChild(photo.image, link.firstChild);
+                            }
+                        });
+                        historyPhotosContainer.style.opacity = 1;
+                    }, FADE_DURATION_MS);
+                    fetchPhotos();
+                }, ROTATION_INTERVAL_MS);
+            })
+            .catch(error => {
+                console.error("Failed to fetch photo list:", error);
+            });
+    }
 
-        handleImagesComplete = function() {
-            let i, factor = photoService.getFactor(images);
-            for (i = 0; i < 6; i++) {
-                images[i].parentNode.style.width = Math.round(images[i].width * factor) + "px";
-            }
-            historyPhotos.style.opacity = 1;
-            photoService.getPhotos();
-        };
+    const imagesComplete = () => {
+        if (links.length !== images.length) {
+            return false;
+        }
+        return images.every(image => image.complete);
+    };
 
-    [].forEach.call(historyPhotos ? historyPhotos.querySelectorAll("img") : [], function(image) {
+    const handleImagesComplete = () => {
+        const factor = getFactor(images);
+        images.slice(0, TOTAL_PHOTOS).forEach(image => {
+            image.parentNode.style.width = `${Math.round(image.width * factor)}px`;
+        });
+        historyPhotosContainer.style.opacity = 1;
+        fetchPhotos();
+    };
+
+    const initialImages = historyPhotosContainer.querySelectorAll("img");
+    initialImages.forEach((image) => {
         images.push(image);
-        if (image.complete) {
-            photoService.resize(image);
+        const processImage = () => {
+            resizeImage(image);
             if (imagesComplete()) {
                 handleImagesComplete();
             }
+        };
+
+        if (image.complete) {
+            processImage();
         } else {
-            image.onload = function() {
-                photoService.resize(this);
-                if (imagesComplete()) {
-                    handleImagesComplete();
-                }
+            image.onload = processImage;
+            image.onerror = () => {
+                console.error("Initial image failed to load", image.src);
+                // Still process to not hang the script
+                processImage();
             };
         }
     });
+
 })();
