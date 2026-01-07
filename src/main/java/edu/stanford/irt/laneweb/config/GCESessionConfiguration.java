e59ffe73b9a18b58942ada7d7b5bb4b4806ea1e5
Alain Boussard <alainb@stanford.edu>
2026-01-07 11:07:54 -0800

    fix: update parent version to 191 and change javamelody dependency for Spring Boot 4

--- src/main/java/edu/stanford/irt/laneweb/config/GCESessionConfiguration.java
+++ src/main/java/edu/stanford/irt/laneweb/config/GCESessionConfiguration.java
@@ -1,14 +1,12 @@
 package edu.stanford.irt.laneweb.config;
 
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @Profile("gce")
-@Import({ RedisAutoConfiguration.class })
+@EnableRedisHttpSession
 public class GCESessionConfiguration {
 
 }
